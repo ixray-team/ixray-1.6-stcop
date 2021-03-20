e9ab0f02f3ee102b521b009b77e17d387d47c3f7
acidicMercury8 <acidicmercury8@outlook.com>
2021-03-20 18:03:04 +0300

    Set verbose converting COFF to OMF libraries

--- ConvertLibraries.bat
+++ ConvertLibraries.bat
@@ -9,11 +9,11 @@ if exist editor.lib (
     del editor.exp
 )
 
-coff2omf.exe BugTrap.lib BugTrapB.lib
-coff2omf.exe openal32.lib openal32B.lib
-coff2omf.exe ETools.lib EToolsB.lib
-coff2omf.exe LWO.lib LWOB.lib
-coff2omf.exe DXT.lib DXTB.lib
-coff2omf.exe xrPhysics.lib xrPhysicsB.lib
+coff2omf.exe BugTrap.lib BugTrapB.lib -v
+coff2omf.exe openal32.lib openal32B.lib -v
+coff2omf.exe ETools.lib EToolsB.lib -v
+coff2omf.exe LWO.lib LWOB.lib -v
+coff2omf.exe DXT.lib DXTB.lib -v
+coff2omf.exe xrPhysics.lib xrPhysicsB.lib -v
 
 pause
