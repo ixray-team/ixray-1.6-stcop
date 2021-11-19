8cf7fb6d3da709ecc08b747ceec3cf7436a8b3ad
acidicMercury8 <acidicmercury8@outlook.com>
2021-11-20 00:30:43 +0300

    Fix typecasting errors

--- src/xrCore/xrMemory_subst_msvc.h
+++ src/xrCore/xrMemory_subst_msvc.h
@@ -159,7 +159,7 @@ IC	void	xr_delete	(T* const &ptr)
 {
 	if (ptr) 
 	{
-		xr_special_free<std::is_polymorphic<T>::value,T>(ptr);
+		xr_special_free<std::is_polymorphic<T>::value,T>()(const_cast<T*&>(ptr));
 		const_cast<T*&>(ptr) = NULL;
 	}
 }
