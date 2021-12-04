fa0ea648479a01181ed47fa916ecf2806725b5b6
acidicMercury8 <acidicmercury8@outlook.com>
2021-12-04 21:36:34 +0300

    Add custom project for `dockpanelsuite`

--- DownloadDependencies.bat
+++ DownloadDependencies.bat
@@ -3,5 +3,6 @@
 title IX-Ray
 
 git.exe clone --branch aug2021 --depth 1 https://github.com/microsoft/DirectXTex.git dep/DirectXTex
+git.exe clone --branch Release_3.1.0 --depth 1 https://github.com/dockpanelsuite/dockpanelsuite.git dep/dockpanelsuite
 
 pause
