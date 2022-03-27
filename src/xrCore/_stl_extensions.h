4b6d2a10f5c5d5d7a740297bbef0535fa117a658
Drombeys <dima.zolotov02@gmail.com>
2022-03-27 19:18:41 +0300

    Delete `DEF_LIST()` macros

--- src/xrCore/_stl_extensions.h
+++ src/xrCore/_stl_extensions.h
@@ -125,8 +125,6 @@ struct pred_stri	: public std::binary_function<char*, char*, bool>	{
 };
 
 // STL extensions
-#define DEF_LIST(N,T)				typedef xr_list< T > N;			typedef N::iterator N##_it;
-
 #define DEFINE_MMAP(K,T,N,I)		typedef xr_multimap< K, T > N;	typedef N::iterator I;
 #define DEFINE_SET_PRED(T,N,I,P)	typedef xr_set< T, P > N;		typedef N::iterator I;
 #define DEFINE_STACK(T,N)			typedef xr_stack< T > N;
