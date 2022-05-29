d789512755adc9f5db88d03f253e53f3de24282e
acidicMercury8 <acidicmercury8@outlook.com>
2022-05-29 22:08:06 +0300

    Add resources for X-Ray 1.6.01

--- gamedata/shaders/r1/effects_wallmarkblend.s
+++ gamedata/shaders/r1/effects_wallmarkblend.s
@@ -1,7 +1,7 @@
 function normal		(shader, t_base, t_second, t_detail)
 	shader:begin	("wmark",	"vert")
 			: sorting	(2, false)
-			: blend		(true,blend.zero,blend.one)
+			: blend		(true,blend.srcalpha,blend.invsrcalpha)
 			: aref 		(true,0)
 			: zb 		(true,false)
 			: fog		(true)
@@ -10,8 +10,10 @@ end
 
 function l_spot    (shader, t_base, t_second, t_detail)
   r1_lspot	(shader, t_base, "wmark_spot")
+	shader		: sorting	(2, false)	
 end
 
 function l_point  (shader, t_base, t_second, t_detail)
   r1_lpoint	(shader, t_base, "wmark_point")
+	shader		: sorting	(2, false)
 end
