22066ebdd088902ec90dd6c964f431dba28a4c18
Drombeys <dima.zolotov02@gmail.com>
2022-06-06 19:26:39 +0300

    Add ability to choose Shader Model 4.0
    Implemented the ability to switch the input point to a specific version of the shader model. This is necessary for unique shaders to work.
    
    Co-authored-by: Hozar2002 <hoz2002team@gmail.com>

--- src/Layers/xrRenderDX10/dx10ResourceManager_Resources.cpp
+++ src/Layers/xrRenderDX10/dx10ResourceManager_Resources.cpp
@@ -193,6 +193,7 @@ SVS*	CResourceManager::_CreateVS		(LPCSTR _name)
 
 		if (strstr(data, "main_vs_1_1"))	{ c_target = "vs_1_1"; c_entry = "main_vs_1_1";	}
 		if (strstr(data, "main_vs_2_0"))	{ c_target = "vs_2_0"; c_entry = "main_vs_2_0";	}
+		if (strstr(data, "main_vs_4_0"))	{ c_target = "vs_4_0"; c_entry = "main_vs_4_0"; }
 
 		HRESULT	const _hr		= ::Render->shader_compile(name,(DWORD const*)data,size, c_entry, c_target, D3D10_SHADER_PACK_MATRIX_ROW_MAJOR, (void*&)_vs );
 
@@ -300,6 +301,7 @@ SPS*	CResourceManager::_CreatePS			(LPCSTR _name)
 		if (strstr(data,"main_ps_1_3"))			{ c_target = "ps_1_3"; c_entry = "main_ps_1_3";	}
 		if (strstr(data,"main_ps_1_4"))			{ c_target = "ps_1_4"; c_entry = "main_ps_1_4";	}
 		if (strstr(data,"main_ps_2_0"))			{ c_target = "ps_2_0"; c_entry = "main_ps_2_0";	}
+		if (strstr(data,"main_ps_4_0"))			{ c_target = "ps_4_0"; c_entry = "main_ps_4_0"; }
 
 		HRESULT	const _hr		= ::Render->shader_compile(name,(DWORD const*)data,size, c_entry, c_target, D3D10_SHADER_PACK_MATRIX_ROW_MAJOR, (void*&)_ps );
 		
