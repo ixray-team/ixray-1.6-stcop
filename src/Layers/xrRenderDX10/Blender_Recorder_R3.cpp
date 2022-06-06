10376056bf0c4db3661cd45f28aae83f4d701303
Drombeys <dima.zolotov02@gmail.com>
2022-06-06 14:45:23 +0300

    Fix crash from volumetric fog on D3D11
    Co-authored-by: Hozar2002 <hoz2002team@gmail.com>

--- src/Layers/xrRenderDX10/Blender_Recorder_R3.cpp
+++ src/Layers/xrRenderDX10/Blender_Recorder_R3.cpp
@@ -206,6 +206,7 @@ void	CBlender_Compile::r_Pass		(LPCSTR _vs, LPCSTR _gs, LPCSTR _ps, bool bFog, B
 #ifdef USE_DX11
 	dest.hs = DEV->_CreateHS("null");
 	dest.ds = DEV->_CreateDS("null");
+	dest.cs = DEV->_CreateCS("null");
 #endif
 	ctable.merge			(&ps->constants);
 	ctable.merge			(&vs->constants);
