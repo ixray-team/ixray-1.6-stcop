77c8cff08a57c2b64eba10028c11a3c29729c513
acidicMercury8 <acidicmercury8@outlook.com>
2022-07-05 17:31:45 +0300

    Replace `DirectXTex` custom project with NuGet package

---
+++ src/utils/xrDXT/ddsheader.h
@@ -0,0 +1,32 @@
+#pragma once
+
+struct DDS_PIXELFORMAT
+{
+	uint32_t    size;
+	uint32_t    flags;
+	uint32_t    fourCC;
+	uint32_t    RGBBitCount;
+	uint32_t    RBitMask;
+	uint32_t    GBitMask;
+	uint32_t    BBitMask;
+	uint32_t    ABitMask;
+};
+
+struct DDS_HEADER
+{
+	uint32_t        size;
+	uint32_t        flags;
+	uint32_t        height;
+	uint32_t        width;
+	uint32_t        pitchOrLinearSize;
+	uint32_t        depth; // only if DDS_HEADER_FLAGS_VOLUME is set in flags
+	uint32_t        mipMapCount;
+	uint32_t        reserved1[11];
+	DDS_PIXELFORMAT ddspf;
+	// Capabilities
+	uint32_t        caps;
+	uint32_t        caps2;
+	uint32_t        caps3;
+	uint32_t        caps4;
+	uint32_t        reserved2;
+};
