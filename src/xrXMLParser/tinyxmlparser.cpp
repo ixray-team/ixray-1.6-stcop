b2c74e5a8a46ef7710f35151bb19318ca1afcb60
Drombeys <dima.zolotov02@gmail.com>
2022-11-24 22:00:34 +0300

    Replace `delete` operator with `xr_delete()` function in xrXMLParser

--- src/xrXMLParser/tinyxmlparser.cpp
+++ src/xrXMLParser/tinyxmlparser.cpp
@@ -1149,7 +1149,7 @@ const char* TiXmlElement::Parse(TiXmlDocument* document, const char* p, TiXmlPar
 			if ( !p || !*p )
 			{
 				if ( document ) document->SetError( TIXML_ERROR_PARSING_ELEMENT, pErr, data, encoding );
-				delete attrib;
+				xr_delete(attrib);
 				return 0;
 			}
 
@@ -1162,7 +1162,7 @@ const char* TiXmlElement::Parse(TiXmlDocument* document, const char* p, TiXmlPar
 			if ( node )
 			{
 				node->SetValue( attrib->Value() );
-				delete attrib;
+				xr_delete(attrib);
 				return 0;
 			}
 
@@ -1208,7 +1208,7 @@ const char* TiXmlElement::ReadValue( const char* p, TiXmlParsingData* data, TiXm
 			if ( !textNode->Blank() )
 				LinkEndChild( textNode );
 			else
-				delete textNode;
+				xr_delete(textNode);
 		} 
 		else 
 		{
