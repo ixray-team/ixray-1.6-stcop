3039637cb9fc9f1c4ec6558b5c3b5c0336e282ef
acidicMercury8 <acidicmercury8@outlook.com>
2022-12-10 14:00:41 +0300

    Fix conflicted `_flags` methods names

--- src/xrGame/PHSkeleton.cpp
+++ src/xrGame/PHSkeleton.cpp
@@ -345,7 +345,7 @@ void CPHSkeleton::UnsplitSingle(CPHSkeleton* SO)
 	mask0.assign(pKinematics->LL_GetBonesVisible());//first part mask
 	VERIFY2(mask0.flags,"mask0 -Zero");
 	mask0.invert();
-	mask1.and(mask0.flags);//second part mask
+	mask1.and_(mask0.flags);//second part mask
 
 
 	newKinematics->LL_SetBoneRoot		(split_bone);
