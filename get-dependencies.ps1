261c70b515247af355ceafc172aa9c5a569deacb
acidicMercury8 <acidicmercury8@outlook.com>
2023-01-08 01:10:43 +0300

    Split lines in get dependencies script

--- get-dependencies.ps1
+++ get-dependencies.ps1
@@ -1,9 +1,12 @@
 # Set path to 7-Zip
-$path = Join-Path -Path ${env:ProgramFiles} -ChildPath "7-Zip\7z.exe"
+$path = Join-Path -Path ${env:ProgramFiles} `
+                  -ChildPath "7-Zip\7z.exe"
 
 # Getting DirectX SDK March 2009 from archive
 If (!(Test-Path "sdk\dxsdk_mar2009")) {
-    Invoke-WebRequest -Uri "https://github.com/ixray-team/ixray-1.6-stcop/releases/download/r0.2/sdk-directxsdk-mar2009.7z" `
+    $uri = "https://github.com/ixray-team/ixray-1.6-stcop/" + `
+           "releases/download/r0.2/sdk-directxsdk-mar2009.7z"
+    Invoke-WebRequest -Uri $uri `
                       -OutFile "directxsdk-mar2009.7z"
     Start-Process -FilePath $path `
                   -ArgumentList "x directxsdk-mar2009.7z" `
@@ -13,7 +16,9 @@ If (!(Test-Path "sdk\dxsdk_mar2009")) {
 
 # Getting DirectX SDK June 2010 from archive
 If (!(Test-Path "sdk\dxsdk_jun2010")) {
-    Invoke-WebRequest -Uri "https://github.com/ixray-team/ixray-1.6-stcop/releases/download/r0.2/sdk-directxsdk-jun2010.7z" `
+    $uri = "https://github.com/ixray-team/ixray-1.6-stcop/" + `
+           "releases/download/r0.2/sdk-directxsdk-jun2010.7z"
+    Invoke-WebRequest -Uri $uri `
                       -OutFile "directxsdk-jun2010.7z"
     Start-Process -FilePath $path `
                   -ArgumentList "x directxsdk-jun2010.7z" `
@@ -22,6 +27,12 @@ If (!(Test-Path "sdk\dxsdk_jun2010")) {
 }
 
 # Getting another dependencies from Git
-git clone --branch jul2022 --depth 1 https://github.com/microsoft/DirectXTex.git dep/DirectXTex
-git clone --branch jul2022 --depth 1 https://github.com/microsoft/DirectXMesh.git dep/DirectXMesh
-git clone --branch Release_3.1.0 --depth 1 https://github.com/dockpanelsuite/dockpanelsuite.git dep/dockpanelsuite
+git clone --branch jul2022 --depth 1 `
+    https://github.com/microsoft/DirectXTex.git `
+    dep/DirectXTex
+git clone --branch jul2022 --depth 1 `
+    https://github.com/microsoft/DirectXMesh.git `
+    dep/DirectXMesh
+git clone --branch Release_3.1.0 --depth 1 `
+    https://github.com/dockpanelsuite/dockpanelsuite.git `
+    dep/dockpanelsuite
