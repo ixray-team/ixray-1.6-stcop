ac3df2f78a46ce2bf8418a7a26df2583004a175c
acidicMercury8 <acidicmercury8@outlook.com>
2023-01-21 00:20:20 +0300

    Update libraries converting script

--- convert-libraries.bat
+++ convert-libraries.bat
@@ -2,15 +2,20 @@
 
 set coff2omf="%programfiles(x86)%\Embarcadero\Studio\21.0\bin\coff2omf.exe"
 
-%coff2omf% sdk\dxsdk_jun2010\Lib\x86\dsound.lib lib\dsoundB.lib
-%coff2omf% sdk\libraries\eax.lib lib\eaxB.lib
+%coff2omf% sdk\dxsdk_jun2010\Lib\x86\dinput8.lib lib\dinput8B.lib -v
+%coff2omf% sdk\dxsdk_jun2010\Lib\x86\dsound.lib lib\dsoundB.lib -v
+%coff2omf% sdk\libraries\eax.lib lib\eaxB.lib -v
+%coff2omf% sdk\libraries\FreeImage.lib lib\FreeImageB.lib -v
 
-%coff2omf% lib\Debug\libogg.lib lib\liboggB.lib
-%coff2omf% lib\Debug\libtheora.lib lib\libtheoraB.lib
-%coff2omf% lib\Debug\libvorbis.lib lib\libvorbisB.lib
-%coff2omf% lib\Debug\libvorbisfile.lib lib\libvorbisfileB.lib
+%coff2omf% lib\Debug\libogg.lib lib\liboggB.lib -v
+%coff2omf% lib\Debug\libtheora.lib lib\libtheoraB.lib -v
+%coff2omf% lib\Debug\libvorbis.lib lib\libvorbisB.lib -v
+%coff2omf% lib\Debug\libvorbisfile.lib lib\libvorbisfileB.lib -v
 
-%coff2omf% lib\openal32.lib lib\openal32B.lib
+%coff2omf% lib\openal32.lib lib\openal32B.lib -v
+%coff2omf% lib\DXT.lib lib\DXTB.lib -v
 %coff2omf% lib\ETools.lib lib\EToolsB.lib -v
+%coff2omf% lib\LWO.lib lib\LWOB.lib -v
+%coff2omf% lib\xrPhysics.lib lib\xrPhysicsB.lib -v
 
 pause
