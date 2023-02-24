15efdb626b92312a557d762cc276a87318eb4670
acidicMercury8 <acidicmercury8@outlook.com>
2023-02-24 13:49:42 +0300

    Fix gamedata packing

--- generate-patch.bat
+++ generate-patch.bat
@@ -1,9 +1,11 @@
 :: Get assets
-git clone ./.git ./temp
-cd temp
-git checkout c87b43f286f4a4fa5873a2eb4977d77571e51991
-cd ..
-move temp/gamedata gamedata_origin
+if not exist gamedata_origin (
+    git clone ./.git ./temp
+    cd temp
+    git checkout c87b43f286f4a4fa5873a2eb4977d77571e51991
+    cd ..
+    move temp/gamedata gamedata_origin
+)
 
 :: Generate patch
 bin\xrCompress.exe -diff gamedata gamedata_origin -out patch
