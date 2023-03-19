16c714348851e8aeb77922fc81875b4b31f79b86
Hrusteckiy <kostia4554t@yandex.ru>
2023-03-19 20:01:34 +0300

    Allow changing color of upgrade icon by config

--- src/xrGame/inventory_upgrade_property.h
+++ src/xrGame/inventory_upgrade_property.h
@@ -36,6 +36,7 @@ public:
 	IC LPCSTR		id_str() const;
 	IC LPCSTR		icon_name() const;
 	IC LPCSTR		name() const;
+	IC u32			icon_color() const;
 
 	IC FunctorParams_type const&	functor_params() const;
 
@@ -48,6 +49,7 @@ protected:
 
 	shared_str		m_name;
 	shared_str		m_icon;
+	u32				m_color;
 
 	StrFunctor			m_desc;
 	FunctorParams_type	m_functor_params;
