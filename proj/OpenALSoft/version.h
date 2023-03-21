9bf2486d64bcdd92647a69309c4d4c7db02a00c5
acidicMercury8 <acidicmercury8@outlook.com>
2023-03-21 18:37:45 +0300

    Add custom project for OpenAL Soft

---
+++ proj/OpenALSoft/version.h
@@ -0,0 +1,9 @@
+/* Define to the library version */
+#define ALSOFT_VERSION "1.23.0"
+#define ALSOFT_VERSION_NUM 1,23,0,0
+
+/* Define the branch being built */
+#define ALSOFT_GIT_BRANCH "HEAD"
+
+/* Define the hash of the head commit */
+#define ALSOFT_GIT_COMMIT_HASH "70c14cd"
