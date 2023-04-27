ff29fb49966309e4709954d4289d0c7cf9f7967b
acidicMercury8 <acidicmercury8@outlook.com>
2023-04-27 18:50:51 +0300

    Delete unused projects and scripts

--- get-dependencies.ps1
+++
@@ -1,7 +0,0 @@
-# Getting another dependencies from Git
-git clone --branch jul2022 --depth 1 `
-    https://github.com/microsoft/DirectXTex.git `
-    dep/DirectXTex
-git clone --branch jul2022 --depth 1 `
-    https://github.com/microsoft/DirectXMesh.git `
-    dep/DirectXMesh
