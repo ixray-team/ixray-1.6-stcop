4f8e4e4070e1376407c20e822baa8d6e5d4eca90
Drombeys <dima.zolotov02@gmail.com>
2023-07-09 18:36:25 +0300

    Fix crashing of sliding type doors

--- src/xrGame/doors_door.cpp
+++ src/xrGame/doors_door.cpp
@@ -33,8 +33,6 @@ door::door						( CPhysicObject* object ) :
 	VERIFY						( valid(m_target_state) );
 	VERIFY						( valid(m_previous_state) );
 
-	R_ASSERT					( m_object.get_door_vectors( m_closed_vector, m_open_vector ) );
-
 	Fmatrix invert;
 	invert.invert				( m_object.XFORM() );
 	invert.transform_dir		( m_open_vector );
