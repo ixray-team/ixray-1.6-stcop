4cf2b620c31d8c888cfcd2def30399563a409315
acidicMercury8 <acidicmercury8@outlook.com>
2023-07-18 17:11:32 +0300

    Delete EAX files

--- src/xrSound/stdafx.cpp
+++ src/xrSound/stdafx.cpp
@@ -16,7 +16,6 @@
 #	pragma comment(lib,	"dsoundb.lib" 		)
 //#	pragma comment(lib,	"xrapi.lib" 		)
 #else
-#	pragma comment(lib,	"eax.lib"			)
 #	pragma comment(lib,	"xrCore.lib"		)
 #	pragma comment(lib,	"xrCDB.lib"			)
 #	pragma comment(lib,	"dsound.lib" 		)
