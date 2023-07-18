432a37a5f3fd8979ffba2d655a5d9b682d716786
acidicMercury8 <acidicmercury8@outlook.com>
2023-07-18 17:11:52 +0300

    Replace zlib with NuGet package

--- src/utils/xrLC_Light/file_compress.cpp
+++ src/utils/xrLC_Light/file_compress.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
-#include "../../3rd party/zlib/zlib.h"
-#pragma comment(lib, "zlib.lib")
+
+#include <zlib.h>
+
 void compress( LPCSTR f_in, LPCSTR f_out );
 void decompress( LPCSTR f_in, LPCSTR f_out );
 
