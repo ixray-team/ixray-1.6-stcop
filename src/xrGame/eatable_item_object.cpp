2ed816bec540a1e9c13580409a25e8d7a5130796
Drombeys <dima.zolotov02@gmail.com>
2023-07-20 01:29:14 +0300

    Fix flickering after game item usage

--- src/xrGame/eatable_item_object.cpp
+++ src/xrGame/eatable_item_object.cpp
@@ -66,6 +66,11 @@ void CEatableItemObject::OnH_A_Independent	()
 {
 	CEatableItem::OnH_A_Independent		();
 	CPhysicItem::OnH_A_Independent		();
+
+	if (!Useful()) {
+		setVisible(false);
+		setEnabled(false);
+	}
 }
 
 void CEatableItemObject::OnH_B_Independent	(bool just_before_destroy)
