4d8211949976740035d748ada39d210ef1a7454e
acidicMercury8 <acidicmercury8@outlook.com>
2023-08-12 18:23:15 +0300

    Fix static `m_etable` variable in `ElevatorState` module

--- src/xrPhysics/ElevatorState.h
+++ src/xrPhysics/ElevatorState.h
@@ -20,7 +20,7 @@ private:
 		u32	  time;
 	};
 
-static SEnertionState m_etable[CElevatorState::clbNoState][CElevatorState::clbNoState];
+static SEnertionState m_etable[clbNoState][clbNoState];
 
 IClimableObject	*m_ladder;	
 CPHCharacter	*m_character;
