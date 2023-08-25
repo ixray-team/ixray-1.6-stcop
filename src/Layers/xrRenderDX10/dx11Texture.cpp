512c17e40955e7d5f66d86697a32c007c793fff1
Drombeys <dima.zolotov02@gmail.com>
2023-08-25 21:29:07 +0300

    Fix mipmap loading

--- src/Layers/xrRenderDX10/dx11Texture.cpp
+++ src/Layers/xrRenderDX10/dx11Texture.cpp
@@ -128,7 +128,7 @@ ID3DBaseTexture* CRender::texture_load(LPCSTR fRName, u32& ret_msize, bool bStag
         img_size = reader->length();
         R_ASSERT(reader);
         R_CHK2(GetMetadataFromDDSMemory(reader->pointer(), reader->length(), DDS_FLAGS::DDS_FLAGS_NONE, imageInfo), fn);
-        if (imageInfo.miscFlags & D3D_RESOURCE_MISC_TEXTURECUBE) {
+        if (imageInfo.IsCubemap()) {
             goto _DDS_CUBE;
         } else {
             goto _DDS_2D;
@@ -169,7 +169,7 @@ ID3DBaseTexture* CRender::texture_load(LPCSTR fRName, u32& ret_msize, bool bStag
             mip_lod = old_mipmap_cnt - imageInfo.mipLevels;
         }
 
-        hr = CreateTextureEx(HW.pDevice, scratchImage->GetImages() + mip_lod, imageInfo.mipLevels,
+        hr = CreateTextureEx(HW.pDevice, scratchImage->GetImages() + mip_lod, scratchImage->GetImageCount(),
             imageInfo, usage, bindFlags, cpuAccessFlags, miscFlags, CREATETEX_FLAGS::CREATETEX_DEFAULT, &pTexture2D);
         FS.r_close(reader);
         
