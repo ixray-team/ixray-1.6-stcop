f7ce69d61e4f94ac53c2bfc4cfe0ce7e7889c43f
ForserX <sv3nk@yandex.ru>
2023-08-28 16:14:25 +0300

    Enable C++17 for xrAI

--- src/utils/xrAI/xr_graph_merge.cpp
+++ src/utils/xrAI/xr_graph_merge.cpp
@@ -20,6 +20,9 @@
 #include "guid_generator.h"
 #include "game_graph_builder.h"
 #include <direct.h>
+#include <random>
+
+thread_local std::mt19937 rng = std::mt19937(std::random_device()());
 
 extern LPCSTR GAME_CONFIG;
 extern LPCSTR LEVEL_GRAPH_NAME;
@@ -373,7 +376,7 @@ public:
 
 		R_ASSERT2				(!l_dwaNodes.empty(),"Can't create at least one death point for specified graph point");
 
-		std::random_shuffle		(l_dwaNodes.begin(),l_dwaNodes.end());
+		std::shuffle		(l_dwaNodes.begin(),l_dwaNodes.end(), rng);
 
 		u32						m = l_dwaNodes.size() > 10 ? _min(iFloor(.1f*l_dwaNodes.size()),255) : l_dwaNodes.size(), l_dwStartIndex = m_tpLevelPoints.size();
 		m_tpLevelPoints.resize	(l_dwStartIndex + m);
