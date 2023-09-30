ed94e533c8e8aace986e732d2b9e5f1ab9160651
Drombeys <dima.zolotov02@gmail.com>
2023-09-30 12:23:29 +0300

    Delete render support checks

--- src/Layers/xrRender/xrRender_console.h
+++ src/Layers/xrRender/xrRender_console.h
@@ -207,7 +207,6 @@ enum
 };
 
 extern void						xrRender_initconsole	();
-extern BOOL						xrRender_test_hw		();
 extern void						xrRender_apply_tf		();
 
 #endif
--- src/Layers/xrRenderPC_R4/xrRender_R4.cpp
+++ src/Layers/xrRenderPC_R4/xrRender_R4.cpp
@@ -1,6 +1,5 @@
-// xrRender_R2.cpp : Defines the entry point for the DLL application.
-//
 #include "stdafx.h"
+
 #include "../xrRender/dxRenderFactory.h"
 #include "../xrRender/dxUIRender.h"
 #include "../xrRender/dxDebugRender.h"
@@ -16,7 +15,6 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 	{
 	case DLL_PROCESS_ATTACH	:
 		//	Can't call CreateDXGIFactory from DllMain
-		//if (!xrRender_test_hw())	return FALSE;
 		::Render					= &RImplementation;
 		::RenderFactory				= &RenderFactoryImpl;
 		::DU						= &DUImpl;
@@ -33,28 +31,4 @@ BOOL APIENTRY DllMain( HANDLE hModule,
 		break;
 	}
 	return TRUE;
-}
-
-
-extern "C"
-{
-	bool _declspec(dllexport) SupportsDX11Rendering();
-};
-
-bool _declspec(dllexport) SupportsDX11Rendering()
-{
-	return xrRender_test_hw()?true:false;
-	/*
-	D3DCAPS9					caps;
-	CHW							_HW;
-	_HW.CreateD3D				();
-	_HW.pD3D->GetDeviceCaps		(D3DADAPTER_DEFAULT,D3DDEVTYPE_HAL,&caps);
-	_HW.DestroyD3D				();
-	u16		ps_ver_major		= u16 ( u32(u32(caps.PixelShaderVersion)&u32(0xf << 8ul))>>8 );
-
-	if (ps_ver_major<3)
-		return false;
-	else
-		return true;
-	*/
 }
