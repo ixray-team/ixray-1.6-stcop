d3f5a9421bb95b33fff675878845a39245b5d844
Drombeys <dima.zolotov02@gmail.com>
2023-10-03 11:00:35 +0300

    Replace missing sound with stub on using scripts

--- src/xrGame/script_sound.cpp
+++ src/xrGame/script_sound.cpp
@@ -18,10 +18,13 @@ CScriptSound::CScriptSound				(LPCSTR caSoundName, ESoundTypes sound_type)
 	m_caSoundToPlay			= caSoundName;
 	string_path				l_caFileName;
 	VERIFY(::Sound)	;
-	if (FS.exist(l_caFileName,"$game_sounds$",caSoundName,".ogg"))
-		m_sound.create		(caSoundName,st_Effect,sound_type);
-	else
-		ai().script_engine().script_log	(ScriptStorage::eLuaMessageTypeError,"File not found \"%s\"!",l_caFileName);
+	if (FS.exist(l_caFileName, "$game_sounds$", caSoundName, ".ogg")) {
+		m_sound.create(caSoundName, st_Effect, sound_type);
+	} else {
+		ai().script_engine().script_log(ScriptStorage::eLuaMessageTypeMessage, "File not found \"%s\"!", l_caFileName);
+		FS.update_path(l_caFileName, "$game_sounds$", "$no_sound.ogg");
+		m_sound.create("$no_sound.ogg", st_Effect, sound_type);
+	}
 }
 
 CScriptSound::~CScriptSound		() noexcept(false)
