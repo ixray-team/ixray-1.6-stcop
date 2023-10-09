d65b00c8a9be1c4e96f20204e30f54c64c889381
Drombeys <dima.zolotov02@gmail.com>
2023-10-09 16:35:37 +0300

    Fix reading `renderer` parameter

--- src/xrPlay/xrPlay.cpp
+++ src/xrPlay/xrPlay.cpp
@@ -112,11 +112,12 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 	else if (strstr(Core.Params, "-r2"))
 		Console->Execute("renderer renderer_r2");
 	else {
-		// Костыль, пока не перепишем итератор рендера с нуля
-		Console->Execute((std::string("renderer ") + Console->GetToken("renderer")).c_str());
+		CCC_LoadCFG_custom* pTmp = xr_new<CCC_LoadCFG_custom>("renderer ");
+		pTmp->Execute(Console->ConfigFile);
+		xr_delete(pTmp);
 	}
-
 	Engine.External.Initialize();
+
 	Console->Execute("stat_memory");
 
 	EngineLoadStage4();
