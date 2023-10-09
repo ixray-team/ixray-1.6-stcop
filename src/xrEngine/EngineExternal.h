7da4c085eba8e9f3c98c23402e911ee89f44d232
ForserX <sv3nk@yandex.ru>
2023-10-09 15:27:52 +0300

    Add EEngineExternalPhysical::DeadBodyRagdoll
    Disable by default

--- src/xrEngine/EngineExternal.h
+++ src/xrEngine/EngineExternal.h
@@ -8,6 +8,7 @@ enum class EEngineExternalUI
 
 enum class EEngineExternalPhysical
 {
+	DeadBodyRagdoll,
 	None
 };
 
--- src/xrEngine/EngineExternalEnums.cpp
+++ src/xrEngine/EngineExternalEnums.cpp
@@ -3,24 +3,28 @@
 #define DECL_ENUM_ELEMENT( element ) #element
 #define BEGIN_ENUM( ENUM_NAME ) char* gs_##ENUM_NAME [] =
 #define END_ENUM( ENUM_NAME ) ; char* getString##ENUM_NAME(\
-            ENUM_NAME index){ return gs_##ENUM_NAME [(size_t)index]; }
+			ENUM_NAME index){ return gs_##ENUM_NAME [(size_t)index]; }
 
 BEGIN_ENUM(EEngineExternalUI)
 {
-    DECL_ENUM_ELEMENT(HQIcons),
-} END_ENUM(EEngineExternalUI)
+	DECL_ENUM_ELEMENT(HQIcons),
+} 
+END_ENUM(EEngineExternalUI)
 
 BEGIN_ENUM(EEngineExternalPhysical)
 {
-    DECL_ENUM_ELEMENT(None),
-} END_ENUM(EEngineExternalPhysical)
+	DECL_ENUM_ELEMENT(DeadBodyRagdoll),
+} 
+END_ENUM(EEngineExternalPhysical)
 
 BEGIN_ENUM(EEngineExternalGame)
 {
-    DECL_ENUM_ELEMENT(None),
-} END_ENUM(EEngineExternalGame)
+	DECL_ENUM_ELEMENT(None),
+} 
+END_ENUM(EEngineExternalGame)
 
 BEGIN_ENUM(EEngineExternalRender)
 {
-    DECL_ENUM_ELEMENT(None),
-} END_ENUM(EEngineExternalRender)
+	DECL_ENUM_ELEMENT(None),
+} 
+END_ENUM(EEngineExternalRender)
