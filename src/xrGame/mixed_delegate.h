53aaa4fa196dcbd4856baec8373ee3ed1c22ecfd
ForserX <sv3nk@yandex.ru>
2023-10-12 20:21:16 +0300

    Apply `xr_stdcall` to `mixed_delegate` to fix Win32 config

--- src/xrGame/mixed_delegate.h
+++ src/xrGame/mixed_delegate.h
@@ -60,7 +60,7 @@ public:
 	}
 
 	template <class ThisRef, class ClassType>
-	void bind_cpp(ThisRef* ptr_this, R(ClassType::* func_ptr)(Param1, Param2)) {
+	void bind_cpp(ThisRef* ptr_this, R(xr_stdcall ClassType::* func_ptr) (Param1, Param2)) {
 		m_cpp_delegate.bind(ptr_this, func_ptr);
 	}
 	
