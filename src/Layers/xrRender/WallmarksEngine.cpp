3f9b1017b18f1f4ee75d6a1a5f67d629ae2a6650
MAYLAYSHEZ <MAYLAYSHEZ@yandex.ru>
2023-11-02 15:09:28 +0400

    Fix game crash when rendering wallmarks

--- src/Layers/xrRender/WallmarksEngine.cpp
+++ src/Layers/xrRender/WallmarksEngine.cpp
@@ -28,7 +28,7 @@ namespace WallmarksEngine {
 const float W_DIST_FADE		= 15.f;
 const float	W_DIST_FADE_SQR	= W_DIST_FADE*W_DIST_FADE;
 const float I_DIST_FADE_SQR	= 1.f/W_DIST_FADE_SQR;
-const int	MAX_TRIS		= 1024;
+const int MAX_TRIS = 16384;
 
 IC bool operator == (const CWallmarksEngine::wm_slot* slot, const ref_shader& shader){return slot->shader==shader;}
 CWallmarksEngine::wm_slot* CWallmarksEngine::FindSlot	(ref_shader shader)
