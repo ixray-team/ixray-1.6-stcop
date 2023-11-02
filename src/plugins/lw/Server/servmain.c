266cb019e1ac6c41aebd9615aca3e9ce31834b92
ForserX <sv3nk@yandex.ru>
2023-11-03 01:51:27 +0300

    Plugins: update LW Server project

--- src/plugins/lw/Server/servmain.c
+++ src/plugins/lw/Server/servmain.c
@@ -1,25 +1,26 @@
 /*
  * LWSDK Startup File
- * Copyright 1995,1997  NewTek, Inc.
+ *
+ *$copyright$
  */
 #include <lwmodule.h>
-
 
 extern void *        Startup (void);
 extern void        Shutdown (void *serverData);
 extern ServerRecord     ServerDesc[];
 
-
- #ifdef __BORLANDC__
+#if defined(__BORLANDC__)
 ModuleDescriptor mod_descrip =
- #else
+#elif defined __GNUC__
+__attribute__ ((visibility("default"))) ModuleDescriptor _mod_descrip =
+#else
 ModuleDescriptor _mod_descrip =
- #endif
- {
+#endif
+{
     MOD_SYSSYNC,
     MOD_SYSVER,
     MOD_MACHINE,
     Startup,
     Shutdown,
     ServerDesc
- };
+};
