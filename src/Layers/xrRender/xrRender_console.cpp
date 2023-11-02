dadff89a6563f7b55b4cd2d9c58c4d61269d50a6
MAYLAYSHEZ <MAYLAYSHEZ@yandex.ru>
2023-11-02 15:15:54 +0400

    Bind wallmark life time on dynamic objects to `r__wallmark_ttl`
    Increased max wallmark lifetime to 10 minutes

--- src/Layers/xrRender/xrRender_console.cpp
+++ src/Layers/xrRender/xrRender_console.cpp
@@ -699,7 +699,7 @@ void		xrRender_initconsole	()
 	CMD4(CCC_Float,		"r__wallmark_shift_v",	&ps_r__WallmarkSHIFT_V,		0.0f,	1.f		);
 	CMD1(CCC_ModelPoolStat,"stat_models"		);
 #endif // DEBUG
-	CMD4(CCC_Float,		"r__wallmark_ttl",		&ps_r__WallmarkTTL,			1.0f,	5.f*60.f);
+	CMD4(CCC_Float, "r__wallmark_ttl", &ps_r__WallmarkTTL, 1.0f, 10.f * 60.f);
 
 	CMD3(CCC_Mask, "r__no_ram_textures", &ps_r__common_flags, RFLAG_NO_RAM_TEXTURES);
 
