142173cdd348e455a3708dbb105539a6a9cb4247
MAYLAYSHEZ <MAYLAYSHEZ@yandex.ru>
2023-11-02 13:36:46 +0400

    Fix luminocity indicator after save load

--- src/xrGame/ui/UIMotionIcon.cpp
+++ src/xrGame/ui/UIMotionIcon.cpp
@@ -53,6 +53,7 @@ void CUIMotionIcon::Init(Frect const& zonemap_rect)
 	xml_init.InitProgressShape	(uiXml, "luminosity_progress", 0, &m_luminosity_progress);	
 	m_luminosity_progress.SetWndSize(sz);
 	m_luminosity_progress.SetWndPos(pos);
+	m_luminosity_progress.SetPos(0.f);
 
 	AttachChild					(&m_noise_progress);
 	xml_init.InitProgressShape	(uiXml, "noise_progress", 0, &m_noise_progress);
