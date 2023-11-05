6c8dc30337b6e79772b4dcbdc8e09f12c8baa583
ForserX <sv3nk@yandex.ru>
2023-11-05 03:13:11 +0300

    Add `-all` key to compilers and enable `-do -ai -lc` modes
    Fixes bug in xrLC that caused `ccc__temp__vertices` to be created
    in the working directory

--- src/utils/xrForms/xrForms.cpp
+++ src/utils/xrForms/xrForms.cpp
@@ -19,33 +19,61 @@ void Help(const char* h_str) {
 	MessageBoxA(0, h_str, "Command line options", MB_OK | MB_ICONINFORMATION);
 }
 
+struct CompilersMode {
+	bool AI = false;
+	bool DO = false;
+	bool LC = false;
+
+	bool Silent = false;
+};
+
+CompilersMode gCompilerMode;
+
 void Startup(LPSTR lpCmdLine) {
 	u32 dwStartupTime = timeGetTime();
 
-	if (strstr(lpCmdLine, "-lc")) {
+	u32 dwTimeLC = 0;
+	if (gCompilerMode.LC) {
+		dwTimeLC = timeGetTime();
 		Phase("xrLC Startup");
 		StartupLC(lpCmdLine);
+
+		dwTimeLC = (timeGetTime() - dwTimeLC) / 1000;
 	}
 
-	if (strstr(lpCmdLine, "-ai")) {
+	u32 dwTimeAI = 0;
+	if (gCompilerMode.AI) {
+		dwTimeAI = timeGetTime();
 		Phase("xrAI Startup");
 		InitialFactory();
 		StartupAI(lpCmdLine);
 		DestroyFactory();
+		dwTimeAI = (timeGetTime() - dwTimeAI) / 1000;
 	}
 
-	if (strstr(lpCmdLine, "-do")) {
+	u32 dwTimeDO = 0;
+	if (gCompilerMode.DO) {
+		dwTimeDO = timeGetTime();
 		Phase("xrDO Startup");
 		StartupDO(lpCmdLine);
+		dwTimeDO = (timeGetTime() - dwTimeDO) / 1000;
 	}
 
 	// Show statistic
 	string256 stats;
 	extern std::string make_time(u32 sec);
 	u32 dwEndTime = timeGetTime();
-	xr_sprintf(stats, "Time elapsed: %s", make_time((dwEndTime - dwStartupTime) / 1000).c_str());
 
-	if (!strstr(lpCmdLine, "-silent")) {
+	xr_sprintf(
+		stats, 
+		"Time elapsed: %s \r\n xrLC: %s\r\n xrAI: %s\r\n xrDO: %s", 
+		make_time((dwEndTime - dwStartupTime) / 1000).c_str(), 
+		make_time(dwTimeLC).c_str(),
+		make_time(dwTimeAI).c_str(), 
+		make_time(dwTimeDO).c_str()
+	);
+
+	if (!gCompilerMode.Silent) {
 		MessageBoxA(logWindow, stats, "Congratulation!", MB_OK | MB_ICONINFORMATION);
 	}
 
@@ -66,6 +94,14 @@ int APIENTRY WinMain (
 	// Initialize debugging
 	Debug._initialize(false);
 	Core._initialize("IX-Ray Compilers");
+
+	// Read modes
+	bool SupportAll = strstr(lpCmdLine, "-all");
+	gCompilerMode.AI = SupportAll || strstr(lpCmdLine, "-ai");
+	gCompilerMode.LC = SupportAll || strstr(lpCmdLine, "-lc");
+	gCompilerMode.DO = SupportAll || strstr(lpCmdLine, "-do");
+
+	gCompilerMode.Silent = strstr(lpCmdLine, "-silent");
 
 	// Give a LOG-thread a chance to startup
 	InitCommonControls();
