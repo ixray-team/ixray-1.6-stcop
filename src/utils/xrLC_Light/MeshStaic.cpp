52b385c57527e7133a01c96fef5dd39c2b8d988e
ForserX <sv3nk@yandex.ru>
2023-11-05 03:13:11 +0300

    Compilers: Add key `-all`: enable -do -ai -lc modes
    xrLC: Fix bug that caused ```ccc__temp__vertices``` to be created in the working directory

--- src/utils/xrLC_Light/MeshStaic.cpp
+++ src/utils/xrLC_Light/MeshStaic.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include "../xrLC/Build.h"
 
 #include "xrLC_GlobalData.h"
 
@@ -14,6 +15,7 @@ Face* xrLC_GlobalData	::create_face	()
 {
 	return _FacePool.create();
 }
+
 void xrLC_GlobalData	::destroy_face	(Face* &f)
 {
 	_FacePool.destroy( f );
@@ -23,13 +25,11 @@ Vertex* xrLC_GlobalData	::create_vertex	()
 {
 	return _VertexPool.create();
 }
+
 void xrLC_GlobalData	::destroy_vertex	(Vertex* &f)
 {
 	_VertexPool.destroy( f );
 }
-
-
-
 
 static struct destruct_vertex_not_uregister
 {
@@ -38,6 +38,7 @@ static struct destruct_vertex_not_uregister
 		::destroy_vertex( v, false );
 	}
 } _destruct_vertex_not_uregister;
+
 static struct destruct_face_not_uregister
 {
 	static void destruct (Face * &f)
@@ -45,6 +46,7 @@ static struct destruct_face_not_uregister
 		::destroy_face( f, false );
 	}
 } _destruct_face_not_uregister;
+
 void xrLC_GlobalData	::gl_mesh_clear	()
 {
 	vec_clear( _g_vertices, _destruct_vertex_not_uregister ); 
@@ -55,85 +57,60 @@ void xrLC_GlobalData	::gl_mesh_clear	()
 }
 
 
-void xrLC_GlobalData	::vertices_isolate_and_pool_reload()
+void xrLC_GlobalData::vertices_isolate_and_pool_reload()
 {
-
-
 	const u32 inital_verts_count = _g_vertices.size();
-		  u32 not_empty_verts = 0;
-	//for(u32 i = 0; i < inital_verts_count; ++i )
-	// if(!_g_vertices[i]->m_adjacents.empty())
-	//	++not_empty_verts;
-/////////////////////////////////////////////////////////
-	
-	string_path			path_name;
-	FS.update_path		( path_name, "$app_root$", "ccc__temp__vertices"  );
+	u32 not_empty_verts = 0;
+
+	string_path path_name;
+	strconcat(sizeof(path_name), path_name, pBuild->path, "build.vertices");
 	{
-		IWriter * file		= FS.w_open( path_name );
-		R_ASSERT( file );
-		for(u32 i = 0; i < inital_verts_count; ++i )
+		IWriter* file = FS.w_open(path_name);
+		R_ASSERT(file);
+		for (u32 i = 0; i < inital_verts_count; ++i)
 		{
-			Vertex	&v = *_g_vertices[i];
-			if( v.m_adjacents.empty() )
+			Vertex& v = *_g_vertices[i];
+			if (v.m_adjacents.empty())
 			{
-				::destroy_vertex( _g_vertices[i], false );
+				::destroy_vertex(_g_vertices[i], false);
 				continue;
 			}
-//isolate_pool_clear_read	
-//isolate_pool_clear_write
 
-			v.isolate_pool_clear_write( *file );
-			::destroy_vertex( _g_vertices[i], false );
+			v.isolate_pool_clear_write(*file);
+			::destroy_vertex(_g_vertices[i], false);
 			++not_empty_verts;
 		}
 		FS.w_close(file);
 	}
-/////////////////////////////////////////////////////////
-	_g_vertices.clear();
-	clMsg( "mem usage before clear pool: %u", Memory.mem_usage() );
 
+	_g_vertices.clear();
+	clMsg("mem usage before clear pool: %u", Memory.mem_usage());
 
 	_VertexPool.clear();
-	
-/////////////////////////////////////////////////////////
+
 	{
 		b_vert_not_register = true;
-		_g_vertices.resize( not_empty_verts, 0 );
+		_g_vertices.resize(not_empty_verts, 0);
 
 		Memory.mem_compact();
-		clMsg( "mem usage after clear pool: %u", Memory.mem_usage() );
+		clMsg("mem usage after clear pool: %u", Memory.mem_usage());
 
-		INetReaderFile r_verts( path_name );
-		for(u32 i = 0; i < not_empty_verts; ++i )
+		INetReaderFile r_verts(path_name);
+		for (u32 i = 0; i < not_empty_verts; ++i)
 		{
-			Vertex* &v = _g_vertices[i];
+			Vertex*& v = _g_vertices[i];
 			v = _VertexPool.create();
-			v->isolate_pool_clear_read( r_verts );
+			v->isolate_pool_clear_read(r_verts);
 		}
 		b_vert_not_register = false;
 	}
 }
 
 
-void	xrLC_GlobalData::clear_mesh		()
+void xrLC_GlobalData::clear_mesh()
 {
-	
-	//R_ASSERT(g_XSplit.empty());
-	clMsg( "mem usage before clear mesh: %u", Memory.mem_usage() );
-	//g_vertices().clear();
-	//g_faces().clear();
-	//_VertexPool.clear();
-	//_FacePool.clear();
-	gl_mesh_clear	();
+	clMsg("mem usage before clear mesh: %u", Memory.mem_usage());
+	gl_mesh_clear();
 	Memory.mem_compact();
-	clMsg( "mem usage after clear mesh: %u", Memory.mem_usage() );
+	clMsg("mem usage after clear mesh: %u", Memory.mem_usage());
 }
-
-
-
-
-
-
-
-
-
