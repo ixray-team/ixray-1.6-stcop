40c7dd4668d00b55d2401f0b922c09cc38aaa940
ForserX <sv3nk@yandex.ru>
2023-11-09 21:14:54 +0300

    Add `enable_dof_reload` console command

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -1903,12 +1903,14 @@ const float &CWeapon::hit_probability	() const
 	return					(m_hit_probability[egdNovice]);
 }
 
+BOOL EnableDof = true;
+
 void CWeapon::OnStateSwitch	(u32 S)
 {
 	inherited::OnStateSwitch(S);
 	m_BriefInfo_CalcFrame = 0;
 
-	if(GetState()==eReload)
+	if(EnableDof && GetState()==eReload)
 	{
 		if(H_Parent()==Level().CurrentEntity() && !fsimilar(m_zoom_params.m_ReloadDof.w,-1.0f))
 		{
