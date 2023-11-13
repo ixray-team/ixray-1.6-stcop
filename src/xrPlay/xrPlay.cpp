819353221c4ff6f42ca18c373fbe89c280e1e07f
MAYLAYSHEZ <MAYLAYSHEZ@yandex.ru>
2023-11-13 17:06:57 +0400

    Fix window initialization problems (test)

--- src/xrPlay/xrPlay.cpp
+++ src/xrPlay/xrPlay.cpp
@@ -19,11 +19,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 void CreateGameWindow() {
 	// Unless a substitute hWnd has been specified, create a window to render into
 	if (g_AppInfo.WindowHandle == NULL) {
-		const wchar_t* wndclass = L"_XRAY_1.6";
+		const wchar_t* wndclass = L"IX-RAY_1.6";
 
 		// Register the windows class
 		HINSTANCE hInstance = (HINSTANCE)GetModuleHandle(0);
-		WNDCLASS wndClass = { 0, WndProc, 0, 0, hInstance,
+		WNDCLASS wndClass = { CS_HREDRAW | CS_VREDRAW | CS_OWNDC, WndProc, 0, 0, hInstance,
 							  LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1)),
 							  LoadCursor(NULL, IDC_ARROW),
 							  (HBRUSH)GetStockObject(BLACK_BRUSH),
@@ -31,19 +31,24 @@ void CreateGameWindow() {
 		RegisterClass(&wndClass);
 
 		// Set the window's initial style
-		Device.m_dwWindowStyle = WS_BORDER | WS_DLGFRAME;
-
-		// Set the window's initial width
-		RECT rc;
-		SetRect(&rc, 0, 0, 640, 480);
-		AdjustWindowRect(&rc, Device.m_dwWindowStyle, FALSE);
+		Device.m_dwWindowStyle = WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_POPUP;
 
 		// Create the render window
-		g_AppInfo.WindowHandle = CreateWindowEx(WS_EX_TOPMOST,
-			wndclass, L"S.T.A.L.K.E.R.: Call of Pripyat", Device.m_dwWindowStyle,
-			/*rc.left, rc.top, */CW_USEDEFAULT, CW_USEDEFAULT,
-			(rc.right - rc.left), (rc.bottom - rc.top), 0L,
-			0, hInstance, 0L);
+		u32 screen_width = GetSystemMetrics(SM_CXSCREEN);
+		u32 screen_height = GetSystemMetrics(SM_CYSCREEN);
+
+		DEVMODE screen_settings;
+		memset(&screen_settings, 0, sizeof(screen_settings));
+		screen_settings.dmSize = sizeof(screen_settings);
+		screen_settings.dmPelsWidth = (unsigned long)screen_width;
+		screen_settings.dmPelsHeight = (unsigned long)screen_height;
+		screen_settings.dmBitsPerPel = 32;
+		screen_settings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+
+		ChangeDisplaySettings(&screen_settings, CDS_FULLSCREEN);
+
+		g_AppInfo.WindowHandle = CreateWindowEx(WS_EX_TOPMOST, wndclass, L"S.T.A.L.K.E.R.: Call of Pripyat", Device.m_dwWindowStyle, 0, 0,
+			screen_width, screen_height, 0L, 0, hInstance, 0L);
 	}
 }
 int APIENTRY WinMain(HINSTANCE hInstance,
