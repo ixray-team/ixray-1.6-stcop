f20f1a7bc2070663e1744153d678a8d21ed48965
MAYLAYSHEZ <MAYLAYSHEZ@yandex.ru>
2023-11-22 12:34:25 +0400

    Add return to `WeaponMagazined.cpp` when `inventory_owner` doesn't have `active_item`

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -542,6 +542,8 @@ void CWeaponMagazined::state_Fire(float dt)
 				Log("next_state", GetNextState());
 				Log("item_sect", cNameSect().c_str());
 				Log("H_Parent", H_Parent()->cNameSect().c_str());
+				StopShooting();
+				return;
 		}
 
 		CEntity* E = smart_cast<CEntity*>(H_Parent());
