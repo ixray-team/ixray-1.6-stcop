ee2421a1ce652c88b1ace507003d92738f477862
Drombeys <dima.zolotov02@gmail.com>
2023-11-30 12:32:54 +0300

    Disable texture dump output on release configuration

--- src/Layers/xrRenderPC_R2/r2_loader.cpp
+++ src/Layers/xrRenderPC_R2/r2_loader.cpp
@@ -233,7 +233,9 @@ void CRender::LoadBuffers		(CStreamReader *base_fs,	BOOL _alternative)
 			// count, size
 			u32 vCount			= fs->r_u32	();
 			u32 vSize = ComputeVertexSize(dcl, 0);
-			Msg	("* [Loading VB] %d verts, %d Kb",vCount,(vCount*vSize)/1024);
+#ifdef DEBUG
+			Msg("* [Loading VB] %d verts, %d Kb", vCount, (vCount * vSize) / 1024);
+#endif // DEBUG
 
 			// Create and fill
 			BYTE*	pData		= 0;
@@ -257,7 +259,9 @@ void CRender::LoadBuffers		(CStreamReader *base_fs,	BOOL _alternative)
 		for (u32 i=0; i<count; i++)
 		{
 			u32 iCount			= fs->r_u32	();
-			Msg("* [Loading IB] %d indices, %d Kb",iCount,(iCount*2)/1024);
+#ifdef DEBUG
+			Msg("* [Loading IB] %d indices, %d Kb", iCount, (iCount * 2) / 1024);
+#endif // DEBUG
 
 			// Create and fill
 			BYTE*	pData		= 0;
