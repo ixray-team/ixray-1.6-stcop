87110da2a98b75492309cc5f33c708862362bfbc
ggGhosTt <artem.babichev.main@gmail.com>
2023-12-05 18:05:38 -0500

    Add exports for camera states in lua
    Gives an opportunity to check for first/ third person camera states and set them if needed.

--- src/xrGame/actor_script.cpp
+++ src/xrGame/actor_script.cpp
@@ -13,14 +13,21 @@
 
 using namespace luabind;
 
+bool isFirstPerson() { return Actor()->active_cam() == eacFirstEye; }
+void setFirstPerson() { Actor()->cam_Set(eacFirstEye); }
+void setThirdPerson() { Actor()->cam_Set(eacLookAt); }
+
 #pragma optimize("s",on)
-void CActor::script_register(lua_State *L)
+void CActor::script_register(lua_State* L)
 {
 	module(L)
-	[
-		class_<CActor,CGameObject>("CActor")
-			.def(constructor<>()),
-		class_<CLevelChanger,CGameObject>("CLevelChanger")
-			.def(constructor<>())
-	];
+		[
+			class_<CActor, CGameObject>("CActor")
+				.def(constructor<>()),
+				class_<CLevelChanger, CGameObject>("CLevelChanger")
+				.def(constructor<>()),
+				def("is_first_person", isFirstPerson),
+				def("set_first_person", setFirstPerson),
+				def("set_third_person", setThirdPerson)
+		];
 }
