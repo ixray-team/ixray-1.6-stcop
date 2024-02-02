af6fa24a1461341626b87939260df1de4b0faefe
ForserX <sv3nk@yandex.ru>
2024-02-02 21:37:50 +0300

    Update Game Spatial Space and Physics Spatial Space upon level changes

--- src/xrCDB/ISpatial.cpp
+++ src/xrCDB/ISpatial.cpp
@@ -165,22 +165,24 @@ ISpatial_DB::~ISpatial_DB()
 	}
 }
 
-void			ISpatial_DB::initialize(Fbox& BB)
+void ISpatial_DB::initialize(Fbox& BB)
 {
-	if (0==m_root)			
+	Fvector bbc, bbd;
+	BB.get_CD(bbc, bbd);
+
+	m_center.set(bbc);
+	m_bounds = _max(_max(bbd.x, bbd.y), bbd.z);
+
+	if (m_root == nullptr)
 	{
 		// initialize
-		Fvector bbc,bbd;
-		BB.get_CD				(bbc,bbd);
-
-		allocator_pool.reserve	(128);
-		m_center.set			(bbc);
-		m_bounds				= _max(_max(bbd.x,bbd.y),bbd.z);
-		rt_insert_object		= NULL;
-		if (0==m_root)	m_root	= _node_create();
-		m_root->_init			(NULL);
+		allocator_pool.reserve(128);
+		rt_insert_object = nullptr;
+		m_root = _node_create();
+		m_root->_init(nullptr);
 	}
 }
+
 ISpatial_NODE*	ISpatial_DB::_node_create		()
 {
 	stat_nodes	++;
