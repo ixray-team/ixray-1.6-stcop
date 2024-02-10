b04fc84785e47f1ce8c88ab3d5a1855b7d0223a1
ForserX <sv3nk@yandex.ru>
2024-02-10 18:59:10 +0300

    Fix basic bugs with vehicles

--- src/xrGame/Actor.cpp
+++ src/xrGame/Actor.cpp
@@ -176,7 +176,7 @@ CActor::CActor() : CEntityAlive(),current_ik_cam_shift(0)
 
 
 	m_anims					= xr_new<SActorMotions>();
-//.	m_vehicle_anims			= xr_new<SActorVehicleAnims>();
+	m_vehicle_anims			= xr_new<SActorVehicleAnims>();
 	m_entity_condition		= nullptr;
 	m_iLastHitterID			= u16(-1);
 	m_iLastHittingWeaponID	= u16(-1);
@@ -221,7 +221,7 @@ CActor::~CActor()
 
 	xr_delete				(m_anims);
 	xr_delete				(pPickup);
-//.	xr_delete				(m_vehicle_anims);
+	xr_delete				(m_vehicle_anims);
 }
 
 void CActor::reinit	()
