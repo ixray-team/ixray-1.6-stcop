ba436761f9ec13eea7aabeb39aebd5306528b677
Shtrecker <gogels235@gmail.com>
2024-03-09 21:00:38 +0300

    Fix resetting parameters after travel to location
    Co-Authored-By: Shitposting Goddess <18101486+mortany@users.noreply.github.com>

--- src/xrServerEntities/xrServer_Objects.h
+++ src/xrServerEntities/xrServer_Objects.h
@@ -162,8 +162,9 @@
 //       CSE_ALifeTraderAbstract		added property m_deadbody_can_take, m_deadbody_closed
 // 127	 CSE_ALifeObjectClimable		added material;
 // 128	 CSE_ALifeObjectClimable		added can_fire in smart covers;
+// 129	 CSE_ALifeItemWeapon			added cur_scope
 //------------------------------------------------------------------------------
-#define SPAWN_VERSION	u16(128)
+#define SPAWN_VERSION	u16(129)
 
 SERVER_ENTITY_DECLARE_BEGIN2(CSE_Shape,ISE_Shape,CShapeData)
 public:
