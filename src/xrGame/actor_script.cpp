d081fb01919928dc389bd5e26562706a65c23143
Shtrecker <gogels235@gmail.com>
2024-03-10 11:26:13 +0200

    Export actor movement states

--- src/xrGame/actor_script.cpp
+++ src/xrGame/actor_script.cpp
@@ -49,6 +49,38 @@ void CActor::script_register(lua_State* L)
 					value("eBoostFireWoundImmunity", eBoostFireWoundImmunity),
 					value("eBoostWoundImmunity", eBoostWoundImmunity),
 					value("eBoostMaxCount", eBoostMaxCount)
+			],
+
+			class_<enum_exporter<EMovementStates>>("EMovementStates").enum_("emovementstates")
+			[
+				value("eOld", eOld),
+				value("eWishful", eWishful),
+				value("eReal", eReal)
+			],
+
+			class_<enum_exporter<EMoveCommand>>("EMoveCommand").enum_("emovecommand")
+			[
+				value("mcFwd", mcFwd),
+				value("mcBack", mcBack),
+				value("mcLStrafe", mcLStrafe),
+				value("mcRStrafe", mcRStrafe),
+				value("mcCrouch", mcCrouch),
+				value("mcAccel", mcAccel),
+				value("mcTurn", mcTurn),
+				value("mcJump", mcJump),
+				value("mcFall", mcFall),
+				value("mcLanding", mcLanding),
+				value("mcLanding2", mcLanding2),
+				value("mcClimb", mcClimb),
+				value("mcSprint", mcSprint),
+				value("mcLLookout", mcLLookout),
+				value("mcRLookout", mcRLookout),
+				value("mcAnyMove", mcAnyMove),
+				value("mcAnyAction", mcAnyAction),
+				value("mcAnyState", mcAnyState),
+				value("mcLookout", mcLookout),
+				value("mcJumpSeq", mcJumpSeq)
+
 			]
 		];
 }
