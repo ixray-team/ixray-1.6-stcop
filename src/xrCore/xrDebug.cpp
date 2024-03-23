f953e6a8681a9566a100404fed334acecdc3e852
Hozar2002 <hoz2002team@gmail.com>
2024-03-23 22:38:09 +0500

    Wait 100ms before game crash
    Дабы крутой мт лог успел написать все ошибки после вылета шейдров

--- src/xrCore/xrDebug.cpp
+++ src/xrCore/xrDebug.cpp
@@ -124,6 +124,7 @@ void xrDebug::do_exit	(const std::string &message)
 
 void xrDebug::backend	(const char *expression, const char *description, const char *argument0, const char *argument1, const char *file, int line, const char *function, bool &ignore_always)
 {
+	Sleep(100);
 	static xrCriticalSection CS
 #ifdef PROFILE_CRITICAL_SECTIONS
 	(MUTEX_PROFILE_ID(xrDebug::backend))
