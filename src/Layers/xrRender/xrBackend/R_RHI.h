e0962842bbdec1f402e0364ceb167c4e9bf63756
Kirill Yurkin <47507219+ugozapad@users.noreply.github.com>
2024-03-24 18:38:28 +0300

    Little backend refactor

---
+++ src/Layers/xrRender/xrBackend/R_RHI.h
@@ -0,0 +1,171 @@
+#ifndef r_rhiH
+#define r_rhiH
+
+// https://wickedengine.net/2021/05/06/graphics-api-abstraction/
+
+// Primitives supported by draw-primitive API
+enum PRIMITIVETYPE {
+	PT_POINTLIST = 1,
+	PT_LINELIST = 2,
+	PT_LINESTRIP = 3,
+	PT_TRIANGLELIST = 4,
+	PT_TRIANGLESTRIP = 5,
+	PT_TRIANGLEFAN = 6,
+	PT_FORCE_DWORD = 0x7fffffff, /* force 32-bit size enum */
+};
+
+/* Flags to construct RS_COLORWRITEENABLE */
+#define COLORWRITEENABLE_RED     (1L<<0)
+#define COLORWRITEENABLE_GREEN   (1L<<1)
+#define COLORWRITEENABLE_BLUE    (1L<<2)
+#define COLORWRITEENABLE_ALPHA   (1L<<3)
+
+// ResourceUsage mapping and usage:
+//	D3DPOOL_MANAGED - ResourceUsage::IMMUTABLE
+//	D3DPOOL_DEFAULT - ResourceUsage::DYNAMIC
+
+enum class ResourceUsage
+{
+	UNKNOWN,
+	IMMUTABLE,	// Static resource, will never change
+	DYNAMIC		// Dynamic resource, CPU Write, update on any Unmap
+};
+
+enum class Mapping
+{
+	MAP_READ,
+	MAP_WRITE,
+	MAP_READ_WRITE,
+	MAP_WRITE_DISCARD,
+	MAP_WRITE_NO_OVERWRITE
+};
+
+enum class TextureType
+{
+	TEXTURE_1D,
+	TEXTURE_2D,
+	TEXTURE_3D
+};
+
+struct TextureDesc
+{
+	TextureType textureType;
+	u32 width = 1;
+	u32 height = 1;
+	u32 mipmapLevel = 0;
+	PixelFormat format;
+	bool renderTargetUsage;
+};
+
+
+//////////////////
+// D3D9 Copy-paste
+
+/* Vertex Buffer Description */
+struct VERTEXBUFFER_DESC
+{
+	D3DFORMAT           Format;
+	D3DRESOURCETYPE     Type;
+	DWORD               Usage;
+	D3DPOOL             Pool;
+	UINT                Size;
+
+	DWORD               FVF;
+
+};
+
+/* Index Buffer Description */
+struct INDEXBUFFER_DESC
+{
+	D3DFORMAT           Format;
+	D3DRESOURCETYPE     Type;
+	DWORD               Usage;
+	D3DPOOL             Pool;
+	UINT                Size;
+};
+
+struct MAPPED_SUBRESOURCE {
+	void* pData;
+	u32 RowPitch;
+	u32 DepthPitch;
+};
+
+/////////////////////////////////////////////////
+// RHI Objects
+
+class IGraphicsResource
+{
+public:
+	virtual ~IGraphicsResource() = default;
+	inline bool IsValid() const { return m_InternalResource != nullptr; }
+
+	std::shared_ptr<void> m_InternalResource;
+
+	// IUnknown interface
+	u64 AddRef();
+	u64 Release();
+};
+
+inline u64 IGraphicsResource::AddRef()
+{
+	return 0;
+}
+
+inline u64 IGraphicsResource::Release()
+{
+	return 0;
+}
+
+///////////////////////////////////////////////////////////
+
+class IBufferBase : public IGraphicsResource
+{
+};
+
+class IVertexBuffer : public IBufferBase
+{
+public:
+	void GetDesc(VERTEXBUFFER_DESC* pDesc);
+	VERTEXBUFFER_DESC m_Desc;
+};
+
+inline void IVertexBuffer::GetDesc(VERTEXBUFFER_DESC* pDesc)
+{
+	R_ASSERT(pDesc);
+	*pDesc = m_Desc;
+}
+
+class IIndexBuffer : public IGraphicsResource
+{
+public:
+	void GetDesc(INDEXBUFFER_DESC* pDesc);
+	INDEXBUFFER_DESC m_Desc;
+};
+
+inline void IIndexBuffer::GetDesc(INDEXBUFFER_DESC* pDesc)
+{
+	R_ASSERT(pDesc);
+	*pDesc = m_Desc;
+}
+
+///////////////////////////////////////////////////////////
+// Texture stuff
+
+class ITexture2D : public IGraphicsResource
+{
+public:
+
+};
+
+///////////////////////////////////////////////////////////
+// Shader stuff
+
+class IVertexShader : public IGraphicsResource
+{
+};
+
+class IPixelShader : public IGraphicsResource
+{
+};
+
+#endif // !r_rhiH
