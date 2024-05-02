49c7dd981952078e6d6677ec07b9021d029e53f2
Shtrecker <gogels235@gmail.com>
2024-05-02 15:09:49 +0300

    Fix `eUnjam` state

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -876,6 +876,7 @@ void CWeaponMagazined::OnAnimationEnd(u32 state)
 		} break;
 		case eUnjam:
 			bMisfire = false;
+			bReloadKeyPressed = false;
 			SwitchState(eIdle);
 		break;
 		case eHiding:
