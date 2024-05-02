61096be60836a9ec3930c45637d5d99bcab3379b
Shtrecker <gogels235@gmail.com>
2024-05-02 15:09:49 +0300

    Change type animations to xr_string

--- src/xrGame/WeaponShotgun.cpp
+++ src/xrGame/WeaponShotgun.cpp
@@ -208,7 +208,7 @@ void CWeaponShotgun::PlayAnimOpenWeapon()
 {
 	VERIFY(GetState() == eReload);
 
-	std::string anm_name = "anm_open";
+	xr_string anm_name = "anm_open";
 
 	if (m_bEmptyPreloadMode && iAmmoElapsed == 0)
 	{
@@ -223,7 +223,7 @@ void CWeaponShotgun::PlayAnimAddOneCartridgeWeapon()
 {
 	VERIFY(GetState() == eReload);
 
-	std::string anm_name = "anm_add_cartridge";
+	xr_string anm_name = "anm_add_cartridge";
 
 	if (m_bEmptyPreloadMode && bPreloadAnimAdapter)
 	{
@@ -244,7 +244,7 @@ void CWeaponShotgun::PlayAnimCloseWeapon()
 {
 	VERIFY(GetState() == eReload);
 
-	std::string anm_name = "anm_close";
+	xr_string anm_name = "anm_close";
 
 	if (m_bEmptyPreloadMode && bPreloadAnimAdapter)
 	{
