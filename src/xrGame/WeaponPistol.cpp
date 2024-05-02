0d0ac6ce32ea5c0046b0938c7a1353c64fda257a
Shtrecker <gogels235@gmail.com>
2024-05-02 15:09:47 +0300

    Delete trash functions in `CWeaponPistol` class

--- src/xrGame/WeaponPistol.cpp
+++ src/xrGame/WeaponPistol.cpp
@@ -9,48 +9,12 @@ CWeaponPistol::CWeaponPistol()
 }
 
 CWeaponPistol::~CWeaponPistol(void)
-{
-}
-
-void CWeaponPistol::net_Destroy()
-{
-	inherited::net_Destroy();
-}
-
+{}
 
 void CWeaponPistol::Load	(LPCSTR section)
 {
-	inherited::Load		(section);
-
+	inherited::Load(section);
 	m_sounds.LoadSound(section, "snd_close", "sndClose", false, m_eSoundClose);
-}
-
-void CWeaponPistol::OnH_B_Chield		()
-{
-	inherited::OnH_B_Chield		();
-}
-
-void CWeaponPistol::OnShot		()
-{
-	PlaySound		(m_sSndShotCurrent.c_str(),get_LastFP());
-
-	AddShotEffector	();
-	
-	PlayAnimShoot	();
-
-	// Shell Drop
-	Fvector vel; 
-	PHGetLinearVell(vel);
-	OnShellDrop					(get_LastSP(),  vel);
-
-	// ����� �� ������
-	
-	StartFlameParticles	();
-	R_ASSERT2(!m_pFlameParticles || !m_pFlameParticles->IsLooped(),
-			  "can't set looped particles system for shoting with pistol");
-	
-	//��� �� ������
-	StartSmokeParticles	(get_LastFP(), vel);
 }
 
 void CWeaponPistol::UpdateSounds()
