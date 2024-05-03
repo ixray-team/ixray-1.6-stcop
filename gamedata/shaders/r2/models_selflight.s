b97f2e4d00b31a63020350215582aa4883d5e0ca
Hozar2002 <hoz2002team@gmail.com>
2024-05-03 09:34:05 +0300

    Update shaders for new render system

--- gamedata/shaders/r2/models_selflight.s
+++ gamedata/shaders/r2/models_selflight.s
@@ -1,12 +1,12 @@
 function normal		(shader, t_base, t_second, t_detail)
-	shader:begin	("deffer_model_flat","deffer_base_flat")
+	shader:begin	("deffer_model","deffer_base")
 			: fog		(false)
 			: emissive 	(true)
 	shader:sampler	("s_base")      :texture	(t_base)
 end
 
 function l_special	(shader, t_base, t_second, t_detail)
-	shader:begin	("shadow_direct_model",	"accum_emissive")
+	shader:begin	("deffer_model", "accum_emissive")
 			: zb 		(true,false)
 			: fog		(false)
 			: emissive 	(true)
