50706bb776fcc50d7e5e178415e80d1514dba42f
Hozar2002 <hoz2002team@gmail.com>
2024-05-03 09:34:05 +0300

    Fix `selflightl` blender crash

--- gamedata/shaders/r3/models_selflightl.s
+++ gamedata/shaders/r3/models_selflightl.s
@@ -1,19 +1,16 @@
-function normal		(shader, t_base, t_second, t_detail)
-	shader:begin	("deffer_model","deffer_base")
-			: fog		(false)
-			: emissive 	(true)
---	shader:sampler	("s_base")      :texture	(t_base)
-	shader:dx10texture	("s_base",	t_base)
-	shader:dx10sampler	("smp_base")
-	shader:dx10stencil	( 	true, cmp_func.always, 
-							255 , 127, 
-							stencil_op.keep, stencil_op.replace, stencil_op.keep)
-	shader:dx10stencil_ref	(1)
+function normal (shader, t_base, t_second, t_detail)
+	shader:begin ("deffer_model","deffer_base")
+		: fog (false)
+		: emissive (true)
+	shader:dx10texture ("s_base", t_base)
+	shader:dx10sampler ("smp_base")
+	shader:dx10stencil (true, cmp_func.always, 255 , 127, stencil_op.keep, stencil_op.replace, stencil_op.keep)
+	shader:dx10stencil_ref (1)
 end
 
-function l_special	(shader, t_base, t_second, t_detail)
-	shader:begin	("shadow_direct_model",	"accum_emissivel")
-			: zb 		(true,false)
-			: fog		(false)
-			: emissive 	(true)
+function l_special (shader, t_base, t_second, t_detail)
+	shader:begin ("deffer_model",	"accum_emissivel")
+	: zb (true,false)
+	: fog (false)
+	: emissive (true)
 end
