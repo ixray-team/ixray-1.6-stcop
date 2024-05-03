0863c19bf7661c1a20ad813f5e7455eb1d79a0d4
Hozar2002 <hoz2002team@gmail.com>
2024-05-03 09:34:04 +0300

    Remove G-buffer optimizations from shaders

--- gamedata/shaders/r3/common_policies.h
+++ gamedata/shaders/r3/common_policies.h
@@ -6,20 +6,7 @@
 #endif	//	ISAMPLE
 
 /////////////////////////////////////////////////////////////////////////////
-// GLD_P - gbuffer_load_data
-#ifdef	GBUFFER_OPTIMIZATION
-	#define	GLD_P( _tc, _pos2d, _iSample ) _tc, _pos2d, _iSample
-#else	//	GBUFFER_OPTIMIZATION
-	#define	GLD_P( _tc, _pos2d, _iSample ) _tc, _iSample
-#endif	//	GBUFFER_OPTIMIZATION
-
-/////////////////////////////////////////////////////////////////////////////
-// CS_P
-
-#	ifdef	GBUFFER_OPTIMIZATION
-#		define	CS_P( _P, _N, _tc0, _tcJ, _pos2d, _iSample ) _P, _N, _tc0, _tcJ, _pos2d
-#	else	//	GBUFFER_OPTIMIZATION
-#		define	CS_P( _P, _N, _tc0, _tcJ, _pos2d, _iSample ) _P, _N, _tc0, _tcJ
-#endif
+#define	GLD_P(_tc, _pos2d, _iSample) _tc, _pos2d
+#define	CS_P(_P, _N, _tc0, _tcJ, _pos2d, _iSample) _P, _N, _tc0, _tcJ, _pos2d
 
 #endif	//	common_policies_h_included
