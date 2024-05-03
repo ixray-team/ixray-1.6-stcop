9dd6cd7232e67bbd9fc5a9a5242944927490dc1b
Hozar2002 <hoz2002team@gmail.com>
2024-05-03 14:25:21 +0300

    Clear `rt_Velocity` on frame start

--- src/Layers/xrRenderPC_R4/r4_rendertarget_phase_scene.cpp
+++ src/Layers/xrRenderPC_R4/r4_rendertarget_phase_scene.cpp
@@ -4,43 +4,16 @@
 void	CRenderTarget::phase_scene_prepare	()
 {
 	PIX_EVENT(phase_scene_prepare);
-	// Clear depth & stencil
-	//u_setrt	( Device.TargetWidth,Device.TargetHeight,RTarget,nullptr,nullptr,RDepth );
-	//CHK_DX	( RDevice->Clear	( 0L, nullptr, D3DCLEAR_ZBUFFER|D3DCLEAR_STENCIL, 0x0, 1.0f, 0L) );
-	//	Igor: soft particles
 
-	CEnvDescriptor&	E = *g_pGamePersistent->Environment().CurrentEnv;
-	float fValue = E.m_fSunShaftsIntensity;
-	//	TODO: add multiplication by sun color here
-	//if (fValue<0.0001) FlagSunShafts = 0;
+	//	TODO: DX10: Check if we need to set RT here.
+	u_setrt(RCache.get_width(), RCache.get_height(), rt_Position->pRT, rt_Velocity->pRT, NULL, RDepth);
 
-	// we need to clean up G-buffer every frame to avoid some glithces
-	u_setrt(rt_Position, rt_Normal, rt_Color, 0);
-	float ColorRGBA[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
-	RContext->ClearRenderTargetView(RCache.get_RT(), ColorRGBA);
+	FLOAT ColorRGBA[4] = {0.0f, 0.0f, 0.0f, 0.0f};
 
-	//	TODO: DX10: Check if complete clear of _ALL_ rendertargets will increase
-	//	FPS. Make check for SLI configuration.
-	if (ps_r2_ls_flags.test(R2FLAG_SOFT_PARTICLES | R2FLAG_DOF) ||
-		((ps_r_sun_shafts > 0) && (fValue >= 0.0001)) ||
-		(ps_r_ssao > 0))
-	{
-		//	TODO: DX10: Check if we need to set RT here.
-		u_setrt((u32)RCache.get_width(), (u32)RCache.get_height(), rt_Position->pRT, nullptr, nullptr, RDepth);
+	RContext->ClearRenderTargetView(rt_Position->pRT, ColorRGBA);
+	RContext->ClearRenderTargetView(rt_Velocity->pRT, ColorRGBA);
 
-		//CHK_DX	( RDevice->Clear	( 0L, nullptr, D3DCLEAR_TARGET|D3DCLEAR_ZBUFFER|D3DCLEAR_STENCIL, 0x0, 1.0f, 0L) );
-		FLOAT ColorRGBA[4] = {0.0f, 0.0f, 0.0f, 0.0f};
-		RContext->ClearRenderTargetView(rt_Position->pRT, ColorRGBA);
-		//RContext->ClearRenderTargetView(rt_Normal->pRT, ColorRGBA);
-		//RContext->ClearRenderTargetView(rt_Color->pRT, ColorRGBA);
-		RContext->ClearDepthStencilView(RDepth, D3D_CLEAR_DEPTH | D3D_CLEAR_STENCIL, 1.0f, 0);
-	}
-	else
-	{
-		u_setrt((u32)RCache.get_width(), (u32)RCache.get_height(), RTarget, nullptr, nullptr, RDepth);
-		//CHK_DX	( RDevice->Clear	( 0L, nullptr, D3DCLEAR_ZBUFFER|D3DCLEAR_STENCIL, 0x0, 1.0f, 0L) );
-		RContext->ClearDepthStencilView(RDepth, D3D_CLEAR_DEPTH | D3D_CLEAR_STENCIL, 1.0f, 0);
-	}
+	RContext->ClearDepthStencilView(RDepth, D3D_CLEAR_DEPTH | D3D_CLEAR_STENCIL, 1.0f, 0);
 
 	//	Igor: for volumetric lights
 	m_bHasActiveVolumetric				= false;
