9a68d0fb9ce60e251ff53a189872ec67a3bcece9
Hozar2002 <hoz2002team@gmail.com>
2024-05-03 09:34:04 +0300

    Refactor shadow blenders

--- src/Layers/xrRenderPC_R4/blender_deffer_flat.cpp
+++ src/Layers/xrRenderPC_R4/blender_deffer_flat.cpp
@@ -35,37 +35,34 @@ void	CBlender_deffer_flat::Load	(	IReader& fs, u16 version )
 
 void	CBlender_deffer_flat::Compile(CBlender_Compile& C)
 {
-	IBlender::Compile		(C);
-	
+	IBlender::Compile (C);
+
 	C.TessMethod = oTessellation.IDselected;
-	// codepath is the same, only the shaders differ
-	switch(C.iElement) 
+	switch (C.iElement)
 	{
-	case SE_R2_NORMAL_HQ: 		// deffer
-		uber_deffer		(C,true,"deffer_base","deffer_base",false,0,true);
+	case SE_R2_NORMAL_HQ: // deffer
+		uber_deffer(C, true, "deffer_base", "deffer_base", false, 0, true);
 
-		C.r_Stencil		( TRUE,D3DCMP_ALWAYS,0xff,0x7f,D3DSTENCILOP_KEEP,D3DSTENCILOP_REPLACE,D3DSTENCILOP_KEEP);
-		C.r_StencilRef	(0x01);
-		C.r_End			();
+		C.r_Stencil(TRUE, D3DCMP_ALWAYS, 0xff, 0x7f, D3DSTENCILOP_KEEP, D3DSTENCILOP_REPLACE, D3DSTENCILOP_KEEP);
+		C.r_StencilRef(0x01);
+		C.r_End();
 		break;
-	case SE_R2_NORMAL_LQ: 		// deffer
-		uber_deffer		(C,false,"deffer_base","deffer_base",false,0,true);
+	case SE_R2_NORMAL_LQ: // deffer
+		uber_deffer(C, false, "deffer_base", "deffer_base", false, 0, true);
 
-		C.r_Stencil		( TRUE,D3DCMP_ALWAYS,0xff,0x7f,D3DSTENCILOP_KEEP,D3DSTENCILOP_REPLACE,D3DSTENCILOP_KEEP);
-		C.r_StencilRef	(0x01);
-		C.r_End			();
+		C.r_Stencil(TRUE, D3DCMP_ALWAYS, 0xff, 0x7f, D3DSTENCILOP_KEEP, D3DSTENCILOP_REPLACE, D3DSTENCILOP_KEEP);
+		C.r_StencilRef(0x01);
+		C.r_End();
 		break;
-	case SE_R2_SHADOW:			// smap-direct
-		//if (RImplementation.o.HW_smap)	C.r_Pass	("shadow_direct_base","dumb",	FALSE,TRUE,TRUE,FALSE);
-		//else							C.r_Pass	("shadow_direct_base","shadow_direct_base",FALSE);
-		uber_shadow(C, "base");
-		//C.r_Pass	("shadow_direct_base","dumb",	FALSE,TRUE,TRUE,FALSE);
-		//C.r_Sampler		("s_base",C.L_textures[0]);
-		C.r_dx10Texture		("s_base",C.L_textures[0]);
-		C.r_dx10Sampler		("smp_base");
-		C.r_dx10Sampler		("smp_linear");
+	case SE_R2_SHADOW: // smap-direct
+		C.r_Pass("shadow_base", "shadow_base", FALSE);
+
+		C.r_dx10Texture("s_base", C.L_textures[0]);
+		C.r_dx10Sampler("smp_base");
+		C.r_dx10Sampler("smp_linear");
 		C.r_ColorWriteEnable(false, false, false, false);
-		C.r_End			();
+
+		C.r_End();
 		break;
 	}
 }
