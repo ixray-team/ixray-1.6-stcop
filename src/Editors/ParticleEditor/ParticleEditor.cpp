b89e44a8025c6bc038938e00f814a7bd2fd39db3
VodkaNET <apatenokdimafly@gmail.com>
2024-05-10 11:02:32 +0300

    Fix control inputs in Particle Editor

--- src/Editors/ParticleEditor/ParticleEditor.cpp
+++ src/Editors/ParticleEditor/ParticleEditor.cpp
@@ -2,6 +2,7 @@
 //
 #include "stdafx.h"
 #include "../xrEProps/UIFileLoad.h"
+#include "../../xrEngine/xr_input.h"
 
 CUFileOpen* FileOpen = nullptr;
 
@@ -48,34 +49,67 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     UI->Push(FileOpen, false);
 
     //MainForm->Frame();
-    while (true)
+    bool NeedExit = false;
+    while (!NeedExit)
     {
         SDL_Event Event;
         while (SDL_PollEvent(&Event))
         {
             switch (Event.type)
             {
-                case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
-                    return 0;
+            case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
+                EPrefs->SaveConfig();
+                NeedExit = true;
+                break;
 
-                case SDL_EVENT_WINDOW_RESIZED:
-                   if (UI && REDevice)
-                   {
-                       UI->Resize(Event.window.data1, Event.window.data2, true);
-                       EPrefs->SaveConfig();
-                   }
-                    break;
-                case SDL_EVENT_WINDOW_SHOWN:
-                case SDL_EVENT_WINDOW_MOUSE_ENTER:
-                    Device.b_is_Active = true;
-                    //if (UI) UI->OnAppActivate();
+            case SDL_EVENT_WINDOW_RESIZED:
+                if (UI && REDevice)
+                {
+                    UI->Resize(Event.window.data1, Event.window.data2, true);
+                    EPrefs->SaveConfig();
+                }
+                break;
+            case SDL_EVENT_WINDOW_SHOWN:
+            case SDL_EVENT_WINDOW_MOUSE_ENTER:
+                Device.b_is_Active = true;
+                //if (UI) UI->OnAppActivate();
 
-                    break;
-                case SDL_EVENT_WINDOW_HIDDEN:
-                case SDL_EVENT_WINDOW_MOUSE_LEAVE:
-                    Device.b_is_Active = false;
-                    //if (UI)UI->OnAppDeactivate();
-                    break;
+                break;
+            case SDL_EVENT_WINDOW_HIDDEN:
+            case SDL_EVENT_WINDOW_MOUSE_LEAVE:
+                Device.b_is_Active = false;
+                //if (UI)UI->OnAppDeactivate();
+                break;
+
+            case SDL_EVENT_KEY_DOWN:
+                if (UI)UI->KeyDown(Event.key.keysym.scancode, UI->GetShiftState());
+                break;
+            case SDL_EVENT_KEY_UP:
+                if (UI)UI->KeyUp(Event.key.keysym.scancode, UI->GetShiftState());
+                break;
+
+            case SDL_EVENT_MOUSE_MOTION:
+                pInput->MouseMotion(Event.motion.xrel, Event.motion.yrel);
+                break;
+            case SDL_EVENT_MOUSE_WHEEL:
+                pInput->MouseScroll(Event.wheel.y);
+                break;
+
+            case SDL_EVENT_MOUSE_BUTTON_DOWN:
+            case SDL_EVENT_MOUSE_BUTTON_UP:
+            {
+                int mouse_button = 0;
+                if (Event.button.button == SDL_BUTTON_LEFT) { mouse_button = 0; }
+                if (Event.button.button == SDL_BUTTON_RIGHT) { mouse_button = 1; }
+                if (Event.button.button == SDL_BUTTON_MIDDLE) { mouse_button = 2; }
+                if (Event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+                    pInput->MousePressed(mouse_button);
+                }
+                else {
+                    pInput->MouseReleased(mouse_button);
+                }
+            }
+            break;
             }
 
             if (!UI->ProcessEvent(&Event))
