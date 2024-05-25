cfdfab7304c833d24cd2861965901d58827ee021
ggGhosTt <artem.babichev.main@gmail.com>
2024-05-25 16:16:09 -0400

    Fix Linux build

--- src/xrCore/Platform/Linux/OSMath.h
+++ src/xrCore/Platform/Linux/OSMath.h
@@ -1,6 +1,15 @@
 #pragma once
 #include <cmath>
 
+inline
+unsigned char _bittest64(std::int64_t *a, std::int64_t b)
+{
+    auto const value{ *a };
+    auto const mask{ 1LL << (b&63) };
+    auto const masked_value{ value & mask };
+    return (unsigned char){ masked_value != 0 };
+}
+
 namespace Platform
 {
 }
