f570af0c862cc1385c967e6cbe7c8e4e6d09d1a2
ForserX <sv3nk@yandex.ru>
2024-05-27 22:02:08 +0300

    Fix ImGui windows hiding

--- src/xrEngine/ImGuiManager.cpp
+++ src/xrEngine/ImGuiManager.cpp
@@ -41,7 +41,7 @@ void CImGuiManager::InitPlatform()
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
-	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+	//io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
 
 	ImGuiPlatformIO& platform_io = ImGui::GetPlatformIO();
 
