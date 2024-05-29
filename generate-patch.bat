ab4b056ab655123e92f1da4772d96b56ac61baa3
acidicMercury8 <acidicmercury8@outlook.com>
2024-05-29 14:44:01 +0300

    Enable Linux presets for compressor in building workflow

--- generate-patch.bat
+++ generate-patch.bat
@@ -8,9 +8,9 @@ if not exist gamedata_origin (
 )
 
 :: Generate patch
-build\x64\Compressor\bin\Release\xrCompress.exe -diff gamedata gamedata_origin -out patch
+build\x64\Compressor-Windows\bin\Release\xrCompress.exe -diff gamedata gamedata_origin -out patch
 
 :: Pack patch
-build\x64\Compressor\bin\Release\xrCompress.exe patch -ltx datapack.ltx
+build\x64\Compressor-Windows\bin\Release\xrCompress.exe patch -ltx datapack.ltx
 mkdir patches
 move patch.pack_#0 patches\xpatch_03.db
