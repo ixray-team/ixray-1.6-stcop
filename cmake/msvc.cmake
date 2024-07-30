60238527958fd97febc99b8a4f4903bc93ee8f62
ForserX <sv3nk@yandex.ru>
2024-07-30 21:22:54 +0300

    Enable /EHsc for all configs

--- cmake/msvc.cmake
+++ cmake/msvc.cmake
@@ -15,13 +15,16 @@ add_compile_definitions(_WINDOWS)
 # Enable gcc/clang style for MSVC
 add_compile_options(/permissive- /fp:fast /wd4073 /wd4390 /wd4273 /sdl)
 string(REGEX REPLACE "/EH[a-z]+" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-add_compile_options("$<$<CONFIG:DEBUG>:/Od>" "$<$<CONFIG:DEBUG>:/MD>" "/Ob1" "$<$<CONFIG:DEBUG>:/EHsc>")
+add_compile_options("$<$<CONFIG:DEBUG>:/Od>" "$<$<CONFIG:DEBUG>:/MD>" "/Ob1")
 add_compile_options("$<$<CONFIG:RELEASE>:/Ot>"  "$<$<CONFIG:RELEASE>:/Ob2>" "$<$<CONFIG:RELWITHDEBINFO>:/wd4577>")
 
 add_compile_options($<$<CXX_COMPILER_ID:MSVC>:/MP>)
 add_compile_options(/wd4595 /wd4996 /wd4005)
 add_link_options("$<$<CONFIG:DEBUG>:/SAFESEH:NO>")
 add_compile_options("$<$<CONFIG:RELEASE>:/wd4530>" "$<$<CONFIG:DEBUG>:/wd4275>" "$<$<CONFIG:DEBUG>:/wd4251>" "$<$<CONFIG:RELWITHDEBINFO>:/wd4530>")
+
+## Exceptions...
+add_compile_options("/EHsc")
 
 ## Edit and Continue mode
 if (IXRAY_ASAN)
