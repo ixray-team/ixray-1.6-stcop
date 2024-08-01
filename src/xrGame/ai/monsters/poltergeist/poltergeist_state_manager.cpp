5fa819bc81c5c9a8919d76af16c1be5149ab7e53
OldSerpskiStalker <ictopiar@gmail.com>
2024-08-01 21:57:28 +0300

    Updated logic poltergeist
    * Added schemes: attacks with hands, the need for hunger, panic and an assessment of the enemy's strength

--- src/xrGame/ai/monsters/poltergeist/poltergeist_state_manager.cpp
+++ src/xrGame/ai/monsters/poltergeist/poltergeist_state_manager.cpp
@@ -19,9 +19,15 @@
 
 CStateManagerPoltergeist::CStateManagerPoltergeist(CPoltergeist *obj) : inherited(obj)
 {
+    CStateMonsterAttackMoveToHomePoint<CPoltergeist>* PolterAttackHands =
+                                            xr_new<CStateMonsterAttackMoveToHomePoint<CPoltergeist>>(obj);
+
 	add_state(eStateRest,					xr_new<CPoltergeistStateRest<CPoltergeist> > (obj));
 	add_state(eStateEat,					xr_new<CStateMonsterEat<CPoltergeist> >(obj));
+
+    add_state(eStateAttack,                 xr_new<CStateMonsterAttack<CPoltergeist>>(obj, PolterAttackHands));
 	add_state(eStateAttack_AttackHidden,	xr_new<CStatePoltergeistAttackHidden<CPoltergeist> > (obj));
+
 	add_state(eStatePanic,					xr_new<CStateMonsterPanic<CPoltergeist> >(obj));
 	add_state(eStateHitted,					xr_new<CStateMonsterHitted<CPoltergeist> >(obj));
 	add_state(eStateHearInterestingSound,	xr_new<CStateMonsterHearInterestingSound<CPoltergeist> >(obj));
@@ -44,91 +50,120 @@ void CStateManagerPoltergeist::reinit()
 
 void CStateManagerPoltergeist::execute()
 {
-	u32 state_id = u32(-1);
+    u32 state_id = u32(-1);
 
-	if ( object->EnemyMan.get_enemy() && object->detected_enemy() )
-	{
-		state_id = eStateAttack_AttackHidden;
-	}
-	else
-	{
-		state_id = eStateRest;
-	}
+    if (object->ability_tele() && object->get_value_old_logic())
+    {
+        if (object->EnemyMan.get_enemy())
+        {
+            if (object->EnemyMan.get_enemy() != Actor())
+            {
+                switch (object->EnemyMan.get_danger_type())
+                {
+                    case eWeak:
+                    {
+                        if (object->EnemyMan.get_enemy()->conditions().health() < 0.5f)
+                        {
+                            object->SetValueDisableHide(false);
 
-	//const CEntityAlive* enemy	= object->EnemyMan.get_enemy();
+                            object->on_deactivate();
+                            state_id = eStateAttack;
+                        }
+                        else
+                        {
+                            object->SetValueDisableHide(true);
 
-	//if (enemy) {
-	//	if (object->is_hidden()) state_id = eStateAttack_AttackHidden;
-	//	else {
-	//		switch (object->EnemyMan.get_danger_type()) {
-	//			case eStrong:	state_id = eStatePanic; break;
-	//			case eWeak:		state_id = eStateAttack; break;
-	//		}
-	//	}
-	//} else if (object->HitMemory.is_hit() && !object->is_hidden()) {
-	//	state_id = eStateHitted;
-	//} else if (object->hear_dangerous_sound) {
-	//	if (!object->is_hidden()) state_id = eStateHearDangerousSound;
-	//	else state_id = eStateHearInterestingSound;
-	//} else if (object->hear_interesting_sound ) {
-	//	state_id = eStateHearInterestingSound;
-	//} else {
-	//	if (can_eat()) state_id = eStateEat;
-	//	else state_id = eStateRest;
-	//	
-	//	if (state_id == eStateEat) {
-	//		if (object->CorpseMan.get_corpse()->Position().distance_to(object->Position()) < 5.f) {
-	//			if (object->is_hidden()) {
-	//				object->CEnergyHolder::deactivate();
-	//			}
-	//			
-	//			object->DisableHide();
-	//		}
-	//	}
+                            object->on_activate();
+                            state_id = eStatePanic;
+                        }
+                    }
+                    break;
 
-	//}
+                    case eStrong:
+                    {
+                        object->on_activate();
+                        state_id = eStatePanic;
+                    }
+                    break;
+                }
+            }
+            else
+            {
+                switch (object->EnemyMan.get_danger_type())
+                {
+                    case eWeak:
+                    case eStrong:
+                    default:
+                    {
+                        if (Actor()->GetfHealth() < 0.5f)
+                        {
+                            object->SetValueDisableHide(false);
 
-	////if (state_id == eStateAttack_AttackHidden) polter_attack();
+                            object->on_deactivate();
+                            state_id = eStateAttack;
+                        }
+                        else if (Actor()->GetfHealth() > 0.5f)
+                        {
+                            object->SetValueDisableHide(false);
 
-	//if ((prev_substate == eStateEat) && (state_id != eStateEat)) 
-	//	object->EnableHide();
+                            object->on_activate();
+                            state_id = eStateAttack_AttackHidden;
+                        }
+                        else
+                        {
+                            object->SetValueDisableHide(false);
 
+                            object->on_activate();
+                            state_id = eStatePanic;
+                        }
+                    }
+                }
+            }
+        }
+        else if (object->HitMemory.is_hit())
+        {
+            state_id = eStateHitted;
+        }
+        else if (object->hear_interesting_sound || object->hear_dangerous_sound)
+        {
+            state_id = eStateHearDangerousSound;
+        }
+        else
+        {
+            if (can_eat())
+            {
+                state_id = eStateEat;
+            }
+            else
+            {
+                object->SetValueDisableHide(false);
 
-	select_state(state_id); 
+                object->on_activate();
+                state_id = eStateRest;
+            }
+        }
 
-	// выполнить текущее состояние
-	get_state_current()->execute();
+        if (state_id == eStateEat)
+        {
+            if (object->CorpseMan.get_corpse()->Position().distance_to(object->Position()) < 10.f)
+            {
+                object->on_deactivate();
+            }
+        }
+    }
+    else
+    {
+        if (object->EnemyMan.get_enemy() && object->detected_enemy())
+        {
+            state_id = eStateAttack_AttackHidden;
+        }
+        else
+        {
+            state_id = eStateRest;
+        }
+    }
 
-	prev_substate = current_substate;
-}
-
-#define TIME_SEEN_FOR_FIRE 5000
-
-void CStateManagerPoltergeist::polter_attack()
-{
-	//u32 cur_time = Device.dwTimeGlobal;
-	//const CEntityAlive* enemy	= object->EnemyMan.get_enemy();
-	//
-	//bool b_aggressive = object->conditions().GetHealth() < 0.5f;
-
-	//if ((time_next_flame_attack < cur_time) && (object->EnemyMan.get_enemy_time_last_seen() + TIME_SEEN_FOR_FIRE > cur_time)) {
-	//	
-
-	//	object->FireFlame(enemy);
-	//	time_next_flame_attack = cur_time + Random.randI(object->m_flame_delay.min, (b_aggressive) ? object->m_flame_delay.aggressive : object->m_flame_delay.normal);
-	//}
-
-	//if (time_next_tele_attack < cur_time) {
-	//	//object->ProcessTelekinesis(enemy);
-	//	time_next_tele_attack = cur_time + Random.randI(object->m_tele_delay.min, (b_aggressive) ? object->m_tele_delay.aggressive : object->m_tele_delay.normal);
-	//}
-
-	//if (time_next_scare_attack < cur_time) {
-	//	if (Random.randI(2))
-	//		object->PhysicalImpulse(enemy->Position());
-	//	else 
-	//		object->StrangeSounds(enemy->Position());
-	//	
-	//	time_next_scare_attack = cur_time + Random.randI(object->m_scare_delay.min, (b_aggressive) ? object->m_scare_delay.aggressive : object->m_scare_delay.normal);
-	//}
+    select_state(state_id);
+    get_state_current()->execute();
+    prev_substate = current_substate;
 }
