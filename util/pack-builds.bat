2eec3f90c01222ed68083fa33f877f032d696d2d
acidicMercury8 <acidicmercury8@outlook.com>
2024-08-04 19:54:45 +0300

    Update publishing scripts

--- util/pack-builds.bat
+++ util/pack-builds.bat
@@ -8,13 +8,13 @@ if "%~1"=="" (
 set release=%~1
 
 :: Pack developer build
-mkdir build\x64\Engine-Windows\temp-developer
-copy fsgame.ltx build\x64\Engine-Windows\temp-developer
-copy .xrignore build\x64\Engine-Windows\temp-developer
-xcopy patches\xpatch_03.db build\x64\Engine-Windows\temp-developer\patches\ /e
-xcopy patch build\x64\Engine-Windows\temp-developer\gamedata /e /i
-xcopy build\x64\Engine-Windows\bin\RelWithDebInfo build\x64\Engine-Windows\temp-developer\bin /e /i
-pushd build\x64\Engine-Windows\temp-developer
+mkdir build\x64\Server-Windows\temp-developer
+copy fsgame.ltx build\x64\Server-Windows\temp-developer
+copy .xrignore build\x64\Server-Windows\temp-developer
+xcopy patches\xpatch_03.db build\x64\Server-Windows\temp-developer\patches\ /e
+xcopy patch build\x64\Server-Windows\temp-developer\gamedata /e /i
+xcopy build\x64\Server-Windows\bin\RelWithDebInfo build\x64\Server-Windows\temp-developer\bin /e /i
+pushd build\x64\Server-Windows\temp-developer
 "%ProgramFiles%"\7-Zip\7z.exe a -tzip "ixray-1.6-r%release%-engine-x64-develop.zip" ^
     "bin" ^
     -ir!"gamedata" ^
@@ -23,17 +23,17 @@ pushd build\x64\Engine-Windows\temp-developer
     -ir!"patches\xpatch_03.db" ^
     -xr!"*.pdb"
 popd
-move build\x64\Engine-Windows\temp-developer\ixray-1.6-r%release%-engine-x64-develop.zip ^
+move build\x64\Server-Windows\temp-developer\ixray-1.6-r%release%-engine-x64-develop.zip ^
     publish\ixray-1.6-r%release%-engine-x64-develop.zip
 
 :: Pack game build
-mkdir build\x64\Engine-Windows\temp-game
-copy fsgame.ltx build\x64\Engine-Windows\temp-game
-copy .xrignore build\x64\Engine-Windows\temp-game
-xcopy patches\xpatch_03.db build\x64\Engine-Windows\temp-game\patches\ /e
-xcopy patch build\x64\Engine-Windows\temp-game\gamedata /e /i
-xcopy build\x64\Engine-Windows\bin\Release build\x64\Engine-Windows\temp-game\bin /e /i
-pushd build\x64\Engine-Windows\temp-game
+mkdir build\x64\Server-Windows\temp-game
+copy fsgame.ltx build\x64\Server-Windows\temp-game
+copy .xrignore build\x64\Server-Windows\temp-game
+xcopy patches\xpatch_03.db build\x64\Server-Windows\temp-game\patches\ /e
+xcopy patch build\x64\Server-Windows\temp-game\gamedata /e /i
+xcopy build\x64\Server-Windows\bin\Release build\x64\Server-Windows\temp-game\bin /e /i
+pushd build\x64\Server-Windows\temp-game
 "%ProgramFiles%"\7-Zip\7z.exe a -tzip "ixray-1.6-r%release%-engine-x64-game.zip" ^
     "bin" ^
     -ir!"gamedata" ^
@@ -42,5 +42,5 @@ pushd build\x64\Engine-Windows\temp-game
     -ir!"patches\xpatch_03.db" ^
     -xr!"*.pdb"
 popd
-move build\x64\Engine-Windows\temp-game\ixray-1.6-r%release%-engine-x64-game.zip ^
+move build\x64\Server-Windows\temp-game\ixray-1.6-r%release%-engine-x64-game.zip ^
     publish\ixray-1.6-r%release%-engine-x64-game.zip
