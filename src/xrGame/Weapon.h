5eed061ab5908965630e53b3578ad5a889f6fd97
Shtrecker <gogels235@gmail.com>
2024-08-04 16:31:34 +0300

    Add change fire mode animations

--- src/xrGame/Weapon.h
+++ src/xrGame/Weapon.h
@@ -108,6 +108,7 @@ public:
 		eReload,
 		eMisfire,
 		eSwitch,
+		eSwitchMode,
 	};
 	enum EWeaponSubStates{
 		eSubstateReloadBegin		=0,
@@ -192,6 +193,8 @@ public:
 	bool m_bUseSilHud;
 	bool m_bUseScopeHud;
 	bool m_bUseGLHud;
+
+	bool m_bUseChangeFireModeAnim;
 
 	shared_str hud_silencer;
 	shared_str hud_scope;
