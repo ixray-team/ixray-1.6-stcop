024c91cca4c0aa131cd42a964fc911510814c87c
Shtrecker <gogels235@gmail.com>
2024-08-04 18:25:16 +0300

    Move shotgun code to based weapon class

--- src/xrGame/WeaponShotgun.h
+++ src/xrGame/WeaponShotgun.h
@@ -7,35 +7,8 @@ class CWeaponShotgun :	public CWeaponCustomPistol
 {
 	typedef CWeaponCustomPistol inherited;
 public:
-					CWeaponShotgun		();
-	virtual			~CWeaponShotgun		();
-
-	virtual void	Load				(LPCSTR section);
-	
-	virtual void	net_Destroy			();
-
-	virtual bool	TryReload			();
-	virtual void	switch2_Fire		();
-	void			switch2_StartReload ();
-	void			switch2_AddCartgidge();
-	void			switch2_EndReload	();
-
-	virtual void	PlayAnimOpenWeapon	();
-	virtual void	PlayAnimAddOneCartridgeWeapon();
-	void			PlayAnimCloseWeapon	();
-
-	bool			m_bAddCartridgeOpen;
-	bool			m_bEmptyPreloadMode;
-	bool			bPreloadAnimAdapter;
-
-protected:
-	virtual void	OnAnimationEnd		(u32 state);
-	bool			TriStateReload		();
-	virtual void	OnStateSwitch		(u32 S);
-
-	ESoundTypes		m_eSoundOpen;
-	ESoundTypes		m_eSoundAddCartridge;
-	ESoundTypes		m_eSoundClose;
+					CWeaponShotgun		() {};
+	virtual			~CWeaponShotgun		() {};
 
 	DECLARE_SCRIPT_REGISTER_FUNCTION
 };
