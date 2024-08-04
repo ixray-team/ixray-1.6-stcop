57a35b6f3f9af0921b4f7019649a0422f62cee58
Shtrecker <77389805+Shtrecker@users.noreply.github.com>
2024-08-04 16:31:33 +0300

    Add engine external gunslinger mode

--- src/xrEngine/EngineExternal.cpp
+++ src/xrEngine/EngineExternal.cpp
@@ -42,7 +42,12 @@ bool CEngineExternal::operator[](const EEngineExternalEnvironment& ID) const {
 	return pOptions->r_bool("environment", magic_enum::enum_name(ID).data());
 }
 
-ENGINE_API CEngineExternal& EngineExternal() {
+bool CEngineExternal::operator[](const EEngineExternalGunslinger& ID) const {
+	return pOptions->r_bool("gunslinger", magic_enum::enum_name(ID).data());
+}
+
+ENGINE_API CEngineExternal& EngineExternal()
+{
 	if (g_pEngineExternal == nullptr) {
 		g_pEngineExternal = new CEngineExternal;
 	}
