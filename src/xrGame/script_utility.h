7edccfa231e7575acd091cf3bf37b8bb7134735b
wh1t3lord <git.matvey@yandex.ru>
2024-08-18 18:21:55 +0300

    ADD: formatted preprocessor

--- src/xrGame/script_utility.h
+++ src/xrGame/script_utility.h
@@ -3,23 +3,114 @@
 //	Created 	: 16.08.2024
 //  Modified 	: 16.08.2024
 //	Author		: wh1t3lord
-//	Description : Implementation of condlist and other stuff based on xr_logic.script
+//	Description : Implementation of condlist and other stuff based on
+// xr_logic.script
 ////////////////////////////////////////////////////////////////////////////
 
 #pragma once
 
-// when you use this you need to provide two versions of your function, one is client that means it accepts only CScriptGameObject* instances and server version that accepts a server instance
-// when it is called it binds client and server version of  your function
-// myfunction = main word without postfix
-#define REGISTER_FUNCTION_TO_SCRIPT(myfunction) this->registerClientFunction(#myfunction, CAnyCallable<std::function<decltype(##myfunction##_client)>::result_type>(##myfunction##_client)); this->registerServerFunction(#myfunction, CAnyCallable<std::function<decltype(##myfunction##_server)>::result_type>(##myfunction##_server)); 
+// when you use this you need to provide two versions of your function, one is
+// client that means it accepts only CScriptGameObject* instances and server
+// version that accepts a server instance when it is called it binds client and
+// server version of  your function myfunction = main word without postfix
+#define REGISTER_FUNCTION_TO_SCRIPT(myfunction)                           \
+	this->registerClientFunction(#myfunction,                             \
+		CAnyCallable<                                                     \
+			std::function<decltype(##myfunction##_client)>::result_type>( \
+			##myfunction##_client));                                      \
+	this->registerServerFunction(#myfunction,                             \
+		CAnyCallable<                                                     \
+			std::function<decltype(##myfunction##_server)>::result_type>( \
+			##myfunction##_server));
 
 // declares two hash maps for two version of function client and server;
 // also implements all methods for accessing these methods:
-//	getRegisteredClientFunctionByName = returns callable (functor) that you can call but you should provide a valid arguments otherwise you will get an runtime error with invalid by type arguments
-//	getRegisteredServerFunctionByName = returns callable (functor) that you can call but you should provide a valid arguments otherwise you will get an runtime error with invalid by type arguments
-//  hasRegisteredClientFunctionByName = checks if you registered your function in storage, false - means you didn't register or you passed an empty string; true - means it was successfully registered to storage;
-//  hasRegisteredServerFunctionByName = checks if you registered your function in storage, false - means you didn't register or you passed an empty string; true - means it was successfully regsitered to storage;
-#define REGISTER_STORAGE_FOR_RETURN_TYPE(return_type_of_function) private: xr_hash_map<xr_string, CAnyCallable<return_type_of_function>> m_mClientStorageOf##return_type_of_function; private: xr_hash_map<xr_string, CAnyCallable<return_type_of_function>> m_mServerStorageOf##return_type_of_function; private: void registerClientFunction(const xr_string& function_name, const CAnyCallable<return_type_of_function>& func) { m_mClientStorageOf##return_type_of_function[function_name] = func; } private: void registerServerFunction(const xr_string& function_name, const CAnyCallable<return_type_of_function>& func) { m_mServerStorageOf##return_type_of_function[function_name] = func; } public: const CAnyCallable<return_type_of_function>& getRegisteredClientFunctionByName(const xr_string& function_name) { if (m_mClientStorageOf##return_type_of_function.find(function_name) != m_mClientStorageOf##return_type_of_function.end()) return m_mClientStorageOf##return_type_of_function.at(function_name); return CAnyCallable<return_type_of_function>([]()->return_type_of_function{return return_type_of_function{};}); } const CAnyCallable<return_type_of_function>& getRegisteredServerFunctionByName(const xr_string& function_name) { if (m_mServerStorageOf##return_type_of_function.find(function_name) != m_mServerStorageOf##return_type_of_function.end()) return m_mServerStorageOf##return_type_of_function.at(function_name); return CAnyCallable<return_type_of_function>([]()->return_type_of_function{return return_type_of_function{};}); } bool hasRegisteredClientFunction(const xr_string& function_name) { if (function_name.empty()) return false; return m_mClientStorageOf##return_type_of_function.find(function_name) != m_mClientStorageOf##return_type_of_function.end(); } bool hasRegisteredServerFunction(const xr_string& function_name) { if (function_name.empty()) return false; return m_mServerStorageOf##return_type_of_function.find(function_name) != m_mServerStorageOf##return_type_of_function.end(); }
+//	getRegisteredClientFunctionByName = returns callable (functor) that you can
+// call but you should provide a valid arguments otherwise you will get an
+// runtime error with invalid by type arguments
+// getRegisteredServerFunctionByName = returns callable (functor) that you can
+// call but you should provide a valid arguments otherwise you will get an
+// runtime error with invalid by type arguments
+//  hasRegisteredClientFunctionByName = checks if you registered your function
+//  in storage, false - means you didn't register or you passed an empty string;
+//  true - means it was successfully registered to storage;
+//  hasRegisteredServerFunctionByName = checks if you registered your function
+//  in storage, false - means you didn't register or you passed an empty string;
+//  true - means it was successfully regsitered to storage;
+#define REGISTER_STORAGE_FOR_RETURN_TYPE(return_type_of_function)              \
+private:                                                                       \
+	xr_hash_map<xr_string, CAnyCallable<return_type_of_function>>              \
+		m_mClientStorageOf##return_type_of_function;                           \
+                                                                               \
+private:                                                                       \
+	xr_hash_map<xr_string, CAnyCallable<return_type_of_function>>              \
+		m_mServerStorageOf##return_type_of_function;                           \
+                                                                               \
+private:                                                                       \
+	void registerClientFunction(const xr_string& function_name,                \
+		const CAnyCallable<return_type_of_function>& func)                     \
+	{                                                                          \
+		R_ASSERT2(                                                             \
+			m_mClientStorageOf##return_type_of_function.find(function_name) == \
+				m_mClientStorageOf##return_type_of_function.end(),             \
+			"you try to register client same function twice, because it is "   \
+		    "already "                                                         \
+			"registered!");                                                    \
+		m_mClientStorageOf##return_type_of_function[function_name] = func;     \
+	}                                                                          \
+                                                                               \
+private:                                                                       \
+	void registerServerFunction(const xr_string& function_name,                \
+		const CAnyCallable<return_type_of_function>& func)                     \
+	{                                                                          \
+		R_ASSERT2(                                                             \
+			m_mServerStorageOf##return_type_of_function.find(function_name) == \
+				m_mServerStorageOf##return_type_of_function.end(),             \
+			"you try to register server same function twice, because it is "   \
+		    "already "                                                         \
+			"registered!");                                                    \
+		m_mServerStorageOf##return_type_of_function[function_name] = func;     \
+	}                                                                          \
+                                                                               \
+public:                                                                        \
+	const CAnyCallable<return_type_of_function>&                               \
+	getRegisteredClientFunctionByName(const xr_string& function_name)          \
+	{                                                                          \
+		if (m_mClientStorageOf##return_type_of_function.find(function_name) != \
+			m_mClientStorageOf##return_type_of_function.end())                 \
+			return m_mClientStorageOf##return_type_of_function.at(             \
+				function_name);                                                \
+		return CAnyCallable<return_type_of_function>(                          \
+			[]() -> return_type_of_function                                    \
+			{ return return_type_of_function{}; });                            \
+	}                                                                          \
+	const CAnyCallable<return_type_of_function>&                               \
+	getRegisteredServerFunctionByName(const xr_string& function_name)          \
+	{                                                                          \
+		if (m_mServerStorageOf##return_type_of_function.find(function_name) != \
+			m_mServerStorageOf##return_type_of_function.end())                 \
+			return m_mServerStorageOf##return_type_of_function.at(             \
+				function_name);                                                \
+		return CAnyCallable<return_type_of_function>(                          \
+			[]() -> return_type_of_function                                    \
+			{ return return_type_of_function{}; });                            \
+	}                                                                          \
+	bool hasRegisteredClientFunction(const xr_string& function_name)           \
+	{                                                                          \
+		if (function_name.empty())                                             \
+			return false;                                                      \
+		return m_mClientStorageOf##return_type_of_function.find(               \
+				   function_name) !=                                           \
+			m_mClientStorageOf##return_type_of_function.end();                 \
+	}                                                                          \
+	bool hasRegisteredServerFunction(const xr_string& function_name)           \
+	{                                                                          \
+		if (function_name.empty())                                             \
+			return false;                                                      \
+		return m_mServerStorageOf##return_type_of_function.find(               \
+				   function_name) !=                                           \
+			m_mServerStorageOf##return_type_of_function.end();                 \
+	}
 
 class CScriptGameObject;
 
@@ -39,10 +130,13 @@ struct CAnyCallable
 	CAnyCallable(void) : m_pFunction{} {}
 	~CAnyCallable(void) {}
 
-	CAnyCallable(const CAnyCallable<ReturnType>& inst) : m_pFunction{ inst.m_pFunction } {}
+	CAnyCallable(const CAnyCallable<ReturnType>& inst) :
+		m_pFunction{inst.m_pFunction}
+	{
+	}
 
 	template <typename F>
-	CAnyCallable(F&& func) : m_pFunction{ func }
+	CAnyCallable(F&& func) : m_pFunction{func}
 	{
 	}
 
@@ -66,4 +160,4 @@ namespace ixray
 	bool is_weapon(CScriptGameObject* pObject);
 	bool has_alife_info(LPCSTR str);
 	int get_script_clsid(LPCSTR str);
-}
+} // namespace ixray
