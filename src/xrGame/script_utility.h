f0a88a601bcf1767d3c80775347dbb300970cb9c
wh1t3lord <git.matvey@yandex.ru>
2024-09-01 22:39:58 +0300

    ADD: keep size at minimum as possible

--- src/xrGame/script_utility.h
+++ src/xrGame/script_utility.h
@@ -147,7 +147,7 @@ class CScriptGameObject;
 
 namespace ixray
 {
-	constexpr size_t kCondlistInfoStringSize = 256;
+	constexpr size_t kCondlistInfoStringSize = 128;
 	constexpr size_t kCondlistDataStringSize = 64;
 	constexpr size_t kCondlistProbabilityStringSize = 6;
 	constexpr size_t kCondlistEmbeddedDataSize = 10;
