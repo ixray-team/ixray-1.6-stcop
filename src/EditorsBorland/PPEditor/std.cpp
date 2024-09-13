5da93d0eac6544d3630f4a59ee2fb9a67ab216ea
ForserX <sv3nk@yandex.ru>
2024-09-13 17:36:36 +0300

    Гооооооооооол!

--- src/EditorsBorland/PPEditor/std.cpp
+++
@@ -1,3 +0,0 @@
-#include <xrCore.h>
-#include "PostprocessAnimator.h"
- 
--- src/EditorsBorland/PPEditor/stdafx.h
+++
@@ -1,6 +0,0 @@
-#define ENGINE_API
-#include <xrCore.h>
-#include "PostprocessAnimator.h"
-#include <vcl.h>
-#include <stdio.h>
- 
