a229fdf2bd684c72f847834be832141834245984
LVutner <kkamil724@gmail.com>
2024-09-14 16:00:06 +0200

    * FIX: [Render] Always bind correct shadowmap for volumetric lighting

--- gamedata/shaders/d3d11/accum_volumetric_sun.lua
+++ gamedata/shaders/d3d11/accum_volumetric_sun.lua
@@ -5,7 +5,7 @@ function normal(shader, t_base, t_second, t_detail)
         :blend(true, blend.one, blend.one)
         :sorting(2, false)
 
-    shader:dx10texture("s_smap", "null")
+    shader:dx10texture("s_smap", "$user$smap_depth")
     shader:dx10texture("s_position", "$user$position")
     shader:dx10texture("jitter0", "$user$jitter_0")
 
