3ee3af6dd914f28027b7ad47790909a7fe6e1db8
Drombeys <dima.zolotov02@gmail.com>
2024-09-25 17:13:59 +0300

    Simplify game cursor customization

--- src/xrGame/UICursor.cpp
+++ src/xrGame/UICursor.cpp
@@ -4,6 +4,7 @@
 #include "ui/UIStatic.h"
 #include "ui/UIBtnHint.h"
 #include "ui/UIXmlInit.h"
+#include <UIHelper.h>
 
 
 constexpr auto C_DEFAULT = color_xrgb(0xff, 0xff, 0xff);
@@ -49,28 +50,9 @@ void CUICursor::Show()
 void CUICursor::InitInternal()
 {
 	CUIXml xml_doc;
-	xml_doc.Load				(CONFIG_PATH, UI_PATH, "cursor.xml");
-
-	LPCSTR nodevalue			= xml_doc.Read("texture_name", 0, "ui\\ui_ani_cursor");
-
-	m_static					= new CUIStatic();
-	m_static->InitTextureEx		(nodevalue, "hud\\cursor");
-
-	float r_x					= xml_doc.ReadFlt("texture_rect_x", 0, 45.f);
-	float r_y					= xml_doc.ReadFlt("texture_rect_y", 0, 45.f);
-
-	Frect						rect;
-	rect.set					(0.0f,0.0f, r_x, r_y);
-	m_static->SetTextureRect	(rect);
-
-	float width					= xml_doc.ReadFlt("width", 0, 40.0f);
-	float height				= xml_doc.ReadFlt("height", 0, 40.0f);
-	width						*= UI().get_current_kx();
-	BOOL stretch				= xml_doc.ReadInt("stretch", 0, TRUE);
-
-
-	m_static->SetWndSize		(Fvector2().set(width, height));
-	m_static->SetStretchTexture	(!!stretch);
+	xml_doc.Load(CONFIG_PATH, UI_PATH, "cursor.xml");
+	m_static = UIHelper::CreateStatic(xml_doc, "cursor", nullptr);
+	m_static->SetWidth(m_static->GetWidth() * UI().get_current_kx());
 }
 
 //--------------------------------------------------------------------
