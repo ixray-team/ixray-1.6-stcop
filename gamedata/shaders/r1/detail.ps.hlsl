a603d08daec987881f4517ccfb82426a71929ce3
Drombeys <dima.zolotov02@gmail.com>
2024-09-25 17:13:59 +0300

    Add fog accounting for details on static lighting

--- gamedata/shaders/r1/detail.ps.hlsl
+++ gamedata/shaders/r1/detail.ps.hlsl
@@ -4,6 +4,7 @@ struct v2p
 {
     float2 tc0 : TEXCOORD0; // base
     float3 c0 : COLOR0;
+     float fog : FOG;
 };
 
 // Pixel
@@ -11,6 +12,7 @@ float4 main(v2p I) : COLOR
 {
     float4 t_base = tex2D(s_base, I.tc0);
     float3 final = t_base * I.c0 * 2.0f;
+    final = lerp(fog_color.xyz, final, I.fog);
 
     // out
     return float4(final, t_base.a);
