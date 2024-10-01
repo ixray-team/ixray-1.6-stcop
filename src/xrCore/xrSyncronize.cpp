599eae3db7c7090ef16e5a59af0fae2b61ec2ae4
v2v3v4 <terran.erre@mail.ru>
2024-10-01 23:54:38 +0300

    Implement `xrSRWLock` and `xrSRWLockGuard` locking
    For thread-safe read/write locking

--- src/xrCore/xrSyncronize.cpp
+++ src/xrCore/xrSyncronize.cpp
@@ -62,3 +62,95 @@ xrCriticalSectionGuard::~xrCriticalSectionGuard()
 {
 	critical_section->Leave();
 }
+
+xrSRWLock::xrSRWLock()
+{
+#ifdef IXR_WINDOWS
+    InitializeSRWLock(&smutex);
+#endif
+}
+
+void xrSRWLock::AcquireExclusive()
+{
+	PROF_EVENT("xrSRWLock::AcquireExclusive");
+#ifdef IXR_WINDOWS
+    AcquireSRWLockExclusive(&smutex);
+#else
+    smutex.lock();
+#endif
+}
+
+void xrSRWLock::ReleaseExclusive()
+{
+	PROF_EVENT("xrSRWLock::ReleaseExclusive");
+#ifdef IXR_WINDOWS
+    ReleaseSRWLockExclusive(&smutex);
+#else
+    smutex.unlock();
+#endif
+}
+
+void xrSRWLock::AcquireShared()
+{
+	PROF_EVENT("xrSRWLock::AcquireShared");
+#ifdef IXR_WINDOWS
+    AcquireSRWLockShared(&smutex);
+#else
+    smutex.lock_shared();
+#endif
+}
+
+void xrSRWLock::ReleaseShared()
+{
+	PROF_EVENT("xrSRWLock::ReleaseShared");
+#ifdef IXR_WINDOWS
+    ReleaseSRWLockShared(&smutex);
+#else
+    smutex.unlock_shared();
+#endif
+}
+
+BOOL xrSRWLock::TryAcquireExclusive()
+{
+#ifdef IXR_WINDOWS
+    return TryAcquireSRWLockExclusive(&smutex);
+#else
+    return smutex.try_lock();
+#endif
+}
+
+BOOL xrSRWLock::TryAcquireShared()
+{
+#ifdef IXR_WINDOWS
+    return TryAcquireSRWLockShared(&smutex);
+#else
+    return smutex.try_lock_shared();
+#endif
+}
+
+
+xrSRWLockGuard::xrSRWLockGuard(xrSRWLock* lock, bool shared)
+    : lock(lock), shared(shared)
+{
+    if (shared)
+        lock->AcquireShared();
+    else
+        lock->AcquireExclusive();
+}
+
+xrSRWLockGuard::xrSRWLockGuard(xrSRWLock& lock, bool shared)
+    : lock(&lock), shared(shared)
+{
+    if (shared)
+        lock.AcquireShared();
+    else
+        lock.AcquireExclusive();
+}
+
+xrSRWLockGuard::~xrSRWLockGuard()
+{
+    if (shared)
+        lock->ReleaseShared();
+    else
+        lock->ReleaseExclusive();
+}
--- src/xrCore/xrSyncronize.h
+++ src/xrCore/xrSyncronize.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <mutex>
+#include <shared_mutex>
 
 // Desc: Simple wrapper for critical section
 class XRCORE_API xrCriticalSection
@@ -31,3 +32,44 @@ public:
 private:
 	xrCriticalSection* critical_section;
 };
+
+
+class XRCORE_API xrSRWLock
+{
+private:
+
+#ifdef IXR_WINDOWS
+    SRWLOCK smutex;
+#else
+    std::shared_mutex smutex;
+#endif
+
+public:
+    xrSRWLock();
+    ~xrSRWLock() {};
+
+    void AcquireExclusive();
+    void ReleaseExclusive();
+
+    void AcquireShared();
+    void ReleaseShared();
+
+    BOOL TryAcquireExclusive();
+    BOOL TryAcquireShared();
+};
+//Write functions guard: lock.AcquireExclusive(); ... lock.ReleaseExclusive();
+//Read functions guard: lock.AcquireShared(); ... lock.ReleaseShared();
+
+class XRCORE_API xrSRWLockGuard
+{
+public:
+    xrSRWLockGuard(xrSRWLock& lock, bool shared = false);
+    xrSRWLockGuard(xrSRWLock* lock, bool shared = false);
+    ~xrSRWLockGuard();
+
+private:
+    xrSRWLock* lock;
+    bool shared;
+};
+//Write functions guard: xrSRWLockGuard guard(lock); ...
+//Read functions guard: xrSRWLockGuard guard(lock, true); ...
