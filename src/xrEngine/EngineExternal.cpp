117eb9c84bc0eeca672d7f147889eb84ab17c1f1
wh1t3lord <git.matvey@yandex.ru>
2024-10-03 21:01:56 +0300

    ADD: refactor 3.1

--- src/xrEngine/EngineExternal.cpp
+++ src/xrEngine/EngineExternal.cpp
@@ -9,11 +9,9 @@ CEngineExternal::CEngineExternal() : m_is_modification_gunslinger{}, pOptions{},
 	FS.update_path(fname, "$game_config$", "engine_external.ltx");
 	pOptions = new CInifile(fname);
 
-	m_pTitle = READ_IF_EXISTS(pOptions, r_string_wb, "general", "title", "IX-Ray Platform").c_str();
-
-
 	if (pOptions)
 	{
+		m_pTitle = READ_IF_EXISTS(pOptions, r_string_wb, "general", "title", "IX-Ray Platform").c_str();
 		m_is_modification_gunslinger = pOptions->r_bool("gunslinger", magic_enum::enum_name(EEngineExternalGunslinger::EnableGunslingerMode).data());
 	}
 }
