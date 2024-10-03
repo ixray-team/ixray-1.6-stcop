1f98ecb47aeb62715207f1a7265417bc0a61e851
Shtrecker <gogels235@gmail.com>
2024-10-03 21:04:00 +0300

    Fix reload

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -1584,7 +1584,7 @@ bool CWeaponMagazined::Action(u16 cmd, u32 flags)
 				if (Actor()->GetDetector() && Actor()->GetDetector()->GetState() != CCustomDetector::eIdle)
 					return false;
 			}
-			else if ((iAmmoElapsed != iMagazineSize || IsMisfire()) && !Weapon_SetKeyRepeatFlagIfNeeded(kfRELOAD))
+			else if ((iAmmoElapsed != GetMagCapacity() || IsMisfire()) && !Weapon_SetKeyRepeatFlagIfNeeded(kfRELOAD))
 				return false;
 
 			if (IsMisfire() && !IsGrenadeMode())
@@ -1593,7 +1593,7 @@ bool CWeaponMagazined::Action(u16 cmd, u32 flags)
 				return true;
 			}
 
-			if (iAmmoElapsed == iMagazineSize)
+			if (iAmmoElapsed == GetMagCapacity())
 			{
 				bReloadKeyPressed = false;
 				return false;
