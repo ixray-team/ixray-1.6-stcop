6ba6729ce1ca3fce7b44c8a7e9649455e4cd0836
Shtrecker <gogels235@gmail.com>
2024-10-03 20:55:11 +0300

    Rework BM-16 reload

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -22,6 +22,7 @@
 #include "script_game_object.h"
 #include "Actor_Flags.h"
 #include "player_hud.h"
+#include "weaponBM16.h"
 
 ENGINE_API	bool	g_dedicated_server;
 
@@ -1001,6 +1002,10 @@ void CWeaponMagazined::DoReload()
 	int def_magsize = iMagazineSize;
 	int mod_magsize = def_magsize;
 
+	CWeaponBM16* bm = smart_cast<CWeaponBM16*>(this);
+
+	if (bm)
+		mod_magsize = ammo_cnt_to_reload;
 
 	iMagazineSize = mod_magsize;
 	ReloadMagazine();
