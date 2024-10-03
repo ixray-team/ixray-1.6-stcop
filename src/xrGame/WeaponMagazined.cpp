1976564b83731dbd66cae12bfef81a875ac716b3
Shtrecker <gogels235@gmail.com>
2024-10-03 20:52:09 +0300

    Add `eCheckMisfire` state

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -115,6 +115,9 @@ void CWeaponMagazined::Load	(LPCSTR section)
 	if (WeaponSoundExist(section, "snd_breechblock"))
 		m_sounds.LoadSound(section, "snd_breechblock", "sndPump", true, m_eSoundReload);
 
+	if (WeaponSoundExist(section, "snd_jammed_click"))
+		m_sounds.LoadSound(section, "snd_jammed_click", "sndJammedClick", true, m_eSoundEmptyClick);
+
 	if (WeaponSoundExist(section, "snd_aim"))
 		m_sounds.LoadSound(section, "snd_aim", "sndAim", true, m_eSoundAim);
 
@@ -248,14 +251,24 @@ void CWeaponMagazined::FireStart()
 	}
 	else
 	{
-		//misfire
+		if (GetState() != eIdle)
+			return;
 
+		//misfire
 		CGameObject* object = smart_cast<CGameObject*>(H_Parent());
 		if (object)
 			object->callback(GameObject::eOnWeaponJammed)(object->lua_game_object(), this->lua_game_object());
 
 		if(smart_cast<CActor*>(this->H_Parent()) && (Level().CurrentViewEntity()==H_Parent()) )
 			CurrentGameUI()->AddCustomStatic("gun_jammed",true);
+
+		bool isGuns = EngineExternal()[EEngineExternalGunslinger::EnableGunslingerMode];
+
+		if (isGuns)
+		{
+			SwitchState(eCheckMisfire);
+			return;
+		}
 
 		OnEmptyClick();
 	}
@@ -586,6 +599,9 @@ void CWeaponMagazined::OnStateSwitch	(u32 S)
 	case eEmptyClick:
 		switch2_Empty();
 		break;
+	case eCheckMisfire:
+		switch2_CheckMisfire();
+		break;
 	}
 }
 
@@ -659,6 +675,7 @@ void CWeaponMagazined::UpdateCL			()
 		case eSwitchMode:
 		case eEmptyClick:
 		case eUnjam:
+		case eCheckMisfire:
 		case eIdle:
 			{
 				fShotTimeCounter	-=	dt;
@@ -896,6 +913,7 @@ void CWeaponMagazined::OnAnimationEnd(u32 state)
 		case eSwitchMode:
 		case eEmptyClick:
 		case eShowing:
+		case eCheckMisfire:
 			SwitchState(eIdle);
 		break;
 	}
@@ -995,6 +1013,14 @@ void CWeaponMagazined::switch2_Empty()
 		if (isGuns)
 			PlayAnimFakeshoot();
 	}
+}
+
+void CWeaponMagazined::switch2_CheckMisfire()
+{
+	PlayAnimFakeshoot();
+
+	if (m_sounds.FindSoundItem("sndJammedClick", false))
+		PlaySound("sndJammedClick", get_LastFP());
 }
 
 void CWeaponMagazined::PlayAnimFakeshoot()
