6b92609fca501726a91b96cf7f0e57a655d1e904
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2024-10-12 12:17:38 +0300

    CoC PDA: Initial commit
    TODO: Test vanilla CoP & add more menus (Contacts, Relations)
    Co-Authored-By: Alundaio <715088+revolucas@users.noreply.github.com>

--- src/xrGame/ui/UIRankingWnd.h
+++ src/xrGame/ui/UIRankingWnd.h
@@ -3,13 +3,14 @@
 //	Created 	: 17.01.2008
 //	Author		: Evgeniy Sokolov
 //	Description : UI Ranking window class
+// 	Modified By	: Alundaio (8/22/2016) & St4lker0k765 (10/05/2024)
 ////////////////////////////////////////////////////////////////////////////
 
 #pragma once
 #include "../../xrUI/Widgets/UIWindow.h"
 #include "../../xrUI/Widgets/UIWndCallback.h"
-//#include "UIRankFaction.h"
 #include "UIAchievements.h"
+#include "UIRankingsCoC.h"
 
 class CUIStatic;
 class CUIXml;
@@ -26,41 +27,36 @@ private:
 
 	CUIFrameWindow*		m_background;
 	CUIFrameWindow*		m_down_background;
-	CUIFrameWindow*		m_icon_overlay;
-
-	CUICharacterInfo*	m_actor_ch_info;
-
-	CUITextWnd*			m_money_caption;
-	CUITextWnd*			m_money_value;
-
-	CUITextWnd*			m_center_caption;
 
 	CUIScrollView*		m_achievements;
 	CUIFrameWindow*		m_achievements_background;
-	CUIFrameWindow*		m_monster_background;
-	CUIFrameWindow*		m_monster_over;
-	CUIFrameWindow*		m_favorite_weapon_ramka;
-	CUIFrameWindow*		m_favorite_weapon_over;
-	CUIStatic*			m_monster_icon_back;
-	CUIStatic*			m_monster_icon;
-	CUIStatic*			m_favorite_weapon_bckgrnd;
-	CUIStatic*			m_favorite_weapon_icon;
+
+	//Alundaio: CoC Rankings
+	CUIScrollView* m_coc_ranking;
+	CUIScrollView* m_coc_ranking_actor_view;
+	CUIFrameWindow* m_coc_ranking_background;
+	//-Alundaio 
 
 	using ACHIEVES_VEC = xr_vector<CUIAchievements*>;
 	using ACHIEVES_VEC_IT = ACHIEVES_VEC::iterator;
 
 	ACHIEVES_VEC		m_achieves_vec;
 
-	enum				{ max_stat_info = 15 };
+	//Alundaio: CoC Rankings
+	using RANKINGCOC_VEC = xr_vector<CUIRankingsCoC*>;
+	using RANKINGCOC_VEC_IT = RANKINGCOC_VEC::iterator;
+	RANKINGCOC_VEC		m_coc_ranking_vec;
+
+	CUIRankingsCoC* m_coc_ranking_actor;
+	//-Alundaio
+
+	enum { max_stat_info = 32 };	
 	CUITextWnd*			m_stat_caption[max_stat_info];
 	CUITextWnd*			m_stat_info[max_stat_info];
 
 	u32					m_delay;
 	u32					m_previous_time;
 	u32					m_stat_count;
-	LPCSTR				m_last_monster_icon_back;
-	LPCSTR				m_last_monster_icon;
-	LPCSTR				m_last_weapon_icon;
 
 public:
 						CUIRankingWnd			();
@@ -77,7 +73,5 @@ public:
 protected:
 			void		add_achievement			(CUIXml& xml, shared_str const& faction_id);
 			void		get_statistic			();
-			void		get_best_monster		();
-			void		get_favorite_weapon		();
 
 }; // class CUIRankingWnd
