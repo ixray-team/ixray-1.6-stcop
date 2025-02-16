7af851dadca4e5e6958877db2cc603d94795940a
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-02-16 12:23:24 +0300

    Fix startup

--- src/xrGameLA/xrGame.cpp
+++ src/xrGameLA/xrGame.cpp
@@ -11,15 +11,36 @@
 #include "ui/xrUIXmlParser.h"
 #include "../xrEngine/xr_level_controller.h"
 extern void CCC_RegisterCommands();
+static LPVOID __cdecl luabind_allocator(
+	luabind::memory_allocation_function_parameter const,
+	void const* const pointer,
+	size_t const size
+)
+{
+	if (!size)
+	{
+		LPVOID	non_const_pointer = const_cast<LPVOID>(pointer);
+		xr_free(non_const_pointer);
+		return	(0);
+	}
 
-#ifdef NDEBUG
-//namespace std {
-//	void terminate()
-//	{
-//		abort();
-//	}
-//}
-#endif // #ifdef NDEBUG
+	if (!pointer)
+	{
+		return	(Memory.mem_alloc(size));
+	}
+
+	LPVOID non_const_pointer = const_cast<LPVOID>(pointer);
+	return (Memory.mem_realloc(non_const_pointer, size));
+}
+
+void setup_luabind_allocator()
+{
+	if (!Device.IsEditorMode())
+	{
+		luabind::allocator = &luabind_allocator;
+		luabind::allocator_parameter = 0;
+	}
+}
 
 extern "C" {
 	DLL_API void __cdecl xrGameInitialize()
@@ -28,6 +49,7 @@ extern "C" {
 		CCC_RegisterCommands();
 		// keyboard binding
 		CCC_RegisterInput			();
+		setup_luabind_allocator		();
 
 	}
 	DLL_API DLL_Pure*	__cdecl xrFactory_Create		(CLASS_ID clsid)
