39ddb365b2742d8585da46beb9226a44e491e3ed
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-02-16 12:23:24 +0300

    Add support of `SoC` particles format

--- src/xrParticles/particle_actions_collection_io.cpp
+++ src/xrParticles/particle_actions_collection_io.cpp
@@ -383,8 +383,11 @@ void PATargetColor::Load	(IReader& F)
 	F.r_fvector3	(color);
     alpha			= F.r_float();
 	scale			= F.r_float();
-	timeFrom		= F.r_float();
-	timeTo			= F.r_float();
+	if (!EngineExternal().LostAlphaMode())
+	{
+		timeFrom = F.r_float();
+		timeTo = F.r_float();
+	}
 }
 
 void PATargetColor::Save	(IWriter& F)
@@ -393,8 +396,11 @@ void PATargetColor::Save	(IWriter& F)
 	F.w_fvector3	(color);
 	F.w_float		(alpha);
 	F.w_float		(scale);
-	F.w_float		(timeFrom);
-	F.w_float		(timeTo);
+	if (!EngineExternal().LostAlphaMode())
+	{
+		F.w_float(timeFrom);
+		F.w_float(timeTo);
+	}
 }
 
 void PATargetSize::Load		(IReader& F)
