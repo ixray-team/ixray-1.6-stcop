6bf8c8bb23471f665bd4693aa5b3199e6750712e
Hozar2002 <hoz2002team@gmail.com>
2025-02-23 12:20:45 +0300

    Improve PBR shaders on D3D11

--- gamedata/shaders/d3d11/water.ps.hlsl
+++ gamedata/shaders/d3d11/water.ps.hlsl
@@ -62,12 +62,9 @@ float4 main(vf I, float4 pos2d : SV_POSITION) : SV_Target
 	sincos(L_sky_color.w, rotation.x, rotation.y);
 	vreflect.xz = float2(vreflect.x * rotation.y - vreflect.z * rotation.x, vreflect.x * rotation.x + vreflect.z * rotation.y);
 	
-	// true remapping. Slow.
-	float3 vreflectabs = abs(vreflect);
-	float vreflectmax = max(vreflectabs.x, max(vreflectabs.y, vreflectabs.z));
-	
-	vreflect /= vreflectmax;
-	vreflect.y = vreflect.y * 2.0f - 1.0f;
+#ifndef USE_FULL_SKY_SPHERE
+	RemapVector(vreflect);
+#endif
 
 	float3 env0 = s_env0.SampleLevel(smp_rtlinear, vreflect, 0).xyz;
 	float3 env1 = s_env1.SampleLevel(smp_rtlinear, vreflect, 0).xyz;
@@ -75,7 +72,7 @@ float4 main(vf I, float4 pos2d : SV_POSITION) : SV_Target
 	float3 env = lerp(env0, env1, L_ambient.w) * L_sky_color.xyz;
 
 #ifdef USE_SSLR_ON_WATER
-	env = lerp(env, sslr.xyz, sslr.w);
+	env = lerp(env, PopGamma(sslr.xyz), sslr.w);
 #endif
 
     float power = pow(fresnel, 5.0f);
@@ -125,8 +122,8 @@ float4 main(vf I, float4 pos2d : SV_POSITION) : SV_Target
 	
 	final = lerp(final, leaves.xyz, leaves.w * fLeavesFactor);
 	alpha = max(alpha, leaves.w * fLeavesFactor);
-#endif //	USE_SOFT_WATER
+#endif
 	
-	return lerp(float4(final, alpha), fog_color, calc_fogging(I.pos.xyz));
+	return PushGamma(lerp(float4(final, PopGamma(alpha)), fog_color, calc_fogging(I.pos.xyz)));
 }
 
