8c33504f64a4806a0947f7d24a6a87a7b526524e
Shtrecker <gogels235@gmail.com>
2025-03-02 13:16:43 +0300

    Block reload if is zoomed

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -1096,6 +1096,8 @@ bool CWeaponMagazined::Action(u16 cmd, u32 flags)
 		{
 			if(flags&CMD_START)
 			{
+				if (IsZoomed())
+					return false;
 
 				if (iAmmoElapsed < iMagazineSize || IsMisfire())
 				{
