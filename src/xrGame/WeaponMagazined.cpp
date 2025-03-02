ac57a2696a0b94a4c8624133167b0cc2c8b46270
Shtrecker <gogels235@gmail.com>
2025-03-02 13:16:43 +0300

    Fix incorrect order of suffixes

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -609,9 +609,6 @@ std::string CWeaponMagazined::NeedAddSuffix(std::string M)
 	if (iAmmoElapsed == 1)
 		new_name = AddSuffixName(new_name, "_last");
 
-	if (IsSilencerAttached())
-		new_name = AddSuffixName(new_name, "_sil");
-
 	if (iAmmoElapsed == 0)
 		new_name = AddSuffixName(new_name, "_empty");
 
@@ -630,6 +627,9 @@ std::string CWeaponMagazined::NeedAddSuffix(std::string M)
 
 		bMisfireReload = true;
 	}
+
+	if (IsSilencerAttached())
+		new_name = AddSuffixName(new_name, "_sil");
 
 	if (!IsScopeAttached())
 		new_name = AddSuffixName(new_name, "_noscope");
