7d12c0282698b8ea7ddf4c073a0a5f3c0d1f2062
Shtrecker <gogels235@gmail.com>
2025-03-02 13:16:43 +0300

    Fix firemode animations with detector

--- src/xrGame/Weapon.h
+++ src/xrGame/Weapon.h
@@ -199,6 +199,8 @@ public:
 	bool bAmmotypeKeyPressed;
 	bool bStopReloadSignal;
 	bool bUnjamKeyPressed;
+	bool bNextModeKeyPressed;
+	bool bPrevModeKeyPressed;
 	bool m_bUseSilHud;
 	bool m_bUseScopeHud;
 	bool m_bUseGLHud;
