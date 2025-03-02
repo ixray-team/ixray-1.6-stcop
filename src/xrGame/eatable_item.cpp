691dc9c59f67c53de36a1d50b1bb734d003a5070
Shtrecker <gogels235@gmail.com>
2025-03-02 13:20:37 +0300

    Fix used items deletion

--- src/xrGame/eatable_item.cpp
+++ src/xrGame/eatable_item.cpp
@@ -112,7 +112,7 @@ bool CEatableItem::Useful() const
 	if (!inherited::Useful()) return false;
 
 	//проверить не все ли еще съедено
-	if (GetRemainingUses() == 0 && CanDelete())
+	if (GetRemainingUses() == 0)
 		return false;
 
 	return true;
@@ -281,7 +281,7 @@ void CEatableItem::UpdateEatable()
 		Actor()->eateable_to_delete = nullptr;
 
 		if (m_iPortionsMarker == 0)
-			object().DestroyObject();
+			SetDropManual(TRUE);
 	};
 
 	if (m_siStartTime == 0)
@@ -361,8 +361,6 @@ void CEatableItem::script_register(lua_State* L)
 	module(L)
 		[
 			class_<CEatableItem>("CEatableItem")
-				.def("Empty", &CEatableItem::Empty)
-				.def("CanDelete", &CEatableItem::CanDelete)
 				.def("GetMaxUses", &CEatableItem::GetMaxUses)
 				.def("GetRemainingUses", &CEatableItem::GetRemainingUses)
 				.def("SetRemainingUses", &CEatableItem::SetRemainingUses)
