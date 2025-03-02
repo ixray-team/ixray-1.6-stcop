39bf7ee09ba6c23f480db6ea4bb7ab847223b4c9
Shtrecker <gogels235@gmail.com>
2025-03-02 13:20:37 +0300

    Fix magazine capacity issues

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -1576,10 +1576,7 @@ int CWeapon::GetMagCapacity() const
 	int ammotype = -1;
 
 	if (IsGrenadeLauncherAttached() && IsGrenadeMode())
-	{
-		CWeaponMagazinedWGrenade* w_gl = smart_cast<CWeaponMagazinedWGrenade*>(this);
-		result = w_gl->iMagazineSize2;
-	}
+		result = iMagazineSize;
 	else
 	{
 		result = iMagazineSize;
