e9b0dc253ec3da6a092d07c165ad7fe5227d893f
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-03-10 14:17:36 +0300

    Implement font gradient modes

--- src/xrEngine/GameFont.h
+++ src/xrEngine/GameFont.h
@@ -21,8 +21,17 @@ public:
 		alRight,
 		alCenter
 	};
+	enum EGradientMode
+	{
+		gm_vert = 0,
+		gm_horz = 1,
+		gm_back = 2,
+		gm_down = 3,
+		gm_count
+	};
 
 private:
+
 	struct String
 	{
 		string1024 string;
@@ -32,6 +41,7 @@ private:
 		u32 c;
 		EAligment align;
 		bool gradient;
+		EGradientMode gradientMode;
 	};
 
 	struct BaseData
@@ -49,6 +59,7 @@ protected:
 	float fCurrentX = 0.0f;
 	float fCurrentY = 0.0f;
 	bool fGradientEnabled = false;
+	EGradientMode fGradientMode = gm_vert;
 
 	u32 uFlags;
 	u32 dwCurrentColor;
@@ -101,6 +112,7 @@ public:
 	u16 GetCutLengthPos(float fTargetWidth, const char* pszText);
 
 	void SetGradient(bool val) { fGradientEnabled = val; }
+	void SetGradientMode(EGradientMode mode) { fGradientMode = mode; }
 	void OutI(float _x, float _y, const char* fmt, ...);
 	void Out(float _x, float _y, const char* fmt, ...);
 	void OutNext(const char* fmt, ...);
