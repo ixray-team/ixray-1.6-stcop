5877b5e0d2ab014d60d6562e8c66743617b00e9d
ForserX <sv3nk@yandex.ru>
2025-03-10 14:17:36 +0300

    Some fixes

--- src/Editors/LevelEditor/Editor/Scene/SceneRender.cpp
+++ src/Editors/LevelEditor/Editor/Scene/SceneRender.cpp
@@ -24,37 +24,10 @@ void  object_StrictB2F_1(EScene::mapObject_Node *N){RENDER_OBJECT(1,true);}
 void  object_StrictB2F_2(EScene::mapObject_Node *N){RENDER_OBJECT(2,true);}
 void  object_StrictB2F_3(EScene::mapObject_Node *N){RENDER_OBJECT(3,true);}
 
-#define RENDER_SCENE_TOOLS(P,B)\
-	{\
-		SceneMToolsIt s_it 	= scene_tools.begin();\
-		SceneMToolsIt s_end	= scene_tools.end();\
-        for (; s_it!=s_end; s_it++){\
-            EDevice->SetShader		(B?EDevice->m_SelectionShader:EDevice->m_WireShader);\
-            RCache.set_xform_world	(Fidentity);\
-            try{\
-            	(*s_it)->OnRenderRoot(P,B);\
-            }catch(...){\
-		        ELog.DlgMsg(mtError, "Please notify AlexMX!!! Critical error has occured in render routine!!! [Type B] - Tools: '%s'",(*s_it)->ClassName());\
-            }\
-        }\
-    }
-
 void EScene::RenderSky(const Fmatrix& camera)
 {
 	if( !valid() )	return;
 
-//	draw sky
-/*
-//.
-	if (m_SkyDome&&fraBottomBar->miDrawSky->Checked){
-        st_Environment& E = m_LevelOp.m_Envs[m_LevelOp.m_CurEnv];
-        m_SkyDome->GetPosition() = camera.c;
-        m_SkyDome->UpdateTransform(true);
-		EDevice->SetRS(D3DRS_TEXTUREFACTOR, E.m_SkyColor.get());
-    	m_SkyDome->RenderSingle();
-	    EDevice->SetRS(D3DRS_TEXTUREFACTOR,	0xffffffff);
-    }
-*/
 }
 
 struct tools_rp_pred
@@ -110,7 +83,26 @@ void EScene::Render( const Fmatrix& camera )
             }
         }
     }
-    
+
+	auto RENDER_SCENE_TOOLS = [scene_tools](int P, bool B)
+	{
+		SceneMToolsIt s_it = scene_tools.begin();
+		SceneMToolsIt s_end = scene_tools.end();
+		for (; s_it != s_end; s_it++) 
+        {
+			EDevice->SetShader(B ? EDevice->m_SelectionShader : EDevice->m_WireShader);
+			RCache.set_xform_world(Fidentity);
+			try 
+            {
+				(*s_it)->OnRenderRoot(P, B);
+			}
+			catch (...) 
+            {
+				ELog.DlgMsg(mtError, "Please notify AlexMX!!! Critical error has occured in render routine!!! [Type B] - Tools: '%s'", (*s_it)->ClassName()); \
+			}
+		}
+	};
+
 // priority #0
     // normal
     mapRenderObjects.traverseLR		(object_Normal_0);
