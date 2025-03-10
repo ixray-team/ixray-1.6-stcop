82ec7ba294c06a8834d961d4512be55828bb7a03
GladiatorVS <gladiatorvsoff@gmail.com>
2025-03-10 14:16:53 +0300

    Compiler UI wip

--- src/utils/xrForms/cl_log.h
+++ src/utils/xrForms/cl_log.h
@@ -9,6 +9,33 @@ public:
 	virtual	void Phase(LPCSTR phase_name) = 0;
 } *lc_log;
 
+enum IterationStatus
+{
+	Skip = 0,
+	InProgress,
+	Pending,
+	Complited,
+};
+
+struct IterationData {
+	std::string iterationName;
+	std::string PhaseName = "";
+
+	u32 elapsed_time = 0;
+	u32 remain_time = 0;
+
+	int warnings = 0;
+	IterationStatus status = Pending;
+
+	float PhasePersent = 0;
+};
+
+static xrCriticalSection	csLog
+#ifdef PROFILE_CRITICAL_SECTIONS
+(MUTEX_PROFILE_ID(csLog))
+#endif // PROFILE_CRITICAL_SECTIONS
+;
+
 void clMsg(const char* format, ...);
 void Status(const char* format, ...);
 void StatusNoMsg(const char* format, ...);
@@ -16,3 +43,11 @@ void Progress(const float F);
 void Phase(const char* phase_name);
 void logThread	    (void *dummy);
 void logCallback	(LPCSTR c);
+
+float GetProgress();
+std::vector<IterationData>& GetIterationData();
+IterationData* GetActiveIteration();
+void SetActiveIteration(IterationData* i);
+xr_vector<xr_string>& GetLogVector();
+u32& GetPhaseStartTime();
+std::string make_time(u32 sec);
