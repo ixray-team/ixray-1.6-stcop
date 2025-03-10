5e09d1d9977a93ae517c1404fd63c4ac83ce0cf9
ForserX <sv3nk@yandex.ru>
2025-03-10 14:16:56 +0300

    Самое ужасное решение, но блять оно работает (вроде бы)

--- src/3rd party/luajit/src/xr_alloc.c
+++ src/3rd party/luajit/src/xr_alloc.c
@@ -1,5 +1,4 @@
 #include <stddef.h>
-
 #include "xr_alloc.h"
 #include "lj_def.h"
 #include "lj_arch.h"
@@ -9,135 +8,126 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-typedef long (*PNTAVM)(HANDLE handle, void **addr, ULONG zbits,
-		       size_t *size, ULONG alloctype, ULONG prot);
+typedef long (*PNTAVM)(HANDLE handle, void** addr, ULONG zbits,
+    size_t* size, ULONG alloctype, ULONG prot);
 extern PNTAVM ntavm;
-/* Number of top bits of the lower 32 bits of an address that must be zero.
-** Apparently 0 gives us full 64 bit addresses and 1 gives us the lower 2GB.
-*/
-#define NTAVM_ZEROBITS		1
 
-#define MAX_SIZE_T		(~(size_t)0)
-#define MFAIL			((void *)(MAX_SIZE_T))
+#define NTAVM_ZEROBITS 1
+#define MAX_SIZE_T (~(size_t)0)
+#define MFAIL ((void *)(MAX_SIZE_T))
 
-// Луаджит выделяет память кусками, кратными 128К
-// Поэтому сделаю два пула по эти размеры
 #define CHUNK_SIZE (128 * 1024)
 #define CHUNK_COUNT 2048
-static int inited = 0;
-void* g_heap;
-char g_heapMap[CHUNK_COUNT + 1];
-char* g_firstFreeChunk;
-char* find_free(int size);
+#define MAX_POOLS 8
 
-//#define DEBUG_MEM
-#ifdef DEBUG_MEM
-static char buf[100];
-void dump_map(void* ptr, size_t size, char c);
-#endif
+static int inited = 0;
+static int g_poolCount = 0;
+void* g_heaps[MAX_POOLS] = { NULL };
+char g_heapMaps[MAX_POOLS][CHUNK_COUNT + 1];
+char* g_firstFreeChunks[MAX_POOLS];
+
+char* find_free(int size, int* poolIndex);
+void* XR_ALLOC_POOL();
 
 void XR_INIT()
 {
-	if (inited)
-		return;
-	g_heap = NULL;
-	size_t size = CHUNK_SIZE * CHUNK_COUNT;
-	long st = ntavm(INVALID_HANDLE_VALUE, &g_heap, NTAVM_ZEROBITS, &size,
-	                MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE);
+    if (inited)
+        return;
+    g_poolCount = 0;
+    if (XR_ALLOC_POOL() == NULL)
+        return;
+    inited = 1;
+}
 
-	for (int i = 0; i < CHUNK_COUNT; i++)
-		g_heapMap[i] = 'x';
-	g_heapMap[CHUNK_COUNT] = '\0';
-	g_firstFreeChunk = g_heapMap;
+void* XR_ALLOC_POOL()
+{
+    if (g_poolCount >= MAX_POOLS)
+        return NULL;
 
-#ifdef DEBUG_MEM	
-	sprintf(buf, "XR_INIT create_block %p result=%X\r\n", g_heap, st);
-	OutputDebugString(buf);
-#endif
-	inited = 1;
+    void* newHeap = NULL;
+    size_t size = CHUNK_SIZE * CHUNK_COUNT;
+    long st = ntavm(INVALID_HANDLE_VALUE, &newHeap, NTAVM_ZEROBITS, &size,
+        MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+
+    if (st != 0)
+        return NULL;
+
+    g_heaps[g_poolCount] = newHeap;
+    memset(g_heapMaps[g_poolCount], 'x', CHUNK_COUNT);
+    g_heapMaps[g_poolCount][CHUNK_COUNT] = '\0';
+    g_firstFreeChunks[g_poolCount] = g_heapMaps[g_poolCount];
+    return g_heaps[g_poolCount++];
 }
 
 void* XR_MMAP(size_t size)
 {
-#ifdef DEBUG_MEM
-	sprintf(buf, "XR_MMAP(%Iu)", size);
-	OutputDebugString(buf);
-#endif
-	int chunks = size / CHUNK_SIZE;
-	char* s = find_free(chunks);
-	void* ptr = MFAIL;
-	if (s != NULL) {
-		ptr = (char*)g_heap + CHUNK_SIZE * (s - g_heapMap);
-		for (int i = 0; i < chunks; i++)
-			s[i] = 'a' + chunks - 1;
-		if (s == g_firstFreeChunk)
-			g_firstFreeChunk = find_free(1);
-	}
-#ifdef DEBUG_MEM
-	sprintf(buf, " ptr=%p chunks %d\r\n", ptr, chunks);
-	OutputDebugString(buf);
-	dump_map(ptr, size, 'U');
-#endif
-	return ptr;
+    int chunks = size / CHUNK_SIZE;
+    int poolIndex = -1;
+    char* s = find_free(chunks, &poolIndex);
+
+    if (s == NULL)
+    {
+        if (XR_ALLOC_POOL() == NULL)
+            return MFAIL;
+        s = find_free(chunks, &poolIndex);
+        if (s == NULL)
+            return MFAIL;
+    }
+
+    void* ptr = (char*)g_heaps[poolIndex] + CHUNK_SIZE * (s - g_heapMaps[poolIndex]);
+    for (int i = 0; i < chunks; i++)
+        s[i] = 'a' + chunks - 1;
+
+    if (s == g_firstFreeChunks[poolIndex])
+        g_firstFreeChunks[poolIndex] = find_free(1, &poolIndex);
+
+    return ptr;
 }
 
-// Судя по комментарию к CALL_MUNMAP, луаджит может объединять выделенные ему чанки
-// и освобождать их как один. Надеюсь он не слепит вместе чанки из разных пулов
 void XR_DESTROY(void* ptr, size_t size)
 {
-#ifdef DEBUG_MEM
-	sprintf(buf, "XR_DESTROY(ptr=%p, size=%Iu)", ptr, size);
-	OutputDebugString(buf);
-#endif
-	char* s = g_heapMap + ((char*)ptr - (char*)g_heap) / CHUNK_SIZE;
-	int count = size / CHUNK_SIZE;
-	for (int i = 0; i < count; i++)
-		s[i] = 'x';
-	if (s < g_firstFreeChunk)
-		g_firstFreeChunk = s;
-#ifdef DEBUG_MEM	
-	dump_map(ptr, size, 'X');
-#endif
+    int poolIndex = -1;
+    for (int i = 0; i < g_poolCount; i++)
+    {
+        if (ptr >= g_heaps[i] && ptr < (char*)g_heaps[i] + CHUNK_SIZE * CHUNK_COUNT)
+        {
+            poolIndex = i;
+            break;
+        }
+    }
+    if (poolIndex == -1)
+        return;
+
+    char* s = g_heapMaps[poolIndex] + ((char*)ptr - (char*)g_heaps[poolIndex]) / CHUNK_SIZE;
+    int count = size / CHUNK_SIZE;
+    for (int i = 0; i < count; i++)
+        s[i] = 'x';
+
+    if (s < g_firstFreeChunks[poolIndex])
+        g_firstFreeChunks[poolIndex] = s;
 }
 
-// Находит подряд идущие свободные чанки количеством size, начиная с первого свободного
-// Возвращает указатель на группу из heapMap или NULL, если найти не удалось
-char* find_free(int size)
+char* find_free(int size, int* poolIndex)
 {
-	char* p = g_firstFreeChunk;
-	int count = 0;
-	while (*p != '\0') {
-		if (*p == 'x')
-			count++;
-		else
-			count = 0;
-		p++;
-		if (count == size)
-			return p - count;
-	}
-	return NULL;
-}
-
-static 	char temp[1025];
-void dump_map(void* ptr, size_t size, char c)
-{
-#ifdef DEBUG_MEM
-	OutputDebugString("heap:\r\n|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------|-------\r\n");
-	strcpy(temp, g_heapMap);
-	char *cur = temp + ((char*)ptr - (char*)g_heap) / CHUNK_SIZE;
-	for (int i = 0; i < size / CHUNK_SIZE; i++)
-		cur[i] = c;
-	
-	for (int i = 0; i < 8; i++)
-	{
-		char a = temp[i * 128 + 128];
-		temp[i * 128 + 128] = '\0';
-		OutputDebugString(temp + i * 128);
-		temp[i * 128 + 128] = a;
-		OutputDebugString("\r\n");
-	}
-	OutputDebugString("--------------------------------------------------------------------------------------------------------------------------------\r\n");
-#endif
+    for (int i = 0; i < g_poolCount; i++)
+    {
+        char* p = g_firstFreeChunks[i];
+        int count = 0;
+        while (*p != '\0')
+        {
+            if (*p == 'x')
+                count++;
+            else
+                count = 0;
+            p++;
+            if (count == size)
+            {
+                *poolIndex = i;
+                return p - count;
+            }
+        }
+    }
+    return NULL;
 }
 
 #endif
