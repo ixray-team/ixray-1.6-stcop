51d64ddb5848ba9f386022505446c46d9dbdc8b7
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-03-11 15:57:57 +0300

    Fix incorrect rifle strings

--- src/xrGame/ui/UIActorMenuInventory.cpp
+++ src/xrGame/ui/UIActorMenuInventory.cpp
@@ -1096,7 +1096,7 @@ void CUIActorMenu::PropertiesBoxForAddon( PIItem item, bool& b_show )
 		}
 		if ( item_in_slot_3 && item_in_slot_3->CanAttach(pScope) )
 		{
-			shared_str str = g_pStringTable->translate("st_attach_scope_to_pistol");
+			shared_str str = EngineExternal()[EEngineExternalGame::EnableInventoryPistolSlot] ? g_pStringTable->translate("st_attach_scope_to_rifle") : g_pStringTable->translate("st_attach_scope_to_pistol");
 			str.printf("%s %s", str.c_str(), item_in_slot_3->m_name.c_str());
 			m_UIPropertiesBox->AddItem( str.c_str(),  (void*)item_in_slot_3, INVENTORY_ATTACH_ADDON );
 //			m_UIPropertiesBox->AddItem( "st_attach_scope_to_rifle",  (void*)item_in_slot_3, INVENTORY_ATTACH_ADDON );
@@ -1112,15 +1112,13 @@ void CUIActorMenu::PropertiesBoxForAddon( PIItem item, bool& b_show )
 			shared_str str = g_pStringTable->translate("st_attach_silencer_to_pistol");
 			str.printf("%s %s", str.c_str(), item_in_slot_2->m_name.c_str());
 			m_UIPropertiesBox->AddItem( str.c_str(),  (void*)item_in_slot_2, INVENTORY_ATTACH_ADDON );
-//			m_UIPropertiesBox->AddItem( "st_attach_silencer_to_pistol",  (void*)item_in_slot_2, INVENTORY_ATTACH_ADDON );
 			b_show			= true;
 		}
 		if ( item_in_slot_3 && item_in_slot_3->CanAttach(pSilencer) )
 		{
-			shared_str str = g_pStringTable->translate("st_attach_silencer_to_pistol");
+			shared_str str = EngineExternal()[EEngineExternalGame::EnableInventoryPistolSlot] ? g_pStringTable->translate("st_attach_silencer_to_rifle") : g_pStringTable->translate("st_attach_silencer_to_pistol");
 			str.printf("%s %s", str.c_str(), item_in_slot_3->m_name.c_str());
 			m_UIPropertiesBox->AddItem( str.c_str(),  (void*)item_in_slot_3, INVENTORY_ATTACH_ADDON );
-//			m_UIPropertiesBox->AddItem( "st_attach_silencer_to_rifle",  (void*)item_in_slot_3, INVENTORY_ATTACH_ADDON );
 			b_show			= true;
 		}
 		return;
