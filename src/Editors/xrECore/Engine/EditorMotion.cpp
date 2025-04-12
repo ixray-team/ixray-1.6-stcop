7bd7b99480711f4a95671a42af7710baf6af742b
mnelenpridumivat <ragrind@mail.ru>
2025-04-12 22:30:11 +0300

    Add serialization of notifies

--- src/Editors/xrECore/Engine/EditorMotion.cpp
+++ src/Editors/xrECore/Engine/EditorMotion.cpp
@@ -5,7 +5,7 @@
 #define EOBJ_OMOTION   			0x1100
 #define EOBJ_SMOTION   			0x1200
 #define EOBJ_OMOTION_VERSION   	0x0005
-#define EOBJ_SMOTION_VERSION   	0x0007
+#define EOBJ_SMOTION_VERSION   	0x0008
 
 CSMotion::CSMotion() :CCustomMotion()
 {
@@ -148,6 +148,15 @@ void CSMotion::Save(IWriter& F)
     for (u32 i = 0; i < sz; ++i)
         marks[i].Save(&F);
 
+    F.w_u32(notify.size());
+    for (auto elem : notify)
+    {
+        F.w_float(elem.first);
+        F.w_stringZ(elem.second.GiveInfo);
+        F.w_stringZ(elem.second.DisableInfo);
+        F.w_stringZ(elem.second.Functor);
+    }
+    
 }
 
 bool CSMotion::Load(IReader& F)
@@ -224,6 +233,22 @@ bool CSMotion::Load(IReader& F)
             marks.resize(sz);
             for (u32 i = 0; i < sz; ++i)
                 marks[i].Load(&F);
+        }
+    }
+    if (vers >= 0x0008)
+    {
+        u32 sz = F.r_u32();
+        if (sz > 0)
+        {
+            notify.reserve(sz);
+            for (u32 i = 0; i < sz; ++i)
+            {
+                float key = F.r_float();
+                notify[key] = {};
+                F.r_stringZ(notify[key].GiveInfo);
+                F.r_stringZ(notify[key].DisableInfo);
+                F.r_stringZ(notify[key].Functor);
+            }
         }
     }
     for (BoneMotionIt bm_it = bone_mots.begin(); bm_it != bone_mots.end(); bm_it++)
