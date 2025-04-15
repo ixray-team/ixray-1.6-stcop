5697ba7f65890077e098e20d7d27a02b515e4fe5
Shtrecker <gogels235@gmail.com>
2025-04-15 18:03:51 +0300

    Add update hud section
    Added fix for alternate sights
    
    Co-authored-by: Drombeys <dima.zolotov02@gmail.com>

--- src/xrGame/WeaponUpgrade.cpp
+++ src/xrGame/WeaponUpgrade.cpp
@@ -29,6 +29,10 @@ bool CWeapon::install_upgrade_impl( LPCSTR section, bool test )
 	result |= install_upgrade_disp      ( section, test );
 	result |= install_upgrade_hit       ( section, test );
 	result |= install_upgrade_addon     ( section, test );
+	result |= install_upgrade_hud_sect(section, test);
+	result |= install_upgrade_hud_sect_silencer(section, test);
+	result |= install_upgrade_hud_sect_scope(section, test);
+	result |= install_upgrade_hud_sect_gl(section, test);
 	result |= install_upgrade_bones		( section, test );
 	return result;
 }
@@ -300,6 +304,81 @@ bool CWeapon::install_upgrade_addon( LPCSTR section, bool test )
 		}
 	}
 	result |= result2;
+	return result;
+}
+
+bool CWeapon::install_upgrade_hud_sect(LPCSTR section, bool test)
+{
+	LPCSTR str;
+
+	bool result = process_if_exists_set(section, "hud", &CInifile::r_string, str, test);
+
+	if (result && !test)
+	{
+		shared_str new_hud_sect = pSettings->r_string(section, "hud");
+		shared_str old_hud = hud_sect_cache;
+
+		if (new_hud_sect == "skip_reassign")
+			hud_sect = old_hud;
+		else
+			hud_sect = new_hud_sect;
+
+		hud_sect_cache = hud_sect;
+	}
+
+	InitAddons();
+
+	return result;
+}
+
+bool CWeapon::install_upgrade_hud_sect_silencer(LPCSTR section, bool test)
+{
+	LPCSTR str;
+
+	bool result = process_if_exists_set(section, "hud_silencer", &CInifile::r_string, str, test);
+
+	if (result && !test)
+	{
+		hud_silencer = pSettings->r_string(section, "hud_silencer");
+		m_bUseSilHud = pSettings->r_bool(section, "hud_when_silencer_is_attached");
+	}
+
+	InitAddons();
+
+	return result;
+}
+
+bool CWeapon::install_upgrade_hud_sect_scope(LPCSTR section, bool test)
+{
+	LPCSTR str;
+
+	bool result = process_if_exists_set(section, "hud_scope", &CInifile::r_string, str, test);
+
+	if (result && !test)
+	{
+		hud_scope = pSettings->r_string(section, "hud_scope");
+		m_bUseScopeHud = pSettings->r_bool(section, "hud_when_scope_is_attached");
+	}
+
+	InitAddons();
+
+	return result;
+}
+
+bool CWeapon::install_upgrade_hud_sect_gl(LPCSTR section, bool test)
+{
+	LPCSTR str;
+
+	bool result = process_if_exists_set(section, "hud_gl", &CInifile::r_string, str, test);
+
+	if (result && !test)
+	{
+		hud_scope = pSettings->r_string(section, "hud_gl");
+		m_bUseGLHud = pSettings->r_bool(section, "hud_when_gl_is_attached");
+	}
+
+	InitAddons();
+
 	return result;
 }
 
