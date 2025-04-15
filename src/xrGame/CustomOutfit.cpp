d1a65b880c6d613dd80ac63c471ee7e1b788026e
Shtrecker <gogels235@gmail.com>
2025-04-15 18:03:49 +0300

    Delayed hands changing

--- src/xrGame/CustomOutfit.cpp
+++ src/xrGame/CustomOutfit.cpp
@@ -262,8 +262,11 @@ void CCustomOutfit::ApplySkinModel(CActor* pActor, bool bDress, bool bHUDOnly)
 		}
 
 
-		if (pActor == Level().CurrentViewEntity())	
-			g_player_hud->load(pSettings->r_string(cNameSect(),"player_hud_section"));
+		if (pActor == Level().CurrentViewEntity())
+		{
+			//g_player_hud->load(pSettings->r_string(cNameSect(),"player_hud_section"));
+			g_player_hud->m_need_reload = false;
+		}
 	}else
 	{
 		if (!bHUDOnly && m_ActorVisual.size())
@@ -275,8 +278,11 @@ void CCustomOutfit::ApplySkinModel(CActor* pActor, bool bDress, bool bHUDOnly)
 			};
 		}
 
-		if (pActor == Level().CurrentViewEntity())	
-			g_player_hud->load_default();
+		if (pActor == Level().CurrentViewEntity())
+		{
+			//g_player_hud->load_default();
+			g_player_hud->m_need_reload = false;
+		}
 	}
 
 }
