3164fc4b3cd2671c68c5c037de95c2b7aed3ac6c
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-04-15 18:03:41 +0300

    Add support of legacy `inventory_upgrade.xml`

--- src/xrEngine/Application.cpp
+++ src/xrEngine/Application.cpp
@@ -314,7 +314,7 @@ void CApplication::Level_Scan()
 	FS.file_list_close(folder);
 }
 
-void gen_logo_name(string_path& dest, LPCSTR level_name, int num)
+void gen_logo_name(string_path& dest, LPCSTR level_name, int num = -1)
 {
 	xr_strconcat(dest, "intro\\intro_", level_name);
 
@@ -322,44 +322,55 @@ void gen_logo_name(string_path& dest, LPCSTR level_name, int num)
 	if (dest[len - 1] == '\\')
 		dest[len - 1] = 0;
 
+	if (num < 0)
+		return;
+
 	string16 buff;
 	xr_strcat(dest, sizeof(dest), "_");
-	xr_strcat(dest, sizeof(dest), _itoa(num + 1, buff, 10));
+	xr_strcat(dest, sizeof(dest), itoa(num + 1, buff, 10));
+}
+
+// Return true if logo exists
+// Always sets the path even if logo doesn't exist
+bool set_logo_path(string_path& path, pcstr levelName, int count = -1)
+{
+	gen_logo_name(path, levelName, count);
+	string_path temp2;
+	return FS.exist(temp2, "$game_textures$", path, ".dds") || FS.exist(temp2, "$level$", path, ".dds");
 }
 
 void CApplication::Level_Set(u32 L)
 {
-	if (L >= Levels.size())	return;
-	FS.get_path("$level$")->_set(Levels[L].folder);
+    if (L >= Levels.size())
+        return;
+    FS.get_path("$level$")->_set(Levels[L].folder);
+    Level_Current = L;
 
-	static string_path			path;
+    static string_path path;
+    path[0] = 0;
 
-	if (Level_Current != L)
-	{
-		path[0] = 0;
+    int count = 0;
+    while (true)
+    {
+        if (set_logo_path(path, Levels[L].folder, count))
+            count++;
+        else
+            break;
+    }
 
-		Level_Current = L;
+    if (count)
+    {
+        const int num = ::Random.randI(count);
+        gen_logo_name(path, Levels[L].folder, num);
+    }
+    else if (!set_logo_path(path, Levels[L].folder))
+    {
+        if (!set_logo_path(path, "no_start_picture"))
+            path[0] = 0;
+    }
 
-		int count = 0;
-		while (true)
-		{
-			string_path			temp2;
-			gen_logo_name(path, Levels[L].folder, count);
-			if (FS.exist(temp2, "$game_textures$", path, ".dds") || FS.exist(temp2, "$level$", path, ".dds"))
-				count++;
-			else
-				break;
-		}
-
-		if (count)
-		{
-			int num = ::Random.randI(count);
-			gen_logo_name(path, Levels[L].folder, num);
-		}
-	}
-
-	if (path[0] && loadingScreen)
-		loadingScreen->SetLevelLogo(path);
+    if (path[0] && loadingScreen)
+        loadingScreen->SetLevelLogo(path);
 }
 
 int CApplication::Level_ID(LPCSTR name, LPCSTR ver, bool bSet)
