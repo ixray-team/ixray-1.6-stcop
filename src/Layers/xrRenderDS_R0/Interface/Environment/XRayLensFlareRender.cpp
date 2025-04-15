95c0cd3718e9358208fc6643eeb16744e9e8bf5a
ForserX <sv3nk@yandex.ru>
2025-04-15 18:03:51 +0300

    Reformat `DS0` render

--- src/Layers/xrRenderDS_R0/Interface/Environment/XRayLensFlareRender.cpp
+++ src/Layers/xrRenderDS_R0/Interface/Environment/XRayLensFlareRender.cpp
@@ -1,11 +1,11 @@
 #include "stdafx.h"
 #include "../../XrEngine/xr_efflensflare.h"
 
-XRayLensFlareRender::XRayLensFlareRender()
+CDS0_LensFlareRender::CDS0_LensFlareRender()
 {
 }
 
-void XRayLensFlareRender::Copy(ILensFlareRender& _in)
+void CDS0_LensFlareRender::Copy(ILensFlareRender& _in)
 {
 	
 }
@@ -13,17 +13,17 @@ void XRayLensFlareRender::Copy(ILensFlareRender& _in)
 #define FAR_DIST g_pGamePersistent->Environment().CurrentEnv->far_plane
 
 
-void XRayLensFlareRender::Render(CLensFlare& owner, BOOL bSun, BOOL bFlares, BOOL bGradient)
+void CDS0_LensFlareRender::Render(CLensFlare& owner, BOOL bSun, BOOL bFlares, BOOL bGradient)
 {
 	
 
 
 }
 
-void XRayLensFlareRender::OnDeviceCreate()
+void CDS0_LensFlareRender::OnDeviceCreate()
 {
 }
 
-void XRayLensFlareRender::OnDeviceDestroy()
+void CDS0_LensFlareRender::OnDeviceDestroy()
 {
 }
