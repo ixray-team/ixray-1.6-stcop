cf36bd5af16597eceb7645c9b898a151b36aae75
Shtrecker <gogels235@gmail.com>
2025-04-15 18:04:03 +0300

    Add aim in/out animations

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -1007,6 +1007,9 @@ void CWeapon::UpdateCL		()
 	if(!IsGameTypeSingle())
 		make_Interpolation		();
 
+	if (!IsZoomed() && m_bIsAimStarted)
+		m_bIsAimStarted = false;
+
 	if (ParentIsActor())
 	{
 		if (GetNightVision() && !GetNightVision()->IsActive() && !need_renderable())
