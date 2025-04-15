5385a954cdb1221d051884e718e29e91a448fd19
Romann <kriworuck.production@yandex.ru>
2025-04-15 18:03:51 +0300

    * [LevelEditor]  A separate window with weather settings.

--- src/Editors/xrECore/Editor/EditorPreferences.h
+++ src/Editors/xrECore/Editor/EditorPreferences.h
@@ -77,6 +77,10 @@ public:
 	AStringVec 		scene_recent_list;
 	// objects
 	Flags32			object_flags;
+    // Weather
+    float           env_from_time;
+    float           env_to_time;
+    float           env_speed;
 	shared_str      sWeather;
 
 	xr_string		Compiler_xrAI;
