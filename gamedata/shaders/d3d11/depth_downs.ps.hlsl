205efb8e0c440ca2334d89d3781250d11866ec7a
LVutner <kkamil724@gmail.com>
2025-04-15 18:04:00 +0300

    Fix 2x2 block sampling

--- gamedata/shaders/d3d11/depth_downs.ps.hlsl
+++ gamedata/shaders/d3d11/depth_downs.ps.hlsl
@@ -7,6 +7,8 @@ struct _input
     float4 pos2d : SV_POSITION;
 };
 
+uniform float4 scaled_screen_res;
+
 float4 main(_input I) : SV_Target0
 {
 	float4 Depth;
@@ -17,7 +19,7 @@ float4 main(_input I) : SV_Target0
     Depth.z = s_position.SampleLevel(smp_nofilter, I.tc0.xy, int2(0, 1), 0).x;
     Depth.w = s_position.SampleLevel(smp_nofilter, I.tc0.xy, int2(0, 0), 0).x;
 #else // !SM_5
-    Depth = s_position.GatherRed(smp_nofilter, I.tc0.xy);
+    Depth = s_position.GatherRed(smp_nofilter, I.tc0.xy + 0.5f * scaled_screen_res.zw);
 #endif // SM_5
 
     Depth = depth_unpack.x * rcp(Depth - depth_unpack.y);
