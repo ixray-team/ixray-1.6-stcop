c78f556822c55ed54768717c145ac1913b8da60e
Shtrecker <gogels235@gmail.com>
2025-04-15 18:04:01 +0300

    Add motion suffix generator

--- src/xrGame/HUDAnimItem.cpp
+++ src/xrGame/HUDAnimItem.cpp
@@ -95,7 +95,7 @@ void CHUDAnimItem::UpdateCL()
 			return;
 	}
 
-	PlayHUDMotion(CurrentMotion, true, this, eIdle);
+	PlayHUDMotion(CurrentMotion, true, eIdle);
 	CurrentMotion = "";
 }
 
--- src/xrGame/WeaponShotgun.cpp
+++ src/xrGame/WeaponShotgun.cpp
@@ -160,18 +160,20 @@ void CWeaponShotgun::switch2_EndReload	()
 void CWeaponShotgun::PlayAnimOpenWeapon()
 {
 	VERIFY(GetState()==eReload);
-	PlayHUDMotion("anm_open",FALSE,this,GetState());
+	PlayHUDMotion("anm_open", FALSE, GetState());
 }
+
 void CWeaponShotgun::PlayAnimAddOneCartridgeWeapon()
 {
 	VERIFY(GetState()==eReload);
-	PlayHUDMotion("anm_add_cartridge",FALSE,this,GetState());
+	PlayHUDMotion("anm_add_cartridge", FALSE, GetState());
 }
+
 void CWeaponShotgun::PlayAnimCloseWeapon()
 {
 	VERIFY(GetState()==eReload);
 
-	PlayHUDMotion("anm_close",FALSE,this,GetState());
+	PlayHUDMotion("anm_close", FALSE, GetState());
 }
 
 void	CWeaponShotgun::net_Export	(NET_Packet& P)
