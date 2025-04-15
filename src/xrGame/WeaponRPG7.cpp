a3107a9994f48820c550a3ccee83713bd619b2b0
Shtrecker <gogels235@gmail.com>
2025-04-15 18:04:01 +0300

    Add motion suffix generator

--- src/xrGame/WeaponRPG7.cpp
+++ src/xrGame/WeaponRPG7.cpp
@@ -168,7 +168,7 @@ void CWeaponRPG7::FireStart()
 void CWeaponRPG7::PlayAnimReload()
 {
 	VERIFY(GetState()==eReload);
-	PlayHUDMotion("anm_reload", FALSE, this, GetState());
+	PlayHUDMotion("anm_reload", FALSE, GetState());
 }
 
 void CWeaponRPG7::OnEvent(NET_Packet& P, u16 type) 
