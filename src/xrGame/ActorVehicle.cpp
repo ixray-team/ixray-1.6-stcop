ba90d4d1ddb91c6106a87eb48eaa69d235608b9f
St4lker0k765 <St4lker0k765@users.noreply.github.com>
2025-04-15 18:03:55 +0300

    Add car exports to Lua
    Co-Authored-By: Alundaio <715088+revolucas@users.noreply.github.com>

--- src/xrGame/ActorVehicle.cpp
+++ src/xrGame/ActorVehicle.cpp
@@ -29,6 +29,9 @@ void CActor::attach_Vehicle(CHolderCustom* vehicle)
 {
 	if(!vehicle) return;
 	if(m_holder) return;
+	CCar* car = smart_cast<CCar*>(vehicle);
+	if (!car)
+		return;
 
 	m_holder=vehicle;
 
@@ -41,7 +44,6 @@ void CActor::attach_Vehicle(CHolderCustom* vehicle)
 		return;
 	}
 	// temp play animation
-	CCar*	car						= smart_cast<CCar*>(m_holder);
 	u16 anim_type					= car->DriverAnimationType();
 	SVehicleAnimCollection& anims	= m_vehicle_anims->m_vehicles_type_collections[anim_type];
 	V->PlayCycle					(anims.idles[0],FALSE);
