bddf724f095a41700cc398486d8bf8f80b767596
Shtrecker <gogels235@gmail.com>
2025-04-25 01:12:08 +0300

    Don't leave zoom in empty

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -1026,8 +1026,6 @@ void CWeaponMagazined::switch2_Fire	()
 
 void CWeaponMagazined::switch2_Empty()
 {
-	OnZoomOut();
-	
 	const static bool isAutoreload = EngineExternal()[EEngineExternalGame::EnableAutoreload];
 	if (!isAutoreload)
 	{
