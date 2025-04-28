c90b62e02108c4d7b39f413600362a575bf1870d
ForserX <sv3nk@yandex.ru>
2025-04-28 09:26:32 +0300

    Implement `RayPick` for heightmap

--- src/Editors/LevelEditor/Editor/Terrain/HeightmapUtils.h
+++ src/Editors/LevelEditor/Editor/Terrain/HeightmapUtils.h
@@ -1,70 +1,8 @@
 #pragma once
-
-class CEditableObject;
+#include "HeightMap.h"
 
 namespace XRay::Editor::HeightmapUtils
 {
-	struct SHeightMapChunk
-	{
-		xr_vector<Fvector> Vertices;    // Вершины чанка (уже оптимизированные)
-		xr_vector<u32> Colors;          // Цвета (если нужны)
-		Fbox BBox;                      // Bounding-box чанка
-		bool IsFlat;					// Можно ли упростить (все Y почти одинаковы)
-		bool IsValid = false;
-		int LodLevel;					// Уровень детализации (0 = максимальный)
-	};
-
-	struct SHeightMapRenderData
-	{
-		static constexpr int CHUNK_SIZE = 32;
-		static constexpr float FLAT_THRESHOLD = 0.1f;
-
-		xr_vector<SHeightMapChunk> Chunks;
-
-		bool IsDirty = true;
-
-		xr_vector<u32> Colors;
-		Fbox BoundingBox;
-
-
-		void BuildFromHeightmap(const float* heightmap, int width, int height);
-		void Clear()
-		{
-			Chunks.clear();
-			Colors.clear();
-			BoundingBox.invalidate();
-		}
-	};
-
-	struct SHeightMap
-	{
-		u32 Width;
-		u32 Height;			// Размеры карты (в точках)
-		float* Data;        // Массив высот [width * height]
-		float MinH, MaxH;   // Минимальная и максимальная высота
-
-		Fvector Pos;
-		Fvector Size = { 1, 1, 1 };
-
-		SHeightMapRenderData RenderData;
-
-		SHeightMap() : Width(0), Height(0), Data(nullptr), MinH(0), MaxH(0) {}
-		~SHeightMap() { if (Data) xr_free(Data); }
-
-		IC float GetHeight(u32 x, u32 z) const
-		{
-			if (!Data || x >= Width || z >= Height) return 0.f;
-			return Data[z * Width + x];
-		}
-
-		bool LoadRAW(const char* filename);
-		bool LoadSteam(IReader* Reader);
-		void PrecacheRenderData(float scaleY, float cellSize, u32 baseColor);
-		void Draw(float scaleY = 100.f, float cellSize = 1.f, u32 color = 0xFF00FF00);
-		void MarkDirty();
-	};
-
 	void GenerateMeshByHeightmap(const SHeightMap& heightmap, CEditableObject* OutMesh);
 	void GenerateHeightmapByMesh(CEditableObject* Mesh, const xr_string& OutputFile);
-
 };
