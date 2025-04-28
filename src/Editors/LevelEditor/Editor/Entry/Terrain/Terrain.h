12954d23a56e7f034e548859164dcd82fe865819
ForserX <sv3nk@yandex.ru>
2025-04-28 09:26:32 +0300

    Implement `RayPick` for heightmap

--- src/Editors/LevelEditor/Editor/Entry/Terrain/Terrain.h
+++ src/Editors/LevelEditor/Editor/Entry/Terrain/Terrain.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "Editor/Terrain/HeightmapUtils.h"
 
-class CTerrain:
+class CTerrain :
 	public CCustomObject
 {
 	typedef CCustomObject inherited;
@@ -14,16 +14,17 @@ public:
 	CTerrain(LPVOID data, LPCSTR name);
 	virtual ~CTerrain();
 
-	void 			Construct	(LPVOID data);
+	void Construct(LPVOID data);
 
-	virtual bool	CanAttach() { return false; }
-  	virtual bool 	LoadStream			(IReader&);
-	virtual void 	SaveStream			(IWriter&);
-	virtual void	OnFrame				() override;
-	virtual void 	Render(int priority, bool strictB2F);
+	virtual bool CanAttach() override { return false; }
+	virtual bool LoadStream(IReader&) override;
+	virtual void SaveStream(IWriter&) override;
+	virtual void OnFrame() override;
+	virtual bool RayPick(float& dist, const Fvector& S, const Fvector& D, SRayPickInfo* pinf) override;
+	virtual void Render(int priority, bool strictB2F) override;
 
-	virtual void 	Move(Fvector& amount);
-	virtual void 	Scale(Fvector& amount);
+	virtual void Move(Fvector& amount);
+	virtual void Scale(Fvector& amount);
 
-	virtual void 	OnUpdateTransform() override;
+	virtual void OnUpdateTransform() override;
 };
