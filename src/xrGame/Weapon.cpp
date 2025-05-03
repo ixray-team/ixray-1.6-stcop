377e8ec456e1c4ad46a709e9f14c2ea1337625c3
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-05-04 02:25:31 +0300

    Fix zoom texture

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -1948,7 +1948,7 @@ void CWeapon::OnZoomOut()
 
 CUIWindow* CWeapon::ZoomTexture()
 {
-	return nullptr; //UseScopeTexture() ? m_UIScope : nullptr;
+	return UseScopeTexture() ? m_UIScope : nullptr;
 }
 
 void CWeapon::SwitchState(u32 S)
