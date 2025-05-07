4d0187af151b1b9ead2b4a5ddaa91823ea830f33
Shtrecker <gogels235@gmail.com>
2025-05-07 08:22:53 +0300

    Add reload motion mark

--- src/xrGame/WeaponMagazined.h
+++ src/xrGame/WeaponMagazined.h
@@ -154,6 +154,8 @@ public:
 	virtual void	save				(NET_Packet &output_packet);
 	virtual void	load				(IReader &input_packet);
 
+	virtual void OnMotionMark(u32 state, const motion_marks&);
+
 protected:
 	virtual bool	install_upgrade_impl( LPCSTR section, bool test );
 
