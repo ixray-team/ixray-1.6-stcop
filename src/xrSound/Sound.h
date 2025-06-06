b1630b6d9fcee748a842ee8928858e924a727797
ForserX <sv3nk@yandex.ru>
2025-06-06 16:25:29 +0300

    Implement switch audio device
    Implement `EAX` values change

--- src/xrSound/Sound.h
+++ src/xrSound/Sound.h
@@ -37,6 +37,7 @@ XRSOUND_API extern u32				snd_device_id			;
 enum {
 	ss_Hardware			= (1ul<<1ul),	//!< Use hardware mixing only
     ss_EFX				= (1ul<<2ul),	//!< Use eax
+    ss_HRTF				= (1ul<<3ul),	//!< Use eax
 	ss_forcedword		= u32(-1)
 };
 
@@ -280,6 +281,7 @@ public:
 	virtual void					refresh_env_library		()																						= 0;
 	virtual void					set_user_env			(CSound_environment* E)																	= 0;
 	virtual void					refresh_sources			()																						= 0;
+	virtual void					SwitchAuidoDevice		(const xr_string& Name) = 0;
 };
 
 extern XRSOUND_API CSound_manager_interface*		Sound;
