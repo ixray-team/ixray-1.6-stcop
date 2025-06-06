262b9696193c8d7a87b2d9687cb7cad3b486db2c
ForserX <sv3nk@yandex.ru>
2025-06-06 16:25:29 +0300

    Implement switch audio device
    Implement `EAX` values change

--- src/xrEngine/xr_ioc_cmd.cpp
+++ src/xrEngine/xr_ioc_cmd.cpp
@@ -552,6 +552,8 @@ public:
 		GetToken				();
 		if(!tokens)				return;
 		inherited::Execute		(args);
+
+		::Sound->SwitchAuidoDevice(args);
 	}
 
 	virtual void	Status	(TStatus& S)
@@ -749,6 +751,7 @@ void CCC_Register()
 	CMD1(CCC_SND_Restart,"snd_restart"			);
 	CMD3(CCC_Mask,		"snd_acceleration",		&psSoundFlags,		ss_Hardware	);
 	CMD3(CCC_Mask,		"snd_efx",				&psSoundFlags,		ss_EFX		);
+	CMD3(CCC_Mask,		"snd_hrtf",				&psSoundFlags,		ss_HRTF		);
 
 #ifdef DEBUG
 	CMD3(CCC_Mask,		"snd_stats",			&g_stats_flags,		st_sound	);
