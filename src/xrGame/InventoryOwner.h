106d37f1fadabea41ad278d29daa7d004eb6cc73
Shtrecker <gogels235@gmail.com>
2025-06-13 00:47:52 +0300

    Add custom character portrait for outfit

--- src/xrGame/InventoryOwner.h
+++ src/xrGame/InventoryOwner.h
@@ -178,7 +178,10 @@ public:
 	virtual void			SetReputation	(CHARACTER_REPUTATION_VALUE);
 	virtual void			ChangeReputation(CHARACTER_REPUTATION_VALUE);
 
-	virtual void			SetIcon(const shared_str& iconName) { CharacterInfo().m_SpecificCharacter.data()->m_icon_name = iconName; };
+	virtual void			SetIcon(const shared_str& iconName)
+	{
+		CharacterInfo().m_SpecificCharacter.data()->m_icon_name = (iconName.size() > 0) ? iconName : CharacterInfo().m_SpecificCharacter.data()->m_saved_icon_name;
+	}
 
 	//для работы с relation system
 	u16								object_id	() const;
