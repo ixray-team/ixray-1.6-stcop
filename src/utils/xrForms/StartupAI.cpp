9764116cdf5ededfca1941add86d54aabbfd36b0
DimaKuzmin <scorpion261096@gmail.com>
2025-06-13 00:47:21 +0300

    Add new xrAI and xrDO parametres

--- src/utils/xrForms/StartupAI.cpp
+++ src/utils/xrForms/StartupAI.cpp
@@ -32,18 +32,14 @@ extern void clear_temp_folder();
 extern void	xrCompiler(LPCSTR name, bool draft_mode, bool pure_covers, LPCSTR out_name);
 extern void	verify_level_graph(LPCSTR name, bool verbose);
 
-void execute(LPSTR cmd) {
+#include "CompilersUI.h"
+extern CompilersMode gCompilerMode;
+
+void StartupAI()
+{
 	// Load project
 	string4096 name;
-	name[0] = 0;
-	if (strstr(cmd, "-f"))
-		sscanf(strstr(cmd, "-f") + 2, "%s", name);
-	else if (strstr(cmd, "-s"))
-		sscanf(strstr(cmd, "-s") + 2, "%s", name);
-	else if (strstr(cmd, "-t"))
-		sscanf(strstr(cmd, "-t") + 2, "%s", name);
-	else if (strstr(cmd, "-verify"))
-		sscanf(strstr(cmd, "-verify") + xr_strlen("-verify"), "%s", name);
+	strcpy(name, gCompilerMode.level_name);
 
 	if (xr_strlen(name))
 		xr_strcat(name, "\\");
@@ -51,61 +47,46 @@ void execute(LPSTR cmd) {
 	string_path			prjName;
 	prjName[0] = 0;
 	bool				can_use_name = false;
-	if (xr_strlen(name) < sizeof(string_path)) {
+	
+	if (xr_strlen(name) < sizeof(string_path))
+	{
 		can_use_name = true;
 		FS.update_path(prjName, "$game_levels$", name);
 	}
 
 	FS.update_path(INI_FILE, "$game_config$", GAME_CONFIG);
 
-	if (strstr(cmd, "-f")) {
+	if (gCompilerMode.AI_BuildLevel)
+	{
 		R_ASSERT3(can_use_name, "Too big level name", name);
 
-		char* output = strstr(cmd, "-out");
-		string256		temp0;
-		if (output) {
-			output += xr_strlen("-out");
-			sscanf(output, "%s", temp0);
-			_TrimLeft(temp0);
-			output = temp0;
-		}
-		else
-			output = (pstr)LEVEL_GRAPH_NAME;
+  		char* output = (pstr) LEVEL_GRAPH_NAME;
 
-		xrCompiler(prjName, !!strstr(cmd, "-draft"), !!strstr(cmd, "-pure_covers"), output);
+		xrCompiler(prjName, gCompilerMode.AI_Draft, gCompilerMode.AI_PureCovers, output);
+
+		
+	}
+	 
+	if (gCompilerMode.AI_Verify)
+	{
+		R_ASSERT3(can_use_name, "Too big level name", name);
+		verify_level_graph(prjName, gCompilerMode.AI_Verbose);
 	}
 
-	if (strstr(cmd, "-s")) {
+
+
+	if (gCompilerMode.AI_Spawn && !gCompilerMode.AI_BuildLevel)
+	{
 		if (xr_strlen(name))
 			name[xr_strlen(name) - 1] = 0;
 
-		char* output = strstr(cmd, "-out");
-		string256 temp0, temp1;
+		char* output = gCompilerMode.AI_spawn_name;
+		char* start_level = gCompilerMode.AI_StartActor;
 
-		if (output)
-		{
-			output += xr_strlen("-out");
-			sscanf(output, "%s", temp0);
-			_TrimLeft(temp0);
-			output = temp0;
-		}
-
-		char* start = strstr(cmd, "-start");
-		if (start) 
-		{
-			start += xr_strlen("-start");
-			sscanf(start, "%s", temp1);
-			_TrimLeft(temp1);
-			start = temp1;
-		}
-
-		char* no_separator_check = strstr(cmd, "-no_separator_check");
-		clear_temp_folder();
-		CGameSpawnConstructor* BuilderSpawn = new CGameSpawnConstructor(name, output, start, !!no_separator_check);
-	} else if (strstr(cmd, "-verify")) {
-		R_ASSERT3(can_use_name, "Too big level name", name);
-		verify_level_graph(prjName, !strstr(cmd, "-noverbose"));
+ 		clear_temp_folder();
+		CGameSpawnConstructor* BuilderSpawn = new CGameSpawnConstructor(name, output, start_level, gCompilerMode.AI_NoSeparatorCheck);
 	}
+	
 }
 
 SEFactory_Create* create_entity = 0;
@@ -135,29 +116,4 @@ void InitialFactory() {
 void DestroyFactory() {
 	FreeLibrary(hFactory);
 }
-
-void StartupAI(LPSTR lpCmdLine) {
-	string4096 cmd;
-
-	xr_strcpy(cmd, lpCmdLine);
-	_strlwr(cmd);
-	if (strstr(cmd, "-?") || strstr(cmd, "-h")) {
-		Help(h_str); 
-		return; 
-	}
-
-	if (
-		   (strstr(cmd, "-f") == 0) 
-		&& (strstr(cmd, "-g") == 0) 
-		&& (strstr(cmd, "-m") == 0) 
-		&& (strstr(cmd, "-s") == 0) 
-		&& (strstr(cmd, "-t") == 0) 
-		&& (strstr(cmd, "-c") == 0) 
-		&& (strstr(cmd, "-verify") == 0)
-		&& (strstr(cmd, "-patch") == 0)
-	) {
-		Help(h_str); return; 
-	}
-
-	execute(cmd);
-}
+ 
