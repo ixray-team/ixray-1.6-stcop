210dda6fae065c0106639623f29c3822c6a496c5
Dimakuzmin <scorpion261096@gmail.com>
2025-06-13 00:49:27 +0300

    Вернул мой метод расположения LM в DDS файле (Уже поченил) (не делает засветы)

--- src/utils/xrLC/xrPhase_MergeLM_Surface.cpp
+++ src/utils/xrLC/xrPhase_MergeLM_Surface.cpp
@@ -67,82 +67,16 @@ bool SurfacePlacePerpixel::Place_Perpixel_tbb(L_rect& R, lm_layer* D)
 	u32	s_y = D->height + 2 * BORDER;
 
 	// Normal
-	const auto mm_alpha_ref256	= _mm256_set1_epi8(alpha_ref);	 
-	const auto mm_zero256		= _mm256_setzero_si256();		 
-
-	const auto mm_alpha_ref		= _mm_set1_epi8(alpha_ref);	 
-	const auto mm_zero			= _mm_setzero_si128();		 
-
 	for (u32 y = 0; y < s_y; y++)
 	{
-		/* ForserX Почини плз.
-		if (s_x >= 32 && CPU::ID.hasFeature(CPUFeature::AVX) )
+		BYTE* P = surface_tbb + (y + R.a.y) * SurfaceGrid + R.a.x;	// destination scan-line
+		u8* S = lm + y * s_x;
+		for (u32 x = 0; x < s_x; x++, P++, S++)
 		{
-			u8* P = surface_tbb + (y + R.a.y) * SurfaceGrid + R.a.x;	// destination scan-line
-			u8* S = lm + y * s_x;
-
- 			u32 x = 0;
-			// Проходим по 32 байт за итерацию
- 			for (x = 0; x < s_x - 32; x += 32, P += 32, S += 32)
-			{
-				auto mm_reg_s	= _mm256_loadu_si256((__m256i*) S);
-				auto mm_reg_p	= _mm256_loadu_si256((__m256i*) P);
-				auto mm_max		= _mm256_max_epu8(mm_reg_s, mm_alpha_ref256);
-				auto mm_cmp		= _mm256_cmpeq_epi8(mm_max, mm_alpha_ref256);
-				auto mm_andn	= _mm256_andnot_si256(mm_cmp, mm_reg_p);
-				auto mm_sad		= _mm256_sad_epu8(mm_andn, mm_zero256);
-
-				__m128i lower_128 = _mm256_castsi256_si128(mm_sad); // взять младшие 128 бит
-				if (_mm_cvtsi128_si32(lower_128))
-					return false;
-			}
-			 
-			// Оставшееся 
-			for (; x < s_x; x++, P++, S++)
-			{
-				if ((*P) && (*S >= alpha_ref))
-					return false;
-			}
+			if ((*P) && (*S >= alpha_ref))
+				return false;
 		}
-		else if (s_x >= 16 && CPU::ID.hasFeature(CPUFeature::SSE2))
-		{
-			u8* P = surface_tbb + (y + R.a.y) * SurfaceGrid + R.a.x;	// destination scan-line
-			u8* S = lm + y * s_x;
-
-			u32 x = 0;
-			// Проходим по 16 байт за итерацию
-			for (x = 0; x < s_x - 16; x += 16, P += 16, S += 16)
-			{
-				auto mm_reg_s = _mm_loadu_si128((__m128i*) S);
-				auto mm_reg_p = _mm_loadu_si128((__m128i*) P);
-				auto mm_max = _mm_max_epu8(mm_reg_s, mm_alpha_ref);
-				auto mm_cmp = _mm_cmpeq_epi8(mm_max, mm_alpha_ref);
-				auto mm_andn = _mm_andnot_si128(mm_cmp, mm_reg_p);
-				auto mm_sad = _mm_sad_epu8(mm_andn, mm_zero);
-
- 				if (_mm_cvtsi128_si32(mm_sad))
-					return false;
-			}
- 
-			// Оставшееся 
-			for (; x < s_x; x++, P++, S++)
-			{
-				if ((*P) && (*S >= alpha_ref))
-					return false;
-			}
-		}
-		else
-		*/
-		{
-			BYTE* P = surface_tbb + (y + R.a.y) * SurfaceGrid + R.a.x;	// destination scan-line
-			u8* S = lm + y * s_x;
-			for (u32 x = 0; x < s_x; x++, P++, S++)
-			{
-				if ((*P) && (*S >= alpha_ref))
-					return false;
-			}
-		}
-	}
+ 	}
  
 	// It's OK to place it
 	return true;
