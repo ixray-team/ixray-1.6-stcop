9c121dcda721abcc10fb2a30b3bd1ca7ef6f1ef4
Drombeys <dima.zolotov02@gmail.com>
2025-06-13 00:47:50 +0300

    Add support for layered sounds for `CCarWeapon` and `CHelicopter`

--- src/xrGame/HelicopterWeapon.cpp
+++ src/xrGame/HelicopterWeapon.cpp
@@ -144,8 +144,7 @@ void CHelicopter::OnShot		()
 	StartSmokeParticles		(fire_pos, zero_vel);
 	OnShellDrop				(fire_pos, zero_vel);
 
-	HUD_SOUND_ITEM::PlaySound	(m_sndShot, fire_pos, this, false);
-
+    m_sounds_layered.PlaySound("m_sndShot", fire_pos, this, false);
 }
 
 void CHelicopter::MGunFireStart()
@@ -313,8 +312,7 @@ void CHelicopter::startRocket(u16 idx)
 		dropCurrentRocket();
 
 		m_last_launched_rocket = idx;
-		HUD_SOUND_ITEM::PlaySound(m_sndShotRocket, xform.c, this, false);
-
+        m_sounds_layered.PlaySound("m_sndShotRocket", xform.c, this, false);
 	}
 }
 
