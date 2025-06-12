2d4f118da7c052c1d901f8344a07159c50501e32
DimaKuzmin <scorpion261096@gmail.com>
2025-06-13 00:47:21 +0300

    Improve IX-Ray UI

--- src/utils/xrForms/CompilersUI.h
+++ src/utils/xrForms/CompilersUI.h
@@ -2,6 +2,8 @@
 
 struct CompilersMode
 {
+	char level_name[128];
+
 	bool Silent = false;
 	bool Embree = true;
 	bool ClearTemp = false;
@@ -16,7 +18,16 @@ struct CompilersMode
 	bool LC_Noise = false;
 	bool LC_Tess = true;
 	bool LC_SkipInvalidFaces = true;
+	bool LC_tex_rgba = false;
+	bool LC_NoSubdivide = false;
+	bool LC_skipWeld = false;
 
+	int LC_sizeLmaps = 1024 * 4;
+
+	int LC_JSampleMU = 6;
+	int LC_JSample = 9;
+	float LC_Pixels  = 10;
+ 
 	bool DO_NoSun = false;
 
 	bool AI_NoSeparatorCheck = true;
