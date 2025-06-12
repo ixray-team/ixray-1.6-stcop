9597a4b6811f7501b199e427a50298a6b3e7558f
St4lker0k765 <St4lker0k765@users.noreply.github.com>
2025-06-13 00:47:41 +0300

    Add support `actor_menu_item` legacy xml

--- src/xrGame/ui/UIOutfitInfo.h
+++ src/xrGame/ui/UIOutfitInfo.h
@@ -42,7 +42,7 @@ public:
 protected:
 	enum				{ max_count = ALife::eHitTypeMax-4 };
 	
-//	CUIStatic*			m_caption;
+	CUIStatic*			m_caption;
 	CUIStatic*			m_Prop_line;
 	CUIOutfitImmunity*	m_items[max_count];
 
