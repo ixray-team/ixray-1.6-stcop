1b2a60283d167f58c3a114923f2cb21dee29491b
Drombeys <dima.zolotov02@gmail.com>
2025-06-22 12:44:39 +0300

    Add resources for X-Ray 1.5.03

--- gamedata_cs/shaders/r2/skin.h
+++ gamedata_cs/shaders/r2/skin.h
@@ -52,7 +52,9 @@ struct 	v_model_skinned_4		// 28 bytes
 float4 	u_position	(float4 v)	{ return float4(v.xyz*(12.f / 32768.f), 1.f);	}	// -12..+12
 
 //////////////////////////////////////////////////////////////////////////////////////////
-uniform float4 	sbones_array	[256-22] : register(vs,c22);
+//uniform float4 	sbones_array	[256-22] : register(vs,c22);
+//  Igor: Max number of bones per mesh - 64. 3 registers per bone.
+uniform float4 	sbones_array	[65*3] : register(vs,c22);
 float3 	skinning_dir 	(float3 dir, float3 m0, float3 m1, float3 m2)
 {
 	float3 	U 	= unpack_normal	(dir);
