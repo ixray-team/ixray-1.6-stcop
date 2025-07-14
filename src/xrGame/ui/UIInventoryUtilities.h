6534efd6c5692779e74a8255cd731c68539ce822
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-15 00:34:53 +0300

    Add working indicators for SoC

--- src/xrGame/ui/UIInventoryUtilities.h
+++ src/xrGame/ui/UIInventoryUtilities.h
@@ -80,7 +80,7 @@ const shared_str GetGameTimeAsString(ETimePrecision timePrec, char timeSeparator
 const shared_str GetDateAsString(ALife::_TIME_ID time, EDatePrecision datePrec, char dateSeparator = ',');
 const shared_str GetDateAsStringLegacy(ALife::_TIME_ID time, EDatePrecision datePrec, char dateSeparator = '/');
 const shared_str GetTimeAsString(ALife::_TIME_ID time, ETimePrecision timePrec, char timeSeparator = ':', bool full_mode = true);
-const shared_str GetTimeAndDateAsString(ALife::_TIME_ID time);
+const shared_str GetTimeAndDateAsString(ALife::_TIME_ID time, bool legacyMode = false);
 const shared_str Get_GameTimeAndDate_AsString();
 
 LPCSTR GetTimePeriodAsString	(LPSTR _buff, u32 buff_sz, ALife::_TIME_ID _from, ALife::_TIME_ID _to);
