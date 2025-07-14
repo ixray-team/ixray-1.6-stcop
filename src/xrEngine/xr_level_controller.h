fe4a9f1866793fdecb0ec0cc152eb06d3d9ce941
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-15 00:34:44 +0300

    Add new values for SoC

--- src/xrEngine/xr_level_controller.h
+++ src/xrEngine/xr_level_controller.h
@@ -64,7 +64,9 @@ enum EGameActions
 	kSKIN,
 	kTEAM,
 	kACTIVE_JOBS,
-						
+	kMAP,
+	kCONTACTS,
+
 	kVOTE_BEGIN,
 	kSHOW_ADMIN_MENU,
 	kVOTE,
@@ -88,6 +90,7 @@ enum EGameActions
 
 	kUSE_BANDAGE,
 	kUSE_MEDKIT,
+	kENGINE,
 
 	kCUSTOM1,
 	kCUSTOM2,
