898c2fcbfe65fdbbfd75c44b7d60551854ab953b
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-15 00:34:55 +0300

    Fix description for `CUICarBodyWnd`

--- src/xrGame/ui/UICarBodyWnd.cpp
+++ src/xrGame/ui/UICarBodyWnd.cpp
@@ -347,7 +347,7 @@ void CUICarBodyWnd::SetCurrentItem(CUICellItem* itm)
 {
 	if(m_pCurrentCellItem == itm) return;
 	m_pCurrentCellItem		= itm;
-	m_pUIItemInfo->InitItem(CurrentItem(), nullptr, CurrentIItem() ? CurrentIItem()->Cost() : u32(-1));
+	m_pUIItemInfo->InitItem(CurrentItem(), nullptr, CurrentIItem() ? CurrentIItem()->Cost() : u32(-1), nullptr, true);
 }
 
 void CUICarBodyWnd::TakeAll()
