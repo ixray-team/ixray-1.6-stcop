687ca3f931832c0e8b1cc7aeed821f4b5917adfc
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-15 00:34:54 +0300

    Fix trade wnd
    # Conflicts:
    #	src/xrGame/ui/UITalkWnd.cpp
    #	src/xrGame/ui/UITalkWnd.h

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -1428,7 +1428,7 @@ void CWeaponMagazined::SelectShotSound()
 			}
 			else
 			{
-				m_sSndShotCurrent = "sndSilencerShot";
+				m_sSndShotCurrent = m_layered_sounds.FindSoundItem("sndSilencerShot", false) ? "sndSilencerShot" : "sndShot";
 			}
 		}
 	}
