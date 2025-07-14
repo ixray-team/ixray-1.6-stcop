fe091001e159ef41d87be71e6612086661bb5ab7
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-15 00:34:52 +0300

    Restore missing PDA pages

--- src/xrGame/saved_game_wrapper.cpp
+++ src/xrGame/saved_game_wrapper.cpp
@@ -145,7 +145,15 @@ CSavedGameWrapper::CSavedGameWrapper			(LPCSTR saved_game_name)
 		}
 
 		chunk					= spawn->open_chunk(4);
-		if (!chunk) {
+		if (!chunk) 
+		{
+			string_path graph_path;
+			FS.update_path(graph_path, "$game_data$", GRAPH_NAME);
+			chunk = FS.r_open(graph_path);
+		}
+
+		if (!chunk)
+		{
 			F_entity_Destroy	(object);
 			if(b_destroy_spawn)
 				FS.r_close		(spawn);
