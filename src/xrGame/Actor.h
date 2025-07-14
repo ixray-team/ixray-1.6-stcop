be237a17e069dad5e4a6497da73e49e5c763d5e4
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-15 00:34:52 +0300

    Add artefact panel support

--- src/xrGame/Actor.h
+++ src/xrGame/Actor.h
@@ -234,6 +234,8 @@ public:
 	void				HitArtefactsCondition(SHit& hit);
 			float		HitArtefactsOnBelt		(float hit_power, ALife::EHitType hit_type);
 			float		GetProtection_ArtefactsOnBelt(ALife::EHitType hit_type);
+	virtual void		MoveArtefactBelt		(const CArtefact* artefact, bool on_belt);
+	const xr_vector<const CArtefact*>& ArtefactsOnBelt() {return m_ArtefactsOnBelt;}
 
 protected:
 	//звук тяжелого дыхания
@@ -241,6 +243,9 @@ protected:
 	ref_sound			m_BloodSnd = {};
 	ref_sound			m_DangerSnd = {};
 	ref_sound			m_rainOnHelmetSnd = {};
+
+	xr_vector<const CArtefact*> m_ArtefactsOnBelt;
+
 protected:
 	// Death
 	float					m_hit_slowmo;
