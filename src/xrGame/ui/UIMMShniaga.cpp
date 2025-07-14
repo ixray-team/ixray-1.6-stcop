20162803f4ec4f81ab34e6578b9f6b18dd834bab
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-15 00:34:52 +0300

    Add SoC-style shniaga support

--- src/xrGame/ui/UIMMShniaga.cpp
+++ src/xrGame/ui/UIMMShniaga.cpp
@@ -13,6 +13,7 @@
 #include "../saved_game_wrapper.h"
 #include "MainMenu.h"
 #include "../gamespy/GameSpy_Full.h"
+#include "../../xrUI/UIHelper.h"
 
 extern string_path g_last_saved_game;
 
@@ -59,12 +60,30 @@ void CUIMMShniaga::InitShniaga(CUIXml& xml_doc, LPCSTR path)
 	m_mag_pos				= m_magnifier->GetWndPos().x;
 	xr_strconcat(_path,path,":shniaga");
 	CUIXmlInit::InitStatic(xml_doc, _path,0,m_shniaga);
+
+	xr_strconcat(_path, path, ":shniaga:left_anim");
+	if (xml_doc.NavigateToNode(_path))
+		m_anims[0] = UIHelper::CreateStatic(xml_doc, _path, m_shniaga);
+	xr_strconcat(_path, path, ":shniaga:right_anim");
+	if (xml_doc.NavigateToNode(_path))
+		m_anims[1] = UIHelper::CreateStatic(xml_doc, _path, m_shniaga);
+
+	xr_strconcat(_path, path, ":shniaga:left_grating");
+	if (xml_doc.NavigateToNode(_path))
+		m_gratings[0] = UIHelper::CreateStatic(xml_doc, _path, m_shniaga);
+	xr_strconcat(_path, path, ":shniaga:right_grating");
+	if (xml_doc.NavigateToNode(_path))
+		m_gratings[1] = UIHelper::CreateStatic(xml_doc, _path, m_shniaga);
+
 	xr_strconcat(_path,path,":buttons_region");
 	CUIXmlInit::InitScrollView(xml_doc, _path,0,m_view);
+
 	xr_strconcat(_path,path,":shniaga:magnifire:y_offset");
 	m_offset = xml_doc.ReadFlt(_path,0,0);
+
+	int def_speed = EngineExternal().ShadowOfChernobylMode() ? 1000 : 300;
 	xr_strconcat(_path, path, ":shniaga:speed");
-	m_run_speed = xml_doc.ReadInt(_path, 0, 300);
+	m_run_speed = xml_doc.ReadInt(_path, 0, def_speed);
 
 	if (!g_pGameLevel || !g_pGameLevel->bReady) 
 	{
@@ -101,8 +120,8 @@ void CUIMMShniaga::CreateList(xr_vector<CUITextWnd*>& lst, CUIXml& xml_doc, LPCS
 {
 	CGameFont* pF;
 	u32	color;
-	float button_height				= xml_doc.ReadAttribFlt("button", 0, "h");
-	R_ASSERT						(button_height);
+	float button_height_legacy = xml_doc.ReadAttribFlt(path, 0, "btn_height", 30.f);
+	float button_height				= xml_doc.ReadAttribFlt("button", 0, "h", button_height_legacy);
 
 	CUIXmlInit::InitFont			(xml_doc, path, 0, color, pF);
 	if (!pF)
@@ -276,8 +295,16 @@ void CUIMMShniaga::Update()
 {
 	if (m_start_time > Device.dwTimeContinual - m_run_time)
 	{
-
 		Fvector2 pos = m_shniaga->GetWndPos();
+		if (m_anims[0])
+		{
+			float l = 2 * PI * m_anims[0]->GetHeight() / 2;
+			int n = iFloor(pos.y / l);
+			float a = 2 * PI * (pos.y - l * n) / l;
+			m_anims[0]->SetHeading(-a);
+			m_anims[1]->SetHeading(a);
+		}
+
 		pos.y = this->pos(m_origin, m_destination, Device.dwTimeContinual - m_start_time);
 		m_shniaga->SetWndPos(pos);		
 	}else
