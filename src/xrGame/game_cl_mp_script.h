d98e29d57adfd4c3259ed4d4ff202943b0ba002e
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-15 00:34:52 +0300

    Launch SoC mode from main xrGame

---
+++ src/xrGame/game_cl_mp_script.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "game_cl_mp.h"
+#include "../xrScripts/script_export_space.h"
+class CScriptGameObject;
+
+class game_cl_mp_script : public game_cl_mp
+{
+	typedef game_cl_mp	inherited;
+protected:
+	CScriptGameObject*				GetObjectByGameID		(u32 id);
+	game_PlayerState*				GetLocalPlayer			(){return local_player;};
+public:
+									game_cl_mp_script		();
+	virtual		bool				CanBeReady				(){return false;};
+	virtual		void				GetMapEntities			(xr_vector<SZoneMapEntityData>& dst)	{};
+	virtual		void				shedule_Update			(u32 dt);
+	virtual		game_PlayerState*	createPlayerState(NET_Packet* account_info)		{return inherited::createPlayerState(account_info); };
+
+				void				EventGen				( NET_Packet* P, u16 type, u16 dest);
+				void				GameEventGen			( NET_Packet* P, u16 dest);
+				void				EventSend				( NET_Packet* P);
+				LPCSTR				GetRoundTime			();
+	DECLARE_SCRIPT_REGISTER_FUNCTION
+};
