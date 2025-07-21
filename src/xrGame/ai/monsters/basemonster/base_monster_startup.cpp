307837bdb43e36652f62f12e37030ecb7c803307
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-21 23:28:42 +0300

    Fix some UI issues

--- src/xrGame/ai/monsters/basemonster/base_monster_startup.cpp
+++ src/xrGame/ai/monsters/basemonster/base_monster_startup.cpp
@@ -146,7 +146,7 @@ void CBaseMonster::Load(LPCSTR section)
 	{
 		LPCSTR protections_sect = pSettings->r_string(section, "protections_sect");
 		m_fSkinArmor = READ_IF_EXISTS(pSettings,r_float,protections_sect,"skin_armor", 0.f);
-		m_fHitFracMonster = READ_IF_EXISTS(pSettings,r_float,protections_sect,"hit_fraction_monster", 0.1f);
+		m_fHitFracMonster = READ_IF_EXISTS(pSettings, r_float, protections_sect, "hit_fraction_monster", 0.1f);
 	}
 
 	m_force_anti_aim						=	false;
