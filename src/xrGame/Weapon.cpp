41069d596d9717a0f7ff974f2e545664dd329563
Drombeys <dima.zolotov02@gmail.com>
2025-07-21 23:28:13 +0300

    Improve hud adjust

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -2694,7 +2694,7 @@ BOOL CWeapon::ParentIsActor	()
 
 void CWeapon::debug_draw_firedeps()
 {
-#ifdef DEBUG
+#ifdef DEBUG_DRAW
 	if(hud_adj_mode==5||hud_adj_mode==6||hud_adj_mode==7)
 	{
 		CDebugRenderer			&render = Level().debug_renderer();
