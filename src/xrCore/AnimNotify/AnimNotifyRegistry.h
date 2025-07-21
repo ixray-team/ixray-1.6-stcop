fbc5afb0f022d1542285d74dbf95317e452936df
mnelenpridumivat <ragrind@mail.ru>
2025-07-21 23:28:23 +0300

    Add different classes for anim notifies types

--- src/xrCore/AnimNotify/AnimNotifyRegistry.h
+++ src/xrCore/AnimNotify/AnimNotifyRegistry.h
@@ -1,27 +1,20 @@
 #pragma once
+#include "AnimNotifyTypes.h"
 
 class XRCORE_API CAnimNotifyRegistry
 {
-public:
+    xr_hash_map<shared_str, IAnimNotify*> map;
     
-    struct SAnimNotifyInfo
-    {
-        shared_str GiveInfo = "";
-        shared_str DisableInfo = "";
-        shared_str Functor = "";
-    };
+    CAnimNotifyRegistry();
+public:
     
     static CAnimNotifyRegistry& GetInstance();
     
     bool contains(const shared_str& name);
-    const SAnimNotifyInfo& get(const shared_str& name);
+    IAnimNotify* get(const shared_str& name);
 
     CAnimNotifyRegistry& operator=( const CAnimNotifyRegistry& ) = delete;
     CAnimNotifyRegistry( const CAnimNotifyRegistry& ) = delete;
     CAnimNotifyRegistry& operator=( CAnimNotifyRegistry&& ) = delete;
     CAnimNotifyRegistry(CAnimNotifyRegistry&& ) = delete;
-
-private:
-    xr_hash_map<shared_str, SAnimNotifyInfo> map;
-    CAnimNotifyRegistry();
 };
