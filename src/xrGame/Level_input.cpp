401cdac3c0cc6ffd16a2fed5bd14de721bf172f5
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-21 23:28:25 +0300

    Add script override for key press

--- src/xrGame/Level_input.cpp
+++ src/xrGame/Level_input.cpp
@@ -23,6 +23,12 @@
 
 #include "../Include/xrRender/DebugRender.h"
 
+//Alundaio
+#include "pch_script.h"
+#include "../../xrScripts/script_engine.h" 
+using namespace luabind;
+//-Alundaio
+
 #ifdef DEBUG
 #	include "ai/monsters/BaseMonster/base_monster.h"
 
@@ -188,6 +194,13 @@ void CLevel::IR_OnKeyboardPress	(int key)
 		)	return;
 
 	if ( game && game->OnKeyboardPress(get_binded_action(key)) )	return;
+
+	luabind::functor<bool>	funct;
+	if (ai().script_engine().functor("level_input.on_key_press", funct))
+	{
+		if (funct(key, _curr))
+			return;
+	}
 
 	if(_curr == kQUICK_SAVE && IsGameTypeSingle())
 	{
