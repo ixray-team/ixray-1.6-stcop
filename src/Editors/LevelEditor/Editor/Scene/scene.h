9594305210a753b911101d894a8635506ea74263
ForserX <sv3nk@yandex.ru>
2025-07-21 23:28:14 +0300

    Fix rain sound in `PIE`

--- src/Editors/LevelEditor/Editor/Scene/scene.h
+++ src/Editors/LevelEditor/Editor/Scene/scene.h
@@ -289,7 +289,8 @@ public:
 	int  			GetQueryObjects(ObjectList& objset, ObjClassID classfilter, int iSel = 1, int iVis = 1, int iLock = 0);
 	const Fvector&	GetCameraPosition() const;
 
-public:
+	bool GetPlayInEditorRayPickCall();
+	virtual void SetPlayInEditorRayPickCall(bool Value) override;
 
 	void 			OnCreate();
 	void 			OnDestroy();
@@ -361,6 +362,7 @@ public:
 	xr_string full_name;
 
 private:
+	bool IsPIERayPick = false;
 	CLevelGraphEditor m_level_graph;
 	CGameGraphEditor m_game_graph;
 	CMemoryWriter	m_spawn_data;
