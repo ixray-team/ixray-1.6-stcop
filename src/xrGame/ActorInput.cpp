309b5f3da5a7db0028382b915cef94ff348cf184
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-31 11:53:00 +0300

    Fix some UI issues

--- src/xrGame/ActorInput.cpp
+++ src/xrGame/ActorInput.cpp
@@ -160,7 +160,7 @@ void CActor::IR_OnKeyboardPress(int cmd)
 			if (itm)
 			{
 				inventory().Eat(itm);
-				SDrawStaticStruct* _s = CurrentGameUI()->AddCustomStatic("item_used", true, EngineExternal().ClearSkyMode() ? 3.0f : -1.0f);
+				SDrawStaticStruct* _s = CurrentGameUI()->AddCustomStatic("item_used", true, 3.0f);
 				string1024					str;
 				xr_strconcat(str, *CStringTable().translate("st_item_used"), ": ", itm->NameItem());
 				_s->wnd()->TextItemControl()->SetText(str);
