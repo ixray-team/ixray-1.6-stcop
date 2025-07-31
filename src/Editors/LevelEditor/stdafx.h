400eb2cb784432e88acbd49990f003aef03a04e0
ForserX <sv3nk@yandex.ru>
2025-07-31 11:52:28 +0300

    Remove dead code

--- src/Editors/LevelEditor/stdafx.h
+++ src/Editors/LevelEditor/stdafx.h
@@ -19,7 +19,6 @@
 #include "../xrECore/Editor/D3DUtils.h"
 #include "../xrECore/Editor/PhysicsShellHolderEditorBase.h"
 #include "../../utils/ETools/ETools.h"
-#include "../xrECore/Engine/Texture.h"
 #include "../xrECore/Editor/Library.h"
 #include "../xrECore/Editor/ImageManager.h"
 #include "../xrECore/Editor/pick_defs.h"
