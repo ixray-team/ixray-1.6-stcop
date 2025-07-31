c88d51304befdc78d894517e7ff8e7ade6b3415c
ForserX <sv3nk@yandex.ru>
2025-07-31 11:53:03 +0300

    Fix sound environiment render in SE

--- src/Editors/LevelEditor/Engine/XRayEditor.cpp
+++ src/Editors/LevelEditor/Engine/XRayEditor.cpp
@@ -1,21 +1,9 @@
 #include "stdafx.h"
 #include "XRayEditor.h"
-struct _SoundProcessor : public pureFrame
-{
-	virtual void	_BCL	OnFrame()
-	{
-		//Msg							("------------- sound: %d [%3.2f,%3.2f,%3.2f]",u32(EngineDevice.dwFrame),VPUSH(EngineDevice.vCameraPosition));
-		Device.Statistic->Sound.Begin();
-		::Sound->update(Device.vCameraPosition, Device.vCameraDirection, Device.vCameraTop);
-		Device.Statistic->Sound.End();
-	}
-}	SoundProcessor;
+
 XRayEditor::XRayEditor()
 {
 	Device.seqFrame.Add(this, REG_PRIORITY_HIGH + 1000);
-
-	if (psDeviceFlags.test(mtSound))	Device.seqFrameMT.Add(&SoundProcessor);
-	else								Device.seqFrame.Add(&SoundProcessor);
 }
 
 XRayEditor::~XRayEditor()
