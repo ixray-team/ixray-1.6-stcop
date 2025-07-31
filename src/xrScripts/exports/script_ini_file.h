185b976202fdd377c1a91a8c2d202344a941aa1c
Drombeys <dima.zolotov02@gmail.com>
2025-07-31 11:51:07 +0300

    Add `CScriptIniFile` scripts export

--- src/xrScripts/exports/script_ini_file.h
+++ src/xrScripts/exports/script_ini_file.h
@@ -35,9 +35,31 @@ public:
 			int			r_s32				(LPCSTR S, LPCSTR L);
 			float		r_float				(LPCSTR S, LPCSTR L);
 			Fvector		r_fvector3			(LPCSTR S, LPCSTR L);
-			void		set_override_names	(bool b);
-			bool		save_as(LPCSTR new_fname);
-			void		set_readonly(bool b);
+
+			void w_bool(LPCSTR S, LPCSTR L, bool V, LPCSTR comment /* = 0 */);
+			void w_color(LPCSTR S, LPCSTR L, u32 V, LPCSTR comment /* = 0 */);
+			void w_fcolor(LPCSTR S, LPCSTR L, const Fcolor& V, LPCSTR comment /* = 0 */);
+			void w_float(LPCSTR S, LPCSTR L, float V, LPCSTR comment /* = 0 */);
+			void w_fvector2(LPCSTR S, LPCSTR L, const Fvector2& V, LPCSTR comment /* = 0 */);
+			void w_fvector3(LPCSTR S, LPCSTR L, const Fvector3& V, LPCSTR comment /* = 0 */);
+			void w_fvector4(LPCSTR S, LPCSTR L, const Fvector4& V, LPCSTR comment /* = 0 */);
+			void w_s16(LPCSTR S, LPCSTR L, s16 V, LPCSTR comment /* = 0 */);
+			void w_s32(LPCSTR S, LPCSTR L, s32 V, LPCSTR comment /* = 0 */);
+			void w_s64(LPCSTR S, LPCSTR L, s64 V, LPCSTR comment /* = 0 */);
+			void w_s8(LPCSTR S, LPCSTR L, s8 V, LPCSTR comment /* = 0 */);
+			void w_string(LPCSTR S, LPCSTR L, LPCSTR V, LPCSTR comment /* = 0 */);
+			void w_u16(LPCSTR S, LPCSTR L, u16 V, LPCSTR comment /* = 0 */);
+			void w_u32(LPCSTR S, LPCSTR L, u32 V, LPCSTR comment /* = 0 */);
+			void w_u64(LPCSTR S, LPCSTR L, u64 V, LPCSTR comment /* = 0 */);
+			void w_u8(LPCSTR S, LPCSTR L, u8 V, LPCSTR comment /* = 0 */);
+			bool save_as(LPCSTR new_fname /* = 0 */);
+			void save_at_end(bool b);
+			void remove_line(LPCSTR S, LPCSTR L);
+			void set_override_names(bool b);
+			u32 section_count();
+			void set_readonly(bool b);
+			void close();
+
 			DECLARE_SCRIPT_REGISTER_FUNCTION
 };
 
