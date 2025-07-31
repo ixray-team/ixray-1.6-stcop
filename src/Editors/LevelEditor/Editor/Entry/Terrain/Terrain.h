2ef2fbb99bd76ecc22a241a8dc740182ea64d85b
ForserX <sv3nk@yandex.ru>
2025-07-31 11:52:43 +0300

    Add support `Snap List` for `CTerrain`

--- src/Editors/LevelEditor/Editor/Entry/Terrain/Terrain.h
+++ src/Editors/LevelEditor/Editor/Entry/Terrain/Terrain.h
@@ -12,6 +12,7 @@ private:
 	CEditableObject* TerrainObject;
 	bool IsPreview = false;
 	int ScaleY = 100.f;
+	Fbox m_TBBox;
 
 public:
 	CTerrain(LPVOID data, LPCSTR name);
@@ -28,18 +29,26 @@ public:
 
 	virtual void Move(Fvector& amount);
 	virtual void Scale(Fvector& amount);
+	virtual void OnUpdateTransform() override;
 
 	virtual void FillProp(LPCSTR pref, PropItemVec& items) override;
 
-	void OnChangeShader(PropValue* sender);
+	virtual void BoxQuery(SPickQuery& pinf) override;
+	virtual void RayQuery(SPickQuery& pinf) override;
 
-	void OnChangeSurface(PropValue* sender);
-	bool OnChangeHMData(PropValue* sender, int& NewValue);
 
-	virtual void OnUpdateTransform() override;
+	virtual bool GetBox(Fbox& box) override;
+	virtual bool GetUTBox(Fbox& box) override;
 	
 	IC CEditableObject* GetReference()
 	{
 		return TerrainObject;
 	}
+	bool BoxPick(const Fbox& box, SBoxPickInfoVec& pinf);
+
+private:
+	void OnChangeShader(PropValue* sender);
+	void OnChangeSurface(PropValue* sender);
+
+	bool OnChangeHMData(PropValue* sender, int& NewValue);
 };
