9a8981d7888b5363d7d8c11379dbb9ac2c6ef2a1
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-31 11:51:06 +0300

    Add third ammo support

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -1481,25 +1481,32 @@ bool CWeaponMagazined::GetBriefInfo( II_BriefInfo& info )
 		info.fmj_ammo._set("∞");
 		info.ap_ammo._set("∞");
 		info.total_ammo._set("∞");
+		info.third_ammo._set("∞");
 	}
 	else
-	{
-		//GetSuitableAmmoTotal(); //mp = all type
-		int add_ammo_count = 0;
-		for (int i = 0; i < at_size; i++) {
-			if (m_ammoType == i) {
-				xr_sprintf(int_str, "%d", GetAmmoCount(i));
-				info.fmj_ammo = int_str;
-			} else {
-				add_ammo_count += GetAmmoCount(i);
-			}
-		}
-		if (at_size > 1)
-			xr_sprintf(int_str, "%d", add_ammo_count);
-		else
-			xr_sprintf(int_str, "%s", "");
-		info.ap_ammo = int_str;
-	}
+    {
+		//Alundaio: Added third ammo type and cleanup
+        info.fmj_ammo._set("");
+        info.ap_ammo._set("");
+        info.third_ammo._set("");
+
+        if (at_size >= 1)
+        {
+            xr_sprintf(int_str, "%d", GetAmmoCount(0));
+            info.fmj_ammo._set(int_str);
+        }
+        if (at_size >= 2)
+        {
+            xr_sprintf(int_str, "%d", GetAmmoCount(1));
+            info.ap_ammo._set(int_str);
+        }
+        if (at_size >= 3)
+        {
+            xr_sprintf(int_str, "%d", GetAmmoCount(2));
+            info.third_ammo._set(int_str);
+        }
+		//-Alundaio
+    }
 	
 	if ( ae != 0 && m_magazine.size() != 0 )
 	{
