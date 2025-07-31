cb35daa95008fe1ad870f8aa4adf6a512a0b8ea4
ForserX <sv3nk@yandex.ru>
2025-07-31 11:53:11 +0300

    Add large level support

--- src/xrCore/Collision/xrCDB_Collector.cpp
+++ src/xrCore/Collision/xrCDB_Collector.cpp
@@ -80,15 +80,17 @@ namespace CDB
 #pragma warning(pop)
 
 #pragma pack(push,1)
-	struct edge {
-		u32		face_id		: 30;
-		u32		edge_id		: 2;
-		u16		vertex_id0;
-		u16		vertex_id1;
+	struct edge
+	{
+		u32 face_id;
+		u32 edge_id;
+		u32 vertex_id0;
+		u32 vertex_id1;
 	};
 #pragma pack(pop)
 
-	struct sort_predicate {
+	struct sort_predicate
+	{
 		IC	bool	operator()	(const edge &edge0, const edge &edge1) const
 		{
 			if (edge0.vertex_id0 < edge1.vertex_id0)
@@ -107,143 +109,46 @@ namespace CDB
 		}
 	};
 
-	void	Collector::calc_adjacency	(xr_vector<u32>& dest)
+	void Collector::calc_adjacency(xr_vector<u32>& dest)
 	{
-#if 1
-		VERIFY							(faces.size() < 65536);
-		const u32						edge_count = (u32)faces.size()*3;
-		edge							*edges = (edge*)_alloca(edge_count*sizeof(edge));
+		const size_t edge_count = faces.size() * 3;
+		edge* edges = new edge[edge_count];
 
-		edge							*i = edges;
-		xr_vector<TRI>::const_iterator	B = faces.begin(), I = B;
-		xr_vector<TRI>::const_iterator	E = faces.end();
-		for ( ; I != E; ++I) {
-			u32							face_id = u32(I - B);
-
-			(*i).face_id				= face_id;
-			(*i).edge_id				= 0;
-			(*i).vertex_id0				= (u16)(*I).verts[0];
-			(*i).vertex_id1				= (u16)(*I).verts[1];
-			if ((*i).vertex_id0 > (*i).vertex_id1)
-				std::swap				((*i).vertex_id0,(*i).vertex_id1);
-			++i;
-			
-			(*i).face_id				= face_id;
-			(*i).edge_id				= 1;
-			(*i).vertex_id0				= (u16)(*I).verts[1];
-			(*i).vertex_id1				= (u16)(*I).verts[2];
-			if ((*i).vertex_id0 > (*i).vertex_id1)
-				std::swap				((*i).vertex_id0,(*i).vertex_id1);
-			++i;
-			
-			(*i).face_id				= face_id;
-			(*i).edge_id				= 2;
-			(*i).vertex_id0				= (u16)(*I).verts[2];
-			(*i).vertex_id1				= (u16)(*I).verts[0];
-			if ((*i).vertex_id0 > (*i).vertex_id1)
-				std::swap				((*i).vertex_id0,(*i).vertex_id1);
-			++i;
-		}
-
-		std::sort						(edges,edges + edge_count,sort_predicate());
-
-		dest.assign						(edge_count,u32(-1));
-
+		size_t edge_idx = 0;
+		for (size_t face_id = 0; face_id < faces.size(); ++face_id)
 		{
-			edge						*I_ = edges, *J;
-			edge						*E_ = edges + edge_count;
-			for ( ; I_ != E_; ++I_) {
-				if (I_ + 1 == E_)
-					continue;
+			const auto& F = faces[face_id];
 
-				J							= I_ + 1;
-
-				if ((*I_).vertex_id0 != (*J).vertex_id0)
-					continue;
-
-				if ((*I_).vertex_id1 != (*J).vertex_id1)
-					continue;
-
-				dest[(*I_).face_id*3 + (*I_).edge_id]	= (*J).face_id;
-				dest[(*J).face_id*3 + (*J).edge_id]	= (*I_).face_id;
-			}
-		}
-#	if 0
-		xr_vector<u32>	test = dest;
-
-		dest.assign		(faces.size()*3,0xffffffff);
-		// Dumb algorithm O(N^2) :)
-		for (u32 f=0; f<faces.size(); f++)
-		{
-			for (u32 t=0; t<faces.size(); t++)
+			for (u32 edge_id = 0; edge_id < 3; ++edge_id)
 			{
-				if (t==f)	continue;
+				u32 v0 = F.verts[edge_id];
+				u32 v1 = F.verts[(edge_id + 1) % 3];
+				if (v0 > v1)
+					std::swap(v0, v1);
 
-				for (u32 f_e=0; f_e<3; f_e++)
-				{
-					u32 f1	= faces[f].verts[(f_e+0)%3];
-					u32 f2	= faces[f].verts[(f_e+1)%3];
-					if (f1>f2)	std::swap(f1,f2);
-
-					for (u32 t_e=0; t_e<3; t_e++)
-					{
-						u32 t1	= faces[t].verts[(t_e+0)%3];
-						u32 t2	= faces[t].verts[(t_e+1)%3];
-						if (t1>t2)	std::swap(t1,t2);
-
-						if (f1==t1 && f2==t2)
-						{
-							// f.edge[f_e] linked to t.edge[t_e]
-							dest[f*3+f_e]	= t;
-							break;
-						}
-					}
-				}
+				edges[edge_idx++] = { (u32)face_id, edge_id, v0, v1 };
 			}
 		}
 
+		std::sort(edges, edges + edge_count, sort_predicate());
+		dest.assign(edge_count, u32(-1));
+
+		// Сопоставление рёбер между треугольниками
+		for (size_t i = 0; i + 1 < edge_count; ++i)
 		{
-			xr_vector<u32>::const_iterator	I = test.begin();
-			xr_vector<u32>::const_iterator	E = test.end();
-			xr_vector<u32>::const_iterator	J = dest.begin();
-			for ( ; I != E; ++I, ++J) {
-				VERIFY	(*I == *J);
-			}
-		}
-#	endif
-#else
-		dest.assign		(faces.size()*3,0xffffffff);
-		// Dumb algorithm O(N^2) :)
-		for (u32 f=0; f<faces.size(); f++)
-		{
-			for (u32 t=0; t<faces.size(); t++)
+			const edge& e1 = edges[i];
+			const edge& e2 = edges[i + 1];
+
+			if (e1.vertex_id0 == e2.vertex_id0 && e1.vertex_id1 == e2.vertex_id1)
 			{
-				if (t==f)	continue;
-
-				for (u32 f_e=0; f_e<3; f_e++)
-				{
-					u32 f1	= faces[f].verts[(f_e+0)%3];
-					u32 f2	= faces[f].verts[(f_e+1)%3];
-					if (f1>f2)	std::swap(f1,f2);
-
-					for (u32 t_e=0; t_e<3; t_e++)
-					{
-						u32 t1	= faces[t].verts[(t_e+0)%3];
-						u32 t2	= faces[t].verts[(t_e+1)%3];
-						if (t1>t2)	std::swap(t1,t2);
-
-						if (f1==t1 && f2==t2)
-						{
-							// f.edge[f_e] linked to t.edge[t_e]
-							dest[f*3+f_e]	= t;
-							break;
-						}
-					}
-				}
+				dest[e1.face_id * 3 + e1.edge_id] = e2.face_id;
+				dest[e2.face_id * 3 + e2.edge_id] = e1.face_id;
+				++i;
 			}
 		}
-#endif
+		xr_delete(edges);
 	}
+
     IC BOOL similar(TRI& T1, TRI& T2)
     {
         if ((T1.verts[0]==T2.verts[0]) && (T1.verts[1]==T2.verts[1]) && (T1.verts[2]==T2.verts[2]) && (T1.dummy==T2.dummy)) return TRUE;
