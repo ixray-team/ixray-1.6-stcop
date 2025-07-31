db93ded6f4a24f37492a1115fe5ef85856b5f098
ForserX <sv3nk@yandex.ru>
2025-07-31 11:52:32 +0300

    Fix HUD animation playback in FreeMP
    Implemented #106

--- src/xrEngine/IGame_Persistent.cpp
+++ src/xrEngine/IGame_Persistent.cpp
@@ -178,11 +178,16 @@ void IGame_Persistent::UpdateParticles()
 		pInstance->Play(false);
 	}
 
-	for (xr_shared_ptr<CPS_Instance> Part : ps_active_deffer)
+	if (!ps_active_deffer.empty())
+	{
+		ps_active.reserve(ps_active.size() + ps_active_deffer.size());
+
+		for (xr_shared_ptr<CPS_Instance>& Part : ps_active_deffer)
 	{
 		ps_active.push_back(Part);
 	}
 	ps_active_deffer.clear();
+	}
 
 	ps_active.erase(std::remove_if
 	(
