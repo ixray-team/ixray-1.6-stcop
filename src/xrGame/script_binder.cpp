cddd75222d4f60c66f84da29d25de406521870d6
ForserX <sv3nk@yandex.ru>
2025-07-31 11:52:33 +0300

    Fix binders in FMP
    #106

--- src/xrGame/script_binder.cpp
+++ src/xrGame/script_binder.cpp
@@ -133,7 +133,8 @@ void CScriptBinder::net_Destroy		()
 
 void CScriptBinder::set_object		(CScriptBinderObject *object)
 {
-	if (OnServer()) {
+	if (IsGameTypeSingleCompatible())
+	{
 		VERIFY2				(!m_object,"Cannot bind to the object twice!");
 #ifdef _DEBUG
 		Msg					("* Core object %s is binded with the script object",smart_cast<CGameObject*>(this) ? *smart_cast<CGameObject*>(this)->cName() : "");
