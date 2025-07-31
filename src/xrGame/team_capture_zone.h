48e469c7ec7443c96cc8332649b5ce1b15cdc4ea
VodkaNET <apatenokdimafly@gmail.com>
2025-07-31 11:52:40 +0300

    Fix infinite stage build spawn

---
+++ src/xrGame/team_capture_zone.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "GameObject.h"
+#include "../xrEngine/feel_touch.h"
+
+class CTeamCaptureZone : 
+	public CGameObject, 
+	public Feel::Touch 
+{
+protected:
+	u8		m_Team;
+	float	m_Capture;
+	u8		m_CapturedTeam;
+	u8		m_CapturingTeam;
+	bool	m_Conflict;
+	xr_vector<CObject*> v_actor;
+
+public:
+	typedef	CGameObject	inherited;
+
+	CTeamCaptureZone();
+	virtual			~CTeamCaptureZone();
+	virtual void	reinit();
+	virtual BOOL	net_Spawn(CSE_Abstract* DC);
+	virtual void	net_Destroy();
+
+	virtual void	Center(Fvector& C)	const;
+	virtual float	Radius() const;
+
+	virtual void	shedule_Update(u32 dt);
+	void			capture_Update(u32 dt);
+	void			update_Capture(u8 team);
+	virtual void	feel_touch_new(CObject* O);
+	virtual void	feel_touch_delete(CObject* O);
+	virtual BOOL	feel_touch_contact(CObject* O);
+
+	virtual u8		GetZoneTeam() { return m_Team; };
+};
