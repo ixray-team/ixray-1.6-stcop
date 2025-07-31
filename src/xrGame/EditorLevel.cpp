e7089177f3f02aba90355b4e4bda8e64030f3b64
ForserX <sv3nk@yandex.ru>
2025-07-31 11:52:44 +0300

    Cleanup Level Editor code

--- src/xrGame/EditorLevel.cpp
+++ src/xrGame/EditorLevel.cpp
@@ -21,7 +21,7 @@ CLevelEditor::~CLevelEditor()
 {
 }
 extern pureFrame* g_pNetProcessor;
-static void 	build_callback(Fvector* V, int Vcnt, CDB::TRI* T, int Tcnt, void* params)
+static void build_callback(Fvector* V, int Vcnt, CDB::TRI* T, int Tcnt, void* params)
 {
 	g_pGameLevel->Load_GameSpecific_CFORM(T, Tcnt);
 }
@@ -29,7 +29,7 @@ static void 	build_callback(Fvector* V, int Vcnt, CDB::TRI* T, int Tcnt, void* p
 BOOL CLevelEditor::net_Start(LPCSTR op_server, LPCSTR op_client)
 {
 	auto& p = g_pGamePersistent->m_game_params;
-	xr_strcpy(p.m_game_type ,"single");
+	xr_strcpy(p.m_game_type, "single");
 	xr_strcpy(p.m_alife, "alife");
 	xr_strcpy(p.m_new_or_load, "editor");
 	xr_strcpy(p.m_game_or_spawn, "editor");
@@ -39,7 +39,7 @@ BOOL CLevelEditor::net_Start(LPCSTR op_server, LPCSTR op_client)
 	map_data.m_name = "test";
 	m_caServerOptions = op_server;
 	m_caClientOptions = op_client;
-	
+
 	GameDescriptionData game_descr;
 	if ((Server->Connect(m_caServerOptions, game_descr)) != xrServer::ErrNoError)
 	{
@@ -47,7 +47,9 @@ BOOL CLevelEditor::net_Start(LPCSTR op_server, LPCSTR op_client)
 		Msg("! Failed to start server.");
 		return false;
 	}
+
 	Server->SLS_Default();
+
 	if (psNET_direct_connect)
 	{
 		Server->create_direct_client();
@@ -78,7 +80,7 @@ BOOL CLevelEditor::net_Start(LPCSTR op_server, LPCSTR op_client)
 		{
 			R_ASSERT(false);
 		}
-	
+
 #ifdef DEBUG
 		Msg("--- net_start_client3: level_id [%d], level_name[%s], level_version[%s]", 0, level_name, level_ver);
 #endif // #ifdef DEBUG
@@ -97,7 +99,7 @@ BOOL CLevelEditor::net_Start(LPCSTR op_server, LPCSTR op_client)
 		map_data.m_level_geom_crc32 = 0;
 	}
 
-	if (connected_to_server) 
+	if (connected_to_server)
 	{
 		g_pGamePersistent->LoadTitle();
 
@@ -116,6 +118,7 @@ BOOL CLevelEditor::net_Start(LPCSTR op_server, LPCSTR op_client)
 		// *note: release version always has "mt_*" enabled
 		Device.seqFrameMT.Remove(g_pNetProcessor);
 		Device.seqFrame.Remove(g_pNetProcessor);
+
 		if (psDeviceFlags.test(mtNetwork))	Device.seqFrameMT.Add(g_pNetProcessor, REG_PRIORITY_HIGH + 2);
 		else								Device.seqFrame.Add(g_pNetProcessor, REG_PRIORITY_LOW - 2);
 
@@ -125,17 +128,25 @@ BOOL CLevelEditor::net_Start(LPCSTR op_server, LPCSTR op_client)
 			CTimer	timer_sync;	timer_sync.Start();
 			while (!net_isCompleted_Connect())	Sleep(5);
 			Msg("* connection sync: %d ms", timer_sync.GetElapsed_ms());
-			while (!net_isCompleted_Sync()) { ClientReceive(); Sleep(5); }
+
+			while (!net_isCompleted_Sync())
+			{
+				ClientReceive();
+				Sleep(5);
+			}
 		}
 	}
 
 	deny_m_spawn = TRUE;
 	sended_request_connection_data = FALSE;
+
 	{
 		IReader F(nullptr, 0, 0);
 		pLevel = new CInifile(&F);
 	}
-	if (connected_to_server) {
+
+	if (connected_to_server)
+	{
 		// Sync
 		while (!synchronize_map_data()) {}
 
@@ -143,11 +154,9 @@ BOOL CLevelEditor::net_Start(LPCSTR op_server, LPCSTR op_client)
 		{
 			return true;
 		}
-		if (!g_dedicated_server)
-		{
-			g_hud->Load();
-			g_hud->OnConnected();
-		}
+
+		g_hud->Load();
+		g_hud->OnConnected();
 
 		if (game)
 		{
@@ -163,9 +172,11 @@ BOOL CLevelEditor::net_Start(LPCSTR op_server, LPCSTR op_client)
 		net_start_result_total = TRUE;
 
 	}
-	else {
+	else
+	{
 		net_start_result_total = FALSE;
 	}
+
 	if (net_start_result_total)
 	{
 		NET_Packet		NP;
@@ -176,37 +187,32 @@ BOOL CLevelEditor::net_Start(LPCSTR op_server, LPCSTR op_client)
 		if (OnClient() && Server)
 		{
 			Server->SLS_Clear();
-		};
-	};
-	{
-		BulletManager().Clear();
-		BulletManager().Load();
-
-
-		if (net_start_result_total) {
-			if (strstr(Core.Params, "-$")) 
-			{
-				string256				buf, cmd, param;
-				sscanf(strstr(Core.Params, "-$") + 2, "%[^ ] %[^ ] ", cmd, param);
-				xr_strconcat(buf, cmd, " ", param);
-				Console->Execute(buf);
-			}
 		}
-		else {
-			Msg("! Failed to start client. Check the connection or level existance.");
-			return false;
-		}
-
-		if (!g_dedicated_server)
-		{
-			if (CurrentGameUI())
-				CurrentGameUI()->OnConnected();
-		}
-
 	}
 
-	/*spawn_item("wpn_svd", Fvector().set(0, 0, 0), 0, 0);
-	spawn_item("bread", Fvector().set(0, 0, 0), 0, 0);*/
+	BulletManager().Clear();
+	BulletManager().Load();
+
+
+	if (net_start_result_total)
+	{
+		if (strstr(Core.Params, "-$"))
+		{
+			string256 buf, cmd, param;
+			sscanf(strstr(Core.Params, "-$") + 2, "%[^ ] %[^ ] ", cmd, param);
+			xr_strconcat(buf, cmd, " ", param);
+			Console->Execute(buf);
+		}
+	}
+	else
+	{
+		Msg("! Failed to start client. Check the connection or level existance.");
+		return false;
+	}
+
+	if (CurrentGameUI())
+		CurrentGameUI()->OnConnected();
+
 	return TRUE;
 }
 
