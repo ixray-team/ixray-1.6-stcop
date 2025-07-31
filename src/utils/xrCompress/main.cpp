8f81c815cf234ace844e8ecd2785ded1fd53e1ab
VOR <vor@mail.com>
2025-07-31 11:53:06 +0300

    Implement `xrCompress` UI

--- src/utils/xrCompress/main.cpp
+++ src/utils/xrCompress/main.cpp
@@ -2,9 +2,102 @@
 #include "xrCompress.h"
 #include "xrDecompress.h"
 
+
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_sdlrenderer3.h"
+
+#include "xrCompressorUI.h"
+// UNCOMMENT TO ENABLE IMGUI
+//#define WIP_UI
+//
+
 #ifndef MOD_COMPRESS
 extern int ProcessDifference();
 #endif
+
+void SDL_Application()
+{
+	if (!SDL_Init(SDL_INIT_EVENTS) != 0)
+	{
+		printf("Error: SDL_Init(): %s\n", SDL_GetError());
+		return;
+	}
+
+	SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_HIDDEN);
+	g_AppInfo.Window = SDL_CreateWindow("IX-Ray Compress Tools", 1000, 560, window_flags);
+	SDL_Renderer* renderer = SDL_CreateRenderer(g_AppInfo.Window, NULL);
+
+	SDL_SetWindowPosition(g_AppInfo.Window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+	SDL_ShowWindow(g_AppInfo.Window);
+
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	XRay::ImGui::MakeEditorTheme();
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplSDL3_InitForSDLRenderer(g_AppInfo.Window, renderer);
+	ImGui_ImplSDLRenderer3_Init(renderer);
+
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	ImFont* defaultFont = io.Fonts->AddFontDefault();
+
+	ImFontConfig config;
+	config.FontDataOwnedByAtlas = false;
+
+	//gCompilerMode.CompilerIconsFont = io.Fonts->AddFontFromMemoryTTF(IconsFont, sizeof(IconsFont), 16.f, &config, io.Fonts->GetGlyphRangesDefault());
+	//gCompilerMode.ThreadsPerWork = CPU::ID.n_threads - 1;
+
+	bool done = false;
+	while (!done)
+	{
+		// Poll and handle events (inputs, window resize, etc.)
+		// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+		// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+		// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+		// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			ImGui_ImplSDL3_ProcessEvent(&event);
+			if (event.type == SDL_EVENT_QUIT)
+				done = true;
+			if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && event.window.windowID == SDL_GetWindowID(g_AppInfo.Window))
+				done = true;
+		}
+
+		// Start the Dear ImGui frame
+		ImGui_ImplSDLRenderer3_NewFrame();
+		ImGui_ImplSDL3_NewFrame();
+		ImGui::NewFrame();
+
+		{
+			RenderMainUI();
+		}
+
+		// Rendering
+		ImGui::Render();
+
+		SDL_SetRenderDrawColor(renderer, (Uint8)(clear_color.x * 255), (Uint8)(clear_color.y * 255), (Uint8)(clear_color.z * 255), (Uint8)(clear_color.w * 255));
+		SDL_RenderClear(renderer);
+		ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData());
+		SDL_RenderPresent(renderer);
+	}
+
+	// Cleanup
+	ImGui_ImplSDLRenderer3_Shutdown();
+	ImGui_ImplSDL3_Shutdown();
+	ImGui::DestroyContext();
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(g_AppInfo.Window);
+	SDL_Quit();
+
+}
 
 void Unpacker(char* argv[])
 {
@@ -32,6 +125,11 @@ int main(int argc, char* argv[])
 #endif
 
 	Debug._initialize	(false);
+
+#ifdef WIP_UI
+	SDL_Application();
+	return 0;
+#endif
 
 	if (strstr(params, "-unpack"))
 	{
