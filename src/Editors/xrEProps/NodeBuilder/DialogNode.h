6695ef592d806fabbe19c854425e9403ce669c01
ForserX <sv3nk@yandex.ru>
2025-07-31 11:52:39 +0300

    Implement partial `Dialog Editor` functional

--- src/Editors/xrEProps/NodeBuilder/DialogNode.h
+++ src/Editors/xrEProps/NodeBuilder/DialogNode.h
@@ -1,13 +1,19 @@
 #pragma once
-
 #include "Nodes.h"
 
-class XREPROPS_API CDialogNode :
+#include "../../xrCore/FormatParsers/XML/xrXMLParser.h"
+
+class XREPROPS_API CDialogNode final:
 	public INodeUnknown
 {
 public:
 	CDialogNode(const xr_string Name);
-	void Draw() override;
+
+	virtual void Draw() override;
+	virtual void AddContactLink(const xr_string& Name, bool IsOut = false) override;
+	virtual void MakeOutNode(INodeUnknown* Node) override;
+
+	void ValidateNodes(shared_str& Data, const char* RawName);
 
 public:
 	shared_str HasInfo;
@@ -16,4 +22,21 @@ public:
 	shared_str Action;
 	shared_str Precondition;
 	shared_str Text;
+
+	bool IsFinal = false;
+
+	XML_NODE* IsFinalNode = nullptr;
+	xr_vector<XML_NODE*> HasInfoNode;
+	xr_vector<XML_NODE*> DontHasInfoNode;
+	xr_vector<XML_NODE*> GiveInfoNode;
+
+	XML_NODE* ActionNode = nullptr;
+	XML_NODE* PreconditionNode = nullptr;
+	XML_NODE* TextNode = nullptr;
+
+	XML_NODE* ParentNode = nullptr;
+
+private:
+	void RenderItemString(const char* RawName, const char* Name, shared_str& Data, XML_NODE*& Node, size_t Size);
+	void RenderItemString(const char* RawName, const char* Name, shared_str& Data, xr_vector<XML_NODE*>& Node, size_t Size);
 };
