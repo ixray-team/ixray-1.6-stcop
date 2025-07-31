3c68aa6da3b325c605c7ff8ea4e2bb879140f61a
v2v3v4 <terran.erre@mail.ru>
2025-07-31 11:52:28 +0300

    Replace `dynamic_cast` with `fast_dynamic_cast` in LuaBind

--- sdk/include/luabind/back_reference.hpp
+++ sdk/include/luabind/back_reference.hpp
@@ -21,7 +21,7 @@
 // OR OTHER DEALINGS IN THE SOFTWARE.
 
 #pragma once
-
+#include "../fast_dynamic_cast/fast_dynamic_cast.hpp"
 #include <luabind/lua_include.hpp>
 #include <luabind/wrapper_base.hpp>
 
@@ -41,12 +41,12 @@ namespace luabind {
     {     
        static wrap_base const* extract(T const* ptr)
         {
-            return dynamic_cast<wrap_base const*>(get_pointer(*ptr));
+            return fast_dynamic_cast<wrap_base const*>(get_pointer(*ptr));
         }
 
         static wrap_base* extract(T* ptr)
         {
-            return dynamic_cast<wrap_base*>(get_pointer(*ptr));
+            return fast_dynamic_cast<wrap_base*>(get_pointer(*ptr));
         }
     };
 
