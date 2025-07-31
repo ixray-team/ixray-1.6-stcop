0eda5df8ac691e55f65c22df1aaaafa67ee462c0
Shtrecker <gogels235@gmail.com>
2025-07-31 11:52:38 +0300

    Add base movement animations

--- src/xrGame/ActorAnimation.cpp
+++ src/xrGame/ActorAnimation.cpp
@@ -426,14 +426,15 @@ void CActor::g_SetAnimation( u32 mstate_rl )
 	}
 
 	if (this == Level().CurrentViewEntity())
-	{	
-		if ((mstate_rl&mcSprint) != (mstate_old&mcSprint))
+	{
+		auto TestState = [&](u32 State)
 		{
-			g_player_hud->OnMovementChanged(mcSprint);
-		}else
-		if ((mstate_rl&mcAnyMove) != (mstate_old&mcAnyMove))
+			return ((mstate_rl & State) != (mstate_old & State));
+		};
+
+		if (TestState(mcSprint) || TestState(mcAnyMove) || TestState(mcAccel) || TestState(mcCrouch))
 		{
-			g_player_hud->OnMovementChanged(mcAnyMove);
+			g_player_hud->OnMovementChanged(ACTOR_DEFS::EMoveCommand(mstate_rl));
 		}
 	};
 
