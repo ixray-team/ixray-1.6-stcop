3b7b235f845de837f5dfc318088a655f4fc53a2b
ForserX <sv3nk@yandex.ru>
2025-07-31 11:53:05 +0300

    Improve SQL database generation

--- src/utils/SQLGen/ImGuiSpinner.cpp
+++ src/utils/SQLGen/ImGuiSpinner.cpp
@@ -4,9 +4,11 @@
 #include<algorithm>
 #include <cmath>
 #include"imgui_internal.h"
-namespace ImGui {
 
-	bool Spinner(const char* label, float radius, int thickness, const ImU32& color) {
+namespace ImGui
+{
+	bool Spinner(const char* label, float radius, int thickness, const ImU32& color)
+	{
 		ImGuiWindow* window = GetCurrentWindow();
 		if (window->SkipItems)
 			return false;
@@ -34,19 +36,22 @@ namespace ImGui {
 
 		const ImVec2 centre = ImVec2(pos.x + radius, pos.y + radius + style.FramePadding.y);
 
-		for (int i = 0; i < num_segments; i++) {
+		for (int i = 0; i < num_segments; i++)
+		{
 			const float a = a_min + ((float)i / (float)num_segments) * (a_max - a_min);
 			window->DrawList->PathLineTo(ImVec2(centre.x + ImCos(a + g.Time * 8) * radius,
 				centre.y + ImSin(a + g.Time * 8) * radius));
 		}
 
 		window->DrawList->PathStroke(color, false, thickness);
+		return true;
 	}
-	void LoadingIndicatorCircle(const char* label, const float indicator_radius,
-		const ImVec4& main_color, const ImVec4& backdrop_color,
-		const int circle_count, const float speed) {
+
+	void LoadingIndicatorCircle(const char* label, float indicator_radius, const ImVec4& main_color, const ImVec4& backdrop_color, int circle_count, float speed)
+	{
 		ImGuiWindow* window = GetCurrentWindow();
-		if (window->SkipItems) {
+		if (window->SkipItems)
+		{
 			return;
 		}
 
@@ -57,24 +62,28 @@ namespace ImGui {
 		const float circle_radius = indicator_radius / 15.0f;
 		const float updated_indicator_radius = indicator_radius - 4.0f * circle_radius;
 		const ImRect bb(pos, ImVec2(pos.x + indicator_radius * 2.0f, pos.y + indicator_radius * 2.0f));
+
 		ItemSize(bb);
-		if (!ItemAdd(bb, id)) {
+		if (!ItemAdd(bb, id))
+		{
 			return;
 		}
+
 		const float t = g.Time;
 		const auto degree_offset = 2.0f * IM_PI / circle_count;
-		for (int i = 0; i < circle_count; ++i) {
+		for (int i = 0; i < circle_count; ++i) 
+		{
 			const auto x = updated_indicator_radius * std::sin(degree_offset * i);
 			const auto y = updated_indicator_radius * std::cos(degree_offset * i);
 			const auto growth = std::max(0.0f, std::sin(t * speed - i * degree_offset));
+
 			ImVec4 color;
 			color.x = main_color.x * growth + backdrop_color.x * (1.0f - growth);
 			color.y = main_color.y * growth + backdrop_color.y * (1.0f - growth);
 			color.z = main_color.z * growth + backdrop_color.z * (1.0f - growth);
 			color.w = 1.0f;
-			window->DrawList->AddCircleFilled(ImVec2(pos.x + indicator_radius + x,
-				pos.y + indicator_radius - y),
-				circle_radius + growth * circle_radius, GetColorU32(color));
+
+			window->DrawList->AddCircleFilled(ImVec2(pos.x + indicator_radius + x, pos.y + indicator_radius - y), circle_radius + growth * circle_radius, GetColorU32(color));
 		}
 	}
 }
