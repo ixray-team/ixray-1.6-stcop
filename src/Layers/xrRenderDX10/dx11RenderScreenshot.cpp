636d5b1099c9f19c0d2d79108cd592475ce5cc59
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-31 11:50:58 +0300

    Implement `r__screenshot_format` console command
    Arguments:
    ss_jpg - Use .jpg format for screenshots
    ss_tga - Use .tga format for screenshots
    ss_png - Use .png format for screenshots

--- src/Layers/xrRenderDX10/dx11RenderScreenshot.cpp
+++ src/Layers/xrRenderDX10/dx11RenderScreenshot.cpp
@@ -76,25 +76,37 @@ void CRender::ScreenshotImpl(ScreenshotMode mode, LPCSTR name, CMemoryWriter* me
     {
         string64 t_stemp;
         string_path buf;
-        xr_sprintf(buf, sizeof(buf), "ss_%s_%s_(%s).jpg", Core.UserName, timestamp(t_stemp), (g_pGameLevel) ? g_pGameLevel->name().c_str() : "mainmenu");
 
         auto scratchImage = std::make_unique<ScratchImage>();
         auto saved = std::make_unique<Blob>();
         CaptureTexture(RDevice, RContext, pSrcTexture, *scratchImage);
-        SaveToWICMemory(*scratchImage->GetImage(0, 0, 0), WIC_FLAGS::WIC_FLAGS_NONE, GUID_ContainerFormatJpeg, *saved);
-
-        auto fs = FS.w_open("$screenshots$", buf); R_ASSERT(fs);
-        fs->w(saved->GetBufferPointer(), (u32)saved->GetBufferSize());
-        FS.w_close(fs);
-
-        // hq
-        if (Core.ParamsData.test(ECoreParams::ss_tga))
+        if (ps_screenshot_format == 0) // jpg screenshots - default one
         {
-            xr_sprintf(buf, sizeof(buf), "ssq_%s_%s_(%s).tga", Core.UserName, timestamp(t_stemp), (g_pGameLevel) ? g_pGameLevel->name().c_str() : "mainmenu");
+            xr_sprintf(buf, sizeof(buf), "ss_%s_%s_(%s).jpg", Core.UserName, timestamp(t_stemp), (g_pGameLevel) ? g_pGameLevel->name().c_str() : "mainmenu");
+            SaveToWICMemory(*scratchImage->GetImage(0, 0, 0), WIC_FLAGS::WIC_FLAGS_NONE, GUID_ContainerFormatJpeg, *saved);
+
+            auto fs = FS.w_open("$screenshots$", buf); R_ASSERT(fs);
+            fs->w(saved->GetBufferPointer(), (u32)saved->GetBufferSize());
+            FS.w_close(fs);
+
+        }
+        else if (ps_screenshot_format == 1) // tga screenshots
+        {
+            xr_sprintf(buf, sizeof(buf), "ss_%s_%s_(%s).tga", Core.UserName, timestamp(t_stemp), (g_pGameLevel) ? g_pGameLevel->name().c_str() : "mainmenu");
 
             SaveToTGAMemory(*scratchImage->GetImage(0, 0, 0), TGA_FLAGS::TGA_FLAGS_NONE, *saved);
 
-            fs = FS.w_open("$screenshots$", buf); R_ASSERT(fs);
+            auto fs = FS.w_open("$screenshots$", buf); R_ASSERT(fs);
+            fs->w(saved->GetBufferPointer(), (u32)saved->GetBufferSize());
+            FS.w_close(fs);
+        }
+        else if (ps_screenshot_format == 2) // png screenshots
+        {
+            xr_sprintf(buf, sizeof(buf), "ss_%s_%s_(%s).png", Core.UserName, timestamp(t_stemp), (g_pGameLevel) ? g_pGameLevel->name().c_str() : "mainmenu");
+
+            SaveToWICMemory(*scratchImage->GetImage(0, 0, 0), WIC_FLAGS::WIC_FLAGS_NONE, GUID_ContainerFormatPng, *saved);
+
+            auto fs = FS.w_open("$screenshots$", buf); R_ASSERT(fs);
             fs->w(saved->GetBufferPointer(), (u32)saved->GetBufferSize());
             FS.w_close(fs);
         }
