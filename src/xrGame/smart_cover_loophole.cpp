f89307253b118cdf180ba398ab9aa5aeeebc3e16
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-31 11:52:34 +0300

    Improve smart cover system
    For Clear Sky compatibility

--- src/xrGame/smart_cover_loophole.cpp
+++ src/xrGame/smart_cover_loophole.cpp
@@ -60,14 +60,23 @@ loophole::loophole	(luabind::object const &description) :
 	else
 		m_fov_direction.normalize	();
 
-	m_danger_fov_direction	= parse_fvector(description, "danger_fov_direction");
-	if (m_danger_fov_direction.square_magnitude() < EPS_L) {
+	// XXX: check for m_wVersion from CSE_SmartCover
+	Fvector dangerFovDirection;
+	if (parse_fvector(description, "danger_fov_direction", dangerFovDirection))
+	{
+		m_danger_fov_direction = dangerFovDirection;
+		if (m_danger_fov_direction.square_magnitude() < EPS_L) {
 		Msg				("! danger fov direction for loophole %s is setup incorrectly", m_id.c_str());
 		m_danger_fov_direction.set(0.f, 0.f, 1.f);
 	}
 	else
 		m_danger_fov_direction.normalize	();
-
+	}
+	else
+	{
+		Msg("~ missing danger fov direction for loophole %s", m_id.c_str());
+		m_danger_fov_direction.set(0.f, 0.f, 1.f);
+	}
 	m_enter_direction	= parse_fvector(description, "enter_direction");
 
 	if (m_enter_direction.square_magnitude() < EPS_L) {
@@ -104,7 +113,12 @@ loophole::loophole	(luabind::object const &description) :
 	fill_transitions(transitions);
 
 	m_fov			= deg2rad(parse_float(description, "fov", 0.f, 360.f));
-	m_danger_fov	= deg2rad(parse_float(description, "danger_fov", 0.f, 360.f));
+
+	// XXX: check for m_wVersion from CSE_SmartCover
+	float dangerFov = 0.f;
+	if (parse_float(dangerFov, description, "danger_fov", 0.f, 360.f))
+		m_danger_fov = deg2rad(dangerFov);
+
 	m_range			= parse_float(description, "range", 0.f);
 }
 
