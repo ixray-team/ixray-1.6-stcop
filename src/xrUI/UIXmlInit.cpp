a387b92161dec8303b0e6c87919d260b05f8d079
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-31 11:52:46 +0300

    Add frame window mode for buttons

--- src/xrUI/UIXmlInit.cpp
+++ src/xrUI/UIXmlInit.cpp
@@ -407,7 +407,7 @@ bool CUIXmlInit::Init3tButton(CUIXml& xml_doc, LPCSTR path, int index, CUI3tButt
 	bool ValidNode = xml_doc.NavigateToNode(path, index);
 	R_ASSERT4(ValidNode, "XML node not found", path, xml_doc.m_xml_file_name);
 
-	pWnd->m_frameline_mode = (xml_doc.ReadAttribInt(path, index, "frame_mode", 0) == 1)? true : false;
+	pWnd->m_frameline_mode = (CUI3tButton::EFrameMode)xml_doc.ReadAttribInt(path, index, "frame_mode", 0);
 
 	pWnd->vertical = (xml_doc.ReadAttribInt(path, index, "vertical", 0) == 1)? true : false;
 
@@ -1032,6 +1032,10 @@ bool CUIXmlInit::InitMultiTexture(CUIXml &xml_doc, LPCSTR path, int index, CUI3t
 			pWnd->m_back_frameline->InitState(S_Enabled, texture.c_str()); 
 			pWnd->m_back_frameline->Get(S_Enabled)->SetHorizontal(!(pWnd->vertical));
 		}
+		else if (pWnd->m_back_framewindow)
+		{
+			pWnd->m_back_framewindow->InitState(S_Enabled, texture.c_str());
+		}
 		success = true;
 	}
 
@@ -1047,6 +1051,10 @@ bool CUIXmlInit::InitMultiTexture(CUIXml &xml_doc, LPCSTR path, int index, CUI3t
 		{ 
 			pWnd->m_back_frameline->InitState(S_Touched, texture.c_str()); 
 			pWnd->m_back_frameline->Get(S_Touched)->SetHorizontal(!(pWnd->vertical));
+		}
+		else if (pWnd->m_back_framewindow)
+		{
+			pWnd->m_back_framewindow->InitState(S_Touched, texture.c_str());
 		}
 		success = true;
 	}
@@ -1064,6 +1072,10 @@ bool CUIXmlInit::InitMultiTexture(CUIXml &xml_doc, LPCSTR path, int index, CUI3t
 			pWnd->m_back_frameline->InitState(S_Disabled, texture.c_str()); 
 			pWnd->m_back_frameline->Get(S_Disabled)->SetHorizontal(!(pWnd->vertical));
 		}
+		else if (pWnd->m_back_framewindow)
+		{
+			pWnd->m_back_framewindow->InitState(S_Disabled, texture.c_str());
+		}
 		success = true;
 	}
 
@@ -1079,6 +1091,10 @@ bool CUIXmlInit::InitMultiTexture(CUIXml &xml_doc, LPCSTR path, int index, CUI3t
 		{ 
 			pWnd->m_back_frameline->InitState(S_Highlighted, texture.c_str()); 
 			pWnd->m_back_frameline->Get(S_Highlighted)->SetHorizontal(!(pWnd->vertical));
+		}
+		else if (pWnd->m_back_framewindow)
+		{
+			pWnd->m_back_framewindow->InitState(S_Highlighted, texture.c_str());
 		}
 		success = true;
 	}
