c797d37449faa12870ae652d9ae22b66608b5dff
ForserX <sv3nk@yandex.ru>
2025-07-31 11:52:51 +0300

    Implement vanilla engine compatibility workaround

--- src/utils/xrAI/compiler_load.cpp
+++ src/utils/xrAI/compiler_load.cpp
@@ -302,15 +302,24 @@ void xrLoad(LPCSTR name, bool draft_mode)
 		H.size_y = 1.f;
 		H.aabb = LevelBB;
 
-		constexpr u32 InvalidNode_v1 = 0x00ffffff;
 		typedef u32 NodeLink;
+#ifdef IXRAY_AI_OLD_FORMAT
 		for (u32 i = 0; i < N_; i++)
 		{
-			NodeLink id;
-			u16 pl;
-			SNodePositionOld _np;
-			NodePosition np;
-
+			NodeLink id{};
+			for (int j = 0; j < 4; ++j)
+			{
+				F->r(&id, 3);
+				id = id & 0x00ffffff;
+				if (id == InvalidNode)
+					id = InvalidNode;
+				g_nodes[i].n[j] = id;
+			}
+#else
+		constexpr u32 InvalidNode_v1 = 0x00ffffff;
+		for (u32 i = 0; i < N_; i++)
+		{
+			NodeLink id{};
 			if (version == 1)
 			{
 				for (int j = 0; j < 4; ++j)
@@ -330,13 +339,15 @@ void xrLoad(LPCSTR name, bool draft_mode)
 					g_nodes[i].n[j] = id;
 				}
 			}
+#endif
 
-			pl = F->r_u16();
+			u16 pl = F->r_u16();
+			NodePosition np{};
+			SNodePositionOld _np;
 			pvDecompress(g_nodes[i].Plane.n, pl);
 			F->r(&_np, sizeof(_np));
 			CNodePositionConverter(_np, H, np);
 			g_nodes[i].Pos = vertex_position(np, LevelBB, g_params);
-
 			g_nodes[i].Plane.build(g_nodes[i].Pos, g_nodes[i].Plane.n);
 		}
 
