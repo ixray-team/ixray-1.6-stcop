9090d60a0dd44e4c739250d89b17e90f1bd069a5
DimaKuzmin <scorpion261096@gmail.com>
2025-07-31 11:52:33 +0300

    Fix Upgrades Debug GameSP

--- src/xrGame/console_commands.cpp
+++ src/xrGame/console_commands.cpp
@@ -1505,16 +1505,12 @@ public:
 	CCC_InvUpgradesCurItem(LPCSTR N) : IConsole_Command(N) { bEmptyArgsHandled = TRUE; };
 	virtual void Execute(LPCSTR args)
 	{
-		if (!g_pGameLevel)
+		if (!g_pGameLevel || !CurrentGameUI())
 		{
 			return;
 		}
-		CUIGameSP* ui_game_sp = smart_cast<CUIGameSP*>(CurrentGameUI());
-		if (!ui_game_sp)
-		{
-			return;
-		}
-		PIItem item = ui_game_sp->ActorMenu().get_upgrade_item();
+ 
+		PIItem item = CurrentGameUI()->ActorMenu().get_upgrade_item();
 		if (item)
 		{
 			item->log_upgrades();
@@ -1532,18 +1528,14 @@ public:
 	CCC_InvDropAllItems(LPCSTR N) : IConsole_Command(N) { bEmptyArgsHandled = TRUE; };
 	virtual void Execute(LPCSTR args)
 	{
-		if (!g_pGameLevel)
+		if (!g_pGameLevel || !CurrentGameUI())
 		{
 			return;
 		}
-		CUIGameSP* ui_game_sp = smart_cast<CUIGameSP*>(CurrentGameUI());
-		if (!ui_game_sp)
-		{
-			return;
-		}
+  
 		int d = 0;
 		sscanf(args, "%d", &d);
-		if (ui_game_sp->ActorMenu().DropAllItemsFromRuck(d == 1))
+		if (CurrentGameUI()->ActorMenu().DropAllItemsFromRuck(d == 1))
 		{
 			Msg("- All items from ruck of Actor is dropping now.");
 		}
