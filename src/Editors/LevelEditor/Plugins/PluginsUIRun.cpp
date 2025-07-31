7b2694ec77cf205169ae0e5360cb58b0427a8d83
ForserX <sv3nk@yandex.ru>
2025-07-31 11:52:30 +0300

    Rework Level Editor toolbar
    * Add shortcat to buttons
    * Cleanup code

--- src/Editors/LevelEditor/Plugins/PluginsUIRun.cpp
+++ src/Editors/LevelEditor/Plugins/PluginsUIRun.cpp
@@ -6,6 +6,7 @@ CPluginUIRun::CPluginUIRun(IPluginBase* Plug):
 	InputPlugin(Plug)
 {
 	bOpen = true;
+	OpenImage = EDevice->Resources->_CreateTexture("ed\\bar\\open_gamedata");
 }
 
 void CPluginUIRun::Draw()
@@ -22,6 +23,14 @@ void CPluginUIRun::Draw()
 
 			ImGui::Text(Desc.c_str());
 			ImGui::InputText(("##" + Arg).c_str(), InputPlugin->InputArgsValues[Arg], 256);
+
+			ImGui::SameLine();
+			if (ImGui::ImageButton(("##Open" + Arg).c_str(), OpenImage->pSurface, { 10, 14 }))
+			{
+				xr_string TempPath;
+				EFS.GetOpenName("$fs_root$", TempPath, false, nullptr, -1, "*.*");
+				xr_strcpy(InputPlugin->InputArgsValues[Arg], TempPath.c_str());
+			}
 		}
 
 		if (ImGui::Button("Run"))
