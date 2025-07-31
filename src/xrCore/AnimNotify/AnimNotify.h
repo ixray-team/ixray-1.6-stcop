6ae1ace2ec08e4c19461edd9a35e3cae67e96a8d
mnelenpridumivat <ragrind@mail.ru>
2025-07-31 11:52:45 +0300

    Implement different animation notify classes

--- src/xrCore/AnimNotify/AnimNotify.h
+++ src/xrCore/AnimNotify/AnimNotify.h
@@ -1,19 +1,21 @@
 #pragma once
+#include "AnimNotifyTypes.h"
+
+class IRenderVisual;
 
 class XRCORE_API IAnimNotifyHandler
 {
 public:
     virtual ~IAnimNotifyHandler() = default;
-    virtual void TriggerGiveInfo(shared_str Info) = 0;
-    virtual void TriggerDisableInfo(shared_str Info) = 0;
-    virtual void TriggerFunctor(shared_str Func) = 0;
-    virtual void TriggerNotify(shared_str Name) = 0;
+    virtual void TriggerNotify(IAnimNotifyMessage* notify) = 0;
 
     virtual void Update() = 0;
 
     static void SetHandler(IAnimNotifyHandler* pHandler);
     static bool IsValid(){return Handler != nullptr;}
     static IAnimNotifyHandler& Get();
+
+    virtual IAnimNotify* ConstructNotify(const EAnimNotifyType type) = 0;
     
 private:
     static IAnimNotifyHandler* Handler;
---
+++ src/xrGame/AnimNotify/AnimNotifyDisableInfo.cpp
@@ -0,0 +1,26 @@
+#include "stdafx.h"
+#include "AnimNotifyDisableInfo.h"
+
+#include "alife_registry_container.h" // without this not compiles
+
+#include "ai_space.h"
+#include "alife_registry_container_composition.h"
+#include "alife_simulator.h"
+#include "InfoPortionDefs.h"
+#include "Level.h"
+
+void CAnimNotifyDisableInfo::Construct(CInifile ini, LPCSTR sect)
+{
+    Info = ini.r_string(sect, "Info");
+}
+
+void CAnimNotifyDisableInfo::Execute(IRenderVisual* visual, u16 bone_id)
+{
+    KNOWN_INFO_VECTOR *known_info = ai().get_alife()->registry(info_portions).object(0, true);
+    VERIFY(known_info);
+    if (auto It = std::find_if(known_info->begin(), known_info->end(), CFindByIDPred(Info));
+        It != known_info->end())
+    {
+        known_info->erase(It);
+    }
+}
