124825fa8e5cf6a9aa66c182cb5b5cc7b1c9026a
Dimakuzmin <scorpion261096@gmail.com>
2025-07-31 11:53:04 +0300

    Disable Lighting for testing

--- src/utils/xrForms/CompilersUI.h
+++ src/utils/xrForms/CompilersUI.h
@@ -23,11 +23,12 @@ struct CompilersMode
 	bool DO = false;
 	bool LC = false;
 
+	bool LC_BackingDisabled = false;
 	bool LC_SaveOFG = false;
 	bool LC_GI = false;
 	bool LC_NoSun = false;
 	bool LC_NoSMG = true;
-	bool LC_Noise = false;
+	bool LC_Noise = true;
 	bool LC_Tess = true;
 	bool LC_SkipInvalidFaces = true;
 	bool LC_tex_rgba = false;
