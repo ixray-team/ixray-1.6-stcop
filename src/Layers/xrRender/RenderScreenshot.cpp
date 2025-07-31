a20821debc3c6d8b0051f2062b4e0b51fcbf603d
Drombeys <dima.zolotov02@gmail.com>
2025-07-31 11:29:50 +0300

    Fix texture preview not updating on subsequent game saves
    Co-authored-by: Hozar2002 <hoz2002team@gmail.com>

--- src/Layers/xrRender/RenderScreenshot.cpp
+++ src/Layers/xrRender/RenderScreenshot.cpp
@@ -6,6 +6,17 @@ int SM_FOR_SEND_HEIGHT = 480;
 
 void CRender::Screenshot(ScreenshotMode mode, LPCSTR name) {
     ScreenshotImpl(mode, name, nullptr);
+
+    if (mode == SM_FOR_GAMESAVE && name && name[0])
+    {
+        xr_string fixName = xr_string(name).Split('\\').back();
+
+        ref_texture pTexture{};
+        pTexture.create(fixName.c_str());
+
+        pTexture->Unload();
+        pTexture->Load();
+    }
 }
 
 void CRender::Screenshot(ScreenshotMode mode, CMemoryWriter& memory_writer) {
