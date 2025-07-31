95843eea8e4c6185f3f9e0e9e60aec54d91b96d1
Drombeys <dima.zolotov02@gmail.com>
2025-07-31 11:51:07 +0300

    Add extensions for table and string in Lua
    Compatibility layer with Call of Chernobyl
    
    Co-Authored-By: Andrey Voitishin <108185+avoitishin@users.noreply.github.com>

--- src/xrScripts/lua_ext.cpp
+++ src/xrScripts/lua_ext.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include <random>
 #include "script_engine.h"
 
 #include "luapanda.h"
@@ -15,6 +16,168 @@ extern "C"
 }
 
 #include "lua_ext.h"
+
+/******************** STRING ********************/
+int str_trim(lua_State* L)
+{
+    const char* front;
+    const char* end;
+    size_t size;
+    front = luaL_checklstring(L, 1, &size);
+    end = &front[size - 1];
+    for (; size && isspace(*front); size--, front++)
+        ;
+    for (; size && isspace(*end); size--, end--)
+        ;
+    lua_pushlstring(L, front, (size_t)(end - front) + 1);
+    return 1;
+}
+
+int str_trim_l(lua_State* L)
+{
+    const char* front;
+    const char* end;
+    size_t size;
+    front = luaL_checklstring(L, 1, &size);
+    end = &front[size - 1];
+    for (; size && isspace(*front); size--, front++)
+        ;
+    lua_pushlstring(L, front, (size_t)(end - front) + 1);
+    return 1;
+}
+
+int str_trim_r(lua_State* L)
+{
+    const char* front;
+    const char* end;
+    size_t size;
+    front = luaL_checklstring(L, 1, &size);
+    end = &front[size - 1];
+    for (; size && isspace(*end); size--, end--)
+        ;
+    lua_pushlstring(L, front, (size_t)(end - front) + 1);
+    return 1;
+}
+
+int str_trim_w(lua_State* L)
+{
+    int i = 0, d, n;
+    const char* s = luaL_checkstring(L, 1);
+
+    while (s[i] == ' ')
+        i++;
+    n = i;
+    while (s[i] != ' ' && s[i])
+        i++;
+    d = i - n;
+    lua_pushlstring(L, s + n, d);
+    return 1;
+}
+
+const luaL_Reg strlib[] = {
+    {"trim", str_trim}, {"trim_l", str_trim_l}, {"trim_r", str_trim_r}, {"trim_w", str_trim_w}, {NULL, NULL} };
+
+int open_string(lua_State* L)
+{
+    luaL_openlib(L, LUA_STRLIBNAME, strlib, 0);
+    return 0;
+}
+/******************** STRING END ********************/
+
+/******************** MATH ********************/
+std::mt19937 intgen(std::random_device{}());
+
+int gen_random_in_range(int a1, int a2)
+{
+    std::uniform_int_distribution<int> dist(a1, a2);
+    return dist(intgen);
+}
+/******************** MATH END ********************/
+
+/******************** TABLE ********************/
+inline DWORD C_get_size(lua_State* L)
+{
+    int i = 0;
+    lua_settop(L, 2);
+    while (lua_next(L, 1))
+    {
+        ++i;
+        lua_pop(L, 1);
+    }
+    return i;
+}
+
+int tab_keys(lua_State* L)
+{
+    int i = 1;
+    luaL_checktype(L, 1, LUA_TTABLE);
+    lua_newtable(L);
+    lua_pushnil(L);
+    while (lua_next(L, 1) != 0)
+    {
+        lua_pushinteger(L, i);
+        ++i;
+        lua_pushvalue(L, -3);
+        lua_settable(L, 2);
+        lua_pop(L, 1);
+    }
+    return 1;
+}
+
+int tab_values(lua_State* L)
+{
+    int i = 1;
+    luaL_checktype(L, 1, LUA_TTABLE);
+    lua_newtable(L);
+    lua_pushnil(L);
+    while (lua_next(L, 1) != 0)
+    {
+        lua_pushinteger(L, i);
+        ++i;
+        lua_pushvalue(L, -2);
+        lua_settable(L, 2);
+        lua_pop(L, 1);
+    }
+    return 1;
+}
+
+int get_size(lua_State* L)
+{
+    luaL_checktype(L, 1, LUA_TTABLE);
+    lua_pushinteger(L, C_get_size(L));
+    return 1;
+}
+
+int get_random(lua_State* L)
+{
+    int i = C_get_size(L);
+    int j = gen_random_in_range(1, i);
+    i = 0;
+    luaL_checktype(L, 1, LUA_TTABLE);
+    lua_settop(L, 2);
+    while (lua_next(L, 1))
+    {
+        ++i;
+        if (i == j)
+        {
+            lua_pushvalue(L, -2);
+            lua_pushvalue(L, -2);
+            return 2;
+        }
+        lua_pop(L, 1);
+    }
+    return 0;
+}
+
+const luaL_Reg tab_funcs[] = {
+    {"keys", tab_keys}, {"values", tab_values}, {"size", get_size}, {"random", get_random}, {NULL, NULL} };
+
+int open_table(lua_State* L)
+{
+    luaL_openlib(L, LUA_TABLIBNAME, tab_funcs, 0);
+    return 0;
+}
+/******************** TABLE END ********************/
 
 bool LoadScriptToGlobal(lua_State* L, const char* name, bool KernelScript)
 {
@@ -70,6 +233,8 @@ bool LoadScriptToGlobal(lua_State* L, const char* name, bool KernelScript)
 void lua_init_ext(lua_State* L)
 {
 	luaopen_marshal(L);
+    open_string(L);
+    open_table(L);
 	luaopen_lfs(L);
 	luaopen_LuaXML_lib(L);
 	luaopen_utf8(L);
