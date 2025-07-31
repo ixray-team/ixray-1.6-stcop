c6773c59ed706df010a0985bab7f8c9bbb0560c0
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-31 11:52:34 +0300

    Fix remaining uses

--- src/xrGame/eatable_item.h
+++ src/xrGame/eatable_item.h
@@ -47,7 +47,7 @@ public:
 	IC bool CanDelete() const { return m_bRemoveAfterUse == 1; };
 	IC bool CanConsumeCharge() const { return m_bConsumeChargeOnUse == 1; };
 	IC u8 GetMaxUses() { return m_iMaxUses; };
-	IC u8 GetRemainingUses() const { return m_iPortionsMarker / m_eat_condition; };
+	IC u8 GetRemainingUses() const { return (u8)roundf(((float)m_iMaxUses) * m_fCondition); };
 	IC void SetRemainingUses(u8 value) { m_fCondition = ((float)value / (float)m_iMaxUses); clamp(m_fCondition, 0.f, 1.f); };
 
 	DECLARE_SCRIPT_REGISTER_FUNCTION
