d04828af726c9bc395e1f9e8d6aec8d21ad6b8be
Graff46 <macter46@yandex.ru>
2025-07-31 11:53:02 +0300

    Механик через скрипты
    св-во mechanic у клиентских НПС читаем/устанавливаем bool. Делает/не делает НПС механиком

--- src/xrGame/script_game_object.h
+++ src/xrGame/script_game_object.h
@@ -816,6 +816,8 @@ public:
 			void				unlock_door_for_npc						();
 			bool				is_door_blocked_by_npc					() const;
 			bool				is_weapon_going_to_be_strapped			( CScriptGameObject const* object ) const;
+			const bool			getMechanic								() const;
+			void				setMechanic								(bool cond);
 
 			_DECLARE_FUNCTION14(cast_GameObject, CScriptGameObject);
 			_DECLARE_FUNCTION14(cast_Car, CCar);
