db3c7d2b84c8f22681408f2cf02a458cb25707da
Drombeys <dima.zolotov02@gmail.com>
2025-07-31 11:52:39 +0300

    Заглушки для худ аниматора

--- src/xrGame/script_game_object_script2.cpp
+++ src/xrGame/script_game_object_script2.cpp
@@ -402,5 +402,12 @@ class_<CScriptGameObject> script_register_game_object1(class_<CScriptGameObject>
 		.def("is_door_blocked_by_npc",			&CScriptGameObject::is_door_blocked_by_npc)
 		.def("is_weapon_going_to_be_strapped",	&CScriptGameObject::is_weapon_going_to_be_strapped)
 
+
+	.def("start_hud_animator", &CScriptGameObject::StartActorAnimator)
+	.def("stop_hud_animator", &CScriptGameObject::StopActorAnimator)
+	.def("is_hud_animator_active", &CScriptGameObject::IsAnimatorActive)
+	.def("get_hud_animator_section", &CScriptGameObject::GetActorAnimatorSection)
+	.def("get_hud_animator_restored_slot", &CScriptGameObject::GetActorAnimatorRestoredSlot)
+
 	;
 }
