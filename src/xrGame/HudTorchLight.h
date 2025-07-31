84f799832124480ebb17f2d285c6a62347b1479c
Shtrecker <gogels235@gmail.com>
2025-07-31 11:53:02 +0300

    Add hud torch light base
    Co-Authored-By: Антон Хозаров <59744335+Hozar2002@users.noreply.github.com>

---
+++ src/xrGame/HudTorchLight.h
@@ -0,0 +1,38 @@
+#pragma once
+
+//#include "HudItem.h"
+
+class CHudItem;
+
+class HudLightTorch
+{
+public:
+	const char* Section = {};
+	ref_light RenderLight = nullptr;
+	ref_light OmniLight = nullptr;
+	bool IsRenderLight = false;
+	const char* LightBone = {};
+	const char* LightDirBoneName = {};
+	bool IsLightDirByBone = false;
+	Fvector3 LightOffset;
+	Fvector3 AimOffset;
+	Fvector3 LightWorldOffset;
+	Fvector3 OmniOffset;
+	Fvector3 OmniWorldOffset;
+	Fcolor LightColor;
+	Fcolor OmniColor;
+	bool IsTorchInstalled = false;
+	RStringVec ConeBones{};
+
+public:
+	HudLightTorch() = default;
+	~HudLightTorch();
+
+public:
+	void NewTorchlight(const char* section);
+	void SwitchTorchlight(bool isActive);
+	void SetInstalled(bool install) { IsTorchInstalled = install; }
+	void UpdateTorchFromObject(CHudItem* item) const;
+	bool GetTorchActive() const { return IsRenderLight; }
+	bool GetTorchInstalled() const { return IsTorchInstalled; }
+};
