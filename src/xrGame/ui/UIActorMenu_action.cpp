928c93ecf228920389e73d33c8a2a205cfe51626
Shtrecker <gogels235@gmail.com>
2025-07-31 11:52:39 +0300

    Add disassembly feature from gunslinger mode

--- src/xrGame/ui/UIActorMenu_action.cpp
+++ src/xrGame/ui/UIActorMenu_action.cpp
@@ -418,10 +418,15 @@ void CUIActorMenu::OnMesBoxYes( CUIWindow*, void* )
 	case mmTrade:
 		break;
 	case mmUpgrade:
-		if ( m_repair_mode )
+		if (m_repair_mode == 1)
 		{
 			RepairEffect_CurItem();
-			m_repair_mode = false;
+			m_repair_mode = 0;
+		}
+		else if (m_repair_mode == 2)
+		{
+			PerformDisassemble();
+			m_repair_mode = 0;
 		}
 		else
 		{
@@ -448,7 +453,7 @@ void CUIActorMenu::OnMesBoxNo(CUIWindow*, void*)
 	case mmTrade:
 		break;
 	case mmUpgrade:
-		m_repair_mode = false;
+		m_repair_mode = 0;
 		break;
 	case mmDeadBodySearch:
 		break;
