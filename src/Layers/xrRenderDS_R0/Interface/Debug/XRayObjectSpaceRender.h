671d4c0bb30e4d712d0a103ef28159b4ce5ddcd6
Иван <i-sobolevskiy@mail.ru>
2025-07-31 11:51:09 +0300

    * ADD: Null render for server
    Fix DS0 render working
    
    xrRenderDS_R0 fix release build

---
+++ src/Layers/xrRenderDS_R0/Interface/Debug/XRayObjectSpaceRender.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifdef DEBUG
+class XRayObjectSpaceRender:public IObjectSpaceRender
+{
+public:
+	XRayObjectSpaceRender();
+	virtual void Copy(IObjectSpaceRender &_in);;
+
+	virtual void dbgRender();;
+	virtual void dbgAddSphere(const Fsphere &sphere, u32 colour);;
+	virtual void SetShader();;
+};
+#endif
