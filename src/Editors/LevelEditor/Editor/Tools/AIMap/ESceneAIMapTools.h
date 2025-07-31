1c59e3b2ddd3e2b6dea1118896e50cd94446f7e8
rederr404 <red.err404@yandex.ru>
2025-07-31 11:51:04 +0300

    Add AIMap and Details save/load to LevelEditor
    Restore support for `0.4` level format after #90

--- src/Editors/LevelEditor/Editor/Tools/AIMap/ESceneAIMapTools.h
+++ src/Editors/LevelEditor/Editor/Tools/AIMap/ESceneAIMapTools.h
@@ -175,7 +175,7 @@ public:
     virtual bool   		LoadLTX            		(CInifile&);
     virtual void   		SaveStream         		(IWriter&);
     virtual void   		SaveLTX            		(CInifile&, int id);
-    virtual bool 		can_use_inifile			()								{return false;}
+    virtual bool 		can_use_inifile			(){return false;}
 
     virtual bool		LoadSelection      		(IReader&);
     virtual void		SaveSelection      		(IWriter&);
