86f3caa1c1b5add461cb7d3f98fdf5dafdfa7ee1
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-31 11:53:02 +0300

    Add SoC ai map & graph support

--- src/xrGame/game_level_cross_table.cpp
+++ src/xrGame/game_level_cross_table.cpp
@@ -1,7 +1,6 @@
 #include "StdAfx.h"
 #include "game_level_cross_table.h"
 
-#ifdef AI_COMPILER
 CGameLevelCrossTable::CGameLevelCrossTable(LPCSTR fName)
 {
 	m_tpCrossTableVFS = FS.r_open(fName);
@@ -12,13 +11,12 @@ CGameLevelCrossTable::CGameLevelCrossTable(LPCSTR fName)
 	chunk->r(&m_tCrossTableHeader, sizeof(m_tCrossTableHeader));
 	chunk->close();
 
-	R_ASSERT2(m_tCrossTableHeader.version() == XRAI_CURRENT_VERSION, "Cross table version mismatch!");
+	R_ASSERT2(m_tCrossTableHeader.version() >= XRAI_SOC_VERSION && m_tCrossTableHeader.version() <= XRAI_CURRENT_VERSION, "Cross table version mismatch!");
 
 	IReader* m_chunk = m_tpCrossTableVFS->open_chunk(CROSS_TABLE_CHUNK_DATA);
 	R_ASSERT2(m_chunk, "Cross table is corrupted!");
 	m_tpaCrossTable = (CCell*)m_chunk->pointer();
 }
-#endif // AI_COMPILER
 
 CGameLevelCrossTable::CGameLevelCrossTable(const void* buffer, const u32& buffer_size)
 {
@@ -26,7 +24,7 @@ CGameLevelCrossTable::CGameLevelCrossTable(const void* buffer, const u32& buffer
 	Memory.mem_copy(&m_tCrossTableHeader, buffer, sizeof(m_tCrossTableHeader));
 	buffer = (const u8*)buffer + sizeof(m_tCrossTableHeader);
 
-	R_ASSERT2(m_tCrossTableHeader.version() >= XRAI_MINIMAL_VERSION && m_tCrossTableHeader.version() <= XRAI_CURRENT_VERSION, "Cross table version mismatch!");
+	R_ASSERT2(m_tCrossTableHeader.version() >= XRAI_SOC_VERSION && m_tCrossTableHeader.version() <= XRAI_CURRENT_VERSION, "Cross table version mismatch!");
 
 	m_tpaCrossTable = (CCell*)buffer;
 }
