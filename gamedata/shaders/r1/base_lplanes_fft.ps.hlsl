ac0e43e77c7ba8c5e9da3c3e6bb179915b17671b
Drombeys <dima.zolotov02@gmail.com>
2025-07-31 11:52:32 +0300

    Implement collimator sight support
    - Both the legacy implementation and the new one from the `Parallax Reflex Sights` addon are taken into account
    - On D3D9 legacy legacy collimator sight shaders, but work completely
    
    Link: https://www.moddb.com/mods/stalker-anomaly/addons/parallax-reflex-sights

---
+++ gamedata/shaders/r1/base_lplanes_fft.ps.hlsl
@@ -0,0 +1,33 @@
+#include "common.hlsli"
+
+struct 	v2p
+{
+    float2 	tc0: TEXCOORD0;	// base
+    float2 	tc1: TEXCOORD1;	// lmap
+    float4	c0: COLOR0;		// sun
+};
+
+uniform	float4		m_hud_params;
+
+inline bool isCollimatorActive()
+{
+	return (m_hud_params.w == 1.0f);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Pixel
+float4 main(v2p I) : COLOR
+{
+	float4 t_base = tex2D(s_base,I.tc0);
+
+	// out
+	
+	if(isCollimatorActive())
+	{
+		return float4(t_base.r,t_base.g,t_base.b,t_base.a * I.c0.a);
+	}
+	else
+	{
+		return float4(t_base.r,t_base.g,t_base.b,t_base.a * 0.0f);
+	}
+}
