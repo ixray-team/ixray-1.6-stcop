63e6f2b5ee93df48b3c57be30e3138731992a37b
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-31 11:53:00 +0300

    Disable CD-Key by default

--- src/xrGame/xrGameSpyServer.cpp
+++ src/xrGame/xrGameSpyServer.cpp
@@ -81,7 +81,7 @@ xrGameSpyServer::EConnect xrGameSpyServer::Connect(shared_str &session_name, Gam
 	m_iReportToMasterServer = game->get_option_i		(*session_name,"public",0);
 	m_iMaxPlayers	= game->get_option_i		(*session_name,"maxplayers",32);
 //	m_bCheckCDKey = game->get_option_i		(*session_name,"cdkey",0) != 0;
-	m_bCheckCDKey = game->get_option_i		(*session_name,"public",0) != 0;
+	m_bCheckCDKey = false;
 	//--------------------------------------------//
 	if (game->Type() != eGameIDSingle) 
 	{
