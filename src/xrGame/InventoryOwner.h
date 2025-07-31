fd845eaf38656e0c3562e906e05e29cc2e87c6ca
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-31 11:50:58 +0300

    Restore legacy ammo counter (from CS)

--- src/xrGame/InventoryOwner.h
+++ src/xrGame/InventoryOwner.h
@@ -27,6 +27,7 @@ class CTradeParameters;
 class CPurchaseList;
 class CWeapon;
 class CCustomOutfit;
+class CHelmet;
 
 class CInventoryOwner : public CAttachmentOwner {							
 public:
@@ -151,6 +152,7 @@ public:
 	virtual float MaxCarryWeight			() const;
 
 	CCustomOutfit* GetOutfit				() const;
+	CHelmet*	   GetHelmet				() const;
 
 	bool CanPlayShHdRldSounds				() const {return m_play_show_hide_reload_sounds;};
 	void SetPlayShHdRldSounds				(bool play) {m_play_show_hide_reload_sounds = play;};
