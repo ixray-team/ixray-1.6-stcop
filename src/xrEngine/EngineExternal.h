c199ce0ddcfc35c5c4eed325c30a51f65f944f6c
Shtrecker <gogels235@gmail.com>
2025-07-31 11:52:28 +0300

    Implement alternate zoom FOV calculation
    For Gunslinger compatibility

--- src/xrEngine/EngineExternal.h
+++ src/xrEngine/EngineExternal.h
@@ -26,6 +26,7 @@ enum class EEngineExternalGame
 	EnableWeaponCollision,
 	EnableActorStepWallmarks,
 	DisableSprintWhileOverweight,
+	EnableAlternateZoomFovCalc,
 };
 
 enum class EEngineExternalRender 
