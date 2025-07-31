49d37d44e3765a494c97c9e476e241f1ea61980c
ForserX <sv3nk@yandex.ru>
2025-07-31 11:53:04 +0300

    Add support mixed level.ai for building spawn

--- src/utils/xrAI/xr_graph_merge.cpp
+++ src/utils/xrAI/xr_graph_merge.cpp
@@ -509,7 +509,7 @@ void read_levels(CInifile *Ini, xr_set<CLevelInfo> &levels, bool rebuild_graph, 
 			CLevelGraph::CHeader	header;
 			reader->r				(&header,sizeof(header));
 			FS.r_close				(reader);
-			if (header.version() != XRAI_CURRENT_VERSION) {
+			if (header.version() < XRAI_MINIMAL_VERSION || header.version() > XRAI_CURRENT_VERSION) {
 				Msg					("! AI-map for the level %s is incompatible (version mismatch)! (level is not included into the game graph)",S);
 				continue;
 			}
