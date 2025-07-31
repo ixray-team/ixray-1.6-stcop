e4b128874ac8b196839b89118067f70689a63e09
Shtrecker <gogels235@gmail.com>
2025-07-31 11:53:02 +0300

    Fix `CWeapon::ScopeFit` method for STCoP

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -3352,14 +3352,16 @@ void CWeapon::UpdateShellBones(u32 idx, u8 type)
 
 bool CWeapon::ScopeFit(CScope* pIItem) const
 {
-	if (bUseAltScope)
-	{
-		return true;
-	}
-
 	for (const shared_str& scope : m_scopes)
 	{
-		if (pSettings->r_string(scope, "scope_name") == pIItem->cNameSect())
+		if (bUseAltScope)
+		{
+			if (scope == pIItem->cNameSect())
+			{
+				return true;
+			}
+		}
+		else if (pSettings->r_string(scope, "scope_name") == pIItem->cNameSect())
 		{
 			return true;
 		}
