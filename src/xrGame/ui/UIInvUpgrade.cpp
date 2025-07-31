4290d9072e76f85fdc0692a17811505ddac9caf3
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-31 11:50:58 +0300

    Implement legacy `inventory_upgrade.xml` support
    Compatibility for Clear Sky

--- src/xrGame/ui/UIInvUpgrade.cpp
+++ src/xrGame/ui/UIInvUpgrade.cpp
@@ -23,7 +23,7 @@
 
 #include "UIInventoryUpgradeWnd.h"
 
-UIUpgrade::UIUpgrade( CUIInventoryUpgradeWnd* parent_wnd )
+UIUpgrade::UIUpgrade( CUIInventoryUpgradeWnd* parent_wnd, bool cellBorder)
 :m_point(nullptr)
 {
 	VERIFY( parent_wnd );
@@ -31,6 +31,21 @@ UIUpgrade::UIUpgrade( CUIInventoryUpgradeWnd* parent_wnd )
 
 	m_item   = new CUIStatic();	m_item->SetAutoDelete(   true );	AttachChild( m_item   );
 	m_color  = new CUIStatic();	m_color->SetAutoDelete(  true );	AttachChild( m_color  );
+
+	if (cellBorder)
+	{
+		m_border = new CUIStatic();
+		m_border->SetAutoDelete(true);
+		AttachChild(m_border);
+		m_ink = new CUIStatic();
+		m_ink->SetAutoDelete(true);
+		AttachChild(m_ink);
+	}
+	else
+	{
+		m_border = nullptr;
+		m_ink = nullptr;
+	}
 
 	m_upgrade_id = nullptr;
 	Reset();
@@ -66,12 +81,14 @@ void UIUpgrade::Reset()
 	m_button_state = BUTTON_FREE;
 	m_state_lock   = false;
 
+	if (m_ink)
+		m_ink->Show(false);
 	m_color->Show( false );
 		
 	inherited::Reset();
 }
 // -----------------------------------------------------------------------------------
-void UIUpgrade::load_from_xml( CUIXml& ui_xml, int i_column, int i_cell, Frect const& t_cell_item )
+void UIUpgrade::load_from_xml(CUIXml& ui_xml, int i_column, int i_cell, Frect const* t_cell_border, Frect const& t_cell_item)
 {
 	m_scheme_index.x = i_column;
 	m_scheme_index.y = i_cell; // row
@@ -79,20 +96,52 @@ void UIUpgrade::load_from_xml( CUIXml& ui_xml, int i_column, int i_cell, Frect c
 	CUIXmlInit::InitWindow( ui_xml, "cell", i_cell, this );
 
 	Fvector2 f2, color;
-	
+	Frect border;
+	if (t_cell_border)
+		border = *t_cell_border;
+
 	f2.set( t_cell_item.x1, t_cell_item.y1 );
 	m_item->SetWndPos( f2 );
-	color.set(f2.x+(UI().is_widescreen()?2.0f:3.0f), f2.y+3.0f);
-	m_color->SetWndPos(color);
+	if (!t_cell_border)
+	{
+		color.set(f2.x + (UI().is_widescreen() ? 2.0f : 3.0f), f2.y + 3.0f);
+		m_color->SetWndPos(color);
+	}
+	else
+	{
+		m_color->SetWndPos(f2);
+	}
 
 	f2.set( t_cell_item.width(), t_cell_item.height() );
 	m_item->SetWndSize( f2 );
-	color.set(UI().is_widescreen()?4.0f:5.0f, 38.0f);
-	m_color->SetWndSize( Fvector2().set(5.0f, 38.0f) );
+	if (!t_cell_border)
+	{
+		color.set(/*UI().is_widescreen() ? 4.0f :*/ 5.0f, 38.0f);
+		m_color->SetWndSize(color);
+	}
+	else
+	{
+		m_color->SetWndSize(f2);
+	}
 	SetWndSize( f2 );
+
+	if (t_cell_border)
+	{
+		f2.set(border.x1, border.y1);
+		m_border->SetWndPos(f2);
+		m_ink->SetWndPos(f2);
+		f2.set(border.width(), border.height());
+		m_border->SetWndSize(f2);
+		m_ink->SetWndSize(f2);
+	}
 
 	m_item->SetStretchTexture(true);
 	m_color->SetStretchTexture(true);
+	if (t_cell_border)
+	{
+		m_border->SetStretchTexture(true);
+		m_ink->SetStretchTexture(true);
+	}
 }
 
 void UIUpgrade::set_texture( Layer layer, LPCSTR texture )
@@ -100,7 +149,18 @@ void UIUpgrade::set_texture( Layer layer, LPCSTR texture )
 	switch( layer )
 	{
 	case LAYER_ITEM:   VERIFY( texture ); m_item->InitTexture( texture ); break;
-	case LAYER_POINT:   VERIFY( texture ); m_point->InitTexture( texture ); break;
+	case LAYER_BORDER:
+		if (m_border)
+			m_border->InitTexture(texture);
+		break;
+	case LAYER_INK:
+		if (m_ink)
+			m_ink->InitTexture(texture);
+		break;
+	case LAYER_POINT:   
+		if (m_point)
+			m_point->InitTexture( texture );
+		break;
 	case LAYER_COLOR:
 		{
 			if ( texture )
@@ -135,13 +195,15 @@ void UIUpgrade::Update()
 	{
 		update_mask();
 	}
-
-	m_point->Show(get_upgrade()->get_highlight());
+	if (m_ink)
+		m_ink->Show(get_upgrade()->get_highlight());
+	if (m_point)
+		m_point->Show(get_upgrade()->get_highlight());
 }
 
 void UIUpgrade::update_upgrade_state()
 {
-	if ( m_bCursorOverWindow || m_point->CursorOverWindow())
+    if (m_bCursorOverWindow || m_point && m_point->CursorOverWindow())
 	{
 		on_over_window();
 	}
@@ -195,7 +257,7 @@ bool UIUpgrade::OnMouseAction( float x, float y, EUIMessages mouse_action )
 	if( inherited::OnMouseAction( x, y, mouse_action ) )
 		return true;
 
-	if ( m_bCursorOverWindow || m_point->CursorOverWindow())
+	if ( m_bCursorOverWindow || m_point && m_point->CursorOverWindow())
 	{
 		highlight_relation( true );
 		if ( mouse_action == WINDOW_LBUTTON_DOWN )
