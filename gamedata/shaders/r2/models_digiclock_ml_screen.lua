08c9a6ee2238b2daf48d37374a3f34f8b33a43f6
Drombeys <dima.zolotov02@gmail.com>
2025-07-31 11:52:40 +0300

    Фиксы шейдеров (перестало вылетать, но не работает)

--- gamedata/shaders/r2/models_digiclock_ml_screen.lua
+++ gamedata/shaders/r2/models_digiclock_ml_screen.lua
@@ -1,5 +1,5 @@
 function normal		(shader, t_base, t_second, t_detail)
-	shader:begin	("deffer_model_flat","model_digiclock_ml")
+	shader:begin	("deffer_model","model_digiclock_ml")
 			: fog		(false)
 			: emissive 	(true)
 	shader:sampler	("s_base")      :texture	(t_base)
@@ -7,7 +7,7 @@ function normal		(shader, t_base, t_second, t_detail)
 end
 
 function l_special	(shader, t_base, t_second, t_detail)
-	shader:begin	("shadow_direct_model",	"accum_emissivel")
+    shader:begin("deffer_model", "accum_emissive")
 			: zb 		(true,false)
 			: fog		(false)
 			: emissive 	(true)
