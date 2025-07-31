115ee5ff28cabdfa7e1d38211d1165320090bade
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-07-31 11:52:35 +0300

    Add `SetFont` & `GetFont` scirpt methods

--- src/xrUI/Widgets/UIWindow.h
+++ src/xrUI/Widgets/UIWindow.h
@@ -107,6 +107,13 @@ public:
 			void			ResetAll			();
 
 
+	virtual void			SetFont				(CGameFont* pFont)			{ m_pFont = pFont;}
+	CGameFont*				GetFont				()							{if(m_pFont) return m_pFont;
+																				if(m_pParentWnd== NULL)	
+																					return  m_pFont;
+																				else
+																					return  m_pParentWnd->GetFont();}
+
 	using WINDOW_LIST = xr_vector<CUIWindow*>;
 	using WINDOW_LIST_it = WINDOW_LIST::iterator;
 
@@ -154,6 +161,8 @@ protected:
 
 	//кому шлем сообщения
 	CUIWindow*				m_pMessageTarget;
+
+	CGameFont*				m_pFont;
 
 	// Последняя позиция мышки
 	Fvector2 cursor_pos;
