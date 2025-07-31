58de18b3743f5d4d2725c675e137d099323b7e70
DimaKuzmin <scorpion261096@gmail.com>
2025-07-31 11:52:33 +0300

    GameSP move to CurrentGameUI()

--- src/xrGame/AI_PhraseDialogManager.cpp
+++ src/xrGame/AI_PhraseDialogManager.cpp
@@ -69,8 +69,9 @@ void CAI_PhraseDialogManager::AnswerPhrase (DIALOG_SHARED_PTR& phrase_dialog)
 
 		shared_str phrase_id = phrase_dialog->PhraseList()[phrase_num]->GetID();
 		
-		CUIGameSP* pGameSP = smart_cast<CUIGameSP*>(CurrentGameUI());
-		pGameSP->TalkMenu->AddAnswer	(phrase_dialog->GetPhraseText(phrase_id), pInvOwner->Name());
+		if (CurrentGameUI() == nullptr)
+			return;
+		CurrentGameUI()->TalkMenu->AddAnswer	(phrase_dialog->GetPhraseText(phrase_id), pInvOwner->Name());
 
 		CPhraseDialogManager::SayPhrase(phrase_dialog, phrase_id);
 	}
