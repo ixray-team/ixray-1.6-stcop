37eb8d654c19306e41702ad5be6f09d167de8ab6
Shtrecker <gogels235@gmail.com>
2025-07-31 11:52:43 +0300

    Add collimator scopes support

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -1229,9 +1229,9 @@ void CWeapon::HideOneUpgradeLevel(const char* section)
 	}
 }
 
-bool  CWeapon::need_renderable()
+bool CWeapon::need_renderable()
 {
-	return !( IsZoomed() && ZoomTexture() && !IsRotatingToZoom() );
+	return !(IsZoomed() && ZoomTexture() && !IsRotatingToZoom() && !IsHudModelForceUnhide());
 }
 
 void CWeapon::renderable_Render		()
