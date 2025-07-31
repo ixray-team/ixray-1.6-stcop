a964d0b277c5e0a4fb4a0bb9676484ac7d9ada5c
Drombeys <dima.zolotov02@gmail.com>
2025-07-31 11:52:30 +0300

    Replace type casts in `CGameObject::net_Destroy` method

--- src/xrGame/GameObject.cpp
+++ src/xrGame/GameObject.cpp
@@ -111,8 +111,8 @@ void CGameObject::net_Destroy	()
 	xr_delete				(m_ini_file);
 
 	m_script_clsid			= -1;
-	if (Visual() && smart_cast<IKinematics*>(Visual()))
-		smart_cast<IKinematics*>(Visual())->Callback	(0,0);
+	if (Visual() && Visual()->dcast_PKinematics())
+		Visual()->dcast_PKinematics()->Callback(0,0);
 
 	inherited::net_Destroy						();
 	setReady									(FALSE);
