05c750ed61b6e4c93f5507cfeacfc92e58f4768c
ForserX <sv3nk@yandex.ru>
2025-07-31 11:53:04 +0300

    Restore `mp_game_cl.on_connected`

--- src/xrGame/FreeMP/game_cl_freemp.cpp
+++ src/xrGame/FreeMP/game_cl_freemp.cpp
@@ -58,9 +58,9 @@ void game_cl_freemp::OnConnected()
 		m_game_ui->SetClGame(this);
 	}
 
-	//luabind::functor<void>	funct;
-	//R_ASSERT(ai().script_engine().functor("mp_game_cl.on_connected", funct));
-	//funct();
+	luabind::functor<void>	funct;
+	R_ASSERT(ai().script_engine().functor("mp_game_cl.on_connected", funct));
+	funct();
 }
 
 void game_cl_freemp::net_import_state(NET_Packet& P)
