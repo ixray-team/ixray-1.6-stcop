bee787f7df4f5b2c15de369010efb6fc59536886
ForserX <sv3nk@yandex.ru>
2025-07-31 11:52:44 +0300

    Implement 25-bit AI-Map support
    Co-authored-by: abramcumner <abramcumner@yandex.ru>

--- src/utils/xrAI/compiler_load.cpp
+++ src/utils/xrAI/compiler_load.cpp
@@ -52,75 +52,75 @@ extern void		Surface_Init	();
 
 void xrLoad(LPCSTR name, bool draft_mode)
 {
-	FS.get_path					("$level$")->_set	((LPSTR)name);
+	FS.get_path("$level$")->_set((LPSTR)name);
 	string256					N;
-	if (!draft_mode)	{
+	if (!draft_mode) {
 		// shaders
 		string_path				N__;
-		FS.update_path			(N__,"$game_data$","shaders_xrlc.xr");
-		g_shaders_xrlc			= new Shader_xrLC_LIB ();
-		g_shaders_xrlc->Load	(N__);
+		FS.update_path(N__, "$game_data$", "shaders_xrlc.xr");
+		g_shaders_xrlc = new Shader_xrLC_LIB();
+		g_shaders_xrlc->Load(N__);
 
 		// Load CFORM
 		{
-			xr_strconcat(N__,name,"build.cform");
-			IReader*			fs = FS.r_open(N__);
-			R_ASSERT			(fs->find_chunk(0));
+			xr_strconcat(N__, name, "build.cform");
+			IReader* fs = FS.r_open(N__);
+			R_ASSERT(fs->find_chunk(0));
 
 			hdrCFORM			H;
-			fs->r				(&H,sizeof(hdrCFORM));
-			R_ASSERT			(CFORM_CURRENT_VERSION==H.version);
+			fs->r(&H, sizeof(hdrCFORM));
+			R_ASSERT(CFORM_CURRENT_VERSION == H.version);
 
-			Fvector*	verts	= (Fvector*)fs->pointer();
-			CDB::TRI*	tris	= (CDB::TRI*)(verts+H.vertcount);
-			LevelPtr->build( verts, H.vertcount, tris, H.facecount );
+			Fvector* verts = (Fvector*)fs->pointer();
+			CDB::TRI* tris = (CDB::TRI*)(verts + H.vertcount);
+			LevelPtr->build(verts, H.vertcount, tris, H.facecount);
 			LevelPtr->syncronize();
-			Msg("* Level CFORM: %dK", LevelPtr->memory()/1024);
+			Msg("* Level CFORM: %dK", LevelPtr->memory() / 1024);
 
-			g_rc_faces.resize	(H.facecount);
+			g_rc_faces.resize(H.facecount);
 			R_ASSERT(fs->find_chunk(1));
-			fs->r				(&*g_rc_faces.begin(),g_rc_faces.size()*sizeof(b_rc_face));
+			fs->r(&*g_rc_faces.begin(), g_rc_faces.size() * sizeof(b_rc_face));
 
-			LevelBB.set			(H.aabb);
-			FS.r_close			(fs);
+			LevelBB.set(H.aabb);
+			FS.r_close(fs);
 		}
 
 		// Load level data
 		{
-			xr_strconcat(N__,name,"build.prj");
-			IReader*	fs		= FS.r_open (N__);
-			IReader*	F;
+			xr_strconcat(N__, name, "build.prj");
+			IReader* fs = FS.r_open(N__);
+			IReader* F;
 
 			// Version
 			u32 version;
-			fs->r_chunk			(EB_Version,&version);
-			R_ASSERT			(XRCL_CURRENT_VERSION >= 17);
-			R_ASSERT			(XRCL_CURRENT_VERSION <= 18);
+			fs->r_chunk(EB_Version, &version);
+			R_ASSERT(XRCL_CURRENT_VERSION >= 17);
+			R_ASSERT(XRCL_CURRENT_VERSION <= 18);
 
 			// Header
 			b_params			Params;
-			fs->r_chunk			(EB_Parameters,&Params);
+			fs->r_chunk(EB_Parameters, &Params);
 
 			// Load level data
-			transfer("materials",	g_materials,			*fs,		EB_Materials);
-			transfer("shaders_xrlc",g_shader_compile,		*fs,		EB_Shaders_Compile);
+			transfer("materials", g_materials, *fs, EB_Materials);
+			transfer("shaders_xrlc", g_shader_compile, *fs, EB_Shaders_Compile);
 
 			// process textures
 			bool is_thm_missing = false;
 			bool is_tga_missing = false;
 
-			Status			("Processing textures...");
+			Status("Processing textures...");
 			{
-				Surface_Init		();
-				F = fs->open_chunk	(EB_Textures);
+				Surface_Init();
+				F = fs->open_chunk(EB_Textures);
 #ifdef _M_X64
 				u32 tex_count = F->length() / sizeof(b_texture64);
 #else
 				u32 tex_count = F->length() / sizeof(b_texture);
 #endif
-				for (u32 t=0; t<tex_count; t++)
+				for (u32 t = 0; t < tex_count; t++)
 				{
-					Progress		(float(t)/float(tex_count));
+					Progress(float(t) / float(tex_count));
 
 #ifdef _M_X64
 					b_texture64	TEX;
@@ -139,22 +139,23 @@ void xrLoad(LPCSTR name, bool draft_mode)
 #endif
 
 					// load thumbnail
-					string128		&N_ = BT.name;
+					string128& N_ = BT.name;
 					LPSTR			extension = strext(N_);
 					if (extension)
-						*extension	= 0;
+						*extension = 0;
 
-					xr_strlwr		(N_);
+					xr_strlwr(N_);
 
-					if (0==xr_strcmp(N_,"level_lods")) {
+					if (0 == xr_strcmp(N_, "level_lods")) {
 						// HACK for merged lod textures
-						BT.dwWidth	= 1024;
-						BT.dwHeight	= 1024;
-						BT.bHasAlpha= TRUE;
-						BT.pSurface	= 0;
-					} else {
-						xr_strcat		(N_,".thm");
-						IReader* THM	= FS.r_open("$game_textures$",N_);
+						BT.dwWidth = 1024;
+						BT.dwHeight = 1024;
+						BT.bHasAlpha = TRUE;
+						BT.pSurface = 0;
+					}
+					else {
+						xr_strcat(N_, ".thm");
+						IReader* THM = FS.r_open("$game_textures$", N_);
 
 						if (!THM) {
 							clMsg("cannot find thm: %s", N_);
@@ -163,35 +164,35 @@ void xrLoad(LPCSTR name, bool draft_mode)
 						}
 
 						// version
-						u32 version_				= 0;
-						R_ASSERT				(THM->r_chunk(THM_CHUNK_VERSION,&version_));
+						u32 version_ = 0;
+						R_ASSERT(THM->r_chunk(THM_CHUNK_VERSION, &version_));
 						// if( version!=THM_CURRENT_VERSION )	FATAL	("Unsupported version of THM file.");
 
 						// analyze thumbnail information
 						R_ASSERT(THM->find_chunk(THM_CHUNK_TEXTUREPARAM));
-						THM->r                  (&BT.THM.fmt,sizeof(STextureParams::ETFormat));
-						BT.THM.flags.assign		(THM->r_u32());
-						BT.THM.border_color		= THM->r_u32();
-						BT.THM.fade_color		= THM->r_u32();
-						BT.THM.fade_amount		= THM->r_u32();
-						BT.THM.mip_filter		= THM->r_u32();
-						BT.THM.width			= THM->r_u32();
-						BT.THM.height           = THM->r_u32();
-						BOOL			bLOD=FALSE;
-						if (N_[0]=='l' && N_[1]=='o' && N_[2]=='d' && N_[3]=='\\') bLOD = TRUE;
+						THM->r(&BT.THM.fmt, sizeof(STextureParams::ETFormat));
+						BT.THM.flags.assign(THM->r_u32());
+						BT.THM.border_color = THM->r_u32();
+						BT.THM.fade_color = THM->r_u32();
+						BT.THM.fade_amount = THM->r_u32();
+						BT.THM.mip_filter = THM->r_u32();
+						BT.THM.width = THM->r_u32();
+						BT.THM.height = THM->r_u32();
+						BOOL			bLOD = FALSE;
+						if (N_[0] == 'l' && N_[1] == 'o' && N_[2] == 'd' && N_[3] == '\\') bLOD = TRUE;
 
 						// load surface if it has an alpha channel or has "implicit lighting" flag
-						BT.dwWidth				= BT.THM.width;
-						BT.dwHeight				= BT.THM.height;
-						BT.bHasAlpha			= BT.THM.HasAlphaChannel();
-						BT.pSurface				= 0;
-						if (!bLOD) 
+						BT.dwWidth = BT.THM.width;
+						BT.dwHeight = BT.THM.height;
+						BT.bHasAlpha = BT.THM.HasAlphaChannel();
+						BT.pSurface = 0;
+						if (!bLOD)
 						{
 							if (BT.bHasAlpha || BT.THM.flags.test(STextureParams::flImplicitLighted))
 							{
-								clMsg		("- loading: %s",N_);
-								u32			w=0, h=0;
-								BT.pSurface = Surface_Load(N_,w,h); 
+								clMsg("- loading: %s", N_);
+								u32			w = 0, h = 0;
+								BT.pSurface = Surface_Load(N_, w, h);
 
 								if (!BT.pSurface) {
 									clMsg("cannot find tga texture: %s", N);
@@ -205,15 +206,16 @@ void xrLoad(LPCSTR name, bool draft_mode)
 									BT.dwWidth = BT.THM.width = w;
 									BT.dwHeight = BT.THM.height = h;
 								}
-								BT.Vflip	();
-							} else {
+								BT.Vflip();
+							}
+							else {
 								// Free surface memory
 							}
 						}
 					}
 
 					// save all the stuff we've created
-					g_textures.push_back	(BT);
+					g_textures.push_back(BT);
 				}
 
 				R_ASSERT2(!is_thm_missing, "Some of required thm's are missing. See log for details.");
@@ -221,120 +223,135 @@ void xrLoad(LPCSTR name, bool draft_mode)
 			}
 		}
 	}
-	
+
 	// Load lights
 	{
-		xr_strconcat(N,name,"build.prj");
+		xr_strconcat(N, name, "build.prj");
 
-		IReader*	F			= FS.r_open(N);
-		R_ASSERT2				(F,"There is no file 'build.prj'!");
-		IReader					&fs= *F;
+		IReader* F = FS.r_open(N);
+		R_ASSERT2(F, "There is no file 'build.prj'!");
+		IReader& fs = *F;
 
 		// Version
 		u32 version;
-		fs.r_chunk				(EB_Version,&version);
-		R_ASSERT				(XRCL_CURRENT_VERSION >= 17);
-		R_ASSERT				(XRCL_CURRENT_VERSION <= 18);
+		fs.r_chunk(EB_Version, &version);
+		R_ASSERT(XRCL_CURRENT_VERSION >= 17);
+		R_ASSERT(XRCL_CURRENT_VERSION <= 18);
 
 		// Header
 		b_params				Params;
-		fs.r_chunk				(EB_Parameters,&Params);
+		fs.r_chunk(EB_Parameters, &Params);
 
 		// Lights (Static)
 		{
 			F = fs.open_chunk(EB_Light_static);
 			b_light_static	temp;
-			u32 cnt		= F->length()/sizeof(temp);
-			for				(u32 i=0; i<cnt; i++)
+			u32 cnt = F->length() / sizeof(temp);
+			for (u32 i = 0; i < cnt; i++)
 			{
 				R_Light_Fast RL;
-				F->r		(&temp,sizeof(temp));
-				Flight&		L = temp.data;
+				F->r(&temp, sizeof(temp));
+				Flight& L = temp.data;
 				if (_abs(L.range) > 10000.f) {
-					Msg		("! BAD light range : %f",L.range);
-					L.range	= L.range > 0.f ? 10000.f : -10000.f;
+					Msg("! BAD light range : %f", L.range);
+					L.range = L.range > 0.f ? 10000.f : -10000.f;
 				}
 
 				// type
-				if			(L.type == D3DLIGHT_DIRECTIONAL)	RL.type	= LT_DIRECT;
+				if (L.type == D3DLIGHT_DIRECTIONAL)	RL.type = LT_DIRECT;
 				else											RL.type = LT_POINT;
 
 				// generic properties
-				RL.position.set				(L.position);
-				RL.direction.normalize_safe	(L.direction);
-				RL.range				=	L.range*1.1f;
-				RL.range2				=	RL.range*RL.range;
-				RL.attenuation0			=	L.attenuation0;
-				RL.attenuation1			=	L.attenuation1;
-				RL.attenuation2			=	L.attenuation2;
+				RL.position.set(L.position);
+				RL.direction.normalize_safe(L.direction);
+				RL.range = L.range * 1.1f;
+				RL.range2 = RL.range * RL.range;
+				RL.attenuation0 = L.attenuation0;
+				RL.attenuation1 = L.attenuation1;
+				RL.attenuation2 = L.attenuation2;
 
-				RL.amount				=	L.diffuse.magnitude_rgb	();
-				RL.tri[0].set			(0,0,0);
-				RL.tri[1].set			(0,0,0);
-				RL.tri[2].set			(0,0,0);
+				RL.amount = L.diffuse.magnitude_rgb();
+				RL.tri[0].set(0, 0, 0);
+				RL.tri[1].set(0, 0, 0);
+				RL.tri[2].set(0, 0, 0);
 
 				// place into layer
-				if (0==temp.controller_ID)	g_lights.push_back		(RL);
+				if (0 == temp.controller_ID)	g_lights.push_back(RL);
 			}
-			F->close		();
+			F->close();
 		}
 	}
 
 	// Load initial map from the Level Editor
 	{
-		string_path			file_name;
-		xr_strconcat(file_name,name,"build.aimap");
-		IReader				*F = FS.r_open(file_name);
-		R_ASSERT2			(F, file_name);
+		string_path file_name;
+		xr_strconcat(file_name, name, "build.aimap");
+		IReader* F = FS.r_open(file_name);
+		R_ASSERT2(F, file_name);
 
-		R_ASSERT			(F->open_chunk(E_AIMAP_CHUNK_VERSION));
-		R_ASSERT			(F->r_u16() == E_AIMAP_VERSION);
+		R_ASSERT(F->open_chunk(E_AIMAP_CHUNK_VERSION));
+		u16 version = F->r_u16();
+		R_ASSERT(version <= E_AIMAP_VERSION);
 
-		R_ASSERT			(F->open_chunk(E_AIMAP_CHUNK_BOX));
-		F->r				(&LevelBB,sizeof(LevelBB));
+		R_ASSERT(F->open_chunk(E_AIMAP_CHUNK_BOX));
+		F->r(&LevelBB, sizeof(LevelBB));
 
-		R_ASSERT			(F->open_chunk(E_AIMAP_CHUNK_PARAMS));
-		F->r				(&g_params,sizeof(g_params));
+		R_ASSERT(F->open_chunk(E_AIMAP_CHUNK_PARAMS));
+		F->r(&g_params, sizeof(g_params));
 
-		R_ASSERT			(F->open_chunk(E_AIMAP_CHUNK_NODES));
-		u32					N_ = F->r_u32();
-		R_ASSERT2			(N_ < ((u32(1) << u32(MAX_NODE_BIT_COUNT)) - 2),"Too many nodes!");
-		g_nodes.resize		(N_);
+		R_ASSERT(F->open_chunk(E_AIMAP_CHUNK_NODES));
+		u32 N_ = F->r_u32();
+		R_ASSERT2(N_ < MAX_AI_NODES - 1, "Too many nodes!");
+		g_nodes.resize(N_);
 
-		hdrNODES			H;
-		H.version			= XRAI_CURRENT_VERSION;
-		H.count				= N_+1;
-		H.size				= g_params.fPatchSize;
-		H.size_y			= 1.f;
-		H.aabb				= LevelBB;
-		
+		hdrNODES H;
+		H.version = XRAI_CURRENT_VERSION;
+		H.count = N_ + 1;
+		H.size = g_params.fPatchSize;
+		H.size_y = 1.f;
+		H.aabb = LevelBB;
+
+		constexpr u32 InvalidNode_v1 = 0x00ffffff;
 		typedef u32 NodeLink;
-		for (u32 i=0; i<N_; i++) {
-			NodeLink			id;
-			u16 				pl;
-			SNodePositionOld 	_np;
-			NodePosition 		np;
-			
-			for (int j=0; j<4; ++j) {
-				F->r(&id, 3);
-				id = id & 0x00ffffff;
-				if (id == InvalidNode)
-					id = InvalidNode;
-				g_nodes[i].n[j] = id;
+		for (u32 i = 0; i < N_; i++)
+		{
+			NodeLink id;
+			u16 pl;
+			SNodePositionOld _np;
+			NodePosition np;
+
+			if (version == 1)
+			{
+				for (int j = 0; j < 4; ++j)
+				{
+					F->r(&id, 3);
+					id = id & 0x00ffffff;
+					if (id == InvalidNode_v1)
+						id = InvalidNode;
+					g_nodes[i].n[j] = id;
+				}
+			}
+			else
+			{
+				for (int j = 0; j < 4; ++j)
+				{
+					F->r(&id, sizeof(NodeLink));
+					g_nodes[i].n[j] = id;
+				}
 			}
 
-			pl				= F->r_u16();
-			pvDecompress	(g_nodes[i].Plane.n,pl);
-			F->r			(&_np,sizeof(_np));
-			CNodePositionConverter(_np,H,np);
-			g_nodes[i].Pos	= vertex_position(np,LevelBB,g_params);
+			pl = F->r_u16();
+			pvDecompress(g_nodes[i].Plane.n, pl);
+			F->r(&_np, sizeof(_np));
+			CNodePositionConverter(_np, H, np);
+			g_nodes[i].Pos = vertex_position(np, LevelBB, g_params);
 
-			g_nodes[i].Plane.build(g_nodes[i].Pos,g_nodes[i].Plane.n);
+			g_nodes[i].Plane.build(g_nodes[i].Pos, g_nodes[i].Plane.n);
 		}
 
-		F->close			();
+		F->close();
 
-		if (strstr(Core.Params,"-clear_temp_files"))
-			DeleteFileA		(file_name);
+		if (strstr(Core.Params, "-clear_temp_files"))
+			DeleteFileA(file_name);
 	}
 }
--- src/xrGame/game_level_cross_table.cpp
+++ src/xrGame/game_level_cross_table.cpp
@@ -26,7 +26,7 @@ CGameLevelCrossTable::CGameLevelCrossTable(const void* buffer, const u32& buffer
 	Memory.mem_copy(&m_tCrossTableHeader, buffer, sizeof(m_tCrossTableHeader));
 	buffer = (const u8*)buffer + sizeof(m_tCrossTableHeader);
 
-	R_ASSERT2(m_tCrossTableHeader.version() == XRAI_CURRENT_VERSION, "Cross table version mismatch!");
+	R_ASSERT2(m_tCrossTableHeader.version() >= XRAI_MINIMAL_VERSION && m_tCrossTableHeader.version() <= XRAI_CURRENT_VERSION, "Cross table version mismatch!");
 
 	m_tpaCrossTable = (CCell*)buffer;
 }
