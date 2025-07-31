1eecf04c2f422ac9e63000c1bcdff09cfc3e6f3c
ForserX <sv3nk@yandex.ru>
2025-07-31 11:52:28 +0300

    Implement Linux support for `generate_guid`
    Implemented #69

--- src/Editors/xrECore/Engine/guid_generator.cpp
+++ src/Editors/xrECore/Engine/guid_generator.cpp
@@ -10,33 +10,42 @@
 
 #include "guid_generator.h"
 
-#ifdef WINVER
+#ifdef IXR_WINDOWS
 #	include <rpcdce.h>
 #	include <rpc.h>
+#	pragma comment(lib, "Rpcrt4.lib")
+#else
+#include <uuid/uuid.h>
 #endif
 
-#pragma comment(lib, "Rpcrt4.lib")
 
 ECORE_API xrGUID generate_guid()
 {
-	xrGUID			result;
-#ifdef WINVER
-	//STATIC_CHECK	(sizeof(xrGUID) == sizeof(GUID),Different_GUID_types);
-	GUID			_result;
-	RPC_STATUS		gen_result = UuidCreate(&_result);
-	Memory.mem_copy	(&result,&_result,sizeof(_result));
-	switch (gen_result) {
-		case RPC_S_OK				: return(result);
-		case RPC_S_UUID_LOCAL_ONLY	: return(result);
-		case RPC_S_UUID_NO_ADDRESS	: 
-		default						: break;
+	xrGUID result;
+
+#ifdef IXR_WINDOWS
+	GUID _result;
+	RPC_STATUS gen_result = UuidCreate(&_result);
+	Memory.mem_copy(&result, &_result, sizeof(_result));
+
+	switch (gen_result)
+	{
+	case RPC_S_OK: return(result);
+	case RPC_S_UUID_LOCAL_ONLY: return(result);
+	case RPC_S_UUID_NO_ADDRESS:
+	default: break;
 	}
+#else
+	uuid_t uuid;
+	uuid_generate(uuid);
+	Memory.mem_copy(&result, &uuid, sizeof(uuid_t));
+
 #endif
-	//STATIC_CHECK	(sizeof(result) >= sizeof(u64),GUID_must_have_size_greater_or_equal_to_the_long_long);
-	ZeroMemory		(&result,sizeof(result));
-	u64				temp = CPU::GetCLK();
-	Memory.mem_copy	(&result,&temp,sizeof(temp));
-	return			(result);
+
+	ZeroMemory(&result, sizeof(result));
+	u64 temp = CPU::GetCLK();
+	Memory.mem_copy(&result, &temp, sizeof(temp));
+	return (result);
 }
 
 u32 GetGpuNum()
