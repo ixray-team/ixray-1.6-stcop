70fa9a715845cb8dd5cc611ca7e30b653f442316
ForserX <sv3nk@yandex.ru>
2025-08-08 18:59:25 +0300

    Add more stacktrace for luabind

--- src/xrScripts/script_engine.cpp
+++ src/xrScripts/script_engine.cpp
@@ -82,11 +82,12 @@ int CScriptEngine::lua_pcall_failed(lua_State* L)
 	return (LUA_ERRRUN);
 }
 
-void lua_cast_failed					(lua_State *L, LUABIND_TYPE_INFO info)
+void lua_cast_failed(lua_State *L, LUABIND_TYPE_INFO info)
 {
+	g_pScriptEngine->print_stack();
 	CScriptEngine::print_output	(L,"",LUA_ERRRUN);
 
-	Debug.fatal				(DEBUG_INFO,"LUA error: cannot cast lua value to %s",info->name());
+	Debug.fatal(DEBUG_INFO,"LUA error: cannot cast lua value to %s",info->name());
 }
 
 void CScriptEngine::setup_callbacks		()
