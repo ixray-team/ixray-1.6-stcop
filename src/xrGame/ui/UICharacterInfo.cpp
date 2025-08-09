9e35da1380ad16c11dadd86ff5a9d9d733fd9a49
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-08-09 22:13:42 +0300

    Refactor outfit params
    # Conflicts:
    #	src/xrGame/ui/UIOutfitInfo.cpp
    #	src/xrGame/ui/UIOutfitInfo.h
    #	src/xrUI/UIXmlInit.h

--- src/xrGame/ui/UICharacterInfo.cpp
+++ src/xrGame/ui/UICharacterInfo.cpp
@@ -273,6 +273,7 @@ void CUICharacterInfo::InitCharacter(u16 id)
 		{
 			CUIStatic* pItem = new CUIStatic();
 			pItem->SetWidth(pUIBio->GetDesiredChildWidth());
+			pItem->SetTextComplexMode(true);
 			pItem->SetText(chInfo.Bio().c_str());
 			pItem->AdjustHeightToText();
 			pUIBio->AddWindow(pItem, true);
