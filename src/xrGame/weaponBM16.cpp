5d2ee4ec128f44def22860d7da6f7abd468eafb1
Shtrecker <gogels235@gmail.com>
2025-08-11 16:18:44 +0300

    Add alternate reload for `CWeaponBM16`

--- src/xrGame/weaponBM16.cpp
+++ src/xrGame/weaponBM16.cpp
@@ -7,6 +7,8 @@ CWeaponBM16::~CWeaponBM16()
 
 void CWeaponBM16::Load(LPCSTR section)
 {
+	m_bUseAltReloadSystem = READ_IF_EXISTS(pSettings, r_bool, section, "use_alt_reload_system", false);
+
 	inherited::Load(section);
 }
 
@@ -14,28 +16,176 @@ void CWeaponBM16::LoadSounds(LPCSTR section)
 {
 	inherited::LoadSounds(section);
 
-	m_sounds.LoadSound(section, "snd_reload_1", "sndReload1", true, m_eSoundShot);
+	if (m_bUseAltReloadSystem)
+	{
+		m_sounds.LoadSound(section, "snd_reload_only", "sndReloadOnly", true, m_eSoundReload);
+		m_sounds.LoadSound(section, "snd_reload_only_ammochange", "sndReloadOnlyAmmochange", true, m_eSoundReload);
+		m_sounds.LoadSound(section, "snd_changecartridgetype_one", "sndChangeCartridgeTypeOne", true, m_eSoundReload);
+		m_sounds.LoadSound(section, "snd_changecartridgetype_only", "sndChangeCartridgeTypeOnly", true, m_eSoundReload);
+		m_sounds.LoadSound(section, "snd_changecartridgetype_one_only", "sndChangeCartridgeTypeOneOnly", true, m_eSoundReload);
+	}
+	else
+	{
+		m_sounds.LoadSound(section, "snd_reload_1", "sndReload1", true, m_eSoundShot);
+	}
 }
 
 void CWeaponBM16::PlayReloadSound()
 {
-	if (m_magazine.size() == 1)	
-		PlaySound	("sndReload1",get_LastFP());
-	else						
-		PlaySound	("sndReload",get_LastFP());
+	if (m_bUseAltReloadSystem)
+	{
+		LPCSTR sound = "sndReload";
+
+		if (GetAmmoElapsed() == 0)
+		{
+			sound = "sndReloadEmpty";
+		}
+
+		u8 get_target_ammotype = GetTargetAmmoType();
+
+		if (GetAmmoCount(get_target_ammotype) < 2 && !unlimited_ammo())
+		{
+			if (!IsChangeAmmoType())
+			{
+				sound = "sndReloadOnly";
+			}
+			else
+			{
+				sound = "sndReloadOnlyAmmochange";
+			}
+		}
+
+		if (IsChangeAmmoType() && GetAmmoElapsed() > 0)
+		{
+			if (GetAmmoCount(get_target_ammotype) < 2 && !unlimited_ammo())
+			{
+				if (GetAmmoElapsed() == 1)
+				{
+					sound = "sndChangeCartridgeTypeOneOnly";
+				}
+				else
+				{
+					sound = "sndChangeCartridgeTypeOnly";
+				}
+			}
+			else
+			{
+				if (GetAmmoElapsed() == 1)
+				{
+					sound = "sndChangeCartridgeTypeOne";
+				}
+				else
+				{
+					sound = "sndChangeCartridgeType";
+				}
+			}
+		}
+
+		if (IsMisfire())
+		{
+			sound = "sndReloadMis";
+		}
+
+		PlaySound(sound, get_LastFP());
+	}
+	else
+	{
+		if ((GetAmmoElapsed() == 1 || !HaveCartridgeInInventory(2)) && (m_set_next_ammoType_on_reload == undefined_ammo_type || m_ammoType == m_set_next_ammoType_on_reload))
+		{
+			PlaySound("sndReload1", get_LastFP());
+		}
+		else
+		{
+			PlaySound("sndReload", get_LastFP());
+		}
+	}
+}
+
+shared_str CWeaponBM16::SetCurrentReloadAnimation()
+{
+	shared_str anim = "anm_reload";
+
+	string16 new_suffix = {};
+
+	if (ParentIsActor())
+	{
+		xr_sprintf(new_suffix, "_%d", iAmmoElapsed);
+
+		m_iAmmoCountToReload = iMagazineSize;
+
+		if (m_bUseAltReloadSystem)
+		{
+			if (IsMisfire() && !AddSuffixName(anim, "_jammed") || !IsMisfire())
+			{
+				u8 get_target_ammotype = GetTargetAmmoType();
+
+				if (GetAmmoElapsed() == 0)
+				{
+					if (GetAmmoCount(get_target_ammotype) < 2 && !unlimited_ammo())
+					{
+						if (!IsChangeAmmoType())
+						{
+							AddSuffixName(anim, "_only");
+						}
+						else
+						{
+							AddSuffixName(anim, "_only_ammochange");
+						}
+
+						m_iAmmoCountToReload = 1;
+					}
+				}
+				else if (IsChangeAmmoType())
+				{
+					AddSuffixName(anim, "_ammochange");
+
+					if (GetAmmoCount(get_target_ammotype) < 2 && !unlimited_ammo())
+					{
+						AddSuffixName(anim, "_only");
+						m_iAmmoCountToReload = 1;
+					}
+				}
+			}
+		}
+		else
+		{
+			if (IsMisfire() && !AddSuffixName(anim, "_jammed") || !IsMisfire())
+			{
+				if ((GetAmmoElapsed() == 1 || !HaveCartridgeInInventory(2)) && (m_set_next_ammoType_on_reload == undefined_ammo_type || m_ammoType == m_set_next_ammoType_on_reload))
+				{
+					anim = "anm_reload_1";
+				}
+				else
+				{
+					anim = "anm_reload_2";
+				}
+			}
+		}
+
+		AddSuffixName(anim, new_suffix);
+	}
+	else
+	{
+		if ((GetAmmoElapsed() == 1 || !HaveCartridgeInInventory(2)) && (m_set_next_ammoType_on_reload == undefined_ammo_type || m_ammoType == m_set_next_ammoType_on_reload))
+		{
+			anim = "anm_reload_1";
+		}
+		else
+		{
+			anim = "anm_reload_2";
+		}
+	}
+
+	return anim;
 }
 
 void CWeaponBM16::PlayAnimReload()
 {
 	UpdateAmmoBones(m_ammo_bones_mag, iAmmoElapsed, m_ammoType);
-	bool b_both = HaveCartridgeInInventory(2);
 
-	VERIFY(GetState()==eReload);
+	VERIFY(GetState() == eReload);
 	
-	if ((m_magazine.size() == 1 || !b_both) && (m_set_next_ammoType_on_reload == undefined_ammo_type || m_ammoType == m_set_next_ammoType_on_reload))
-		PlayHUDMotion("anm_reload_1", TRUE, GetState());
-	else
-		PlayHUDMotion("anm_reload_2", TRUE, GetState());
+	PlayHUDMotion(SetCurrentReloadAnimation(), TRUE, GetState());
 }
 
 shared_str CWeaponBM16::SetCurrentShootAnimation()
@@ -43,7 +193,7 @@ shared_str CWeaponBM16::SetCurrentShootAnimation()
 	shared_str anim = (HudAnimationExist("anm_shot_2") || HudAnimationExist("anm_shot_1")) ? "anm_shot" : "anm_shoot";
 	string16 new_suffix = {};
 
-	if (H_Parent() && H_Parent() == Level().CurrentControlEntity())
+	if (ParentIsActor())
 	{
 		xr_sprintf(new_suffix, "_%d", iAmmoElapsed);
 
@@ -62,7 +212,7 @@ shared_str CWeaponBM16::SetCurrentShootAnimation()
 	}
 	else
 	{
-		xr_sprintf(new_suffix, "%s%s", anim.c_str(), "_2");
+		xr_sprintf(new_suffix, "%s_%d", *anim, iAmmoElapsed);
 		anim = new_suffix;
 	}
 
@@ -73,9 +223,9 @@ shared_str CWeaponBM16::SetCurrentStateAnimation(const shared_str& first_name)
 {
 	shared_str anim = first_name;
 
-	string16 new_suffix = {};
+	string64 new_suffix = {};
 
-	if (H_Parent() && H_Parent() == Level().CurrentControlEntity())
+	if (ParentIsActor())
 	{
 		xr_sprintf(new_suffix, "_%d", iAmmoElapsed);
 
@@ -94,7 +244,7 @@ shared_str CWeaponBM16::SetCurrentStateAnimation(const shared_str& first_name)
 	}
 	else
 	{
-		xr_sprintf(new_suffix, "%s%s", anim.c_str(), "_2");
+		xr_sprintf(new_suffix, "%s_%d", *anim, iAmmoElapsed);
 		anim = new_suffix;
 	}
 
--- src/xrGame/weaponBM16.h
+++ src/xrGame/weaponBM16.h
@@ -19,11 +19,13 @@ protected:
 	virtual void	PlayReloadSound					();
 	virtual shared_str SetCurrentStateAnimation(const shared_str& first_name);
 	virtual shared_str SetCurrentShootAnimation();
+	virtual shared_str SetCurrentReloadAnimation();
 
 	virtual CWeaponBM16* cast_weapon_bm16() { return this; }
 
-	virtual int GetMagCapacity() override { int size = m_iAmmoCountToReload; m_iAmmoCountToReload = 2; return size; }
+	virtual int GetMagCapacity() override { int size = m_iAmmoCountToReload; m_iAmmoCountToReload = iMagazineSize; return size; }
 
+	bool m_bUseAltReloadSystem = false;
 	int m_iAmmoCountToReload = 2;
 
 	DECLARE_SCRIPT_REGISTER_FUNCTION
