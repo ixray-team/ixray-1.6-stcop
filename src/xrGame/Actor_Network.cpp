ca83d875666c61d8e0578c4767335c3d2d506b42
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-08-17 17:12:53 +0300

    Add saves for inventory & PDA status

--- src/xrGame/Actor_Network.cpp
+++ src/xrGame/Actor_Network.cpp
@@ -1437,6 +1437,7 @@ void CActor::save(NET_Packet &output_packet)
 	output_packet.w_stringZ(g_quick_use_slots[3]);
 
 	output_packet.w_u8(u8(m_inventory_disabled));
+	output_packet.w_u8(u8(m_pda_disabled));
 }
 
 void CActor::load(IReader &input_packet)
@@ -1461,6 +1462,7 @@ void CActor::load(IReader &input_packet)
 	input_packet.r_stringZ(g_quick_use_slots[3], sizeof(g_quick_use_slots[3]));
 
 	set_inventory_disabled(!!input_packet.r_u8());
+	set_pda_disabled(!!input_packet.r_u8());
 }
 
 #ifdef DEBUG
