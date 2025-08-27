6718bac4ebe3fd6fc40bea81393e154757b2457f
Shtrecker <gogels235@gmail.com>
2025-08-27 21:45:28 +0300

    Move `XRay::Concepts` to `Concepts.h`

--- src/xrCore/FormatParsers/json/JsonSerialize.h
+++ src/xrCore/FormatParsers/json/JsonSerialize.h
@@ -1,24 +1,10 @@
 #pragma once
 #include <json/json.hpp>
+#include "../../Concepts.h"
 
 #ifdef XRGAME_EXPORTS
 #	error Don't use in Game code! Only for utils!
 #endif
-
-namespace XRay::Concepts
-{
-	template <typename T>
-	concept CharPtr = std::is_pointer_v<T> && std::same_as<std::remove_pointer_t<T>, char>;
-
-	template <typename T>
-	concept CharArray = std::is_array_v<T> && std::same_as<std::remove_extent_t<T>, char>;
-
-	template <typename T>
-	concept XRayString = std::same_as<std::remove_cvref_t<T>, xr_string> || std::same_as<std::remove_cvref_t<T>, shared_str>;
-
-	template <typename T>
-	concept Arithmetic = std::integral<T> || std::floating_point<T>;
-}
 
 class XRCORE_API CJsonSerializer
 {
