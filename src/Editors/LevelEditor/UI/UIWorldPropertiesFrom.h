ce04326f0c63fa8a65cdd1294f5a68a01d79204c
ForserX <sv3nk@yandex.ru>
2025-08-29 15:30:58 +0300

    Implement texture channels viewer

--- src/Editors/LevelEditor/UI/UIWorldPropertiesFrom.h
+++ src/Editors/LevelEditor/UI/UIWorldPropertiesFrom.h
@@ -8,4 +8,15 @@ public:
 	virtual void Draw();
 	IC void Open() { bOpen = true; }
 	IC void Close() { bOpen = false; }
+
+private:
+    ref_texture m_dds_texture;
+
+    bool m_show_r;
+    bool m_show_g;
+    bool m_show_b;
+    bool m_show_a;
+
+    u32 m_texture_width;
+    u32 m_texture_height;
 };
