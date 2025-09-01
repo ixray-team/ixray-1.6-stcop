46a1b3b9428f924ddd9aa311f2374c024f75cb8d
Drombeys <dima.zolotov02@gmail.com>
2025-09-01 15:12:10 +0300

    Схлопнуть с луа саб директориям

--- src/xrGame/fs_registrator_script.cpp
+++ src/xrGame/fs_registrator_script.cpp
@@ -110,10 +110,10 @@ FS_file_list_ex::FS_file_list_ex(LPCSTR path, u32 flags, LPCSTR mask)
 	FS_Path* P = FS.get_path(path);
 	P->m_Flags.set	(FS_Path::flNeedRescan,TRUE);
 	FS.m_Flags.set	(CLocatorAPI::flNeedCheck,TRUE);
-	FS.rescan_pathes();
+	//FS.rescan_pathes();
 	FS.IsAddonPhase = true;
 	FS.get_path("$arch_dir_addons$")->m_Flags.set(FS_Path::flNeedRescan, TRUE);
-	FS.rescan_pathes();
+	//FS.rescan_pathes();
 	FS.IsAddonPhase = false;
 
 	FS_FileSet		files;
