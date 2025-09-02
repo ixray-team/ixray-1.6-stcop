81d9f750da441b11eefa329f31cb853c29b79063
ForserX <sv3nk@yandex.ru>
2025-09-02 23:36:54 +0300

    Fix rendering wallmarks in LE

--- src/Editors/LevelEditor/UI/UITopBarForm.h
+++ src/Editors/LevelEditor/UI/UITopBarForm.h
@@ -1,5 +1,6 @@
 #pragma once
-class UITopBarForm :public IEditorWnd
+class UITopBarForm :
+	public IEditorWnd
 {
 public:
 	UITopBarForm();
@@ -7,28 +8,42 @@ public:
 	virtual void Draw();
 	bool VerifySpaceRestrictors()const { return m_VerifySpaceRestrictors; }
 	bool UseCameraPosForActor = false;
+
 private:
+	u32 m_timeUndo;
+	u32 m_timeRedo;
 
-	void ClickUndo(); ref_texture m_tUndo; u32 m_timeUndo;
-	void ClickRedo(); ref_texture m_tRedo; u32 m_timeRedo;
+	ref_texture m_tReloadConfigs;
+	ref_texture m_tCForm;
+	ref_texture m_tAIMap;
+	ref_texture m_tGGraph;
+	ref_texture m_tPlayInEditor;
+	ref_texture m_tTerminated;
+	ref_texture m_tPlayPC;
+	ref_texture m_tPlayCleanGame;
+	ref_texture	m_PreferencesIcon;
 
-	void ClickNew(); ref_texture m_tNew;
-	void ClickOpen(); ref_texture m_tOpen;
-	void ClickSave(); ref_texture m_tSave;
-
-	void ClickReloadConfigs();  ref_texture m_tReloadConfigs;
-	void ClickOpenGameData();  ref_texture m_tOpenGameData;
-
-	void ClickCForm();  ref_texture m_tCForm;
-	void ClickAIMap();  ref_texture m_tAIMap;
-	void ClickGGraph();  ref_texture m_tGGraph;
-	void ClickPlayInEditor();  ref_texture m_tPlayInEditor;
-	void ClickBuildAndMake(); ref_texture m_tBuildAndMake;
-	void ClickTerminated();  ref_texture m_tTerminated;
-	void ClickPlayPC();  ref_texture m_tPlayPC;
-	void ClickPlayCleanGame();  ref_texture m_tPlayCleanGame;
 	bool m_VerifySpaceRestrictors;
-    bool m_Simulate;
+	bool m_Simulate;
 
-	void ClickPreferences(); ref_texture	m_PreferencesIcon;
+	void ClickUndo();
+	void ClickRedo();
+
+	void ClickNew();
+	void ClickOpen();
+	void ClickSave();
+
+	void ClickReloadConfigs();
+	void ClickOpenGameData();
+
+	void ClickCForm();
+	void ClickAIMap();
+	void ClickGGraph();
+	void ClickPlayInEditor();
+	void ClickBuildAndMake();
+	void ClickTerminated();
+	void ClickPlayPC();
+	void ClickPlayCleanGame();
+
+	void ClickPreferences();
 };
