f4ec5e0a3f7b3ce2329d52d0518de6c8d312fc4c
Hozar2002 <hoz2002team@gmail.com>
2025-09-03 13:37:26 +0300

    Improve reflections quality

--- gamedata/shaders/d3d11/sslr_render.ps.hlsl
+++ gamedata/shaders/d3d11/sslr_render.ps.hlsl
@@ -12,7 +12,7 @@ Texture3D s_blue_noise;
 // TODO: Это можно упростить потом
 float3 TangentToWorld(in float3 N, in float3 H)
 {
-    float3 UpVector = abs(N.z) < 0.999 ? float3(0.0, 0.0, 1.0) : float3(1.0, 0.0, 0.0);
+    float3 UpVector = abs(N.y) < 0.999 ? float3(0.0, 1.0, 0.0) : float3(0.0, 0.0, -1.0);
     float3 T = normalize(cross(UpVector, N));
     float3 B = cross(N, T);
 				 
@@ -57,36 +57,43 @@ float4 main(PSInput I) : SV_Target
 	float3 Enviroment = reflect(O.View, O.Normal) * fog_params.z;
 	
 #ifdef USE_VASYAN_CUTOFF
-	O.Roughness = min(O.Roughness, 0.5f);
+	// O.Roughness = min(O.Roughness, 0.5f);
 #endif
 	
 #ifndef USE_JITTER_FOR_ENV
-	if(O.Depth > 0.02f && O.Roughness > 0.5f) {
-		return float4(Enviroment, 0.0f);
-	}
+	// if(O.Depth > 0.02f && O.Roughness > 0.5f) {
+		// return float4(Enviroment, 0.0f);
+	// }
 #endif
+
+// O.Roughness = 1.0f;
 	
 	float3 ReflectPoint = GbufferGetPointRealUnjitter(I.texcoord.xy, O.Depth);
 	float3 ViewVec = O.View;
 	
 	float2 Jitter = s_blue_noise[uint3(uint2(I.hpos.xy) % 128, uint(m_taa_jitter.w) % 32)].xy;
-	Jitter.y *= 0.3f; // Bias like screen space stochastic reflections 2015
+	Jitter.y *= 0.7f; // Bias like screen space stochastic reflections 2015
 	
 	// O.Roughness = saturate(O.Roughness * 1.1f - 0.1f);
 	
+	//O.Normal.xyz = normalize(normalize(cross(ddx(O.PointReal.xyz), ddy(O.PointReal.xyz))) + O.Normal);
 	float4 H = ImportanceSampleGGX(O.Normal, Jitter, O.Roughness);
 	
-	// H.xyz = normalize(cross(ddx(O.PointReal.xyz), ddy(O.PointReal.xyz)));
 	// H.xyz = O.Normal;
 	
 	float3 RefRef = reflect(ViewVec, H.xyz);
 	
+	if (dot(RefRef, O.Normal) < 0.0f)
+    {
+        RefRef = normalize(RefRef + O.Normal * 0.5f);
+    }
+	
 #ifdef USE_JITTER_FOR_ENV
 	Enviroment = RefRef * fog_params.z;
 	
-	if(O.Depth > 0.02f && O.Roughness > 0.5f) {
-		return float4(Enviroment, 0.0f);
-	}
+	// if(O.Depth > 0.02f && O.Roughness > 0.5f) {
+		// return float4(Enviroment, 0.0f);
+	// }
 #endif
 	
 	float3 StartPoint = ReflectPoint * 0.996f;
