f08863d3f4d29d4766260b859311a5b5104bcbe3
mnelenpridumivat <ragrind@mail.ru>
2025-09-15 15:53:01 +0300

    Optimize prefixes

--- src/Editors/ParticleEditor/UI_ParticleTools.h
+++ src/Editors/ParticleEditor/UI_ParticleTools.h
@@ -59,8 +59,8 @@ class CParticleTool:
 
     void 		OnControlClick		(ButtonValue* sender, bool& bDataModified, bool& bSafe);
 
-	LPCSTR InsertBeforeLast(LPSTR buffer, u32 buf_size, LPCSTR path, LPCSTR insert_str);
-	EEditMode GetAffectedItemType(LPCSTR path);
+	//LPCSTR InsertBeforeLast(LPSTR buffer, u32 buf_size, LPCSTR path, LPCSTR insert_str);
+	//EEditMode GetAffectedItemType(LPCSTR path);
 	
 public:
     virtual void    	RemoveAction(u32 idx, bool bForced=false) {remove_action_num=idx;m_Flags.set(flRemoveAction,TRUE); if (bForced) RealRemoveAction();}
