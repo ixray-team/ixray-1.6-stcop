5f31d73e5ae5994667395ad099cb31ab756c37d6
mnelenpridumivat <ragrind@mail.ru>
2025-09-15 22:10:18 +0300

    Implement particle curve animators
    Added animator ParticleAnimCurve class
    Added reading .pac files
    Added ChooseType for AnimCurve
    Added ColorAnimator, SizeAnimator and VelocityAnimator particle actions
    Added opening PAC Editor window
    Added basic pac visualization in editor
    Added pac keyframe time change
    Added pac keyframe params editor
    Added insert and delete actions
    Added applying editions in PAC editor
    Added "Add PAC" button and fix PAC editor crashing if no keys present
    Implemented animators
    Added PAC ini serialization
    Added PAC validation
    Added Animators PE parameters (+ refactor Compile functions)

---
+++ src/xrParticles/ParticleAnimCurveInterface.h
@@ -0,0 +1,49 @@
+#pragma once
+
+namespace PS
+{
+    class PARTICLES_API IPAC
+    {
+    public:
+
+        virtual ~IPAC(){};
+    
+        virtual Fvector4 GetValueOnIndex(int index) = 0;
+        virtual Fvector4 GetValueOnTime(float time) = 0;
+        virtual float GetMaxTime() const = 0;
+        virtual float GetMinValue() const = 0;
+        virtual float GetMaxValue() const = 0;
+
+        /*
+         *  Based on current index, current time, delta time and reverse + loop flag update current index and time and return current RGBA value
+         *  Faster than externally calculate current time and search for value on time
+         */
+        virtual Fvector4 FastUpdateValue(size_t& CurrentIndex, float& CurrentTime, float dt, bool Loop, bool Reverse = false) = 0;
+    };
+
+    class PARTICLES_API IPACLibrary;
+
+    class PARTICLES_API CPACLibraryWrapper
+    {
+        IPACLibrary* PACLibrary = nullptr;
+
+        CPACLibraryWrapper() = default;
+        
+    public:
+        void SetPACLibrary(IPACLibrary* NewPACLibrary);
+        IPACLibrary* GetPACLibrary(){return this->PACLibrary;}
+
+        static CPACLibraryWrapper& GetInstance();
+
+        CPACLibraryWrapper(const CPACLibraryWrapper&) = delete;
+        CPACLibraryWrapper& operator=(const CPACLibraryWrapper&) = delete;
+        CPACLibraryWrapper(CPACLibraryWrapper&&) = delete;
+        CPACLibraryWrapper& operator=(CPACLibraryWrapper&&) = delete;
+    };
+
+    class PARTICLES_API IPACLibrary
+    {
+    public:
+        virtual PS::IPAC* FindIPAC(LPCSTR name) = 0;
+    };
+}
