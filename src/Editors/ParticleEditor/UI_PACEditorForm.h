ab62f9920060dda6cb77f33e9ba2860c85e6e276
mnelenpridumivat <ragrind@mail.ru>
2025-09-16 21:45:43 +0300

    Add basic pac visualization in editor

--- src/Editors/ParticleEditor/UI_PACEditorForm.h
+++ src/Editors/ParticleEditor/UI_PACEditorForm.h
@@ -14,4 +14,9 @@ public:
 private:
     static UIPACEditorForm* Form;
     PS::CPACDef* EditedPAC = nullptr;
+    
+    xr_vector<float> R_keys_y, G_keys_y, B_keys_y, A_keys_y, keys_x, keys_y_dummy;
+    double LinkXMin = 0.0f, LinkXMax = 1.0f;
+
+    void DrawCurves();
 };
