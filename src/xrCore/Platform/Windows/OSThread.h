0302c38bf21608de8979b8970a05a75896164cc4
ForserX <sv3nk@yandex.ru>
2025-09-16 18:51:53 +0300

    Port `xrNetServer` to Linux

--- src/xrCore/Platform/Windows/OSThread.h
+++ src/xrCore/Platform/Windows/OSThread.h
@@ -1,16 +1,27 @@
 #pragma once
 using ThreadID = HANDLE;
+using ProcessID = DWORD;
 
 namespace Platform
 {
 	XRCORE_API unsigned int GetCoresCount();
 	XRCORE_API ThreadID GetCurrentThread();
-    XRCORE_API size_t GetThreadId(ThreadID ID);
+	XRCORE_API size_t GetThreadId(ThreadID ID);
 	XRCORE_API void SetThreadName(const char* Name);
-    XRCORE_API long AtomicCompareExchange(long* ptr, long expected, long desired);
+	XRCORE_API long AtomicCompareExchange(long* ptr, long expected, long desired);
 
 	inline void SetCurrentThreadHighPriority()
-    {
-        SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
-    }
+	{
+		SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
+	}
+
+	inline void SetCurrentThreadNormalPriority()
+	{
+		SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_BELOW_NORMAL);
+	}
+
+	inline ProcessID GetCurrentProcessId()
+	{
+		return ::GetCurrentProcessId();
+	}
 }
