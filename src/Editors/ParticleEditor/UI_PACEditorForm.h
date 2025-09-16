72c5146627bcad7ebe286c57c9a60fadb7c4609d
mnelenpridumivat <ragrind@mail.ru>
2025-09-16 22:58:28 +0300

    Add pac keyframe time change

--- src/Editors/ParticleEditor/UI_PACEditorForm.h
+++ src/Editors/ParticleEditor/UI_PACEditorForm.h
@@ -14,8 +14,22 @@ public:
 private:
     static UIPACEditorForm* Form;
     PS::CPACDef* EditedPAC = nullptr;
+
+    struct DoubleKey
+    {
+        double Value = 0.0;
+        size_t Index = -1;
+        bool clicked = false;
+        bool hovered = false;
+        bool hold = false;
+        bool held = false;
+
+        //DoubleKey(float v): Value(v), clicked(false), hovered(false), hold(false) {}
+    };
     
-    xr_vector<float> R_keys_y, G_keys_y, B_keys_y, A_keys_y, keys_x, keys_y_dummy;
+    xr_vector<float> R_keys_y, G_keys_y, B_keys_y, A_keys_y, keys_x; //, keys_y_fdummy;
+    xr_vector<double> keys_y_ddummy;
+    xr_vector<DoubleKey> dkeys_x;
     double LinkXMin = 0.0f, LinkXMax = 1.0f;
 
     void DrawCurves();
