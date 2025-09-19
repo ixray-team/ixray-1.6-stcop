b13488f759a73dc4245bfb67f237ad32418a8d7b
Dimakuzmin <scorpion261096@gmail.com>
2025-09-19 12:15:06 +0300

    Add color collection implementation
    Добаил сбор Colors
    Last Code
    Добавил прогонку на Embree Буфера Задач по трейсу

--- src/utils/xrLC_Light/CUDA/CuTrace.cu
+++ src/utils/xrLC_Light/CUDA/CuTrace.cu
@@ -10,26 +10,18 @@
 #define __shared__
 #define __host__
 #endif
- 
-struct ColorsRessult
-{
-	hardware_color Color;
-	int		       Configured;
-	int			   RealProcessed;
-};
 
 struct OPTICK_Params
 {
 	OptixTraversableHandle handle;
 
-	ColorsRessult*		colors;
-	hardware_lighting*  lights;
+	// 
+	hardware_income*	rays;
+	ColorsRessult*		colors;			// Position, Direction, Color
+	hardware_lighting*	lights;
 	int					counts_lights;
-
-	float3 rayOrigin;
-	float3 rayDir;
 };
-
+ 
 extern "C"
 {
 	__constant__ OPTICK_Params g_params;
@@ -47,7 +39,7 @@ enum eType
 	eRGB  = 2
 };
 
-__device__ void RunOptickTask(HardwareVector& P, HardwareVector& N, float Range, float& ret)
+__device__ float RunOptickTask(HardwareVector& P, HardwareVector& N, float Range)
 {
 	const float3 origin = P.getVector3();
 	const float3 dir    = N.getVector3();
@@ -66,37 +58,36 @@ __device__ void RunOptickTask(HardwareVector& P, HardwareVector& N, float Range,
 	);
 
 	if (distance > 0)
-		ret = 1;
+		return  1;
 	else
-		ret = 0;
+		return  0;
 }
  
 
-__device__ void CalculatePoint(hardware_lighting& L, HardwareVector& P, HardwareVector& N, hardware_color& C, int& RealProcessed)
+__device__ void CalculatePoint(hardware_lighting& L, HardwareVector& P, HardwareVector& N, hardware_color& C, int& RealProcessed, int Index)
 {
 	HardwareVector Ldir;
 	HardwareVector Pnew = P;
 	Pnew.Mad_Self(N, 0.01f);
 
-	HardwareVector LightPosition(L.position);
-	HardwareVector LightDirection(L.direction);
-	HardwareVector LightDiffuse(L.diffuse);
+	HardwareVector LightPosition	= { L.position.x, L.position.y, L.position.z };
+	HardwareVector LightDirection	= { L.direction.x, L.direction.y, L.direction.z };
+	HardwareVector LightDiffuse		= { L.diffuse.x, L.diffuse.y,L.diffuse.z };
 
 	bool isSunOrHemi = L.light_type != eType::eRGB;
 	float att = 0;
+	
 	switch (L.type)
 	{
-		case LT_DIRECT:
+ 		case LT_DIRECT:
 		{
 			Ldir.Inverted(LightDirection);
 			float D = Ldir.DotProduct(N);
 			if (D <= 0)
 				return;
 
-			float trace = 1;
-			RunOptickTask(Pnew, Ldir, 1000.f, trace); // todo skip
+			float trace = RunOptickTask(Pnew, Ldir, 1000.f); // todo skip
 			att = isSunOrHemi ? L.energy * trace : D * L.energy * trace;
-			RealProcessed++;
 		}
 		break;
 
@@ -112,10 +103,8 @@ __device__ void CalculatePoint(hardware_lighting& L, HardwareVector& P, Hardware
 				return;
 
 			float R = sqrt(sqD); // from api
-			float trace = 1;
-			RunOptickTask(Pnew, Ldir, R, trace); // todo skip
+			float trace = RunOptickTask(Pnew, Ldir, R); // todo skip
 			float scale = D * L.energy * trace;
-			RealProcessed++;
 
 			if (isSunOrHemi)
 			{
@@ -133,34 +122,34 @@ __device__ void CalculatePoint(hardware_lighting& L, HardwareVector& P, Hardware
 			float sqD = P.DistanceSquared(LightPosition);
 			if (sqD > L.range2)
 				return;
-
+		
 			Ldir.Subtract(LightPosition, P).Normalize_Safe();
 			float D = Ldir.DotProduct(N);
 			if (D <= 0)
 				return;
-
+		
 			D *= -Ldir.DotProduct(LightDirection);
 			if (D <= 0)
 				return;
-
+		
 			float R = sqrt(sqD);
-			float trace = 1;
-			RunOptickTask(Pnew, Ldir, R, trace); // todo skip
-			RealProcessed++;
-
+			float trace = RunOptickTask(Pnew, Ldir, R); // todo skip
+		
 			att = powf(D, 0.125f) * L.energy * trace * (1 - R / L.range);
-
+		
 		}break;
 	}
-	
+	 
+	RealProcessed++;
+
 
 	switch (L.light_type)
 	{
 		case eSun:
-			C.sun += att;
+			C.sun	+= att;
 			break;
 		case eHemi:
-			C.hemi += att;
+			C.hemi	+= att;
 			break;
 		case eRGB:
 			C.rgb.x += att * L.diffuse.x;
@@ -172,41 +161,27 @@ __device__ void CalculatePoint(hardware_lighting& L, HardwareVector& P, Hardware
  
 __device__ void run_tracing_new(int index)
 {
-	HardwareVector P(g_params.rayOrigin);
-	HardwareVector N(g_params.rayDir);
+	hardware_income& Rays = g_params.rays[index];
+	HardwareVector P(Rays.Position);
+	HardwareVector N(Rays.Direction);
 
 	ColorsRessult& Result = g_params.colors[index];
+
 	Result.RealProcessed = 0;
 
-	for (int i = 0; i < g_params.counts_lights; i++)
+	Result.ResultIndex = index;
+ 	for (int i = 0; i < g_params.counts_lights; i++)
 	{
-		hardware_lighting& Light = g_params.lights[i];
-		CalculatePoint(Light, P, N, Result.Color, Result.RealProcessed);
+		hardware_lighting& L = g_params.lights[i];
+		CalculatePoint(L, P, N, Result.Color, Result.RealProcessed, index);
 	}
-
-	Result.Configured += 123;
+	Result.Configured = g_params.counts_lights;
 }
 
 
 // Callers
 extern "C" __global__ void __raygen__rg()
 {
- 	// const float3 origin = g_params.rayOrigin;
-	// const float3 dir    = g_params.rayDir;
- 	// 
-	// float maxT = 1000.0f;
-	// unsigned int distance, faceID;
-	// optixTrace(
-	// 	g_params.handle,
-	// 	origin,
-	// 	dir,
-	// 	0.0f, maxT, 0.0f,           // minT, maxT, time
-	// 	OptixVisibilityMask(255),
-	// 	OPTIX_RAY_FLAG_NONE,
-	// 	0, 1, 0,
-	// 	distance, faceID
-	// );
-	
 	const uint3 launch_idx = optixGetLaunchIndex();
  	run_tracing_new (launch_idx.x);
 }
