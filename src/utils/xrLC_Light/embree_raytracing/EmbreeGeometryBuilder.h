9fadf853d55f8bda6e726571732821e0f4a55090
Dimakuzmin <scorpion261096@gmail.com>
2025-09-19 12:15:07 +0300

    Add materials to EmbreeBuilder

--- src/utils/xrLC_Light/embree_raytracing/EmbreeGeometryBuilder.h
+++ src/utils/xrLC_Light/embree_raytracing/EmbreeGeometryBuilder.h
@@ -24,6 +24,13 @@ struct Triangle
 	}
 };
 
+struct CFormTriangle  
+{
+	u16 MaterialID;
+	u16 Sector;
+};
+
+
 struct IndexedTri
 {
 	uint32_t i1, i2, i3;
@@ -56,9 +63,11 @@ struct IndexedTri
 
 struct TriangleContainer
 {
-  	xr_vector<Fvector>		verts_v;
-	xr_vector<Triangle>		faces_v;
-	xr_vector<Face*>		dummy;
+  	xr_vector<Fvector>				verts_v;
+	xr_vector<Triangle>				faces_v;
+
+	xr_vector<CFormTriangle>		cform_data;
+	xr_vector<Face*>				dummy;
 
 	xr_vector<Fvector>& vertex() { return verts_v; }
 	xr_vector<Triangle>& faces() { return faces_v; }
@@ -68,7 +77,21 @@ struct TriangleContainer
 	// Add Faces
 	size_t AddVertex(Fvector& V);
 	void AddFace	(void* F, Fvector& v1, Fvector& v2, Fvector& v3);
-  	void ClearAll();
+	void AddFaceMaterial (void* F, Fvector& v1, Fvector& v2, Fvector& v3, u16 Material, u16 SectorID);
+	
+	void ClearAll();
+
+	CDB::TRI GetCDBMaterial(u32 IndexTriangle)
+	{
+		CDB::TRI tri;
+		tri.verts[0] = faces_v[IndexTriangle].point1;
+		tri.verts[1] = faces_v[IndexTriangle].point2;
+		tri.verts[2] = faces_v[IndexTriangle].point3;
+
+		tri.material = cform_data[IndexTriangle].MaterialID;
+		tri.sector   = cform_data[IndexTriangle].Sector;
+		return tri;
+	}
 
  	// Removeing Dublicates
 	struct IndexedVertex
@@ -82,13 +105,23 @@ struct TriangleContainer
 	{
 		Fvector v[3];
 		Face* F;
-	};
+
+		u16 material;
+		u16 Sector;
+ 	};
 
 	xr_vector<FaceRaw> raw_faces;
 	void AddFaceRaw(Face* F, const Fvector& v1, const Fvector& v2, const Fvector& v3)
 	{
-		raw_faces.push_back({ {v1, v2, v3}, F });
+		raw_faces.push_back({ {v1, v2, v3}, F, 0, 0 });
 	};
+
+	void AddFaceRawMaterial(Face* F, const Fvector& v1, const Fvector& v2, const Fvector& v3, u16 MaterialID, u16 SectorID)
+	{
+		raw_faces.push_back({ {v1, v2, v3}, F, MaterialID, SectorID });
+	};
+
+
 	void RemoveDublicates();
 	void RemoveDublicatesFaces();
 };
