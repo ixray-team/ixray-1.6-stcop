c31e744d779adbd0f720fc684a64e3937e3bc5c2
FFx0001 <46628552+FFx0001@users.noreply.github.com>
2025-09-19 12:15:08 +0300

    Extend gasmask and raindrop functionality
    Implemented #179
    
    Газ маска и капли дождя теперь доступны для костюмов с запрещенным слотом шлема по умолчанию если включены консольные команды:
        r_use_gasmask и r_use_rain_drops
    
    Добавлены параметры для секции костюмов и шлемов по умолчанию true если не задать
    hud_gas_mask_avaliable = true // доступна ли отрисовка капель дождя
    hud_rain_drops_avaliable = true // доступна ли отрисовка газ маски

--- src/xrGame/CustomOutfit.h
+++ src/xrGame/CustomOutfit.h
@@ -63,6 +63,8 @@ public:
 	shared_str				m_NightVisionSect;
 
 	bool					bIsHelmetAvaliable;
+	bool					bIsHudGasMaskAvialable;		// FFx0001 ++
+	bool					bIsHudRainDropsAvialable;	// FFx0001 ++
 	bool					isDisableChangeSkin = true;
 
 	virtual u32				ef_equipment_type		() const;
