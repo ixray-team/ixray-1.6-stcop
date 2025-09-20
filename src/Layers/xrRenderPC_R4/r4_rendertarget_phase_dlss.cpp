fe9f8276eac86f61db67ca51015ab04f9f7cf7a1
Hozar2002 <hoz2002team@gmail.com>
2025-09-20 14:06:32 +0300

    Скажем дружно - Нахуй нужно

--- src/Layers/xrRenderPC_R4/r4_rendertarget_phase_dlss.cpp
+++ src/Layers/xrRenderPC_R4/r4_rendertarget_phase_dlss.cpp
@@ -40,7 +40,7 @@ bool CRenderTarget::phase_dlss()
 	dlssParams.cameraJitterX = ps_r_taa_jitter_full.x;
 	dlssParams.cameraJitterY = ps_r_taa_jitter_full.y;
 
-	dlssParams.sharpness = ps_r4_cas_sharpening;
+	dlssParams.sharpness = 0.0f;
 
 	dlssParams.frameTimeDelta = std::max(1.0f + EPS_L, float(Device.dwTimeDelta));
 
