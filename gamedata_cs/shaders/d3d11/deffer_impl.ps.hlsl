7909474e4a5c0491ed2c59e82947bb104abfc1cd
Drombeys <dima.zolotov02@gmail.com>
2025-09-21 21:56:24 +0300

    Update Clear Sky res

--- gamedata_cs/shaders/d3d11/deffer_impl.ps.hlsl
+++ gamedata_cs/shaders/d3d11/deffer_impl.ps.hlsl
@@ -110,7 +110,7 @@ void main(p_bumped_new I, out OutStructure O)
     M.Roughness = 1.0f - M.Roughness * 0.9f;
 #endif
 
-#ifdef IGNORE_SNOW_MASK
+#ifdef IGNORE_SNOW_MASK_ON_TERRAIN
 	M.SnowMask = 0.0f;
 #else
     M.SnowMask = 1.0f;
