09a0a9c132afd971f0a68a7b849ee5c5353904dc
mnelenpridumivat <ragrind@mail.ru>
2025-09-23 18:37:57 +0300

    Add universal VERIFY and R_ASSERT

--- src/xrCore/xrDebug_macros.h
+++ src/xrCore/xrDebug_macros.h
@@ -8,15 +8,22 @@
 
 #	define CHECK_OR_EXIT(expr,message)	do {if (!(expr)) ::Debug.do_exit(message);} while (0)
 
+#define GET_5TH_ARG(arg1, arg2, arg3, arg4, arg5, ...) arg5
+#define MACRO_CHOOSER_ASSERT(...)\
+GET_5TH_ARG(__VA_ARGS__, \
+R_ASSERT4, R_ASSERT3, R_ASSERT2, R_ASSERT1, )
+
 #ifdef SHIPPING_BUILD
 #	define R_ASSERT(expr)
+#	define R_ASSERT1(expr)
 #	define R_ASSERT2(expr,e2)
 #	define R_ASSERT3(expr,e2,e3)
 #	define R_ASSERT4(expr,e2,e3,e4)
 #	define R_CHK(expr) expr
 #	define R_CHK2(expr,e2) expr
 #else
-#	define R_ASSERT(expr)				do {static bool ignore_always = false; if (!ignore_always && !(expr)) ::Debug.fail(_TRE(#expr),DEBUG_INFO,ignore_always);} while(0)
+#	define R_ASSERT(...) MACRO_CHOOSER_ASSERT(__VA_ARGS__)(__VA_ARGS__)
+#	define R_ASSERT1(expr)				do {static bool ignore_always = false; if (!ignore_always && !(expr)) ::Debug.fail(_TRE(#expr),DEBUG_INFO,ignore_always);} while(0)
 #	define R_ASSERT2(expr,e2)			do {static bool ignore_always = false; if (!ignore_always && !(expr)) ::Debug.fail(_TRE(#expr),_TRE(e2),DEBUG_INFO,ignore_always);} while(0)
 #	define R_ASSERT3(expr,e2,e3)		do {static bool ignore_always = false; if (!ignore_always && !(expr)) ::Debug.fail(_TRE(#expr),_TRE(e2),_TRE(e3),DEBUG_INFO,ignore_always);} while(0)
 #	define R_ASSERT4(expr,e2,e3,e4)		do {static bool ignore_always = false; if (!ignore_always && !(expr)) ::Debug.fail(_TRE(#expr),_TRE(e2),_TRE(e3),_TRE(e4),DEBUG_INFO,ignore_always);} while(0)
@@ -31,8 +38,12 @@
 #	endif // VERIFY
 
 #	ifdef DEBUG
+#		define MACRO_CHOOSER_VERIFY(...)\
+GET_5TH_ARG(__VA_ARGS__, \
+VERIFY4, VERIFY3, VERIFY2, VERIFY1, )
 #		define NODEFAULT				FATAL("nodefault reached")
-#		define VERIFY(expr)				do {static bool ignore_always = false; if (!ignore_always && !(expr)) ::Debug.fail(#expr,DEBUG_INFO,ignore_always);} while(0)
+#		define VERIFY(...) MACRO_CHOOSER_VERIFY(__VA_ARGS__)(__VA_ARGS__)
+#		define VERIFY1(expr)			do {static bool ignore_always = false; if (!ignore_always && !(expr)) ::Debug.fail(#expr,DEBUG_INFO,ignore_always);} while(0)
 #		define VERIFY2(expr,e2)		do {static bool ignore_always = false; if (!ignore_always && !(expr)) ::Debug.fail(#expr,e2,DEBUG_INFO,ignore_always);} while(0)
 #		define VERIFY3(expr,e2,e3)	do {static bool ignore_always = false; if (!ignore_always && !(expr)) ::Debug.fail(#expr,e2,e3,DEBUG_INFO,ignore_always);} while(0)
 #		define VERIFY4(expr,e2,e3,e4)do {static bool ignore_always = false; if (!ignore_always && !(expr)) ::Debug.fail(#expr,e2,e3,e4,DEBUG_INFO,ignore_always);} while(0)
