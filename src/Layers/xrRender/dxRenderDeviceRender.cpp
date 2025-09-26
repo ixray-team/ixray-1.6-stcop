950fd42796da52967d9cb87106bbed419c5c56da
ForserX <sv3nk@yandex.ru>
2025-09-26 04:11:19 +0300

    Implement RHI Present method

--- src/Layers/xrRender/dxRenderDeviceRender.cpp
+++ src/Layers/xrRender/dxRenderDeviceRender.cpp
@@ -416,8 +416,6 @@ void dxRenderDeviceRender::End()
 {
 #ifndef _EDITOR
 
-	VERIFY	(RDevice);
-
 #ifdef USE_DX11
 	{
 		GPU_EVENT(GAMMA_APPLY);
@@ -447,7 +445,7 @@ void dxRenderDeviceRender::End()
 		MyImGui.AfterRender();
 
 		DebugRenderImpl.m_lines.resize(0);
-#if defined(USE_DX11) && defined(DEBUG_DRAW)
+#if defined(USE_DX11)
 		GPUEvents_EndRendering();
 #endif
 	}
@@ -463,12 +461,7 @@ void dxRenderDeviceRender::End()
 #endif
 
 	PROF_EVENT("Present");
-#ifdef USE_DX11
-	RSwapchain->Present(psDeviceFlags.test(rsVSync) ? 1 : 0, 0);
-#else
-	CHK_DX				(RDevice->EndScene());
-	RDevice->Present( nullptr, nullptr, nullptr, nullptr );
-#endif
+	GRHI->Present();
 #endif
 }
 
