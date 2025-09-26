c4fb2a5a6d2df6cac7811b22068c34236d350d7f
ForserX <sv3nk@yandex.ru>
2025-09-26 03:31:44 +0300

    Implement `ClearTarget` functionality

--- src/Layers/xrRenderPC_R2/r2_rendertarget_phase_smap_D.cpp
+++ src/Layers/xrRenderPC_R2/r2_rendertarget_phase_smap_D.cpp
@@ -37,10 +37,9 @@ void	CRenderTarget::phase_smap_direct		(light* L, u32 sub_phase)
 	else								RCache.set_ColorWriteEnable	( );
 }
 
-void	CRenderTarget::phase_smap_direct_tsh	(light* L, u32 sub_phase)
+void CRenderTarget::phase_smap_direct_tsh(light* L, u32 sub_phase)
 {
-	VERIFY								(RImplementation.o.Tshadows);
-	u32		_clr						= 0xffffffff;	//color_rgba(127,127,12,12);
-	RCache.set_ColorWriteEnable			();
-	CHK_DX								(RDevice->Clear( 0L, nullptr, D3DCLEAR_TARGET,	_clr,	1.0f, 0L));
+	VERIFY(RImplementation.o.Tshadows);
+	RCache.set_ColorWriteEnable();
+	GRHI->ClearTarget(RCache.get_RT(), ERTColor::Black);
 }
