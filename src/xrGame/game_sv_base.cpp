d9958d8b7e70270fa8b43820ec781f5ef1aacb02
ForserX <sv3nk@yandex.ru>
2025-09-27 05:55:00 +0300

    Cleanup

--- src/xrGame/game_sv_base.cpp
+++ src/xrGame/game_sv_base.cpp
@@ -15,8 +15,6 @@
 #include "debug_renderer.h"
 #include "xrGameSpyServer.h"
 
-ENGINE_API	bool g_dedicated_server;
-
 #define			MAPROT_LIST_NAME		"maprot_list.ltx"
 string_path		MAPROT_LIST		= "";
 BOOL	net_sv_control_hit	= FALSE		;
