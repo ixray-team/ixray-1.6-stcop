27b59923a063026abff9d11f917a4bcf3d864a39
ForserX <sv3nk@yandex.ru>
2025-09-29 01:35:10 +0300

    фикс билд

--- src/Editors/xrECore/Editor/EStats.cpp
+++ src/Editors/xrECore/Editor/EStats.cpp
@@ -159,9 +159,6 @@ void CEStats::Show()
     DPS.polys = 0;
     DPS.verts = 0;
     DPS.calls = 0;
-    DPS.vs = 0;
-    //	DPS.vb		= 0;
-    //	DPS.ib		= 0;
 
     dwLevelSelFaceCount = 0;
     dwLevelSelVertexCount = 0;
