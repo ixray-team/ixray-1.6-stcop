3947f86fb85a511506d4308686c10a69a7908d63
ForserX <sv3nk@yandex.ru>
2025-10-06 14:04:18 +0300

    Render cleanup

--- src/Layers/xrRenderDX10/DX10 Rain/dx10RainBlender.cpp
+++ src/Layers/xrRenderDX10/DX10 Rain/dx10RainBlender.cpp
@@ -8,8 +8,6 @@ void CBlender_rain::Compile(CBlender_Compile& C)
 	switch (C.iElement) 
 	{
 	case 0:			// Test
-		//C.r_Pass	("stub_notransform_2uv", "rain_layer", false,	TRUE,	FALSE, TRUE, D3DBLEND_ONE, D3DBLEND_ONE);
-		//C.r_Pass	("stub_notransform_2uv", "rain_layer", false,	TRUE,	FALSE, TRUE, D3DBLEND_SRCALPHA, D3DBLEND_INVSRCALPHA);
 		C.r_Pass	("stub_notransform_2uv", "rain_layer", false,	TRUE,	FALSE, FALSE);
 		C.PassSET_ZB		(TRUE,FALSE,TRUE	);	// force inverted Z-Buffer
 		
@@ -26,11 +24,8 @@ void CBlender_rain::Compile(CBlender_Compile& C)
 		C.r_dx10Sampler		("smp_material");
 		C.r_dx10Sampler		("smp_linear");
 		jitter				(C);
+
 		C.r_dx10Sampler		("smp_smap");
-
-//		C.r_dx10Texture		("s_water",	"water\\water_water");
-
-		//C.r_dx10Texture		("s_water",	"water\\water_studen");
 		C.r_dx10Texture		("s_water",	"water\\water_normal");
 
 		C.r_End				();
@@ -38,15 +33,12 @@ void CBlender_rain::Compile(CBlender_Compile& C)
 		break;
 
 	case 1:			// Patch normals
-		//C.r_Pass	("stub_notransform_2uv", "rain_layer", false,	TRUE,	FALSE, TRUE, D3DBLEND_ONE, D3DBLEND_ONE);
-		//C.r_Pass	("stub_notransform_2uv", "rain_layer", false,	TRUE,	FALSE, TRUE, D3DBLEND_SRCALPHA, D3DBLEND_INVSRCALPHA);
 		C.r_Pass	("stub_notransform_2uv", "rain_patch_normal", false,	TRUE,	FALSE, FALSE);
 		C.PassSET_ZB		(TRUE,FALSE,TRUE	);	// force inverted Z-Buffer
 
 		C.r_dx10Texture		("s_position",		r2_RT_P);
 		C.r_dx10Texture		("s_normal",		r2_RT_N);
 		C.r_dx10Texture		("s_material",		r2_material);
-		//C.r_dx10Texture		("s_accumulator",	r2_RT_accum);
 		C.r_dx10Texture		("s_lmap",			r2_sunmask);
 		C.r_dx10Texture		("s_smap",			r2_RT_smap_depth);
 
@@ -58,15 +50,9 @@ void CBlender_rain::Compile(CBlender_Compile& C)
 		C.r_dx10Sampler		("smp_linear");
 		C.r_dx10Sampler		("smp_base");
 		jitter				(C);
+
 		C.r_dx10Sampler		("smp_smap");
-
-		//		C.r_dx10Texture		("s_water",	"water\\water_water");
-
-		//C.r_dx10Texture		("s_water",	"water\\water_studen");
-		//C.r_dx10Texture		("s_water",	"water\\water_normal");
-
 		C.r_dx10Texture		("s_water",	"water\\water_SBumpVolume");
-		//C.r_dx10Texture		("s_waterFall",	"water\\water_normal");
 		C.r_dx10Texture		("s_waterFall",	"water\\water_flowing_nmap");
 
 		C.r_End				();
@@ -74,15 +60,11 @@ void CBlender_rain::Compile(CBlender_Compile& C)
 		break;
 
 	case 2:			// Apply normals
-		//C.r_Pass	("stub_notransform_2uv", "rain_layer", false,	TRUE,	FALSE, TRUE, D3DBLEND_ONE, D3DBLEND_ONE);
-		//C.r_Pass	("stub_notransform_2uv", "rain_layer", false,	TRUE,	FALSE, TRUE, D3DBLEND_SRCALPHA, D3DBLEND_INVSRCALPHA);
 		C.r_Pass	("stub_notransform_2uv", "rain_apply_normal", false,	TRUE,	FALSE, FALSE);
 		C.PassSET_ZB		(TRUE,FALSE,TRUE	);	// force inverted Z-Buffer
 
 		C.r_dx10Texture		("s_position",		r2_RT_P);
-		//C.r_dx10Texture		("s_normal",		r2_RT_N);
 		C.r_dx10Texture		("s_material",		r2_material);
-		//C.r_dx10Texture		("s_accumulator",	r2_RT_accum);
 		C.r_dx10Texture		("s_lmap",			r2_sunmask);
 		C.r_dx10Texture		("s_smap",			r2_RT_smap_depth);
 
@@ -90,17 +72,9 @@ void CBlender_rain::Compile(CBlender_Compile& C)
 		C.r_dx10Sampler		("smp_material");
 		C.r_dx10Sampler		("smp_linear");
 		jitter				(C);
+
 		C.r_dx10Sampler		("smp_smap");
-
-		//		C.r_dx10Texture		("s_water",	"water\\water_water");
-
-		//C.r_dx10Texture		("s_water",	"water\\water_studen");
 		C.r_dx10Texture		("s_patched_normal",	r2_RT_accum);
-
-		//	Normal can be packed into R and G
-		//if (RImplementation.o.dx10_gbuffer_opt)
-		//	C.r_ColorWriteEnable( true, true, false, false );
-		//else
 		C.r_ColorWriteEnable( true, true, true, false );
 
 		C.r_End				();
@@ -108,15 +82,11 @@ void CBlender_rain::Compile(CBlender_Compile& C)
 		break;
 
 	case 3:			// Apply gloss
-		//C.r_Pass	("stub_notransform_2uv", "rain_layer", false,	TRUE,	FALSE, TRUE, D3DBLEND_ONE, D3DBLEND_ONE);
-		//C.r_Pass	("stub_notransform_2uv", "rain_layer", false,	TRUE,	FALSE, TRUE, D3DBLEND_SRCALPHA, D3DBLEND_INVSRCALPHA);
 		C.r_Pass	("stub_notransform_2uv", "rain_apply_gloss", false,	TRUE,	FALSE, TRUE, D3DBLEND_ONE, D3DBLEND_ONE);
 		C.PassSET_ZB		(TRUE,FALSE,TRUE	);	// force inverted Z-Buffer
 
 		C.r_dx10Texture		("s_position",		r2_RT_P);
-		//C.r_dx10Texture		("s_normal",		r2_RT_N);
 		C.r_dx10Texture		("s_material",		r2_material);
-		//C.r_dx10Texture		("s_accumulator",	r2_RT_accum);
 		C.r_dx10Texture		("s_lmap",			r2_sunmask);
 		C.r_dx10Texture		("s_smap",			r2_RT_smap_depth);
 
@@ -124,14 +94,9 @@ void CBlender_rain::Compile(CBlender_Compile& C)
 		C.r_dx10Sampler		("smp_material");
 		C.r_dx10Sampler		("smp_linear");
 		jitter				(C);
+
 		C.r_dx10Sampler		("smp_smap");
-
-		//		C.r_dx10Texture		("s_water",	"water\\water_water");
-
-		//C.r_dx10Texture		("s_water",	"water\\water_studen");
 		C.r_dx10Texture		("s_patched_normal",	r2_RT_accum);
-
-		//C.r_ColorWriteEnable( false, false, false, true );
 
 		C.RS.SetRS(D3DRS_SRCBLEND,		D3DBLEND_ZERO	);
 		C.RS.SetRS(D3DRS_DESTBLEND,		D3DBLEND_SRCCOLOR	);
