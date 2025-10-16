670bd9c44089a643ae157c8b50148f2207c5d6d5
ForserX <sv3nk@yandex.ru>
2025-10-17 00:44:15 +0300

    Add `GameNetworkingSockets` module

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -1,31 +1,28 @@
 if(NOT TARGET copy_all_dlls)
-    add_custom_target(copy_all_dlls ALL
-        #COMMENT "Copying all DLLs"
-    )
+    add_custom_target(copy_all_dlls ALL)
     set_target_properties(copy_all_dlls PROPERTIES FOLDER "Pre-Build")
 endif()
 
-function(add_imported_lib name include_dir lib_path dll_path)
+function(add_imported_lib name include_dir lib_path dll_paths)
     add_library(${name} INTERFACE IMPORTED)
     set_target_properties(${name} PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${include_dir}"
         INTERFACE_LINK_LIBRARIES "${lib_path}"
     )
 
-    if(EXISTS "${dll_path}")
-        set(target_dir "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>")
+    foreach(dll_file IN LISTS dll_paths)
+        if(EXISTS "${dll_file}")
+            set(target_dir "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/$<CONFIG>")
+            add_custom_command(TARGET copy_all_dlls PRE_BUILD
+                COMMAND ${CMAKE_COMMAND} -E make_directory "${target_dir}"
+                COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                        "${dll_file}" "${target_dir}/"
+            )
+        endif()
+    endforeach()
 
-        add_custom_command(TARGET copy_all_dlls PRE_BUILD
-            COMMAND ${CMAKE_COMMAND} -E make_directory "${target_dir}"
-            COMMAND ${CMAKE_COMMAND} -E copy_if_different
-                    "${dll_path}" "${target_dir}/"
-            #COMMENT "Copying ${dll_path} to ${target_dir}"
-        )
-
-        add_dependencies(${name} copy_all_dlls)
-    endif()
+    add_dependencies(${name} copy_all_dlls)
 endfunction()
-
 
 function(target_copy_dependency TARGET_NAME FILE_PATH)
     if(WIN32)
