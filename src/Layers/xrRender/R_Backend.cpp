572cd5e8c73fe7637250d8c26a8bb7f8e033cd4b
ForserX <sv3nk@yandex.ru>
2025-10-20 10:58:40 +0300

    Cleanup

--- src/Layers/xrRender/R_Backend.cpp
+++ src/Layers/xrRender/R_Backend.cpp
@@ -1,93 +1,13 @@
 #include "stdafx.h"
-
 #include "R_Backend.h"
 
-#ifdef USE_DX11
-#include "../xrRenderDX10/dx10BufferUtils.h"
-#endif // USE_DX11
-
-CBackend			RCache;
+CBackend RCache;
 
 // Create Quad-IB
-#ifdef USE_DX11
-
-// Igor: is used to test bug with rain, particles corruption
-void CBackend::RestoreQuadIBData()
-{
-	// Igor: never seen this corruption for DX10
-	;
-}
-
 void CBackend::CreateQuadIB()
 {
-	static const u32 dwTriCount = 4 * 1024;
-	static const u32 dwIdxCount = dwTriCount * 2 * 3;
-	u16	IndexBuffer[dwIdxCount];
-	u16* Indices = IndexBuffer;
-
-	RHIBufferDesc desc;
-	desc.Size = dwIdxCount * 2;
-	desc.Usage = ERHI_USAGE::USAGE_DEFAULT;
-	desc.Type = ERHI_BUFFER_TYPE::INDEX;
-
-	RHIBufferSubresource subData;
-	subData.pSysMem = IndexBuffer;
-
-	int	Cnt = 0;
-	int	ICnt = 0;
-	for (int i = 0; i < dwTriCount; i++)
-	{
-		Indices[ICnt++] = u16(Cnt + 0);
-		Indices[ICnt++] = u16(Cnt + 1);
-		Indices[ICnt++] = u16(Cnt + 2);
-
-		Indices[ICnt++] = u16(Cnt + 3);
-		Indices[ICnt++] = u16(Cnt + 2);
-		Indices[ICnt++] = u16(Cnt + 1);
-
-		Cnt += 4;
-	}
-
-	QuadIB = GRHI->CreateBuffer(desc, &subData);
-}
-
-#else //USE_DX11
-
-// Igor: is used to test bug with rain, particles corruption
-void CBackend::RestoreQuadIBData()
-{
-	const u32 dwTriCount	= 4*1024;
-	u16		IndexBuffer[dwTriCount * 2 * 3];
-	u16* Indices = IndexBuffer;
-	int	Cnt = 0;
-	int	ICnt = 0;
-	for (int i = 0; i < dwTriCount; ++i)
-	{
-		Indices[ICnt++] = u16(Cnt + 0);
-		Indices[ICnt++] = u16(Cnt + 1);
-		Indices[ICnt++] = u16(Cnt + 2);
-
-		Indices[ICnt++] = u16(Cnt + 3);
-		Indices[ICnt++] = u16(Cnt + 2);
-		Indices[ICnt++] = u16(Cnt + 1);
-
-		Cnt += 4;
-	}
-
-	// Update QuadIB via Map/Unmap
-	RHIMappedSubresource mapped = {};
-	if (QuadIB->Map(ERHI_BUFFER_MAP::WRITE, 0, &mapped))
-	{
-		memcpy(mapped.pData, IndexBuffer, dwTriCount * 2 * 3 * sizeof(u16));
-		QuadIB->Unmap();
-	}
-}
-
-
-void CBackend::CreateQuadIB		()
-{
-	const u32 dwTriCount	= 4*1024;
-	const u32 dwIdxCount	= dwTriCount*2*3;
+	const u32 dwTriCount = 4 * 1024;
+	const u32 dwIdxCount = dwTriCount * 2 * 3;
 	u16 IndexBufferStatic[dwIdxCount];
 	u16* Indices = IndexBufferStatic;
 
@@ -109,8 +29,6 @@ void CBackend::CreateQuadIB		()
 	// Create immutable index buffer with initial data
 	R_ASSERT(RHIUtils::CreateIndexBuffer(&QuadIB, IndexBufferStatic, dwIdxCount * 2));
 }
-
-#endif
 
 // Device dependance
 void CBackend::OnDeviceCreate	()
