efde6901bdc4f7af9ef49f04567f2c0e39445a75
ForserX <sv3nk@yandex.ru>
2025-10-23 12:59:17 +0300

    Add `RHISampleDesc`

--- src/xrRHI/RHIShaderResourceCache.h
+++ src/xrRHI/RHIShaderResourceCache.h
@@ -24,3 +24,32 @@ public:
 	virtual void SetHSResource(u32 uiSlot, IRHIShaderResourceView* pRes) {};
 	virtual void SetCSResource(u32 uiSlot, IRHIShaderResourceView* pRes) {};
 };
+
+class IRHISamplerStateCache
+{
+public:
+	enum
+	{
+		hInvalidHandle = 0xFFFFFFFF
+	};
+
+	//	State handle
+	using HArray = xr_vector<u32>;
+public:
+	virtual ~IRHISamplerStateCache() = default;
+
+	virtual void ClearStateArray() = 0;
+
+	//u32	GetState(D3D_SAMPLER_DESC& desc);
+
+	virtual void VSApplySamplers(HArray& samplers) = 0;
+	virtual void PSApplySamplers(HArray& samplers) = 0;
+	virtual void GSApplySamplers(HArray& samplers) {};
+	virtual void HSApplySamplers(HArray& samplers) {};
+	virtual void DSApplySamplers(HArray& samplers) {};
+	virtual void CSApplySamplers(HArray& samplers) {};
+
+	virtual void SetMaxAnisotropy(UINT uiMaxAniso) = 0;
+	virtual void SetMipLodBias(float mipMapLODBias) = 0;
+	virtual void ResetDeviceState() = 0;
+};
