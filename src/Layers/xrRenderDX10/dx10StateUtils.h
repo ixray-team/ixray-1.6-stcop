7da4125c0c0fbc68a795a07599daed6de195b03c
ForserX <sv3nk@yandex.ru>
2025-10-23 12:59:17 +0300

    Add `RHISampleDesc`

--- src/Layers/xrRenderDX10/dx10StateUtils.h
+++ src/Layers/xrRenderDX10/dx10StateUtils.h
@@ -9,31 +9,30 @@ namespace dx10StateUtils
 	D3D_STENCIL_OP				ConvertStencilOp(D3DSTENCILOP Op);
 	D3D_BLEND					ConvertBlendArg(D3DBLEND Arg);
 	D3D_BLEND_OP				ConvertBlendOp(D3DBLENDOP Op);
-	D3D_TEXTURE_ADDRESS_MODE	ConvertTextureAddressMode(D3DTEXTUREADDRESS Mode);
+	ERHI_TEXTURE_ADDRESS_MODE	ConvertTextureAddressMode(D3DTEXTUREADDRESS Mode);
 
 	//	Set description to default values
 	void	ResetDescription( D3D_RASTERIZER_DESC &desc );
 	void	ResetDescription( D3D_DEPTH_STENCIL_DESC &desc );
 	void	ResetDescription( D3D_BLEND_DESC &desc );
-	void	ResetDescription( D3D_SAMPLER_DESC &desc );
+	void	ResetDescription(RHISampleDesc&desc );
 
 	//	State comparison (memcmp doesn't work due to padding bytes in structure)
 	bool	operator==(const D3D_RASTERIZER_DESC &desc1, const D3D_RASTERIZER_DESC &desc2);
 	bool	operator==(const D3D_DEPTH_STENCIL_DESC &desc1, const D3D_DEPTH_STENCIL_DESC &desc2);
 	bool	operator==(const D3D_BLEND_DESC &desc1, const D3D_BLEND_DESC &desc2);
-	bool	operator==(const D3D_SAMPLER_DESC &desc1, const D3D_SAMPLER_DESC &desc2);
 
 	//	Calculate hash values
 	u32		GetHash( const D3D_RASTERIZER_DESC &desc );
 	u32		GetHash( const D3D_DEPTH_STENCIL_DESC &desc );
 	u32		GetHash( const D3D_BLEND_DESC &desc );
-	u32		GetHash( const D3D_SAMPLER_DESC &desc );
+	u32		GetHash( const RHISampleDesc&desc );
 
 	//	Modify state to meet DX10 automatic modifications
 	void	ValidateState(D3D_RASTERIZER_DESC &desc);
 	void	ValidateState(D3D_DEPTH_STENCIL_DESC &desc);
 	void	ValidateState(D3D_BLEND_DESC &desc);
-	void	ValidateState(D3D_SAMPLER_DESC &desc);
+	void	ValidateState(RHISampleDesc&desc);
 };
 
 #endif	//	dx10StateUtils_included
