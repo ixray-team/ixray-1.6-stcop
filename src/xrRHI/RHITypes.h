6e7cc6bab8a06cc92073e6458b8f330af9708202
ForserX <sv3nk@yandex.ru>
2025-10-23 12:59:17 +0300

    Add `RHISampleDesc`

--- src/xrRHI/RHITypes.h
+++ src/xrRHI/RHITypes.h
@@ -27,3 +27,34 @@ struct RHIInputElementDesc
 	ERHI_INPUT_CLASSIFICATION InputSlotClass;
 	u32 InstanceDataStepRate;
 };
+
+struct RHI_API RHISampleDesc
+{
+	ERHI_FILTER Filter;
+	ERHI_TEXTURE_ADDRESS_MODE AddressU;
+	ERHI_TEXTURE_ADDRESS_MODE AddressV;
+	ERHI_TEXTURE_ADDRESS_MODE AddressW;
+	float MipLODBias;
+	u32 MaxAnisotropy;
+	ERHI_COMPARISON_FUNC ComparisonFunc;
+	float BorderColor[4];
+	float MinLOD;
+	float MaxLOD;
+
+	bool operator==(const RHISampleDesc& desc2) const
+	{
+		if (Filter != desc2.Filter) return false;
+		if (AddressU != desc2.AddressU) return false;
+		if (AddressV != desc2.AddressV) return false;
+		if (AddressW != desc2.AddressW) return false;
+		if (ComparisonFunc != desc2.ComparisonFunc) return false;
+		if (BorderColor[0] != desc2.BorderColor[0]) return false;
+		if (BorderColor[1] != desc2.BorderColor[1]) return false;
+		if (BorderColor[2] != desc2.BorderColor[2]) return false;
+		if (BorderColor[3] != desc2.BorderColor[3]) return false;
+		if (MinLOD != desc2.MinLOD) return false;
+		if (MaxLOD != desc2.MaxLOD) return false;
+
+		return true;
+	}
+};
