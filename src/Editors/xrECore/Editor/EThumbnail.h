8ccd022501b21c89d9f79f7bdafcb1e252ec47ea
ForserX <sv3nk@yandex.ru>
2025-10-24 21:06:03 +0300

    Remove partial dx code into editors

--- src/Editors/xrECore/Editor/EThumbnail.h
+++ src/Editors/xrECore/Editor/EThumbnail.h
@@ -53,7 +53,7 @@ protected:
 public:
 					EImageThumbnail	(LPCSTR src_name, THMType type):ECustomThumbnail(src_name, type){};
 	virtual			~EImageThumbnail();
-	virtual void 	Update			(ID3DBaseTexture*& Texture);
+	virtual void 	Update			(IRHISurface*& Texture);
 //	virtual void 	Draw			(TMxPanel* panel){Irect r; r.set(1,1,1+panel->Width,1+panel->Height); Draw(panel->Canvas->Handle,r);}
     u32*			Pixels			(){return &*m_Pixels.begin();}
     virtual	int		MemoryUsage		(){return 0;};
@@ -84,7 +84,7 @@ public:
         	void    SetValid        () {m_bValid = true;}
 	virtual void	FillProp		(PropItemVec& values, PropValue::TOnChange on_type_change);
 	virtual void	FillInfo		(PropItemVec& values);
-	virtual void 	Update			(ID3DBaseTexture*& Texture) override;
+	virtual void 	Update			(IRHISurface*& Texture) override;
 //	virtual void 	Draw			(TMxPanel* panel){inherited::Draw(panel);}
 
     virtual int		MemoryUsage		();
