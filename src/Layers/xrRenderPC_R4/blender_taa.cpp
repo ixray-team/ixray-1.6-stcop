8ed8288aa1eb2dd95895f6aef329ef47466b1abe
Hozar2002 <hoz2002team@gmail.com>
2025-10-25 15:35:19 +0300

    Add per-object mblur

--- src/Layers/xrRenderPC_R4/blender_taa.cpp
+++ src/Layers/xrRenderPC_R4/blender_taa.cpp
@@ -10,15 +10,30 @@ void CBlender_taa::Compile(CBlender_Compile& C)
 
     switch (C.iElement)
     {
-    case 0:
-        C.r_Pass("stub_fullscreen_triangle", "taa_render", FALSE, FALSE, FALSE);
-		C.r_dx10Texture("s_image", r2_RT_generic0);
-		C.r_dx10Texture("s_image_prev", r2_RT_generic0_prev);	
-		C.r_dx10Texture("s_position", r2_RT_P);
-		C.r_dx10Texture("s_velocity", r2_RT_velocity);
-        C.r_dx10Sampler("smp_rtlinear");
-        C.r_dx10Sampler("smp_nofilter");
-        C.r_End();
-        break;
-	}
+        case 0:
+        {
+            C.r_Pass("stub_fullscreen_triangle", "taa_render", FALSE, FALSE, FALSE);
+            C.r_dx10Texture("s_image", r2_RT_generic0);
+            C.r_dx10Texture("s_image_prev", r2_RT_generic0_prev);
+            C.r_dx10Texture("s_position", r2_RT_P);
+            C.r_dx10Texture("s_velocity", r2_RT_velocity);
+            C.r_dx10Sampler("smp_rtlinear");
+            C.r_dx10Sampler("smp_nofilter");
+            C.r_End();
+            break;
+        }
+        case 1:
+        {
+            C.r_Pass("stub_fullscreen_triangle", "combine_mblur", FALSE, FALSE, FALSE);
+            C.r_dx10Texture("s_image", r2_RT_generic0);
+
+            C.r_dx10Texture("s_velocity", r2_RT_velocity);
+            C.r_dx10Texture("s_position", r2_RT_P);
+
+            C.r_dx10Sampler("smp_rtlinear");
+            C.r_dx10Sampler("smp_nofilter");
+            C.r_End();
+            break;
+        }
+    }
 }
