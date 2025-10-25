11feec5daa27a5d10626e7e194d3e06a16dd9fbf
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-10-25 17:36:21 +0300

    Fix build

--- src/Layers/xrRenderPC_R4/r4_rendertarget_phase_taa.cpp
+++ src/Layers/xrRenderPC_R4/r4_rendertarget_phase_taa.cpp
@@ -22,13 +22,13 @@ void CRenderTarget::phase_taa()
 
     RCache.set_Element(s_taa->E[0]);
 	RCache.set_Geometry(g_combine);
-	RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 3, 0, 1);
+	RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, Offset, 0, 3, 0, 1);
 	
 	//Save previous frame
-	RContext->CopyResource(rt_Generic_0_prev->pSurface, rt_Generic_2->pSurface);
+	GRHI->CopySurface(rt_Generic_0_prev->pSurface, rt_Generic_2->pSurface);
 
 	//Copy to rt_Generic_0... waste more perf...
-	RContext->CopyResource(rt_Generic_0->pSurface, rt_Generic_2->pSurface);
+	GRHI->CopySurface(rt_Generic_0->pSurface, rt_Generic_2->pSurface);
 }
 
 void CRenderTarget::phase_mblur()
@@ -62,7 +62,7 @@ void CRenderTarget::phase_mblur()
 		RCache.set_Element(s_taa->E[1]); RCache.set_Geometry(g_combine);
 		RCache.set_c("mblur_params", mblur_power / i, i, 1.0f / dwWidth, 1.0f / dwHeight);
 
-		RCache.Render(D3DPT_TRIANGLELIST, Offset, 0, 3, 0, 1);
-		RContext->CopyResource(rt_Generic_0->pSurface, rt_Generic_2->pSurface);
+		RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, Offset, 0, 3, 0, 1);
+		GRHI->CopySurface(rt_Generic_0->pSurface, rt_Generic_2->pSurface);
 	}
 }
