8f4251230e06cac089bc50f42fc228c3944f9815
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-10-27 00:26:18 +0300

    Add ability to change item count prefix

--- src/xrGame/ui/UICellCustomItems.cpp
+++ src/xrGame/ui/UICellCustomItems.cpp
@@ -122,7 +122,7 @@ void CUIInventoryCellItem::UpdateItemText()
 
 	if ( count > 1 || helper_count )
 	{
-		xr_sprintf						( str, "%d", count );
+		xr_sprintf						( str, "%s%d", EngineExternal().GetInventoryItemCountPrefix().c_str(), count );
 		m_text->TextItemControl()->SetText	( str );
 		m_text->Show					( true );
 	}
