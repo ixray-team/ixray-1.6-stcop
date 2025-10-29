45556554a3cff9a8c4990d136ffc28c13eea0e0b
ForserX <sv3nk@yandex.ru>
2025-10-29 13:34:56 +0300

    State Manager переехал в RHI. Плюс переписал для него кеш

---
+++ src/xrRHI/RHIStateManager.h
@@ -0,0 +1,39 @@
+#pragma once
+
+enum class ERHI_STATE_CACHE_TYPE
+{
+	RS,
+	DS,
+	BS
+};
+
+class IRHIStateManager
+{
+public:
+	using BindAlphaCallbackDecl = std::function<void(u32)>;
+
+public:
+	virtual ~IRHIStateManager() = default;
+
+	virtual void Apply() = 0;
+	virtual void Reset() = 0;
+	virtual void EnableScissoring(bool enable = true) = 0;
+	virtual void SetStencil(u32 enable, u32 func, u32 ref, u32 mask, u32 writemask, u32 fail, u32 pass, u32 zfail) = 0;
+	virtual void SetDepthEnable(u32 enable) = 0;
+	virtual void SetDepthFunc(u32 func) = 0;
+	virtual void SetColorWriteEnable(u32 mask) = 0;
+	virtual void SetCullMode(u32 mode) = 0;
+	virtual void UnmapConstants() = 0;
+	virtual void BindAlphaRefCallback(const BindAlphaCallbackDecl& Callback) {};
+
+	virtual void SetRasterizerState(void* NewState) {};
+	virtual void SetDepthStencilState(void* NewState) {};
+	virtual void SetBlendState(void* NewState) {};
+	virtual void SetStencilRef(u32 NewStencilRef) {};
+	virtual void SetAlphaRef(u32 NewAlphaRef) {};
+	virtual void* GetCache(ERHI_STATE_CACHE_TYPE Type, void* Desc) { return nullptr; };
+	virtual void OverrideScissoring(bool Override = true, bool Value = true) {};
+
+protected:
+	BindAlphaCallbackDecl BindAlphaCallback;
+};
