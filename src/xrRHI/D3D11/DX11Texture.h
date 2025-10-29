e2f0396b2cccd9ce593f910b8b0c837b693dbb85
ForserX <sv3nk@yandex.ru>
2025-10-29 15:59:02 +0300

    Очистка

--- src/xrRHI/D3D11/DX11Texture.h
+++ src/xrRHI/D3D11/DX11Texture.h
@@ -63,10 +63,6 @@ public:
     
     virtual void* GetRawSRV() override;
     virtual IRHISurface* GetSurface() override;
-    virtual void BindToPixelShader(u32 slot) override;
-    virtual void BindToVertexShader(u32 slot) override;
-    virtual void BindToGeometryShader(u32 slot) override;
-    virtual void BindToComputeShader(u32 slot) override;
 
     virtual void AddRef() override;
     virtual u32 Release() override;
@@ -87,8 +83,6 @@ public:
     
     virtual void* GetRawRTV() override;
     virtual IRHISurface* GetSurface() override;
-    virtual void BindAsRenderTarget(u32 slot = 0) override;
-    virtual void UnbindRenderTarget() override;
     virtual void AddRef() override;
     virtual u32 Release() override;
 
@@ -108,8 +102,6 @@ public:
     
     virtual void* GetRawDSV() override;
     virtual IRHISurface* GetSurface() override;
-    virtual void BindAsDepthStencil() override;
-    virtual void UnbindDepthStencil() override;
 
     virtual void AddRef() override;
     virtual u32 Release() override;
