cd79918ca2fe51d65e2a94fb748108e63ed20f1a
ForserX <sv3nk@yandex.ru>
2025-11-09 04:47:16 +0300

    Add optimized geom for render screen quad
    @LVutner

--- src/Layers/xrRenderPC_R4/r4_rendertarget_phase_screen_postprocess.cpp
+++ src/Layers/xrRenderPC_R4/r4_rendertarget_phase_screen_postprocess.cpp
@@ -6,13 +6,9 @@ bool UseRainDrops = false;
 
 void CRenderTarget::RenderEffect(ScreenPostProcessType postProcessType, bool postProcessMode)
 {
-	u32 Offset = 0;
-	float d_Z = EPS_S;
-	float d_W = 1.0f;
-	constexpr u32 color = color_rgba(0, 0, 0, 255);
-
 	// Set render target
-	if (postProcessMode) {
+	if (postProcessMode)
+	{
 		u_setrt(rt_Back_Buffer_AA, nullptr, nullptr, nullptr);
 	}
 
@@ -21,19 +17,13 @@ void CRenderTarget::RenderEffect(ScreenPostProcessType postProcessType, bool pos
 	RCache.set_Stencil(false);
 
 	// Lock and set vertices
-	FVF::TL* pv = (FVF::TL*)RCache.Vertex.Lock(4, g_combine->vb_stride, Offset);
-	pv->set(0.f, (float)Device.TargetHeight, d_Z, d_W, color, 0.f, 1.f); pv++;
-	pv->set(0.f, 0.f, d_Z, d_W, color, 0.f, 0.f); pv++;
-	pv->set((float)Device.TargetWidth, (float)Device.TargetHeight, d_Z, d_W, color, 1.f, 1.f); pv++;
-	pv->set((float)Device.TargetWidth, 0.f, d_Z, d_W, color, 1.f, 0.f); pv++;
-	RCache.Vertex.Unlock(4, g_combine->vb_stride);
 
 	// Set shader and geometry
 	RCache.set_Element(s_spp->E[postProcessType]);
-	RCache.set_Geometry(g_combine);
+	RCache.set_Geometry(QuadGeom);
 
 	// Render
-	RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, Offset, 0, 4, 0, 2);
+	RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, 0, 0, 4, 0, 2);
 
 	// Copy resource
 	if (postProcessMode)
