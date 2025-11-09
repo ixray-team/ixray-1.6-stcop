41acfee17fc058b14a39ef7d53045fb582fa2d20
LVutner <kkamil724@gmail.com>
2025-11-09 05:21:54 +0100

    * UPD: [Render/Shaders] Moved most of postprocessing to new VS and geometry

--- src/Layers/xrRenderPC_R4/r4_rendertarget_phase_gtao.cpp
+++ src/Layers/xrRenderPC_R4/r4_rendertarget_phase_gtao.cpp
@@ -6,9 +6,6 @@ void CRenderTarget::phase_gtao()
 {
 	GPU_EVENT(phase_gtao);
 
-	u32 Offset = 0;
-    constexpr u32 vertex_color = color_rgba(0, 0, 0, 255);
-
 	//Calculate projection factor, to transform world radius to screen space
 	float p_scale = RCache.get_height() / (tan(deg2rad(Device.fFOV) * 0.5f) * 2.0f);
 	p_scale *= 0.5;
@@ -21,20 +18,11 @@ void CRenderTarget::phase_gtao()
 		RCache.set_CullMode(CULL_NONE);
 		RCache.set_Stencil(FALSE);
 
-		pv = (FVF::TL*)RCache.Vertex.Lock(3, g_combine->vb_stride, Offset);
-		pv->set(-1.0, 1.0, 1.0, 1.0, vertex_color, 0.0, 0.0);
-		pv++;
-		pv->set(3.0, 1.0, 1.0, 1.0, vertex_color, 2.0, 0.0);
-		pv++;
-		pv->set(-1.0, -3.0, 1.0, 1.0, vertex_color, 0.0, 2.0);
-		pv++;
-		RCache.Vertex.Unlock(3, g_combine->vb_stride);
-
 		//Go go power rangers
 		RCache.set_Element(s_gtao->E[0]);
 		RCache.set_c("gtao_parameters", p_scale);
-		RCache.set_Geometry(g_combine);
-		RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, Offset, 0, 3, 0, 1);
+		RCache.set_Geometry(FSTriangleGeom);
+		RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, 0, 0, 3, 0, 1);
 	}
 
 	{
@@ -44,27 +32,15 @@ void CRenderTarget::phase_gtao()
 		RCache.set_CullMode(CULL_NONE);
 		RCache.set_Stencil(FALSE);
 
-		pv = (FVF::TL*)RCache.Vertex.Lock(3, g_combine->vb_stride, Offset);
-		pv->set(-1.0, 1.0, 1.0, 1.0, vertex_color, 0.0, 0.0);
-		pv++;
-		pv->set(3.0, 1.0, 1.0, 1.0, vertex_color, 2.0, 0.0);
-		pv++;
-		pv->set(-1.0, -3.0, 1.0, 1.0, vertex_color, 0.0, 2.0);
-		pv++;
-		RCache.Vertex.Unlock(3, g_combine->vb_stride);
-
 		//Go go power rangers
 		RCache.set_Element(s_gtao->E[1]);
-		RCache.set_Geometry(g_combine);
-		RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, Offset, 0, 3, 0, 1);
+		RCache.set_Geometry(FSTriangleGeom);
+		RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, 0, 0, 3, 0, 1);
 	}
 }
 
 void CRenderTarget::phase_sslr() {
 	GPU_EVENT(phase_sslr);
-	u32 Offset = 0;
-	constexpr u32 vertex_color = color_rgba(0, 0, 0, 255);
-	FVF::TL* pv = nullptr;
 
 	{
 		GPU_EVENT(sslr_render);
@@ -72,19 +48,10 @@ void CRenderTarget::phase_sslr() {
 		u_setrt(rt_sslr, rt_sslr_data, nullptr, nullptr);
 		RCache.set_CullMode(CULL_NONE);
 
-		pv = (FVF::TL*)RCache.Vertex.Lock(3, g_combine->vb_stride, Offset);
-		pv->set(-1.0, 1.0, 1.0, 1.0, vertex_color, 0.0, 0.0);
-		pv++;
-		pv->set(3.0, 1.0, 1.0, 1.0, vertex_color, 2.0, 0.0);
-		pv++;
-		pv->set(-1.0, -3.0, 1.0, 1.0, vertex_color, 0.0, 2.0);
-		pv++;
-		RCache.Vertex.Unlock(3, g_combine->vb_stride);
-
 		//Go go power rangers
 		RCache.set_Element(s_gtao->E[2]);
-		RCache.set_Geometry(g_combine);
-		RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, Offset, 0, 3, 0, 1);
+		RCache.set_Geometry(FSTriangleGeom);
+		RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, 0, 0, 3, 0, 1);
 	}
 
 	{
@@ -92,19 +59,10 @@ void CRenderTarget::phase_sslr() {
 		u_setrt(rt_sslr_temp, nullptr, nullptr, nullptr);
 		RCache.set_CullMode(CULL_NONE);
 
-		pv = (FVF::TL*)RCache.Vertex.Lock(3, g_combine->vb_stride, Offset);
-		pv->set(-1.0, 1.0, 1.0, 1.0, vertex_color, 0.0, 0.0);
-		pv++;
-		pv->set(3.0, 1.0, 1.0, 1.0, vertex_color, 2.0, 0.0);
-		pv++;
-		pv->set(-1.0, -3.0, 1.0, 1.0, vertex_color, 0.0, 2.0);
-		pv++;
-		RCache.Vertex.Unlock(3, g_combine->vb_stride);
-
 		//Go go power rangers
 		RCache.set_Element(s_gtao->E[3]);
-		RCache.set_Geometry(g_combine);
-		RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, Offset, 0, 3, 0, 1);
+		RCache.set_Geometry(FSTriangleGeom);
+		RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, 0, 0, 3, 0, 1);
 	}
 
 	{
@@ -112,19 +70,10 @@ void CRenderTarget::phase_sslr() {
 		u_setrt(rt_sslr, nullptr, nullptr, nullptr);
 		RCache.set_CullMode(CULL_NONE);
 
-		pv = (FVF::TL*)RCache.Vertex.Lock(3, g_combine->vb_stride, Offset);
-		pv->set(-1.0, 1.0, 1.0, 1.0, vertex_color, 0.0, 0.0);
-		pv++;
-		pv->set(3.0, 1.0, 1.0, 1.0, vertex_color, 2.0, 0.0);
-		pv++;
-		pv->set(-1.0, -3.0, 1.0, 1.0, vertex_color, 0.0, 2.0);
-		pv++;
-		RCache.Vertex.Unlock(3, g_combine->vb_stride);
-
 		//Go go power rangers
 		RCache.set_Element(s_gtao->E[4]);
-		RCache.set_Geometry(g_combine);
-		RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, Offset, 0, 3, 0, 1);
+		RCache.set_Geometry(FSTriangleGeom);
+		RCache.Render(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST, 0, 0, 3, 0, 1);
 	}
 
 	GRHI->CopySurface(rt_sslr_old->pSurface, rt_sslr->pSurface);
