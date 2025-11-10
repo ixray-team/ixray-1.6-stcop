506b51e335fb50efdc01d54b05462ad27db953c3
ForserX <sv3nk@yandex.ru>
2025-11-10 21:48:57 +0300

    Fix make levelmap screenshot

--- src/xrEngine/Xr_input.cpp
+++ src/xrEngine/Xr_input.cpp
@@ -451,7 +451,7 @@ void CInput::iCapture(IInputReceiver *p)
 {
 	VERIFY(p);
 
-	if (!Device.IsEditorMode() && (KBState[SDL_SCANCODE_F11] || CImGuiManager::Instance().IsCapturingInputs()))
+	if (!Device.IsEditorMode() && CImGuiManager::Instance().IsCapturingInputs())
 	{
 		NoInputUpdate();
 	} 
