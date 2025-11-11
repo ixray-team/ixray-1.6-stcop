ef4330d83584f4ff670b726ca736d27d7dc283a5
ForserX <sv3nk@yandex.ru>
2025-11-11 03:46:03 +0300

    Очередная вычистка `CHWCaps`

--- src/Layers/xrRender/HWCaps.h
+++ src/Layers/xrRender/HWCaps.h
@@ -16,18 +16,11 @@ public:
 	struct		caps_Geometry
 	{
 		u32	dwRegisters		: 16;
-		u32	bSoftware		: 1;
-		u32	bVTF			: 1;		// vertex-texture-fetch
-		u32	bNPatches		: 1;
-		u32 dwVertexCache	: 8;
 	};
 	struct		caps_Raster
 	{
 		u32	dwRegisters		: 16;
 		u32	dwStages		: 4;		// number of tex-stages
-		u32	dwMRT_count		: 4;
-		u32 b_MRT_mixdepth	: 1;
-		u32	bCubemap		: 1;
 	};
 public:
 	// force flags
@@ -39,16 +32,10 @@ public:
 	u32				iGPUNum;
 
 	// device format
-	D3DFORMAT		fTarget;
-	D3DFORMAT		fDepth;
 	u32				dwRefreshRate;
 
 	// caps itself
-	u16				geometry_major	;
-	u16				geometry_minor	;
 	caps_Geometry	geometry		;
-	u16				raster_major	;
-	u16				raster_minor	;
 	caps_Raster		raster			;
 
 	u32				id_device		;
@@ -56,10 +43,6 @@ public:
 	BOOL			bStencil;			// stencil buffer present
 	BOOL			bScissor;			// scissor rect supported
 	BOOL			bTableFog;			//
-
-	// some precalculated values
-	D3DSTENCILOP	soDec, soInc;		// best stencil OPs for shadows
-	u32				dwMaxStencilValue;  // maximum value the stencil buffer can hold
 
 	void			Update(void);
 };
