9cdb90f718103e20dfbc6cff08f7d214a6e4eddc
ForserX <sv3nk@yandex.ru>
2025-11-11 03:46:03 +0300

    Очередная вычистка `CHWCaps`

--- src/Layers/xrRenderDX10/dx11HWCaps.cpp
+++ src/Layers/xrRenderDX10/dx11HWCaps.cpp
@@ -9,50 +9,20 @@ ECORE_API CHWCaps Caps;
 void CHWCaps::Update()
 {
 	// ***************** GEOMETRY
-	geometry_major				= 4;
-	geometry_minor				= 0;
-	geometry.bSoftware			= FALSE;
-	geometry.bNPatches			= FALSE;
-	DWORD cnt					= 256;
-	clamp<DWORD>(cnt,0,256);
-	geometry.dwRegisters		= cnt;
-	geometry.bVTF				= TRUE;
+	DWORD cnt = 256;
+	clamp<DWORD>(cnt, 0, 256);
+	geometry.dwRegisters = cnt;
 
 	// ***************** PIXEL processing
-	raster_major				= 4;
-	raster_minor				= 0;
 	raster.dwStages = 15; //unused
-	raster.bCubemap				= TRUE;
-	raster.dwMRT_count			= 4;
-	raster.b_MRT_mixdepth		= TRUE;
 
-	// ***************** Info
-	Msg							("* GPU shading: vs(%x/%d.%d/%d), ps(%x/%d.%d/%d)",
-		0,	geometry_major, geometry_minor, CAP_VERSION(geometry_major,	geometry_minor),
-		0,	raster_major,	raster_minor,	CAP_VERSION(raster_major,	raster_minor)
-		);
-
-	// *******1********** Vertex cache
-	//	TODO: DX10: Find a way to detect cache size
-	geometry.dwVertexCache = 24;
-	Msg					("* GPU vertex cache: %s, %d","unrecognized",u32(geometry.dwVertexCache));
-
-	// *******1********** Compatibility : vertex shader
-	if (0==raster_major)		geometry_major=0;		// Disable VS if no PS
-
-	//
-	bTableFog			=	FALSE;	//BOOL	(caps.RasterCaps&D3DPRASTERCAPS_FOGTABLE);
+	bTableFog = FALSE;	//BOOL	(caps.RasterCaps&D3DPRASTERCAPS_FOGTABLE);
 
 	// Detect if stencil available
-	bStencil			=	TRUE;
+	bStencil = TRUE;
 
 	// Scissoring
-	bScissor	= TRUE;
-
-	// Stencil relative caps
-	soInc=D3DSTENCILOP_INCRSAT;
-	soDec=D3DSTENCILOP_DECRSAT;
-	dwMaxStencilValue=(1<<8)-1;
+	bScissor = TRUE;
 
 	// DEV INFO
 
