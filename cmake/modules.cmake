920cc277a4cdbad7694650f4f36f7735cb476500
ForserX <sv3nk@yandex.ru>
2025-11-12 20:37:12 +0300

    Удалён OpenAL Soft

--- cmake/modules.cmake
+++ cmake/modules.cmake
@@ -5,7 +5,6 @@ include("${CMAKE_MODULE_PATH}/LuaJIT.cmake")
 include("${CMAKE_MODULE_PATH}/LuaBind.cmake")
 include("${CMAKE_MODULE_PATH}/Ogg.cmake")
 include("${CMAKE_MODULE_PATH}/Vorbis.cmake")
-include("${CMAKE_MODULE_PATH}/OpenalSoft.cmake")
 include("${CMAKE_MODULE_PATH}/FreeImage.cmake")
 
 if (IXRAY_MP)
