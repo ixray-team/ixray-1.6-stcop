3e0b08997f8bec9976dbb861aee9d4265cd1b938
Drombeys <dima.zolotov02@gmail.com>
2025-11-13 12:24:51 +0300

    Update shaders CS

--- gamedata_cs/shaders/d3d11/smaa_neighbour_blend.ps.hlsl
+++ gamedata_cs/shaders/d3d11/smaa_neighbour_blend.ps.hlsl
@@ -27,11 +27,11 @@ Texture2D s_blendtex;
 struct p_smaa
 {
     float4 hpos : SV_POSITION;
-    float2 tc0 : TEXCOORD0; // Texture coordinates         (for sampling maps)
+    float2 texcoord : TEXCOORD0; // Texture coordinates         (for sampling maps)
     float4 offset : TEXCOORD2;
 };
 
 float4 main(p_smaa I) : SV_Target
 {
-    return SMAANeighborhoodBlendingPS(I.tc0, I.offset, s_image, s_blendtex);
+    return SMAANeighborhoodBlendingPS(I.texcoord, I.offset, s_image, s_blendtex);
 }
