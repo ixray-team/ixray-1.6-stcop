c37635fa9f8e17ac51b59bc41150cc2b83afea68
xr-Renn <102355826+xr-Renn@users.noreply.github.com>
2025-11-16 21:57:50 +0300

    Implement context menu icons in editors

--- src/Editors/ActorEditor/UIMainMenuForm.h
+++ src/Editors/ActorEditor/UIMainMenuForm.h
@@ -5,5 +5,11 @@ public:
 	UIMainMenuForm();
 	virtual ~UIMainMenuForm();
 	virtual void Draw();
-};
 
+private:
+	shared_str GetCommandShortcat(int CommandID) const;
+	void DrawMenuItem(const char* label, int command, int param = 0, int flag = 0);
+
+	void DrawMenuItemI(const char* label, const char* icon, int command, const xr_string& param, int flag = 0);
+	void DrawMenuItemI(const char* label, const char* icon, int command, int param = 0, int flag = 0);
+};
