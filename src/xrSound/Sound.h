c580432241b9f35826af627105bbbb1b4ffdee5e
ForserX <sv3nk@yandex.ru>
2025-11-25 00:49:07 +0300

    ClangCI (Windows): Initial

--- src/xrSound/Sound.h
+++ src/xrSound/Sound.h
@@ -112,7 +112,7 @@ public:
 	{
 		Fvector* params = XRay::Sound::Mixer::GetParameters(slot);
 		if (params) {
-			CSound_params out_params = { 0 };
+			CSound_params out_params = {};
 			out_params.position = params[(u32)XRay::Sound::Mixer::ParameterId::Position];
 			out_params.base_volume = params[(u32)XRay::Sound::Mixer::ParameterId::VolumePerChannel].x;
 			out_params.volume = params[(u32)XRay::Sound::Mixer::ParameterId::VolumePerChannel].y;
@@ -353,7 +353,7 @@ IC CSound_params ref_sound::get_params()
 {
 	Fvector* params = XRay::Sound::Mixer::GetParameters(slot());
 	if (slot()) {
-		CSound_params out_params = { 0 };
+		CSound_params out_params = {};
 		out_params.position = params[(u32)XRay::Sound::Mixer::ParameterId::Position];
 		out_params.volume = params[(u32)XRay::Sound::Mixer::ParameterId::VolumePerChannel].x;
 		out_params.freq = params[(u32)XRay::Sound::Mixer::ParameterId::Pitch].x;
