f0d4e864c99f6ebac214de5124198f4b1cdbcd84
Shtrecker <gogels235@gmail.com>
2025-11-25 22:20:59 +0200

    Add backpack animator

--- src/xrGame/HudAnimatorManager.h
+++ src/xrGame/HudAnimatorManager.h
@@ -8,6 +8,8 @@ class CActor;
 class CMotionDef;
 //class CHudPdaAnimator;
 class CHudItemAnimator;
+class CBackpackAnimator;
+class CHudStateAnimator;
 
 class CHudAnimatorBase
 {
@@ -31,6 +33,8 @@ protected:
 	CActor* m_actor = nullptr;
 	u8 m_iRestoreSlot = 0;
 
+	bool HudAnimationExist(const shared_str& name);
+
 public:
 
 	CHudAnimatorBase(CActor* parent) : m_actor(parent) {}
@@ -44,9 +48,66 @@ public:
 	u8 GetSlotToRestore() const { return m_iRestoreSlot; }
 	float GetHudFov() const;
 	bool CanSprint() const { return m_bCanSprint; }
+	bool NeedRestoreDetector() const { return m_bRestoreDetector; }
 
 	virtual CHudItemAnimator* cast_item_animator() { return nullptr; }
+	virtual CHudStateAnimator* cast_hud_state_animator() { return nullptr; }
+	virtual CBackpackAnimator* cast_backpack_animator() { return nullptr; }
 	//virtual CHudPdaAnimator* cast_pda_animator() { return nullptr; }
+};
+
+class CHudStateAnimator : public CHudAnimatorBase
+{
+	u32 m_current_state = eHidden;
+	u32 m_on_animation_end_state = eHidden;
+
+	u32 m_dw_curr_state_time = 0;
+	u32	m_dw_curr_substate_time = 0;
+
+	bool m_bDisableBore = true;
+	bool m_bSwitchSprint = false;
+
+protected:
+	u32 CurrStateTime() const { return Device.dwTimeGlobal - m_dw_curr_state_time; }
+	void ResetSubStateTime() { m_dw_curr_substate_time = Device.dwTimeGlobal; }
+	virtual void OnAnimationEnd(u32 state);
+	void OnStateSwitch(u32 state);
+	void UpdateAnimation();
+	void PlayMotion(const shared_str& name, bool blend, u32 state);
+	//void OnMotionMark(const motion_marks& mark);
+	void PlayAnimIdle();
+	void PlayAnimIdleMoving();
+	void PlayAnimIdleMovingSlow();
+	void PlayAnimIdleMovingCrouch();
+	void PlayAnimIdleMovingCrouchSlow();
+	void PlayAnimIdleSprint();
+	bool TryPlayAnimIdle();
+
+public:
+	enum EAnimatorStates
+	{
+		eHidden = 0,
+		eIdle,
+		eBore,
+		eSprintStart,
+		eSprintEnd,
+		eShowing,
+		eHiding,
+		eLastAnimatorState = eHiding
+	};
+
+	CHudStateAnimator(CActor* parent, const shared_str& section);
+	virtual ~CHudStateAnimator() = default;
+
+	virtual void Load() override;
+	void Update();
+	void OnMovementChanged();
+	virtual void SwitchAnimator();
+
+	void SetState(u32 state) { OnStateSwitch(state); }
+	u32 GetState() const { return m_current_state; }
+
+	virtual CHudStateAnimator* cast_hud_state_animator() override { return this; }
 };
 
 //#include "HudPdaAnimator.h"
@@ -54,12 +115,17 @@ public:
 
 class CHudAnimatorManager
 {
+	CActor* m_actor = nullptr;
+	//CHudPdaAnimator* m_pda_animator = nullptr;
+	CHudItemAnimator* m_item_animator = nullptr;
+	CBackpackAnimator* m_backpack_animator = nullptr;
 public:
 	CHudAnimatorManager(CActor* parent);
 	~CHudAnimatorManager();
 
 	void Update();
 	void StopGetAnimator();
+	void OnMovementChanged();
 	bool IsAnyAnimatorActive();
 	bool CanSprint();
 	float GetHudFov();
@@ -68,9 +134,5 @@ public:
 
 	//CHudPdaAnimator* PdaAnimator() { return m_pda_animator; }
 	CHudItemAnimator* ItemAnimator() { return m_item_animator; }
-
-private:
-	CActor* m_actor = nullptr;
-	//CHudPdaAnimator* m_pda_animator = nullptr;
-	CHudItemAnimator* m_item_animator = nullptr;
+	CBackpackAnimator* BackpackAnimator() { return m_backpack_animator; }
 };
