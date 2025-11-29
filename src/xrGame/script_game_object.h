8046a245a8fbaf8dbac5532c6e9a44da5406e402
Shtrecker <gogels235@gmail.com>
2025-11-29 18:36:36 +0200

    Fix & refactor hud animator classes

--- src/xrGame/script_game_object.h
+++ src/xrGame/script_game_object.h
@@ -1002,6 +1002,8 @@ public:
 	float GetGasmaskCondition();
 	bool GetAnimatorForceHideItems();
 	void SetAnimatorForceHideItems(bool status);
+	void ShowStateAnimator(LPCSTR section);
+	void HideStateAnimator();
 	bool IsInCar();
 	void IterateFeelTouch(const luabind::functor<bool>& functor);
 			bool IsActorLadder() const;
