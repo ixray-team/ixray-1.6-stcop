149378d010c8c9ff2560c0249db127acc9f32518
ForserX <sv3nk@yandex.ru>
2025-11-29 22:10:54 +0300

    Improve build system and fix clang-cl warnings

---
+++ cmake/modules/discord_game_sdk.cmake
@@ -0,0 +1,17 @@
+# Discord SDK
+if(NOT "${CMAKE_VS_PLATFORM_NAME}" MATCHES "(x64)")
+    set(DISCORD_PLATFORM_EX x86)
+else()
+    set(DISCORD_PLATFORM_EX x86_64)
+endif()
+
+set(DISCORD_SDK_DIR ${CMAKE_BINARY_DIR}/dep/discord_gamesdk)
+set(DISCORD_LIB   ${DISCORD_SDK_DIR}/lib/${DISCORD_PLATFORM_EX}/discord_game_sdk.dll.lib)
+set(DISCORD_DLL   ${DISCORD_SDK_DIR}/lib/${DISCORD_PLATFORM_EX}/discord_game_sdk.dll)
+
+add_imported_lib(
+    DiscordSDK::discord
+    ""                # include dirs (SDK не требует)
+    "${DISCORD_LIB}"
+    "${DISCORD_DLL}"
+)
