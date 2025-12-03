5e87290c7cbc822e09573f611a893fdd2de06ce1
FFx0001 <46628552+FFx0001@users.noreply.github.com>
2025-12-03 22:48:36 +0300

    Переработаны лодауты (Восстановлен режим который должен был быть в CoC по умолчанию но не работал у нас) (#240)
    Переработаны лодауты (Восстановлен режим который должен был быть в CoC по умолчанию но не работал у нас)
    
    Опции лодаутов в engine_external.ltx
    
    EnableLoadoutsSupplies -- вкл выкл использование лодаутов у нпц
    
    EnableSpawnFullRandomLoadout -- спавн всех секций одного случайного лодаута из файла у нпц
    
    EnableSpawnOnceRandomItemPerEachLoadouts -- спавн по одному случайному предмету из всех лодаутов добавленных в нпц
    
    EnableSpawnOnceRandomitemByRandomLoadout -- спавн одного случайного предмета среди всех лодаутов добавленных в нпц

--- src/xrServerEntities/xrServer_Objects_ALife.h
+++ src/xrServerEntities/xrServer_Objects_ALife.h
@@ -165,8 +165,9 @@ private:
 	virtual void					spawnAmmoForWeapon(LPCSTR wpnSection, CSE_Abstract* E, int i_ammo_type, u32 countAmmoBoxesToSpawn);
 	virtual void					setItemCondition(CSE_Abstract* E, float condition);
 	virtual xr_vector <CInifile::Sect*> parseLoadouts(CInifile& ini);
-	virtual void					processingSpawnOnceFullRandomLoadout(CInifile& ini);
-	virtual void					processingSpawnOnceRandomItemInRandomLoadout(CInifile& ini);
+	virtual void					processingSpawnFullRandomLoadout(CInifile& ini);
+	virtual void					processingSpawnOnceRandomitemByRandomLoadout(CInifile& ini);
+	virtual void					processingSpawnOnceRandomItemPerEachLoadouts(CInifile& ini);
 	virtual void					processingVanillaSpawn(CInifile& ini);
 #endif
 
