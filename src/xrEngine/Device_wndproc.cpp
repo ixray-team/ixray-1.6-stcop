b744d11d90f16c79ebc4f0af0309ddd03693f3b9
ForserX <sv3nk@yandex.ru>
2025-12-05 00:03:35 +0300

    Fix gamepad working

--- src/xrEngine/Device_wndproc.cpp
+++ src/xrEngine/Device_wndproc.cpp
@@ -10,191 +10,174 @@ bool CRenderDevice::on_event	(SDL_Event& Event)
 	// this is needed because default event handler that goes after if (SDL_GetWindowID(g_AppInfo.Window) != Event.window.windowID) is not reachable
 	switch (Event.type)
 	{
-	case SDL_EVENT_GAMEPAD_REMOVED:
-	{
-//		SDL_CloseGamepad(pInput->pGamePad);
-	//	pInput->pGamePad = nullptr;
-		
-		if (pInput->receive_gamepad_addedorremoved)
+		case SDL_EVENT_GAMEPAD_REMOVED:
 		{
-			pInput->receive_gamepad_addedorremoved(nullptr, false);
+			SDL_CloseGamepad(pInput->pGamePad);
+			pInput->pGamePad = nullptr;
+			
+			break;
 		}
-
-		break;
-	}
-	case SDL_EVENT_GAMEPAD_ADDED:
-	{
-	//	if (SDL_IsGamepad(Event.jdevice.which))
-	//	{
-	//		pInput->pGamePad = SDL_OpenGamepad(Event.jdevice.which);
-		if (pInput->receive_gamepad_addedorremoved)
+		case SDL_EVENT_GAMEPAD_ADDED:
 		{
-			pInput->receive_gamepad_addedorremoved(reinterpret_cast<void*>(Event.jdevice.which), true);
-		}
-	//	}
-		break;
-	}
+			if (SDL_IsGamepad(Event.jdevice.which))
+			{
+				pInput->pGamePad = SDL_OpenGamepad(Event.jdevice.which);
 
-	case SDL_EVENT_KEYBOARD_ADDED:
-	{
-		if (pInput->receive_keyboard_addedorremoved)
+				if (pInput->receive_gamepad_addedorremoved)
+				{
+					pInput->receive_gamepad_addedorremoved(reinterpret_cast<void*>(Event.jdevice.which), true);
+				}
+			}
+			break;
+		}
+		case SDL_EVENT_GAMEPAD_BUTTON_DOWN:
 		{
-			pInput->receive_keyboard_addedorremoved(reinterpret_cast<void*>(Event.kdevice.which), true);
+			pInput->GamepadButtonUpdate(Event.gbutton.button, true);
+			break;
 		}
-
-		break;
-	}
-	case SDL_EVENT_KEYBOARD_REMOVED:
-	{
-		if (pInput->receive_keyboard_addedorremoved)
+		case SDL_EVENT_GAMEPAD_BUTTON_UP:
 		{
-			pInput->receive_keyboard_addedorremoved(nullptr, false);
+			pInput->GamepadButtonUpdate(Event.gbutton.button, false);
+			break;
 		}
-
-		break;
-	}
-	case SDL_EVENT_MOUSE_ADDED:
-	{
-		if (pInput->receive_mouse_addedorremoved)
+		case SDL_EVENT_GAMEPAD_AXIS_MOTION:
 		{
-			pInput->receive_mouse_addedorremoved(reinterpret_cast<void*>(Event.mdevice.which), true);
-		}
+			float Value = std::clamp((float)Event.gaxis.value / 32767.0f, -1.0f, 1.0f);
 
-		break;
-	}
-	case SDL_EVENT_MOUSE_REMOVED:
-	{
-		if (pInput->receive_mouse_addedorremoved)
+			if ((Value > 0 && Value < 0.1f) || (Value < 0 && Value > -0.1f))
+				Value = 0;
+
+			if (Event.gaxis.axis < 2)
+			{
+				pInput->LeftAxisUpdate(Event.gaxis.axis == 0, Value);
+			}
+			else if (Event.gaxis.axis < 4)
+			{
+				pInput->RightAxisUpdate(Event.gaxis.axis == 2, Value);
+			}
+			else
+			{
+				// L2 & R2 Triggers
+				pInput->AdaptiveTriggerUpdate(Event.gaxis.axis == 4, Value);
+			}
+
+			break;
+		}
+		case SDL_GAMEPAD_AXIS_LEFTY:
 		{
-			pInput->receive_mouse_addedorremoved(nullptr, false);
+			break;
 		}
+		case SDL_EVENT_KEYBOARD_ADDED:
+		{
+			if (pInput->receive_keyboard_addedorremoved)
+			{
+				pInput->receive_keyboard_addedorremoved(reinterpret_cast<void*>(Event.kdevice.which), true);
+			}
 
-		break;
-	}
+			break;
+		}
+		case SDL_EVENT_KEYBOARD_REMOVED:
+		{
+			if (pInput->receive_keyboard_addedorremoved)
+			{
+				pInput->receive_keyboard_addedorremoved(nullptr, false);
+			}
+
+			break;
+		}
+		case SDL_EVENT_MOUSE_ADDED:
+		{
+			if (pInput->receive_mouse_addedorremoved)
+			{
+				pInput->receive_mouse_addedorremoved(reinterpret_cast<void*>(Event.mdevice.which), true);
+			}
+
+			break;
+		}
+		case SDL_EVENT_MOUSE_REMOVED:
+		{
+			if (pInput->receive_mouse_addedorremoved)
+			{
+				pInput->receive_mouse_addedorremoved(nullptr, false);
+			}
+
+			break;
+		}
 	}
 
 	if (SDL_GetWindowID(g_AppInfo.Window) != Event.window.windowID)
+	{
 		return true;
+	}
 
-	switch (Event.type) {
-	case SDL_EVENT_WINDOW_MOUSE_ENTER:
-		OnWM_Activate(true, false);
-		break;
-	case SDL_EVENT_WINDOW_MOUSE_LEAVE:
-		OnWM_Activate(false, false);
-		break;
-	case SDL_EVENT_WINDOW_SHOWN:
-		OnWM_Activate(true, false);
-		break;
-	case SDL_EVENT_WINDOW_HIDDEN:
-		OnWM_Activate(false, true);
-		break;
-	case SDL_EVENT_QUIT:
-	case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
-		g_pEventManager->Event.Signal("KERNEL:disconnect");
-		g_pEventManager->Event.Signal("KERNEL:quit");
-		return false;
-	case SDL_EVENT_GAMEPAD_REMOVED:
-		SDL_CloseGamepad(pInput->pGamePad);
-		pInput->pGamePad = nullptr;
-	//	--pInput->gamepad_added_event_received;
-
-		break;
-	case SDL_EVENT_GAMEPAD_ADDED:
-		if (SDL_IsGamepad(Event.jdevice.which))
+	switch (Event.type)
+	{
+		case SDL_EVENT_WINDOW_MOUSE_ENTER:
+		{	OnWM_Activate(true, false);
+			break;
+		}
+		case SDL_EVENT_WINDOW_MOUSE_LEAVE:
 		{
-			pInput->pGamePad = SDL_OpenGamepad(Event.jdevice.which);
-	//		++pInput->gamepad_added_event_received;
+			OnWM_Activate(false, false);
+			break;
 		}
-		break;
-
-	case SDL_EVENT_KEYBOARD_ADDED:
-	{
-	//	++pInput->keyboard_added_event_received;
-
-		break;
-	}
-	case SDL_EVENT_KEYBOARD_REMOVED:
-	{
-	//	--pInput->keyboard_added_event_received;
-
-		break;
-	}
-	case SDL_EVENT_MOUSE_ADDED:
-	{
-	//	++pInput->mouse_added_event_received;
-
-		break;
-	}
-	case SDL_EVENT_MOUSE_REMOVED:
-	{
-	//	--pInput->mouse_added_event_received;
-
-		break;
-	}
-	case SDL_EVENT_GAMEPAD_BUTTON_DOWN:
-		pInput->GamepadButtonUpdate(Event.gbutton.button, true);
-		break;
-	case SDL_EVENT_GAMEPAD_BUTTON_UP:
-	{
-		pInput->GamepadButtonUpdate(Event.gbutton.button, false);
-		break;
-	}
-	case SDL_EVENT_GAMEPAD_AXIS_MOTION:
-	{
-		float Value = std::clamp((float)Event.gaxis.value / 32767.0f, -1.0f, 1.0f);
-
-		if ((Value > 0 && Value < 0.1f) || (Value < 0 && Value > -0.1f))
-			Value = 0;
-
-		if (Event.gaxis.axis < 2)
+		case SDL_EVENT_WINDOW_SHOWN:
 		{
-			pInput->LeftAxisUpdate(Event.gaxis.axis == 0, Value);
+			OnWM_Activate(true, false);
+			break;
 		}
-		else if (Event.gaxis.axis < 4)
+		case SDL_EVENT_WINDOW_HIDDEN:
 		{
-			pInput->RightAxisUpdate(Event.gaxis.axis == 2, Value);
+			OnWM_Activate(false, true);
+			break;
 		}
-		else
+		case SDL_EVENT_QUIT:
+		case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
 		{
-			// L2 & R2 Triggers
-			pInput->AdaptiveTriggerUpdate(Event.gaxis.axis == 4, Value);
+			g_pEventManager->Event.Signal("KERNEL:disconnect");
+			g_pEventManager->Event.Signal("KERNEL:quit");
+			return false;
 		}
-		
-		break;
-	}
-	case SDL_GAMEPAD_AXIS_LEFTY:
-		break;
-	case SDL_EVENT_KEY_DOWN:
-	{
-		pInput->KeyboardButtonUpdate(Event.key.scancode, true);
-		break;
-	}
-	case SDL_EVENT_KEY_UP:
-		pInput->KeyboardButtonUpdate(Event.key.scancode, false);
-		break;
-	case SDL_EVENT_MOUSE_MOTION:
-		pInput->MouseMotion(Event.motion.xrel, Event.motion.yrel);
-		break;
-	case SDL_EVENT_MOUSE_WHEEL:
-		pInput->MouseScroll(Event.wheel.y);
-		break;
-	case SDL_EVENT_MOUSE_BUTTON_DOWN:	
-	case SDL_EVENT_MOUSE_BUTTON_UP:
-	{
-		int mouse_button = 0;
-		if (Event.button.button == SDL_BUTTON_LEFT) { mouse_button = 0; }
-		if (Event.button.button == SDL_BUTTON_RIGHT) { mouse_button = 1; }
-		if (Event.button.button == SDL_BUTTON_MIDDLE) { mouse_button = 2; }
-		if (Event.button.button == SDL_BUTTON_X1) { mouse_button = 3; }
-		if (Event.button.button == SDL_BUTTON_X2) { mouse_button = 4; }
-		if (Event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
-			pInput->MousePressed(mouse_button);
-		} else {
-			pInput->MouseReleased(mouse_button);
+		case SDL_EVENT_KEY_DOWN:
+		{
+			pInput->KeyboardButtonUpdate(Event.key.scancode, true);
+			break;
 		}
-	}
-		break;
+		case SDL_EVENT_KEY_UP:
+		{
+			pInput->KeyboardButtonUpdate(Event.key.scancode, false);
+			break;
+		}
+		case SDL_EVENT_MOUSE_MOTION:
+		{
+			pInput->MouseMotion(Event.motion.xrel, Event.motion.yrel);
+			break;
+		}
+		case SDL_EVENT_MOUSE_WHEEL:
+		{
+			pInput->MouseScroll(Event.wheel.y);
+			break;
+		}
+		case SDL_EVENT_MOUSE_BUTTON_DOWN:
+		case SDL_EVENT_MOUSE_BUTTON_UP:
+		{
+			int mouse_button = 0;
+			if (Event.button.button == SDL_BUTTON_LEFT) { mouse_button = 0; }
+			if (Event.button.button == SDL_BUTTON_RIGHT) { mouse_button = 1; }
+			if (Event.button.button == SDL_BUTTON_MIDDLE) { mouse_button = 2; }
+			if (Event.button.button == SDL_BUTTON_X1) { mouse_button = 3; }
+			if (Event.button.button == SDL_BUTTON_X2) { mouse_button = 4; }
+
+			if (Event.type == SDL_EVENT_MOUSE_BUTTON_DOWN)
+			{
+				pInput->MousePressed(mouse_button);
+			}
+			else
+			{
+				pInput->MouseReleased(mouse_button);
+			}
+			break;
+		}
 	}
 
 	return true;
