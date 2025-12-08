d27f4f5fe64dbe32a9222ddfcb874f79ac865df7
Drombeys <dima.zolotov02@gmail.com>
2025-12-08 20:22:31 +0300

    Add `ik_shift_object` console command

--- src/xrGame/IKLimbsController.h
+++ src/xrGame/IKLimbsController.h
@@ -23,8 +23,6 @@ public:
 			void	PlayLegs					( CBlend *b );
 			void	Update						( );
 			float	Shift						( ){ return _object_shift.shift(); }
-
-			int ik_shift_object = 1;
 private:
 			void	Calculate					( );
 			void	LimbCalculate				(  SCalculateData &cd );
