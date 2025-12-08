3de888991c6ef8a2f56b1b0ff4209582b7e8c671
ForserX <sv3nk@yandex.ru>
2025-12-09 00:40:12 +0300

    CMake refactoring (again)

--- cmake/modules/amd_ags.cmake
+++ cmake/modules/amd_ags.cmake
@@ -3,4 +3,4 @@ set(AMD_AGS_INCLUDE "${CMAKE_BINARY_DIR}/dep/amd_ags_sdk/inc/")
 set(AMD_AGS_LIB     "")  # SDK линковка через DLL
 set(AMD_AGS_DLL     "${CMAKE_BINARY_DIR}/dep/amd_ags_sdk/lib/amd_ags_${NUGET_PACKAGE_PLATFORM}.dll")
 
-add_imported_lib(AMD_AGS::AMD_AGS "${AMD_AGS_INCLUDE}" "${AMD_AGS_LIB}" "${AMD_AGS_DLL}")
+add_imported_lib(AMD::AMD_AGS "${AMD_AGS_INCLUDE}" "${AMD_AGS_LIB}" "${AMD_AGS_DLL}")
