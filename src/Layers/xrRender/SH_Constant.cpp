fdb432cfbccbb0d51485dd95bbf5ca6d856378f2
ForserX <sv3nk@yandex.ru>
2025-12-10 03:22:00 +0300

    Render: cleanup

--- src/Layers/xrRender/SH_Constant.cpp
+++ src/Layers/xrRender/SH_Constant.cpp
@@ -1,52 +1,32 @@
 #include "stdafx.h"
 
-
-/*
-#include "../../xrCore/xr_resource.h"
-
-// res
-class	test_resource	: public xr_resource	{
-public:
-	void				_release_	(test_resource * ptr)	{	xr_delete	(ptr);	}
-};
-
-// ptr
-typedef	xr_resorce_ptr<test_resource>	test_ptr;
-
-// the test itself
-BOOL	AAA_test		()
+void CConstant::Calculate()
 {
-	test_ptr	A		(new test_resource());
-	test_ptr	B		= new test_resource	();
-	A					= B;
-	return		A!=B;
-}
-*/
+	if (dwFrame == RDEVICE.dwFrame)
+		return;
 
-void	CConstant::Calculate()
-{
-	if (dwFrame==RDEVICE.dwFrame)	return;
-	dwFrame		= RDEVICE.dwFrame;
-	if (modeProgrammable==dwMode)	return;
+	dwFrame = RDEVICE.dwFrame;
 
-	float	t	= RDEVICE.fTimeGlobal;
-	set_float	(_R.Calculate(t),_G.Calculate(t),_B.Calculate(t),_A.Calculate(t));
+	if (modeProgrammable == dwMode)
+		return;
+
+	float t = RDEVICE.fTimeGlobal;
+	set_float(_R.Calculate(t), _G.Calculate(t), _B.Calculate(t), _A.Calculate(t));
 }
 
-void	CConstant::Load	(IReader* fs)
+void CConstant::Load(IReader* fs)
 {
-	dwMode		= modeWaveForm;
-	fs->r		(&_R,sizeof(WaveForm));
-	fs->r		(&_G,sizeof(WaveForm));
-	fs->r		(&_B,sizeof(WaveForm));
-	fs->r		(&_A,sizeof(WaveForm));
+	dwMode = modeWaveForm;
+	fs->r(&_R, sizeof(WaveForm));
+	fs->r(&_G, sizeof(WaveForm));
+	fs->r(&_B, sizeof(WaveForm));
+	fs->r(&_A, sizeof(WaveForm));
 }
 
-void	CConstant::Save	(IWriter* fs)
+void CConstant::Save(IWriter* fs)
 {
-	fs->w		(&_R,sizeof(WaveForm));
-	fs->w		(&_G,sizeof(WaveForm));
-	fs->w		(&_B,sizeof(WaveForm));
-	fs->w		(&_A,sizeof(WaveForm));
+	fs->w(&_R, sizeof(WaveForm));
+	fs->w(&_G, sizeof(WaveForm));
+	fs->w(&_B, sizeof(WaveForm));
+	fs->w(&_A, sizeof(WaveForm));
 }
-
