2616721e5ea14da7dfc7b12f70468aec33671cd3
Shtrecker <gogels235@gmail.com>
2025-12-11 00:20:50 +0200

    Может быть фикс...

--- src/xrGame/Actor.cpp
+++ src/xrGame/Actor.cpp
@@ -1570,9 +1570,9 @@ void CActor::UpdateCL()
 				inventory().Activate(old_slot);
 				old_slot = 0;
 			}
-
+	
 			bool bres = (saved_old_slot == NO_ACTIVE_SLOT || saved_old_slot == INV_SLOT_2 || saved_old_slot == PISTOL_SLOT_NEW || saved_old_slot == KNIFE_SLOT || saved_old_slot == BOLT_SLOT);
-
+	
 			if (bres && need_restore_detector && GetDevice(true) != nullptr)
 			{
 				need_restore_detector = false;
