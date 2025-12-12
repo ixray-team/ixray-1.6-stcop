d614a7d83089ccbc64814acaa6935f24c8527cb6
mnelenpridumivat <ragrind@mail.ru>
2025-12-12 17:16:15 +0300

    Add SEH exception catch on Windows Debug and RelWithDebInfo configs

--- src/xrEngine/EngineThreading.cpp
+++ src/xrEngine/EngineThreading.cpp
@@ -9,39 +9,69 @@
 #include "../xrCore/discord/discord.h"
 #include "../xrCore/Save/SaveManager.h"
 
+
+#if !defined(MASTER_GOLD) && defined(IXR_WINDOWS) && (defined(_MSC_VER) || (defined(__clang__) && defined(_MSC_EXTENSIONS)))
+#define ALLOW_SEH_EXCEPTIONS
+#include <windows.h> // for EXCEPTION_ACCESS_VIOLATION
+#include <excpt.h>
+
+int ex_filter(unsigned int code, struct _EXCEPTION_POINTERS *ep)
+{
+	if (IsDebuggerPresent())
+	{
+		DebugBreak();
+	}
+	ProcessStackTrace(ep);
+	return EXCEPTION_EXECUTE_HANDLER;
+}
+#endif
+
 void XRay::Engine::PreRenderThread()
 {
+#ifdef ALLOW_SEH_EXCEPTIONS
+	__try
+#endif
 	{
-		PROF_EVENT("Discord Sync");
-		g_Discord.Update();
-	}
-
+		{
+			PROF_EVENT("Discord Sync");
+			g_Discord.Update();
+		}
+	
+		{
+			PROF_EVENT("seqParallelRender");
+			for (auto& it : Device.seqParallelRender)
+				it();
+		}
+	
+		if(g_pGamePersistent && !g_pGamePersistent->m_pMainMenu->IsActive())
+		{
+			if (g_pGamePersistent->pEnvironment && g_pGamePersistent->pEnvironment->eff_Rain)
+				g_pGamePersistent->pEnvironment->eff_Rain->UpdateItems();
+	
+			g_pGamePersistent->UpdateParticles();
+		}
+		PROF_STOP_THREAD();
+		Platform::SetThreadName("X-Ray Empty Task");
+#ifdef ALLOW_SEH_EXCEPTIONS
+	} __except(ex_filter(GetExceptionCode(), GetExceptionInformation()))
 	{
-		PROF_EVENT("seqParallelRender");
-		for (auto& it : Device.seqParallelRender)
-			it();
-	}
-
-	if (g_pGamePersistent && !g_pGamePersistent->m_pMainMenu->IsActive())
-	{
-		if (g_pGamePersistent->pEnvironment && g_pGamePersistent->pEnvironment->eff_Rain)
-			g_pGamePersistent->pEnvironment->eff_Rain->UpdateItems();
-
-		g_pGamePersistent->UpdateParticles();
+		FATAL("Unhandled exception in PreRenderThread!");
+#endif
 	}
 }
 
 void XRay::Engine::GameThread()
 {
-	// Device.GCThread.Wait();
-
+#ifdef ALLOW_SEH_EXCEPTIONS
+	__try
+#endif
 	{
-		PROF_EVENT("g_hud OnFrameMT");
-		if (g_hud)
+		// we has granted permission to execute
 		{
-			g_hud->OnFrameMT();
+			PROF_EVENT("g_hud OnFrameMT");
+			if (g_hud)
+				g_hud->OnFrameMT();
 		}
-	}
 
 	{
 		PROF_EVENT("SoundEvent_Dispatch");
@@ -51,20 +81,20 @@ void XRay::Engine::GameThread()
 		}
 	}
 
-	{
-		PROF_EVENT("Save Writing");
-		while (auto task = CSaveManager::GetInstance().PopSaveTask())
 		{
-			Device.async_tasks.run([=]()
+			PROF_EVENT("Save Writing");
+			while (auto task = CSaveManager::GetInstance().PopSaveTask())
 			{
-				PROF_START_THREAD("Async Task");
-				PROF_EVENT("Save writing task");
-				task->WriteSavedDataImpl();
-				xr_delete(task);
-				PROF_STOP_THREAD();
-			});
+				Device.async_tasks.run([=]()
+				{
+					PROF_START_THREAD("Async Task");
+					PROF_EVENT("Save writing task");
+					task->WriteSavedDataImpl();
+					xr_delete(task);
+					PROF_STOP_THREAD();
+				});
+			}
 		}
-	}
 
 	{
 		PROF_EVENT("Sheduler");
@@ -83,10 +113,10 @@ void XRay::Engine::GameThread()
 		Device.seqParallel.clear();
 	}
 
-	{
-		PROF_EVENT("seqFrameMT");
-		Device.seqFrameMT.Process<&pureFrame::OnFrame>();
-	}
+		{
+			PROF_EVENT("seqFrameMT");
+			Device.seqFrameMT.Process<&pureFrame::OnFrame>();
+		}
 
 	// Device.GCThread.Run();
 
@@ -101,15 +131,10 @@ void XRay::Engine::GameThread()
 		::Sound->update(Device.mView_saved, Device.vCameraPosition_saved, Device.vCameraDirection_saved, Device.vCameraTop_saved);
 		Device.Statistic->Sound.End();
 	}
-}
-
-extern volatile bool quiting;
-
-void XRay::Engine::GCThread()
-{
-	if (Device.LuaGC != nullptr)
+#ifdef ALLOW_SEH_EXCEPTIONS
+	} __except(ex_filter(GetExceptionCode(), GetExceptionInformation()))
 	{
-		PROF_EVENT("LuaGC");
-		Device.LuaGC();
+		FATAL("Unhandled exception in GameThread!");
+#endif
 	}
 }
