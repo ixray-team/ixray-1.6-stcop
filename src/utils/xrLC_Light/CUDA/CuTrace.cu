efca96ffdfa10f1610681cbb7c1059351986a049
Dimakuzmin <scorpion261096@gmail.com>
2025-12-15 09:43:30 +0200

    Rework GPU deflectors building (speedup ~x3)
    Каждый поток создает Stream для ГПУ
    таски отделены по потокам CPU (Нету лишних блокировок) до 4х раз быстрее стало чем было ранее
    
    Переработал логику работы Deflectors GPU (Теперь нету лишних 4х проходов)
    Edge GPU (Убрал) Запихал в LDirect (Намудрил там .markers можно сразу задавать)

--- src/utils/xrLC_Light/CUDA/CuTrace.cu
+++ src/utils/xrLC_Light/CUDA/CuTrace.cu
@@ -11,22 +11,8 @@
 #define __host__
 #endif
 
-struct OPTICK_Params
-{
-	OptixTraversableHandle handle;
-
-	unsigned char		flags;
-	Hardware_Raytask*	rays;
-	Hardware_Color*		colors;		// Раньше rays == colors
-
-	Hardware_Lighting*	lights;
-	int					counts_lights;
-
-	CDeflector_GPU*		GpuDeflectors;
-	bool				DeflectorsBig;
-
-};
-
+// Jitter Select
+ 
 extern "C"
 {
 	__constant__ OPTICK_Params g_params;
@@ -50,8 +36,7 @@ __device__ float RunOptickTask(Hardware_Vector& P, Hardware_Vector& N, float Ran
 	const float maxT = Range;
 
 	unsigned int hit = 0;
-
-	optixTrace(
+ 	optixTrace(
 		g_params.handle,
 		origin,
 		dir,
@@ -62,7 +47,8 @@ __device__ float RunOptickTask(Hardware_Vector& P, Hardware_Vector& N, float Ran
 		hit
 	);
 
-	return (hit == 0) ? 1.0f : 0.0f;
+	// return 0.0f;
+ 	return (hit == 0) ? 1.0f : 0.0f;
 }
 
 __device__ void CalculatePoint(Hardware_Lighting& L, Hardware_Vector& P, Hardware_Vector& N, Hardware_Color& C)
@@ -141,22 +127,21 @@ __device__ void CalculatePoint(Hardware_Lighting& L, Hardware_Vector& P, Hardwar
 
 	switch (L.light_type)
 	{
-	case eSun:
-		C.sun += att;
-		break;
-	case eHemi:
-		C.hemi += att;
-		break;
-	case eRGB:
-	{
-		float3 rgb = C.get_rgb_f32();
-		rgb.x += att * L.diffuse.x;
-		rgb.y += att * L.diffuse.y;
-		rgb.z += att * L.diffuse.z;
+		case eSun:
+			C.sun += att;
+			break;
+		case eHemi:
+			C.hemi += att;
 
-		C.set_rgb_f32(rgb.x, rgb.y, rgb.z);
-	}
-		break;
+			break;
+		case eRGB:
+		{
+			Hardware_Vector& rgb = C.rgb;
+			rgb.x += att * L.diffuse.x;
+			rgb.y += att * L.diffuse.y;
+			rgb.z += att * L.diffuse.z;
+  		} break;
+
 	}
 }
 
@@ -197,105 +182,13 @@ __device__ void run_tracing_new(int index)
 	 
 	LightPoint(P, N, ColorUV, flags);
 }
-
-
-
-__device__ void DeflectorsProcess(CDeflector_GPU& Defl)
-{
-	// Setup variables
-	Hardware_Vector2	dim, half;
-	dim.set(float(Defl.Width), float(Defl.Height));
-	half.set(.5f / dim.x, .5f / dim.y);
-
-	// Jitter data
-	Hardware_Vector2	JS;
-	JS.set(.4999f / dim.x, .4999f / dim.y);
-
-	unsigned int      Jcount;
-	Hardware_Vector2* Jitter;
-	Jitter_Select_GPU(Jitter, Jcount);
-
-	// Lighting itself
-	for (unsigned int V = 0; V < Defl.Height; V++)
-	{
-		for (unsigned int U = 0; U < Defl.Width; U++)
-		{
-			unsigned int	Fcount = 0;
-			Hardware_Color	C;
  
-			for (unsigned int J = 0; J < Jcount; J++)
-			{
-				// LUMEL space
-				Hardware_Vector2 P;
-				P.x = float(U) / dim.x + half.x + Jitter[J].x * JS.x;
-				P.y = float(V) / dim.y + half.y + Jitter[J].y * JS.y;
-				 
-				// World space
-				Hardware_Vector		wP, wN, B;
-				for (auto TRI_INDEX =0; TRI_INDEX < Defl.UVTrisSize; TRI_INDEX++)
-				{
-					auto TRI = Defl.UVTris[TRI_INDEX];
-					if (TRI.isInside(P, B))
-					{
-						// We found triangle and have barycentric coords
-							 
-						VertexGPU& V1 = TRI.V[0];
-						VertexGPU& V2 = TRI.V[1];
-						VertexGPU& V3 = TRI.V[2];
-
-						wP.from_bary(V1.P, V2.P, V3.P, B);
-
-						{
-							wN.from_bary(V1.N, V2.N, V3.N, B);
-							// exact_normalize(wN); // TODO ! se7kills
-							wN.Add(TRI.N);
-							// exact_normalize(wN);  // TODO ! se7kills
-						}
-
-						LightPoint(wP, wN, C, 0);
- 						 
-						Fcount += 1;
-	
-						break;
-					}
-				}
-			}
  
-			if (Fcount)
-			{
-				C.scale(Fcount);
-				C.mul(.5f);
-				Defl.surfaces[V * Defl.Width + U] = C;
-				Defl.marker  [V * Defl.Width + U] = 255;
-			}
-			else {
-				Defl.surfaces[V * Defl.Width + U] = C;
-				Defl.marker  [V * Defl.Width + U]   = 0;
-			}
-		}
-	}
-
-}
-
-__device__ void run_deflectors(int index)
-{
-	if (g_params.GpuDeflectors != nullptr)
-	{
- 		DeflectorsProcess(g_params.GpuDeflectors[index]);
- 	}
-	else
-	{
-		run_tracing_new(index);
-	}
-}
-
 // Entry points
 extern "C" __global__ void __raygen__rg()
 {
 	const uint3 launch_idx = optixGetLaunchIndex();
-	run_deflectors(launch_idx.x);
- 
- 	// run_tracing_new(launch_idx.x);
+	run_tracing_new(launch_idx.x);
 }
 
 extern "C" __global__ void __miss__ms()
@@ -317,3 +210,65 @@ extern "C" __global__ void __anyhit__ah()
 // (На гпу расщитываем все даже for ( auto K : UVTri ) { if ( K.isInsize() ) { light_point() } } )
 
 // Добавил VertexGPU, UVTriGPU, _TCF_GPU Для копирования в GPU
+
+
+/*
+__device__ void DeflectorProcessOne(CDeflector_GPU& Defl, int TaskJitter, int U, int V)
+{
+	Hardware_Vector2 Jitter9[9] =
+	{
+		{-1,-1},	{0,-1},		{1,-1},
+		{-1,0},		{0,0},		{1,0},
+		{-1,1},		{0,1},		{1,1}
+	};
+
+	Hardware_Color	C;
+
+	// LUMEL space
+	Hardware_Vector2 P;
+	P.x = float(U) / Defl.Jitter.dim.x + Defl.Jitter.half.x + Jitter9[TaskJitter].x * Defl.Jitter.JS.x;
+	P.y = float(V) / Defl.Jitter.dim.y + Defl.Jitter.half.y + Jitter9[TaskJitter].y * Defl.Jitter.JS.y;
+
+	// World space
+	Hardware_Vector		wP, wN, B;
+	for (auto TRI_INDEX = 0; TRI_INDEX < Defl.UVTrisSize; TRI_INDEX++)
+	{
+		auto TRI = Defl.UVTris[TRI_INDEX];
+		if (TRI.isInside(P, B))
+		{
+			// We found triangle and have barycentric coords
+			VertexGPU& V1 = TRI.V[0];
+			VertexGPU& V2 = TRI.V[1];
+			VertexGPU& V3 = TRI.V[2];
+
+			wP.from_bary(V1.P, V2.P, V3.P, B);
+
+			{
+				wN.from_bary(V1.N, V2.N, V3.N, B);
+				// exact_normalize(wN);  // TODO ! se7kills
+				wN.Add(TRI.N);
+				// exact_normalize(wN);  // TODO ! se7kills
+			}
+
+			LightPoint(wP, wN, C, 0);
+			Defl.Jitter.SamplesCaptured += 1;
+
+			break;
+		}
+	}
+
+	// if (Fcount)
+	// {
+	// 	C.scale(Fcount);
+	// 	C.mul(.5f);
+	// 	Defl.surfaces[V * Defl.Width + U] = C;
+	// 	Defl.marker[V * Defl.Width + U] = 255;
+	// }
+	// else
+	// {
+	// 	Defl.surfaces[V * Defl.Width + U] = C;
+	// 	Defl.marker[V * Defl.Width + U] = 0;
+	// }
+
+}
+*/
