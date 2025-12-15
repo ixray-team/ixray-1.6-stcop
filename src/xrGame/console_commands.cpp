c5e41f20033813e11c3d00551040f6c593e4afa5
Romann <kriworuck.production@yandex.ru>
2025-12-15 20:52:27 +0300

    Compilation fix.

--- src/xrGame/console_commands.cpp
+++ src/xrGame/console_commands.cpp
@@ -1,5 +1,6 @@
 #include "StdAfx.h"
 #include "pch_script.h"
+#include "../xrEngine/XR_IOConsole.h"
 #include "../xrEngine/xr_ioc_cmd.h"
 #include "../xrEngine/CustomHUD.h"
 #include "../xrEngine/FDemoRecord.h"
