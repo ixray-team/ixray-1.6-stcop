c0f922a024fe84c99f9094112d07c245a0af2285
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-12-15 20:57:56 +0300

    Add test support for gamepad bindings

--- src/xrGame/MainMenu.cpp
+++ src/xrGame/MainMenu.cpp
@@ -357,17 +357,7 @@ void	CMainMenu::IR_OnKeyboardHold(int dik)
 
 void CMainMenu::IR_GamepadKeyPress(int id)
 {
-	if (id == SDL_GamepadButton::SDL_GAMEPAD_BUTTON_DPAD_DOWN)
-		IR_UIOnKeyboardPress(SDL_SCANCODE_DOWN);
-
-	if (id == SDL_GamepadButton::SDL_GAMEPAD_BUTTON_DPAD_UP)
-		IR_UIOnKeyboardPress(SDL_SCANCODE_UP);
-
-	if (id == SDL_GamepadButton::SDL_GAMEPAD_BUTTON_SOUTH)
-		IR_UIOnKeyboardPress(SDL_SCANCODE_RETURN);
-
-	if (id == SDL_GamepadButton::SDL_GAMEPAD_BUTTON_EAST)
-		IR_UIOnKeyboardPress(SDL_SCANCODE_ESCAPE);
+	if (!IsActive()) return;
 
 	CDialogHolder::IR_UIOnGamepadKeyPress(id);
 }
