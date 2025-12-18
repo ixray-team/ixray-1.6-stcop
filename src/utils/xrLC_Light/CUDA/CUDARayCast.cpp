b28c405af4f7e8ad88eb42ac84b347bfe18c3a46
Dimakuzmin <scorpion261096@gmail.com>
2025-12-18 09:36:22 +0200

    Доработал CUDA код проверять (есть фильтрация прозрачностей)
    Добавлена загрузка Геометрии на ГПУ (flags.bOpacue, flags.bWater)
    Добавлена загрузка Текстур на ГПУ (Alpha канала) для затона 68мб
    Переписал загрузку света на ГПУ  (Теперь раз инициализируем а не для каждого thread_local RayTrace)

--- src/utils/xrLC_Light/CUDA/CUDARayCast.cpp
+++ src/utils/xrLC_Light/CUDA/CUDARayCast.cpp
@@ -10,16 +10,199 @@
 // Пример использования:
 OptixContext optixContext;
 OptixMeshBuffers CommitedScene;
-
-
 static CUstream cudaStream = nullptr;
-FaceData* d_faces;
-MaterialData* d_materials;
 
+// Lighting 
+int					  size_lights;
+Hardware_Lighting*	  gpu_lights   = nullptr;			// GPU alloc
 
-cudaTextureObject_t* d_textures;
-u32 g_faceCount;
+int					  size_faces;
+Hardware_FaceData*	  gpu_faces    = nullptr;
+
+int					  size_textures;
+Hardware_TextureData* gpu_textures = nullptr;
+
+struct TextureDataCPU
+{
+	xr_vector<unsigned char> alpha;
+	u32 Width;
+	u32 Height;
+};
+
+void XRay::RayTrace::CUDA::InitializeLights()
+{
+	auto Lights = lc_global_data()->L_static();
+
+	enum eType : u16
+	{
+		eSun,
+		eHemi,
+		eRGB
+	};
+
+	auto Light = [&](R_Light& L, eType type)
+		{
+			Hardware_Lighting cuL;
+			cuL.type = L.type;
+			cuL.light_type = type;
+			cuL.diffuse = { L.diffuse.x, L.diffuse.y, L.diffuse.z };
+			cuL.position = { L.position.x, L.position.y, L.position.z };
+			cuL.direction = { L.direction.x, L.direction.y, L.direction.z };
+			cuL.range = L.range;
+			cuL.range2 = L.range2;
+			cuL.falloff = L.falloff;
+			cuL.attenuation0 = L.attenuation0;
+			cuL.attenuation1 = L.attenuation1;
+			cuL.attenuation2 = L.attenuation2;
+			cuL.energy = L.energy;
+			return cuL;
+		};
+
+	u32 numLights = Lights.rgb.size() + Lights.hemi.size() + Lights.sun.size();
+
+	Hardware_Lighting* h_lights = nullptr;			// CPU alloc
+	// Заполняем буфер Источников света
+	CUDA_CHECK(cudaMallocHost(&h_lights, numLights * sizeof(Hardware_Lighting)));
+
+	int INDEX_LIGHT = 0;
+	for (auto& RGB : Lights.rgb)
+	{
+		h_lights[INDEX_LIGHT] = Light(RGB, eRGB);
+		INDEX_LIGHT++;
+	}
+	for (auto& SUN : Lights.sun)
+	{
+		h_lights[INDEX_LIGHT] = Light(SUN, eSun);
+		INDEX_LIGHT++;
+	}
+	for (auto& HEMI : Lights.hemi)
+	{
+		h_lights[INDEX_LIGHT] = Light(HEMI, eHemi);
+		INDEX_LIGHT++;
+	}
+
+	CUDA_CHECK(cudaMalloc(&gpu_lights, sizeof(Hardware_Lighting) * numLights));
+	CUDA_CHECK(cudaMemcpy(gpu_lights, h_lights, sizeof(Hardware_Lighting) * numLights, cudaMemcpyHostToDevice));
+
+	cudaFreeHost(h_lights);
+	size_lights = numLights;
+}
+
+void XRay::RayTrace::CUDA::InitializeFaces(xr_vector<Face*> Faces)
+{
+	Hardware_FaceData* faces_host = nullptr;
+	int alloc_size = Faces.size() * sizeof(Hardware_FaceData);
+ 	CUDA_CHECK ( cudaMallocHost(&faces_host, alloc_size) );
+	
+	int IndexFace = 0;
+	for (auto& F : Faces)
+	{
+ 		unsigned short surface = lc_global_data()->materials()[F->dwMaterial].surfidx;
+		Hardware_FaceData& FaceGPU = faces_host[IndexFace];
+		FaceGPU.bOpacue = F->flags.bOpaque;
+		FaceGPU.bWater  = F->flags.bWater;
+
+		FaceGPU.surfidx = surface;
+
+		auto TC = F->getTC0();
+		FaceGPU.TC0[0].set(TC[0].x, TC[0].y);
+		FaceGPU.TC0[1].set(TC[1].x, TC[1].y);
+		FaceGPU.TC0[2].set(TC[2].x, TC[2].y);
+
+		IndexFace++;
+	}
+
+	CUDA_CHECK( cudaMalloc(&gpu_faces, alloc_size) );
+	CUDA_CHECK( cudaMemcpy(gpu_faces, faces_host, alloc_size, cudaMemcpyHostToDevice) );
+ 	cudaFreeHost(faces_host);
+
+	size_faces = Faces.size();
+
+	Msg("[GPU] FACES[%u] Allocate : %llu kb", Faces.size(), Faces.size() * sizeof(Hardware_FaceData) / 1024);
+}
+
+void XRay::RayTrace::CUDA::InitializeTexturesAlpha()
+{
+ 	u32 SizeT = lc_global_data()->textures().size();
  
+	xr_vector<TextureDataCPU>  Textures;
+ 	for (auto& T : lc_global_data()->textures())
+	{
+		if (!T.pSurface || !T.bHasAlpha)
+		{
+ 			Textures.push_back(TextureDataCPU());
+ 			
+			TextureDataCPU& data = Textures.back();
+			data.Width  = T.dwWidth;
+			data.Height = T.dwHeight;
+			data.alpha.clear();
+
+			continue; 
+		}
+
+ 		Textures.push_back(TextureDataCPU());
+
+		TextureDataCPU& data = Textures.back();
+		data.alpha.resize(T.dwWidth*T.dwHeight);
+ 		u8* ALPHA = data.alpha.data();
+
+		const uint32_t* raw = static_cast<const uint32_t*>(T.pSurface);
+		for (auto i = 0; i < T.dwWidth * T.dwHeight; i++)
+		{
+			uint32_t pixel = raw[i];
+			uint8_t pixel_a = (pixel >> 24) & 0xFF;
+			ALPHA[i] = pixel_a;
+		}
+		data.Width  = T.dwWidth;
+		data.Height = T.dwHeight;
+ 	}
+
+	// Textures (alpha only)
+	xr_vector<Hardware_TextureData> cpu_tex_gpu(Textures.size());
+
+	size_t allocated = 0;
+	for (size_t i = 0; i < Textures.size(); ++i)
+	{
+		const auto& src = Textures[i];
+
+		if (!src.alpha.size())
+		{
+			cpu_tex_gpu[i].pSurface = nullptr;
+		}
+		else
+		{
+			unsigned char* d_alpha = nullptr;
+			CUDA_CHECK( cudaMalloc(&d_alpha, src.Width * src.Height * sizeof(uint8_t)) );
+
+			CUDA_CHECK( cudaMemcpy(
+				d_alpha,
+				src.alpha.data(),
+				src.Width * src.Height * sizeof(uint8_t),
+				cudaMemcpyHostToDevice) );
+
+
+			cpu_tex_gpu[i].pSurface = d_alpha;
+			allocated += src.Width * src.Height;
+		}
+
+ 		cpu_tex_gpu[i].width     = src.Width;
+		cpu_tex_gpu[i].height    = src.Height;
+	
+	}
+ 
+	CUDA_CHECK ( cudaMalloc((void**)&gpu_textures, cpu_tex_gpu.size() * sizeof(Hardware_TextureData)) );
+	CUDA_CHECK(  cudaMemcpy(gpu_textures,
+		cpu_tex_gpu.data(),
+		cpu_tex_gpu.size() * sizeof(Hardware_TextureData),
+		cudaMemcpyHostToDevice) );
+
+
+	allocated += cpu_tex_gpu.size() * sizeof(Hardware_TextureData);
+	size_textures = cpu_tex_gpu.size();
+
+	Msg("[GPU] Textures[%u] Allocate : %llu kb", cpu_tex_gpu.size(), allocated / 1024);
+}
+
 void XRay::RayTrace::CUDA::InitializeRayTracing()
 {
 	// Однократная инициализация
@@ -43,6 +226,8 @@ void XRay::RayTrace::CUDA::InitializeRayTracing()
 	BuildSceneFromLCGlobalData(context, cudaStream, CommitedScene);
  
 	clMsg("Processing Memory: %u mb", GetHeapMemory() / 1024 / 1024);
+	InitializeLights();
+	InitializeTexturesAlpha();
 }
 
 // При завершении работы
@@ -52,58 +237,6 @@ void CleanupRayTracing()
 	optixContext.Destroy();
 }
 
-void XRay::RayTrace::CUDA::InitializeTextures(xr_vector<TextureData>& gpuTextures, cudaTextureObject_t*& d_texObjects)
-{
-	// 1. Создаем CPU-копии текстурных данных
-	gpuTextures.resize(inlc_global_data()->textures().size());
-
-	// 2. Создаем массив для объектов текстур CUDA
-	cudaTextureObject_t* texObjects = new cudaTextureObject_t[gpuTextures.size()];
-
-	for (u32 i = 0; i < gpuTextures.size(); i++) 
-	{
-		b_texture& srcTex = inlc_global_data()->textures()[i];
-
-		// Заполняем TextureData
-		TextureData& dstTex = gpuTextures[i];
-		dstTex.width = srcTex.dwWidth;
-		dstTex.height = srcTex.dwHeight;
-		dstTex.hasAlpha = srcTex.bHasAlpha;
-
-		// Выделяем GPU память и копируем данные
-		size_t texSize = srcTex.dwWidth * srcTex.dwHeight * sizeof(u32);
-		CUDA_CHECK(cudaMalloc(&dstTex.pSurface, texSize));
-		CUDA_CHECK(cudaMemcpy(dstTex.pSurface, srcTex.pSurface, texSize, cudaMemcpyHostToDevice));
-
-		// Создаем CUDA texture object
-		cudaResourceDesc resDesc = {};
-		resDesc.resType = cudaResourceTypePitch2D;
-		resDesc.res.pitch2D.devPtr = dstTex.pSurface;
-		resDesc.res.pitch2D.width = dstTex.width;
-		resDesc.res.pitch2D.height = dstTex.height;
-		resDesc.res.pitch2D.pitchInBytes = dstTex.width * sizeof(u32);
-		resDesc.res.pitch2D.desc = cudaCreateChannelDesc<uchar4>();
-
-		cudaTextureDesc texDesc = {};
-		texDesc.addressMode[0] = cudaAddressModeWrap;
-		texDesc.addressMode[1] = cudaAddressModeWrap;
-		texDesc.filterMode = cudaFilterModeLinear;
-		texDesc.readMode = cudaReadModeNormalizedFloat;
-		texDesc.normalizedCoords = 1;
-
-		CUDA_CHECK(cudaCreateTextureObject(&texObjects[i], &resDesc, &texDesc, nullptr));
-		dstTex.texObj = texObjects[i];
-	}
-
-	// Копируем массив texture objects на GPU
-	CUDA_CHECK(cudaMalloc(&d_texObjects, gpuTextures.size() * sizeof(cudaTextureObject_t)));
-	CUDA_CHECK(cudaMemcpy(d_texObjects, texObjects,
-		gpuTextures.size() * sizeof(cudaTextureObject_t),
-		cudaMemcpyHostToDevice));
-
-	delete[] texObjects;
-}
- 
 class RayTracer
 {
 	// Colors (Result)
@@ -113,13 +246,6 @@ class RayTracer
 	// Positions Rays (Incoming)
 	Hardware_Raytask*		h_rays;			// CPU alloc
 	Hardware_Raytask*		d_rays;			// GPU alloc
-
-
-	// Lighting 
-	int					  size_lights;
-	Hardware_Lighting*    h_lights;			// CPU alloc
-	Hardware_Lighting*	  d_lights;			// GPU alloc
-
 
 	// Parrrams (.cu export __constant__ Params g_params; )
 	OPTICK_Params*		  h_params;			// CPU alloc
@@ -163,64 +289,8 @@ public:
 		CUDA_CHECK(cudaMalloc(&d_rays, max_rays * sizeof(Hardware_Raytask)));								// Device Alloc
 		
 		isInitialized = true;
-
-		InitializeLights(lc_global_data()->L_static());
 	}
-
-	void InitializeLights(base_lighting& Lights)
-	{
-		enum eType : u16
-		{
-			eSun, 
-			eHemi, 
-			eRGB
-		};
- 
- 		auto Light = [&](R_Light& L, eType type)
-		{
-			Hardware_Lighting cuL;
- 			cuL.type			= L.type;
- 			cuL.light_type		= type;
-			cuL.diffuse			= { L.diffuse.x, L.diffuse.y, L.diffuse.z };
-			cuL.position		= { L.position.x, L.position.y, L.position.z };
-			cuL.direction		= { L.direction.x, L.direction.y, L.direction.z };
-			cuL.range			= L.range;
-			cuL.range2			= L.range2;
-			cuL.falloff			= L.falloff;
-			cuL.attenuation0	= L.attenuation0;
-			cuL.attenuation1	= L.attenuation1;
-			cuL.attenuation2	= L.attenuation2;
-			cuL.energy			= L.energy;
-			return cuL;
- 		};
- 		
-		u32 numLights = Lights.rgb.size() + Lights.hemi.size() + Lights.sun.size();
- 		
-		// Заполняем буфер Источников света
- 		CUDA_CHECK(cudaMallocHost(&h_lights, numLights * sizeof(Hardware_Lighting)));
-
-		int INDEX_LIGHT = 0;
-		for (auto& RGB : Lights.rgb)
-		{
-			h_lights[INDEX_LIGHT] = Light(RGB, eRGB);
-			INDEX_LIGHT++;
-		}
-		for (auto& SUN : Lights.sun)
-		{
-			h_lights[INDEX_LIGHT] = Light(SUN, eSun);
-			INDEX_LIGHT++;
-		}
-		for (auto& HEMI : Lights.hemi)
-		{
-			h_lights[INDEX_LIGHT] = Light(HEMI, eHemi);
-			INDEX_LIGHT++;
-		}
- 	
-		CUDA_CHECK( cudaMalloc(&d_lights, sizeof(Hardware_Lighting) * numLights) );
-		CUDA_CHECK( cudaMemcpy(d_lights, h_lights, sizeof(Hardware_Lighting) * numLights, cudaMemcpyHostToDevice) );
-		size_lights = numLights;
-	}
- 
+	 
 	// Вызывать только после вызова RayTrace
  	xr_vector<base_color_c>& GetColors()
 	{
@@ -239,8 +309,9 @@ public:
 		h_rays[INDEX] =
 		{
 			.Position = make_float3(Task.P.x, Task.P.y, Task.P.z),
-			.Direction = make_float3(Task.N.x, Task.N.y, Task.N.z)
-		};
+			.Direction = make_float3(Task.N.x, Task.N.y, Task.N.z),
+			.SkipFace = Task.FaseSkip
+ 		};
 	}
 	 
 	void TraceRaysNew(size_t INDEX)
@@ -248,16 +319,23 @@ public:
 		size_t CurrentWritedRays = INDEX;
 
   		// Подготавливаем данные на хосте
- 		h_params[0] =
+		h_params[0] =
 		{
 			.handle = CommitedScene.tlasHandle,
-			
+
 			// Result Buffer
-			.flags  = current_flags,
-			.rays   = d_rays,
+			.flags = current_flags,
+			.rays = d_rays,
 			.colors = d_colors,
-			.lights = d_lights,
+
+			.lights = gpu_lights,
 			.counts_lights = size_lights,
+
+			.faces = gpu_faces,
+			.count_faces = size_faces,
+
+			.textures = gpu_textures,
+			.count_textures = size_textures,
 		};
  		 
 		// Копируем Стартовые параметры !!! асинхронно
@@ -304,6 +382,7 @@ public:
  
 		// Синхронизируем только один раз
 		CUDA_CHECK(cudaStreamSynchronize(stream));
+
 
 		// Копия цветов
 		auto copy_color = [&](Hardware_Color& Chw, base_color_c& C)
