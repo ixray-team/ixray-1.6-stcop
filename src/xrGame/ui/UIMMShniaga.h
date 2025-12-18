c1bb53da933a4912d07661e0a21f65652314f64e
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-12-18 15:58:49 +0300

    Revert "Add test support for gamepad bindings"
    This reverts commit 7124f12203b7c38fd69e383b5e30f39e6b5e4498.
    
    # Conflicts:
    #	gamedata/configs/ixray_settings/default_controls.ltx
    #	gamedata_cs/configs/ixray_settings/default_controls.ltx

--- src/xrGame/ui/UIMMShniaga.h
+++ src/xrGame/ui/UIMMShniaga.h
@@ -21,7 +21,6 @@ public:
 
 	virtual bool 		OnMouseAction					(float x, float y, EUIMessages mouse_action);
 	virtual bool 		OnKeyboardAction				(int dik, EUIMessages keyboard_action);
-	virtual bool 		OnGamepadKeyAction				(int id, EUIMessages gamepad_action);
 	virtual void 		SendMessage				(CUIWindow* pWnd, s16 msg, void* pData = 0);
 			void 		SetVisibleMagnifier		(bool f);
 	virtual void		OnDeviceReset			();
