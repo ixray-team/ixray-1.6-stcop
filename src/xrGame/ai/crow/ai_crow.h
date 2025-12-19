eb2986185d1341814476cff7cf45293126b9b255
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-12-19 23:29:30 +0300

    Add crows flying to corpses from B.O.R.S.C.H.T
    by @redpython

--- src/xrGame/ai/crow/ai_crow.h
+++ src/xrGame/ai/crow/ai_crow.h
@@ -100,6 +100,9 @@ protected:
 	
 	void			UpdateWorkload				(float DT);
 
+	xr_vector<CObject*> nearbyObjects;
+	xr_vector<CEntityAlive*> deadNPCs;
+
 public:						
 	u32				o_workload_frame	;
 	u32				o_workload_rframe	;
