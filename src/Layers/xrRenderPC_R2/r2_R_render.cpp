21d42ee0b5378263e3a4bec4e6d7157fd2a7d556
ForserX <sv3nk@yandex.ru>
2025-12-19 22:56:31 +0300

    Add `ERHI_CULLMODE`

--- src/Layers/xrRenderPC_R2/r2_R_render.cpp
+++ src/Layers/xrRenderPC_R2/r2_R_render.cpp
@@ -296,7 +296,7 @@ void CRender::render_main	(bool deffered, bool zfill)
 void CRender::render_menu	()
 {
 	//	Globals
-	RCache.set_CullMode				(CULL_CCW);
+	GRHI->StateManager->SetCullMode(ERHI_CULLMODE::BACK);
 	RCache.set_Stencil				(FALSE);
 	RCache.set_ColorWriteEnable		();
 
@@ -529,28 +529,28 @@ void CRender::Render()
 	}
 
 	// Directional light - fucking sun
-	if (bSUN)	{
-		RImplementation.stats.l_visible		++;
+	if (bSUN)
+	{
+		RImplementation.stats.l_visible++;
 		render_sun_cascades();
 		Target->increment_light_marker();
 	}
 
 	{
-		Target->phase_accumulator					();
+		Target->phase_accumulator();
 		// Render emissive geometry, stencil - write 0x0 at pixel pos
-		RCache.set_xform_project					(Device.mProject); 
-		RCache.set_xform_view						(Device.mView);
+		RCache.set_xform_project(Device.mProject);
+		RCache.set_xform_view(Device.mView);
 		// Stencil - write 0x1 at pixel pos - 
-		RCache.set_Stencil							( TRUE,D3DCMP_ALWAYS,0x01,0xff,0xff,D3DSTENCILOP_KEEP,D3DSTENCILOP_REPLACE,D3DSTENCILOP_KEEP);
-		//RCache.set_Stencil						(TRUE,D3DCMP_ALWAYS,0x00,0xff,0xff,D3DSTENCILOP_KEEP,D3DSTENCILOP_REPLACE,D3DSTENCILOP_KEEP);
-		RCache.set_CullMode							(CULL_CCW);
-		RCache.set_ColorWriteEnable					();
-		RImplementation.r_dsgraph_render_emissive	();
+		RCache.set_Stencil(TRUE, D3DCMP_ALWAYS, 0x01, 0xff, 0xff, D3DSTENCILOP_KEEP, D3DSTENCILOP_REPLACE, D3DSTENCILOP_KEEP);
+		GRHI->StateManager->SetCullMode(ERHI_CULLMODE::BACK);
+		RCache.set_ColorWriteEnable();
+		RImplementation.r_dsgraph_render_emissive();
 
 		// Stencil	- draw only where stencil >= 0x1
-		RCache.set_Stencil					(TRUE,D3DCMP_LESSEQUAL,0x01,0xff,0x00);
-		RCache.set_CullMode					(CULL_NONE);
-		RCache.set_ColorWriteEnable			();
+		RCache.set_Stencil(TRUE, D3DCMP_LESSEQUAL, 0x01, 0xff, 0x00);
+		GRHI->StateManager->SetCullMode(ERHI_CULLMODE::NONE);
+		RCache.set_ColorWriteEnable();
 	}
 
 	// Lighting, non dependant on OCCQ
