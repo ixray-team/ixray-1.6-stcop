7f55f691a5bf4bb0c3c1ab06ab55d54de067ab2e
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-12-19 11:40:38 +0300

    Sync `gamedata` folders

--- gamedata_soc/shaders/d3d11/accum_sun.ps.hlsl
+++ gamedata_soc/shaders/d3d11/accum_sun.ps.hlsl
@@ -1,64 +1,61 @@
 #include "common.hlsli"
-
-#if SUN_QUALITY > 2
-    #define USE_ULTRA_SHADOWS
-#endif
-
-#include "shadow.hlsli"
 #include "metalic_roughness_light.hlsli"
 #include "ScreenSpaceContactShadows.hlsl"
+#include "shadow.hlsli"
 
-float4 main(v2p_volume I) : SV_Target
+struct PSInput
 {
-    float2 tcProj = I.tc.xy / I.tc.w;
+	float4 hpos : SV_POSITION;
+	float2 texcoord : TEXCOORD0;
+};
+
+float4 main(PSInput I) : SV_Target
+{
+	IXrayGbuffer O;
+	GbufferUnpack(I.texcoord, I.hpos.xy, O);
 	
-    IXrayGbuffer O;
-    GbufferUnpack(tcProj, I.hpos.xy, O);
-
-    float3 Shift = O.Normal;
-
-    if (O.SSS > 0.0f)
-    {
-        Shift *= dot(Ldynamic_dir.xyz, Shift) >= 0.0 ? -1.0f : 1.0f;
-    }
-
-    float4 Point = float4(O.Point.xyz, 1.f);
-    Point.xyz += Shift * 0.025f;
+	float3 Shift = O.Normal;
 	
-    float4 PS = mul(m_shadow, Point);
-
-#ifdef USE_FAR_ATTENTION
-    float3 Factor = smoothstep(0.5f, 0.45f, abs(PS.xyz / PS.w - 0.5f));
-    float Fade = Factor.x * Factor.y * Factor.z;
+	if (O.SSS > 0.0f)
+	{
+		Shift *= dot(Ldynamic_dir.xyz, Shift) >= 0.0 ? -1.0f : 1.0f;
+	}
 	
-    O.SSS *= 0.5f + 0.5f * Fade;
-#endif
-
+	float4 Point = float4(O.Point.xyz, 1.f);
+	Point.xyz += Shift * 0.025f;
+	
+	int cascade_index;
+	float3 smap_texcoord;
+	bool is_in_bounds = calc_cascades(mul(m_invV, Point).xyz, m_shadow_sun, cascade_index, smap_texcoord);
+	
+	float Shadow = 1.0f;
+	
+	if(is_in_bounds)
+	{
+		Shadow *= shadow_sun(smap_texcoord, cascade_index);
+	}
+	
+	if(cascade_index >= 2)
+	{
+		float3 Factor = smoothstep(0.5f, 0.49f, abs(smap_texcoord - 0.5f));
+		float Fade = Factor.x * Factor.y * Factor.z;
+	
+		O.SSS *= 0.5f + 0.5f * Fade;	
+		float FarShadow = dot(Ldynamic_dir.xyz, O.Normal.xyz);
+		FarShadow = smoothstep(0.75f, 0.6f, FarShadow) * saturate(O.Hemi * 8.0f - 2.0f);
+		Shadow = lerp(FarShadow, Shadow, Fade);
+	}
+	
+	Shadow *= sunmask(Point);
+	
     float3 Light = DirectLight(Ldynamic_color, Ldynamic_dir.xyz, O.Normal, O.View.xyz, O.Color, O.Metalness, O.Roughness, O.F0);
     Light += SimpleTranslucency(Ldynamic_color.xyz, Ldynamic_dir.xyz, O.Normal) * O.SSS * O.Color;
-
-#if SUN_QUALITY == 2
-    float Shadow = shadow_high(PS);
-#else
-    float Shadow = shadow(PS);
-#endif
-
-#ifdef USE_FAR_ATTENTION
-	float FarShadow = dot(Ldynamic_dir.xyz, O.Normal.xyz);
-	FarShadow = smoothstep(0.75f, 0.6f, FarShadow) * saturate(O.Hemi * 8.0f - 2.0f);
 	
-    Shadow = lerp(FarShadow, Shadow, Fade);
-
-#elif defined(USE_HUD_SHADOWS)
-    if (O.Depth < 0.02f && dot(Shadow.xxx, Light.xyz) > 0.0001f)
-    {
-        RayTraceContactShadow(tcProj, O.PointHud, Ldynamic_dir.xyz, Light);
-    }
-#endif
-
-    Shadow *= sunmask(Point);
-	Shadow = PushGamma(Shadow);
+	if (O.Depth < 0.02f && dot(Shadow.xxx, Light.xyz) > EPS)
+	{
+		RayTraceContactShadow(I.texcoord, O.PointHud, Ldynamic_dir.xyz, Light);
+	}
 	
-    return float4(Light * Shadow, Shadow);
+	Light *= PushGamma(Shadow);
+	return float4(Light, 0);
 }
-
