bb73c365ed7c5275d0b6df0b33ff35780d16e184
Dimakuzmin <scorpion261096@gmail.com>
2025-12-20 09:42:58 +0200

    Optimize face build in cform stage

--- src/utils/xrLC_Light/embree_raytracing/EmbreeGeometryBuilder.h
+++ src/utils/xrLC_Light/embree_raytracing/EmbreeGeometryBuilder.h
@@ -22,14 +22,7 @@ struct Triangle
 		point2 = T.verts[1];
 		point3 = T.verts[2];
 	}
-};
-
-struct CFormTriangle  
-{
-	u16 MaterialID;
-	u16 Sector;
-};
-
+}; 
 
 struct IndexedTri
 {
@@ -66,32 +59,15 @@ struct TriangleContainer
   	xr_vector<Fvector>				verts_v;
 	xr_vector<Triangle>				faces_v;
 
-	xr_vector<CFormTriangle>		cform_data;
-	xr_vector<Face*>				dummy;
+ 	xr_vector<Face*>				dummy;
 
-	xr_vector<Fvector>& vertex() { return verts_v; }
-	xr_vector<Triangle>& faces() { return faces_v; }
+	xr_vector<Fvector>&				vertex() { return verts_v; }
+	xr_vector<Triangle>&			faces() { return faces_v; }
 	u32 vertex_cnt() { return verts_v.size(); }
 	u32 faces_cnt() { return faces_v.size(); }
  	
 	// Add Faces
-	size_t AddVertex(Fvector& V);
-	void AddFace	(void* F, Fvector& v1, Fvector& v2, Fvector& v3);
-	void AddFaceMaterial (void* F, Fvector& v1, Fvector& v2, Fvector& v3, u16 Material, u16 SectorID);
-	
 	void ClearAll();
-
-	CDB::TRI GetCDBMaterial(u32 IndexTriangle)
-	{
-		CDB::TRI tri;
-		tri.verts[0] = faces_v[IndexTriangle].point1;
-		tri.verts[1] = faces_v[IndexTriangle].point2;
-		tri.verts[2] = faces_v[IndexTriangle].point3;
-
-		tri.material = cform_data[IndexTriangle].MaterialID;
-		tri.sector   = cform_data[IndexTriangle].Sector;
-		return tri;
-	}
 
  	// Removeing Dublicates
 	struct IndexedVertex
@@ -115,13 +91,7 @@ struct TriangleContainer
 	{
 		raw_faces.push_back({ {v1, v2, v3}, F, 0, 0 });
 	};
-
-	void AddFaceRawMaterial(Face* F, const Fvector& v1, const Fvector& v2, const Fvector& v3, u16 MaterialID, u16 SectorID)
-	{
-		raw_faces.push_back({ {v1, v2, v3}, F, MaterialID, SectorID });
-	};
-
-
+ 
 	void RemoveDublicatesVertexs(bool isTransparent);
 	void RemoveDublicatesFaces(bool isTransparent);
 };
