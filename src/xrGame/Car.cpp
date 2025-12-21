6f0364bde5aaedd9d552aa6e5031c639a0caea5c
v2v3v4 <terran.erre@mail.ru>
2025-12-21 13:36:53 +0300

    фикс двойного апдейта камеры в машине

--- src/xrGame/Car.cpp
+++ src/xrGame/Car.cpp
@@ -417,8 +417,13 @@ void CCar::UpdateEx(float fov)
 	if (OwnerLocal && OwnerLocal->IsMyCamera() && OwnerLocal->HasCameraEffector())
 	{
 		cam_Update(Device.fTimeDelta, fov);
-		OwnerLocal->Cameras().UpdateFromCamera(Camera());
-		OwnerLocal->Cameras().ApplyDevice(Device.fViewportNear);
+		if(active_camera->tag==ectFirst)
+		{
+			OwnerLocal->Cameras().UpdateFromCamera(Camera());
+			OwnerLocal->Cameras().ApplyDevice(Device.fViewportNear);
+		}
+		else
+			Level().Cameras().UpdateFromCamera(active_camera);
 	}
 }
 
