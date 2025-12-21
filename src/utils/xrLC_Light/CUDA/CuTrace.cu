1b4587373c8eeb01620a399db7b3d10958bcc88b
Dimakuzmin <scorpion261096@gmail.com>
2025-12-21 09:50:08 +0200

    Фиксы для правильности расчета освещения Substract в векторе местами попутал

--- src/utils/xrLC_Light/CUDA/CuTrace.cu
+++ src/utils/xrLC_Light/CUDA/CuTrace.cu
@@ -1,4 +1,5 @@
 ﻿#include <cuda_runtime.h>
+
 #include <optix.h>
 #include <optix_device.h>
 #include "Vector3HW.h"
@@ -32,17 +33,20 @@ enum eType
 __device__ float RunOptickTask(Hardware_Vector& P, Hardware_Vector& N, float Range, unsigned int SkipID)
 {
 	const float3 origin = P.getVector3();
-	const float3 dir = N.getVector3();
-	const float maxT = Range;
+	const float3 dir    = N.getVector3();
+	
+	const float minT	= 0.f;
+	const float maxT    = Range;
+	const float RayTime = 0;
 
  	unsigned int Energy = 100;
-
+ 
 	//// Обновить размер в CUDAContext В pipelineCompileOptions.numPayloadValues (Если менять кол-во Payloads)
  	optixTrace(
 		g_params.handle,
 		origin,
 		dir,
-		0.0f, maxT, 0.0f,
+		minT, maxT, RayTime,
 		OptixVisibilityMask(255),
 		OPTIX_RAY_FLAG_NONE,
 		//OPTIX_RAY_FLAG_DISABLE_ANYHIT,
@@ -50,7 +54,7 @@ __device__ float RunOptickTask(Hardware_Vector& P, Hardware_Vector& N, float Ran
 		Energy, SkipID
 	);
 
-	// Баг был тут поченил
+ 	// Баг был тут поченил
 	return float (Energy) / 100.0f ;  
 }
 
@@ -62,7 +66,7 @@ __device__ void CalculatePoint(Hardware_Raytask& Task, Hardware_Lighting& L, Har
 
 	Hardware_Vector Ldir;
 	Hardware_Vector Pnew = P;
-	Pnew.Mad_Self(N, 0.1f);
+	Pnew.Mad_Self(N, 0.01f);
 
 	Hardware_Vector LightPosition  = { L.position.x, L.position.y, L.position.z };
 	Hardware_Vector LightDirection = { L.direction.x, L.direction.y, L.direction.z };
@@ -72,87 +76,90 @@ __device__ void CalculatePoint(Hardware_Raytask& Task, Hardware_Lighting& L, Har
 
 	switch (L.type)
 	{
-	case LT_DIRECT:
-	{
-		Ldir.Inverted(LightDirection);
-		float D = Ldir.DotProduct(N);
-		if (D <= 0)
-			return;
-
-		float trace = RunOptickTask(Pnew, Ldir, 1000.f, SkipID);
-		att = isSunOrHemi ? L.energy * trace : D * L.energy * trace;
-	}
-	break;
-
-	case LT_POINT:
-	{
-		// Хрень для дефлекторов 
-		if (Task.UseSphere)
+		case LT_DIRECT:
 		{
-			Hardware_Vector SpherePos(Task.Sphere_Pos);
-			float			SphereRange = Task.Sphare_Range;
-		
-			float dist = SpherePos.DistanceTo(LightPosition);
-			if (dist > (SphereRange + L.range))
+ 			Ldir.Inverted(LightDirection);
+			float D = Ldir.DotProduct(N);
+			if (D <= 0)
 				return;
+
+			float Range = 1000.f;
+			float trace = RunOptickTask(Pnew, Ldir, Range, SkipID);
+			att = isSunOrHemi ? L.energy * trace : D * L.energy * trace;
 		}
+		break;
 
-
-		float sqD = P.DistanceSquared(LightPosition);
-		if (sqD > L.range2)
-			return;
-
-		Ldir.Subtract(LightPosition, P).Normalize_Safe();
-		float D = Ldir.DotProduct(N);
-		if (D <= 0)
-			return;
-
-		float R = sqrtf(sqD);
-		float trace = RunOptickTask(Pnew, Ldir, R, SkipID);
-		float scale = D * L.energy * trace;
-
-		if (isSunOrHemi)
+		case LT_POINT:
 		{
-			att = scale / (L.attenuation0 + L.attenuation1 * R + L.attenuation2 * sqD);
+			// Хрень для дефлекторов 
+			if (Task.UseSphere)
+			{
+				Hardware_Vector SpherePos    (Task.Sphere_Pos);
+				float			SphereRange = Task.Sphare_Range;
+			
+				float dist		= SpherePos.DistanceTo(LightPosition);
+				if (dist >		(SphereRange + L.range))
+					return;
+			}
+		
+			float sqD = P.DistanceSquared(LightPosition);
+			if (sqD > L.range2)
+				return;
+		
+			Ldir.Subtract(LightPosition, P).Normalize_Safe();
+			float D = Ldir.DotProduct(N);
+			if (D <= 0)
+				return;
+		
+			float R     = sqrtf(sqD);
+			float trace = RunOptickTask(Pnew, Ldir, R, SkipID);
+			float scale = D * L.energy * trace;
+		
+			if (isSunOrHemi)
+			{
+				att = scale / (L.attenuation0 + L.attenuation1 * R + L.attenuation2 * sqD);
+			}
+			else
+			{
+				att = scale * (1.0f / (L.attenuation0 + L.attenuation1 * R + L.attenuation2 * sqD) - R * L.falloff);
+			}
 		}
-		else
+		break;
+
+		case LT_SECONDARY:
 		{
-			att = scale * (1.0f / (L.attenuation0 + L.attenuation1 * R + L.attenuation2 * sqD) - R * L.falloff);
+			float sqD = P.DistanceSquared(LightPosition);
+			if (sqD > L.range2)
+				return;
+		
+			Ldir.Subtract(LightPosition, P).Normalize_Safe();
+			float D = Ldir.DotProduct(N);
+			if (D <= 0)
+				return;
+		
+			D *= -Ldir.DotProduct(LightDirection);
+			if (D <= 0)
+				return;
+		
+			float R = sqrtf(sqD);
+			float trace = RunOptickTask(Pnew, Ldir, R, SkipID);
+			att = powf(D, 0.125f) * L.energy * trace * (1.0f - R / L.range);
 		}
-	}
-	break;
-
-	case LT_SECONDARY:
-	{
-		float sqD = P.DistanceSquared(LightPosition);
-		if (sqD > L.range2)
-			return;
-
-		Ldir.Subtract(LightPosition, P).Normalize_Safe();
-		float D = Ldir.DotProduct(N);
-		if (D <= 0)
-			return;
-
-		D *= -Ldir.DotProduct(LightDirection);
-		if (D <= 0)
-			return;
-
-		float R = sqrtf(sqD);
-		float trace = RunOptickTask(Pnew, Ldir, R, SkipID);
-		att = powf(D, 0.125f) * L.energy * trace * (1.0f - R / L.range);
-	}
-	break;
+		break;
 	}
 
 	switch (L.light_type)
 	{
 		case eSun:
+		{
 			C.sun += att;
-			break;
-		case eHemi:
-			C.hemi += att;
+		} break;
 
-			break;
+		case eHemi:
+		{
+			C.hemi += att;
+		} break;
+
 		case eRGB:
 		{
 			Hardware_Vector& rgb = C.rgb;
@@ -166,10 +173,11 @@ __device__ void CalculatePoint(Hardware_Raytask& Task, Hardware_Lighting& L, Har
 
 enum Flags
 {
-	LP_Default   =  0,
-	LP_dont_rgb  = (1 << 1),
-	LP_dont_hemi = (1 << 2),
-	LP_dont_sun  = (1 << 3),
+	LP_Default		  =  0,
+	LP_UseFaceDisable = (1 << 0),
+	LP_dont_rgb		  = (1 << 1),
+	LP_dont_hemi	  = (1 << 2),
+	LP_dont_sun		  = (1 << 3),
 };
 
 __device__ void LightPoint(Hardware_Raytask& task, Hardware_Color& ColorUV, unsigned char flags)
@@ -224,8 +232,7 @@ __device__ void calculate_energy(Hardware_FaceData& F, Hardware_TextureData& T, 
 	const float2 bc  = optixGetTriangleBarycentrics();
 	float hitU = bc.x;
 	float hitV = bc.y;
-
-	float b0 = 1.0f - hitU - hitV;
+ 	float b0 = 1.0f - hitU - hitV;
  	 
 	// interpolate UV
 	float u = F.TC0[0].x * b0 + F.TC0[1].x * hitU + F.TC0[2].x * hitV;
@@ -238,11 +245,7 @@ __device__ void calculate_energy(Hardware_FaceData& F, Hardware_TextureData& T, 
 
 	float a = ( T.pSurface[V * T.width + U] / 255.0f );
 	float Transparency = (1.f - a * a);
-	
-	if (F.bWater)
-		energy *= 0.9991f; // 10 / 255
-	else 
-		energy *= Transparency;
+	energy *= Transparency;
 }
 
 extern "C" __global__ void __anyhit__ah()
@@ -266,8 +269,7 @@ extern "C" __global__ void __anyhit__ah()
 	if (F.bOpacue || T.pSurface == nullptr)
 	{
 		// Не имеюь прозрачности  → останавливаемся
-		energy = 0;
-		optixSetPayload_0(0);
+ 		optixSetPayload_0(0);
 		return;
 	}
 	 
