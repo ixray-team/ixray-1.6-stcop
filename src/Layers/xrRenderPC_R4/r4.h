d9a8618a20c1d8d4d9864d95d1e2bbd5af8d5379
v2v3v4 <terran.erre@mail.ru>
2025-12-23 16:44:48 +0300

    делитнул не используемую функцию

--- src/Layers/xrRenderPC_R4/r4.h
+++ src/Layers/xrRenderPC_R4/r4.h
@@ -128,7 +128,6 @@ private:
 	void							LoadSWIs					(CStreamReader	*fs);
 	void							Load3DFluid					();
 
-	BOOL							add_Dynamic					(dxRender_Visual*pVisual, u32 planes);		// normal processing
 	void							add_Static					(dxRender_Visual*pVisual, u32 planes);
 	void							add_leafs_Dynamic			(dxRender_Visual*pVisual);	// if detected node's full visibility
 	void							add_leafs_Static			(dxRender_Visual*pVisual);						// if detected node's full visibility
