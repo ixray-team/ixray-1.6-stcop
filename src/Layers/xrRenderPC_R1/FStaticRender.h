3425cc374d6e29289fc84c7f0ba59ec8123e6ae5
v2v3v4 <terran.erre@mail.ru>
2025-12-23 16:44:48 +0300

    делитнул не используемую функцию

--- src/Layers/xrRenderPC_R1/FStaticRender.h
+++ src/Layers/xrRenderPC_R1/FStaticRender.h
@@ -99,7 +99,6 @@ private:
 	void								LoadSectors				(IReader *fs);
 	void								LoadSWIs				(CStreamReader	*fs);
 
-	BOOL								add_Dynamic				(dxRender_Visual	*pVisual, u32 planes);		// normal processing
 	void								add_Static				(dxRender_Visual	*pVisual, u32 planes);
 	void								add_leafs_Dynamic		(dxRender_Visual	*pVisual); // if detected node's full visibility
 	void								add_leafs_Static		(dxRender_Visual	*pVisual);						// if detected node's full visibility
