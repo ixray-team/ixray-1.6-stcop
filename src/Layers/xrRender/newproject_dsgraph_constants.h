d68032cd64a18f5f9028daa3d30b376b8dd9a544
ForserX <sv3nk@yandex.ru>
2025-12-24 08:51:42 +0300

    Restore dsgraph opt from NewProject

---
+++ src/Layers/xrRender/newproject_dsgraph_constants.h
@@ -0,0 +1,186 @@
+// NP: tatarinrafa
+#pragma once
+
+// Static geometry optimization
+#define O_S_L1_S_LOW 10.f  // geometry 3d volume size
+#define O_S_L1_D_LOW 150.f // distance, after which it is not rendered
+#define O_S_L2_S_LOW 100.f
+#define O_S_L2_D_LOW 200.f
+#define O_S_L3_S_LOW 500.f
+#define O_S_L3_D_LOW 250.f
+#define O_S_L4_S_LOW 2500.f
+#define O_S_L4_D_LOW 350.f
+#define O_S_L5_S_LOW 7000.f
+#define O_S_L5_D_LOW 400.f
+#define O_S_L6_S_LOW 12000.f
+#define O_S_L6_D_LOW 800.f
+#define O_S_L7_S_LOW 18000.f
+#define O_S_L7_D_LOW 1000.f
+#define O_S_L8_S_LOW 25000.f
+#define O_S_L8_D_LOW 1200.f
+#define O_S_L9_S_LOW 40000.f
+#define O_S_L9_D_LOW 1500.f
+#define O_S_L10_S_LOW 60000.f
+#define O_S_L10_D_LOW 1800.f
+#define O_S_L11_S_LOW 100000.f
+#define O_S_L11_D_LOW 2000.f
+#define O_S_L12_S_LOW 150000.f
+#define O_S_L12_D_LOW 2500.f
+
+#define O_S_L1_S_MED 25.f
+#define O_S_L1_D_MED 50.f
+#define O_S_L2_S_MED 200.f
+#define O_S_L2_D_MED 150.f
+#define O_S_L3_S_MED 1000.f
+#define O_S_L3_D_MED 200.f
+#define O_S_L4_S_MED 2500.f
+#define O_S_L4_D_MED 300.f
+#define O_S_L5_S_MED 7000.f
+#define O_S_L5_D_MED 400.f
+#define O_S_L6_S_MED 15000.f
+#define O_S_L6_D_MED 750.f
+#define O_S_L7_S_MED 20000.f
+#define O_S_L7_D_MED 900.f
+#define O_S_L8_S_MED 35000.f
+#define O_S_L8_D_MED 1100.f
+#define O_S_L9_S_MED 55000.f
+#define O_S_L9_D_MED 1250.f
+#define O_S_L10_S_MED 75000.f
+#define O_S_L10_D_MED 1500.f
+#define O_S_L11_S_MED 120000.f
+#define O_S_L11_D_MED 1750.f
+#define O_S_L12_S_MED 200000.f
+#define O_S_L12_D_MED 2000.f
+
+#define O_S_L1_S_HII 50.f
+#define O_S_L1_D_HII 50.f
+#define O_S_L2_S_HII 400.f
+#define O_S_L2_D_HII 150.f
+#define O_S_L3_S_HII 1500.f
+#define O_S_L3_D_HII 200.f
+#define O_S_L4_S_HII 5000.f
+#define O_S_L4_D_HII 300.f
+#define O_S_L5_S_HII 20000.f
+#define O_S_L5_D_HII 350.f
+#define O_S_L6_S_HII 30000.f
+#define O_S_L6_D_HII 700.f
+#define O_S_L7_S_HII 45000.f
+#define O_S_L7_D_HII 900.f
+#define O_S_L8_S_HII 55000.f
+#define O_S_L8_D_HII 1100.f
+#define O_S_L9_S_HII 70000.f
+#define O_S_L9_D_HII 1250.f
+#define O_S_L10_S_HII 90000.f
+#define O_S_L10_D_HII 1500.f
+#define O_S_L11_S_HII 150000.f
+#define O_S_L11_D_HII 1750.f
+#define O_S_L12_S_HII 250000.f
+#define O_S_L12_D_HII 2000.f
+
+#define O_S_L1_S_ULT 50.f
+#define O_S_L1_D_ULT 40.f
+#define O_S_L2_S_ULT 500.f
+#define O_S_L2_D_ULT 125.f
+#define O_S_L3_S_ULT 1750.f
+#define O_S_L3_D_ULT 175.f
+#define O_S_L4_S_ULT 5250.f
+#define O_S_L4_D_ULT 250.f
+#define O_S_L5_S_ULT 25000.f
+#define O_S_L5_D_ULT 300.f
+#define O_S_L6_S_ULT 40000.f
+#define O_S_L6_D_ULT 600.f
+#define O_S_L7_S_ULT 50000.f
+#define O_S_L7_D_ULT 800.f
+#define O_S_L8_S_ULT 65000.f
+#define O_S_L8_D_ULT 1000.f
+#define O_S_L9_S_ULT 85000.f
+#define O_S_L9_D_ULT 1200.f
+#define O_S_L10_S_ULT 150000.f
+#define O_S_L10_D_ULT 1500.f
+#define O_S_L11_S_ULT 250000.f
+#define O_S_L11_D_ULT 1750.f
+#define O_S_L12_S_ULT 500000.f
+#define O_S_L12_D_ULT 2000.f
+
+// Dyn geometry optimization
+#define O_D_L1_S_LOW 1.f  // geometry 3d volume size
+#define O_D_L1_D_LOW 80.f // distance, after which it is not rendered
+#define O_D_L2_S_LOW 3.f
+#define O_D_L2_D_LOW 150.f
+#define O_D_L3_S_LOW 4000.f
+#define O_D_L3_D_LOW 250.f
+#define O_D_L4_S_LOW 10000.f
+#define O_D_L4_D_LOW 500.f
+#define O_D_L5_S_LOW 25000.f
+#define O_D_L5_D_LOW 750.f
+
+#define O_D_L1_S_MED 2.f
+#define O_D_L1_D_MED 40.f
+#define O_D_L2_S_MED 4.f
+#define O_D_L2_D_MED 100.f
+#define O_D_L3_S_MED 4000.f
+#define O_D_L3_D_MED 200.f
+#define O_D_L4_S_MED 10000.f
+#define O_D_L4_D_MED 400.f
+#define O_D_L5_S_MED 25000.f
+#define O_D_L5_D_MED 600.f
+
+#define O_D_L1_S_HII 5.0f
+#define O_D_L1_D_HII 30.f
+#define O_D_L2_S_HII 10.f
+#define O_D_L2_D_HII 80.f
+#define O_D_L3_S_HII 4000.f
+#define O_D_L3_D_HII 150.f
+#define O_D_L4_S_HII 10000.f
+#define O_D_L4_D_HII 300.f
+#define O_D_L5_S_HII 25000.f
+#define O_D_L5_D_HII 500.f
+
+#define O_D_L1_S_ULT 7.5f
+#define O_D_L1_D_ULT 30.f
+#define O_D_L2_S_ULT 15.f
+#define O_D_L2_D_ULT 50.f
+#define O_D_L3_S_ULT 4000.f
+#define O_D_L3_D_ULT 110.f
+#define O_D_L4_S_ULT 10000.f
+#define O_D_L4_D_ULT 250.f
+#define O_D_L5_S_ULT 25000.f
+#define O_D_L5_D_ULT 400.f
+
+Fvector4 o_optimize_static_l1_dist = { O_S_L1_D_LOW, O_S_L1_D_MED, O_S_L1_D_HII, O_S_L1_D_ULT };
+Fvector4 o_optimize_static_l1_size = { O_S_L1_S_LOW, O_S_L1_S_MED, O_S_L1_S_HII, O_S_L1_S_ULT };
+Fvector4 o_optimize_static_l2_dist = { O_S_L2_D_LOW, O_S_L2_D_MED, O_S_L2_D_HII, O_S_L2_D_ULT };
+Fvector4 o_optimize_static_l2_size = { O_S_L2_S_LOW, O_S_L2_S_MED, O_S_L2_S_HII, O_S_L2_S_ULT };
+Fvector4 o_optimize_static_l3_dist = { O_S_L3_D_LOW, O_S_L3_D_MED, O_S_L3_D_HII, O_S_L3_D_ULT };
+Fvector4 o_optimize_static_l3_size = { O_S_L3_S_LOW, O_S_L3_S_MED, O_S_L3_S_HII, O_S_L3_S_ULT };
+Fvector4 o_optimize_static_l4_dist = { O_S_L4_D_LOW, O_S_L4_D_MED, O_S_L4_D_HII, O_S_L4_D_ULT };
+Fvector4 o_optimize_static_l4_size = { O_S_L4_S_LOW, O_S_L4_S_MED, O_S_L4_S_HII, O_S_L4_S_ULT };
+Fvector4 o_optimize_static_l5_dist = { O_S_L5_D_LOW, O_S_L5_D_MED, O_S_L5_D_HII, O_S_L5_D_ULT };
+Fvector4 o_optimize_static_l5_size = { O_S_L5_S_LOW, O_S_L5_S_MED, O_S_L5_S_HII, O_S_L5_S_ULT };
+Fvector4 o_optimize_static_l6_dist = { O_S_L6_D_LOW, O_S_L6_D_MED, O_S_L6_D_HII, O_S_L6_D_ULT };
+Fvector4 o_optimize_static_l6_size = { O_S_L6_S_LOW, O_S_L6_S_MED, O_S_L6_S_HII, O_S_L6_S_ULT };
+Fvector4 o_optimize_static_l7_dist = { O_S_L7_D_LOW, O_S_L7_D_MED, O_S_L7_D_HII, O_S_L7_D_ULT };
+Fvector4 o_optimize_static_l7_size = { O_S_L7_S_LOW, O_S_L7_S_MED, O_S_L7_S_HII, O_S_L7_S_ULT };
+Fvector4 o_optimize_static_l8_dist = { O_S_L8_D_LOW, O_S_L8_D_MED, O_S_L8_D_HII, O_S_L8_D_ULT };
+Fvector4 o_optimize_static_l8_size = { O_S_L8_S_LOW, O_S_L8_S_MED, O_S_L8_S_HII, O_S_L8_S_ULT };
+Fvector4 o_optimize_static_l9_dist = { O_S_L9_D_LOW, O_S_L9_D_MED, O_S_L9_D_HII, O_S_L9_D_ULT };
+Fvector4 o_optimize_static_l9_size = { O_S_L9_S_LOW, O_S_L9_S_MED, O_S_L9_S_HII, O_S_L9_S_ULT };
+Fvector4 o_optimize_static_l10_dist = { O_S_L10_D_LOW, O_S_L10_D_MED, O_S_L10_D_HII, O_S_L10_D_ULT };
+Fvector4 o_optimize_static_l10_size = { O_S_L10_S_LOW, O_S_L10_S_MED, O_S_L10_S_HII, O_S_L10_S_ULT };
+Fvector4 o_optimize_static_l11_dist = { O_S_L11_D_LOW, O_S_L11_D_MED, O_S_L11_D_HII, O_S_L11_D_ULT };
+Fvector4 o_optimize_static_l11_size = { O_S_L11_S_LOW, O_S_L11_S_MED, O_S_L11_S_HII, O_S_L11_S_ULT };
+Fvector4 o_optimize_static_l12_dist = { O_S_L12_D_LOW, O_S_L12_D_MED, O_S_L12_D_HII, O_S_L12_D_ULT };
+Fvector4 o_optimize_static_l12_size = { O_S_L12_S_LOW, O_S_L12_S_MED, O_S_L12_S_HII, O_S_L12_S_ULT };
+
+Fvector4 o_optimize_dynamic_l1_dist = { O_D_L1_D_LOW, O_D_L1_D_MED, O_D_L1_D_HII, O_D_L1_D_ULT };
+Fvector4 o_optimize_dynamic_l1_size = { O_D_L1_S_LOW, O_D_L1_S_MED, O_D_L1_S_HII, O_D_L1_S_ULT };
+Fvector4 o_optimize_dynamic_l2_dist = { O_D_L2_D_LOW, O_D_L2_D_MED, O_D_L2_D_HII, O_D_L2_D_ULT };
+Fvector4 o_optimize_dynamic_l2_size = { O_D_L2_S_LOW, O_D_L2_S_MED, O_D_L2_S_HII, O_D_L2_S_ULT };
+Fvector4 o_optimize_dynamic_l3_dist = { O_D_L3_D_LOW, O_D_L3_D_MED, O_D_L3_D_HII, O_D_L3_D_ULT };
+Fvector4 o_optimize_dynamic_l3_size = { O_D_L3_S_LOW, O_D_L3_S_MED, O_D_L3_S_HII, O_D_L3_S_ULT };
+Fvector4 o_optimize_dynamic_l4_dist = { O_D_L4_D_LOW, O_D_L4_D_MED, O_D_L4_D_HII, O_D_L4_D_ULT };
+Fvector4 o_optimize_dynamic_l4_size = { O_D_L4_S_LOW, O_D_L4_S_MED, O_D_L4_S_HII, O_D_L4_S_ULT };
+Fvector4 o_optimize_dynamic_l5_dist = { O_D_L5_D_LOW, O_D_L5_D_MED, O_D_L5_D_HII, O_D_L5_D_ULT };
+Fvector4 o_optimize_dynamic_l5_size = { O_D_L5_S_LOW, O_D_L5_S_MED, O_D_L5_S_HII, O_D_L5_S_ULT };
+
+#define BASE_FOV 67.f
