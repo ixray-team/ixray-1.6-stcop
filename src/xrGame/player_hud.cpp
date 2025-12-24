536d7446d2ec68597c0af1eedec7cd001ed73fe4
ForserX <sv3nk@yandex.ru>
2025-12-24 08:51:42 +0300

    Restore dsgraph opt from NewProject

--- src/xrGame/player_hud.cpp
+++ src/xrGame/player_hud.cpp
@@ -475,7 +475,7 @@ bool  attachable_hud_item::need_renderable()
 void attachable_hud_item::render()
 {
 	::Render->set_Transform		(&m_item_transform);
-	::Render->add_Visual		(m_model->dcast_RenderVisual());
+	::Render->add_Visual		(m_model->dcast_RenderVisual(), true);
 	debug_draw_firedeps			();
 	m_parent_hud_item->render_hud_mode();
 }
@@ -1399,7 +1399,7 @@ void player_hud::render_hud()
 	if(b_r0 || b_r1 || m_animator_item && m_animator_item->IsPlaying || m_bhands_visible)
 	{
 		::Render->set_Transform(&m_transform);
-		::Render->add_Visual(m_model->dcast_RenderVisual());
+		::Render->add_Visual(m_model->dcast_RenderVisual(), true);
 	}
 
 	if(b_r0) {
@@ -1459,7 +1459,7 @@ void player_hud::render_hud()
 			::Render->set_HUD(FALSE);
 
 			::Render->set_Transform(&Actor()->XFORM());
-			::Render->add_Visual(m_legs_model->dcast_RenderVisual());
+			::Render->add_Visual(m_legs_model->dcast_RenderVisual(), true);
 
 			::Render->set_HUD(bHud);
 		}
@@ -2297,10 +2297,12 @@ void animator_item::update(bool bForce)
 void animator_item::render()
 {
 	if (!m_item)
+	{
 		return;
+	}
 
 	::Render->set_Transform(&m_item_transform);
-	::Render->add_Visual(m_item->dcast_RenderVisual());
+	::Render->add_Visual(m_item->dcast_RenderVisual(), true);
 }
 
 void animator_item::anim_play(const shared_str& item_anm_name, BOOL bMixIn, float speed)
