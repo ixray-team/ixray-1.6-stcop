613b41376a3216ceabfa0cc9c79efbfdca6cabe4
Romann <kriworuck.production@yandex.ru>
2025-12-26 20:57:12 +0300

    Methods for spawning an actor, regardless of which location the actor is in.

--- src/xrGame/alife_graph_registry.cpp
+++ src/xrGame/alife_graph_registry.cpp
@@ -9,6 +9,7 @@
 #include "StdAfx.h"
 #include "alife_graph_registry.h"
 #include "../xrEngine/x_ray.h"
+#include <Actor.h>
 
 using namespace ALife;
 
@@ -45,7 +46,7 @@ void CALifeGraphRegistry::on_load			()
 	}
 }
 
-void CALifeGraphRegistry::update			(CSE_ALifeDynamicObject *object)
+void CALifeGraphRegistry::update(CSE_ALifeDynamicObject *object)
 {
 	if (!object->m_bDirectControl)
 		return;
@@ -54,6 +55,12 @@ void CALifeGraphRegistry::update			(CSE_ALifeDynamicObject *object)
 	{
 		m_actor = smart_cast<CSE_ALifeCreatureActor*>(object);
 		R_ASSERT2(m_actor, "Invalid flag M_SPAWN_OBJECT_ASPLAYER for non-actor object!");
+
+        if (g_start_game_vertex_id)
+        {
+            m_actor->m_tGraphID = g_start_game_vertex_id;
+            m_actor->o_Position = g_start_position;
+        }
 	}
 
 	if (m_actor && !m_level)
@@ -90,6 +97,8 @@ void CALifeGraphRegistry::setup_current_level	()
 	int							id = pApp->Level_ID(*(*I).second.name(),"1.0", true);
 	VERIFY3						(id >= 0,"Level is corrupted or doesn't exist",*(*I).second.name());
 	ai().load					(*(*I).second.name());
+
+    g_start_game_vertex_id = 0;
 }
 
 void CALifeGraphRegistry::attach	(CSE_Abstract &object, CSE_ALifeInventoryItem *item, GameGraph::_GRAPH_ID game_vertex_id, bool alife_query, bool add_children)
--- src/xrGame/alife_spawn_registry.h
+++ src/xrGame/alife_spawn_registry.h
@@ -42,6 +42,8 @@ private:
 	IReader									*m_chunk;
 	IGameGraph								*m_game_graph;
 
+    void load_spawn_position_from_smart();
+
 protected:
 			void							save_updates				(IWriter &stream);
 			void							load_updates				(IReader &stream);
