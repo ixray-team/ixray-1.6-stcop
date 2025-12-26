87350691cc2134a13d3d9e91f8fe9aac3ffe886d
Shtrecker <gogels235@gmail.com>
2025-12-26 14:57:29 +0200

    Fix build

--- src/xrGame/HudAnimatorManager.cpp
+++ src/xrGame/HudAnimatorManager.cpp
@@ -11,7 +11,6 @@
 
 bool m_AnimatorForceHideItems = false;
 
-extern float g_motion_speed;
 
 CHudAnimatorBase::~CHudAnimatorBase()
 {
@@ -35,13 +34,10 @@ void CHudAnimatorBase::Load()
 	m_sLuaRight2Callback = READ_IF_EXISTS(pSettings, r_string, m_section, "right2_lua_callback", "null");
 	m_sLuaStartCallback = READ_IF_EXISTS(pSettings, r_string, m_section, "start_lua_callback", "null");
 	m_sLuaEndCallback = READ_IF_EXISTS(pSettings, r_string, m_section, "end_lua_callback", "null");
-
-	m_fMotionHideSpeed = READ_IF_EXISTS(pSettings, r_float, m_section, "hide_speed_factor", 1.0f);
 }
 
 void CHudAnimatorBase::StopAnimator()
 {
-	g_motion_speed = 1.0f;
 	m_bIsPlaying = false;
 	m_manager->Parent()->set_inventory_disabled(false);
 	m_manager->Parent()->set_pda_disabled(false);
@@ -287,14 +283,11 @@ void CHudStateAnimator::Update()
 		bool wpn_hide = !g_player_hud->attached_item(0) && !m_manager->Parent()->inventory().ActiveItem() && !m_manager->Parent()->inventory().GetNextActiveSlot() && !m_manager->Parent()->inventory().GetActiveSlot();
 		if (wpn_hide && g_player_hud->GetAnimator() == nullptr && !g_player_hud->attached_item(1))
 		{
-			g_motion_speed = 1.0f;
 			m_bNeedActivated = false;
 			SetState(eShowing);
 		}
 		else
 		{
-			g_motion_speed = m_fMotionHideSpeed;
-
 			CHudAnimatorBase* current_animator = m_manager->Parent()->HudAnimator()->CurrentAnimator();
 			if (current_animator != nullptr && this != current_animator)
 			{
