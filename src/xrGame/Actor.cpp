f7937b0a7777893dddc531eb6692b6e321eef9b8
MAYLAYSHEZ <MAYLAYSHEZ@yandex.ru>
2025-12-27 03:53:51 +0400

    Добавлены и выведены в скрипты функции запрета обыска ящиков и трупов

--- src/xrGame/Actor.cpp
+++ src/xrGame/Actor.cpp
@@ -173,6 +173,7 @@ CActor::CActor() : CEntityAlive(),current_ik_cam_shift(0)
 	m_disabled_hitmarks		= false;
 	m_inventory_disabled	= false;
 	m_pda_disabled			= false;
+	m_use_disabled			= false;
 
 	m_hud_animator			= new CHudAnimatorManager(this);
 
