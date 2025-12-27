08658eeb94b054692497f46761762e3dddca2ecb
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-12-27 23:52:52 +0300

    Implement custom volume for weapon shots

--- src/xrGame/CarWeapon.cpp
+++ src/xrGame/CarWeapon.cpp
@@ -80,7 +80,7 @@ CCarWeapon::~CCarWeapon()
 void CCarWeapon::Load(LPCSTR section)
 {
 	inheritedShooting::Load(section);
-    SoundsLayered.LoadSound(section, "snd_shoot", "m_sndShot", false);
+    SoundsLayered.LoadSound(section, "snd_shoot", "m_sndShot", false, -1, st_Shooting);
 	m_Ammo->Load(pSettings->r_string(section, "ammo_class"), 0);
 }
 
