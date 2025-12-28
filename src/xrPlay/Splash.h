cddbe0000a3aa872af388382971f6d6202bc958d
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2025-12-28 14:01:58 +0300

    Add text & progress bar to game splash

--- src/xrPlay/Splash.h
+++ src/xrPlay/Splash.h
@@ -3,5 +3,6 @@
 namespace splash
 {
 	void show(void*&window);
+	void update(int, const char*);
 	void hide();
 }
--- src/xrPlay/resource.h
+++ src/xrPlay/resource.h
@@ -1,12 +1,14 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by resource.rc
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется resource.rc
 //
 #define IDDEBUG                         3
 #define IDI_ICON1                       111
 #define IDB_BITMAP2                     116
 #define IDB_BITMAP3                     120
 #define IDB_PNG1                        126
+#define IDB_PNG2                        127
+#define IDB_FONT                        127
 #define IDC_LIST                        1000
 #define IDC_FILE                        1001
 #define IDC_LINE                        1002
@@ -20,7 +22,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        127
+#define _APS_NEXT_RESOURCE_VALUE        128
 #define _APS_NEXT_COMMAND_VALUE         40002
 #define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
