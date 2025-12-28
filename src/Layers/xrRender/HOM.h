414ffe6f32669ab53d9e80b03a2561a504e4070b
v2v3v4 <terran.erre@mail.ru>
2025-12-28 10:48:53 +0300

    тут тоже нужен инлайн

--- src/Layers/xrRender/HOM.h
+++ src/Layers/xrRender/HOM.h
@@ -3,7 +3,7 @@
 //////////////////////////////////////////////////////////////////////
 #pragma once
 #include "../../xrEngine/IGame_Persistent.h"
-
+#include "occRasterizer.h"
 class occTri;
 
 class CHOM  
@@ -37,11 +37,143 @@ public:
 
 	void MT_RENDER();
 
-	bool visible(vis_data& vis);
-	bool visible(Fbox3& B);
-	bool visible(Fsphere& S);
-	bool visible(sPoly& P);
-	bool visible(Fbox2& B, float depth);	// viewport-space (0..1)
+	ICF	bool xform_b0(Fvector2& min, Fvector2& max, float& minz, Fmatrix& X, float _x, float _y, float _z)
+	{
+		float z = _x * X._13 + _y * X._23 + _z * X._33 + X._43;
+		if (z < EPS)
+		{
+			return TRUE;
+		}
+
+		float iw = 1.f / (_x * X._14 + _y * X._24 + _z * X._34 + X._44);
+		min.x = max.x = (_x * X._11 + _y * X._21 + _z * X._31 + X._41) * iw;
+		min.y = max.y = (_x * X._12 + _y * X._22 + _z * X._32 + X._42) * iw;
+		minz = 0.f + z * iw;
+		return FALSE;
+	}
+
+	ICF	bool xform_b1(Fvector2& min, Fvector2& max, float& minz, Fmatrix& X, float _x, float _y, float _z)
+	{
+		float t;
+		float z = _x * X._13 + _y * X._23 + _z * X._33 + X._43;
+		if (z < EPS)
+		{
+			return TRUE;
+		}
+
+		float iw = 1.f / (_x * X._14 + _y * X._24 + _z * X._34 + X._44);
+		t = (_x * X._11 + _y * X._21 + _z * X._31 + X._41) * iw;
+		if (t < min.x) min.x = t; else if (t > max.x) max.x = t;
+		t = (_x * X._12 + _y * X._22 + _z * X._32 + X._42) * iw;
+		if (t < min.y) min.y = t; else if (t > max.y) max.y = t;
+		t = 0.f + z * iw;
+
+		if (t < minz)
+			minz = t;
+		return false;
+	}
+
+	ICF bool _visible(Fbox& B, Fmatrix& m_xform_01)
+	{
+		// Find min/max points of xformed-box
+		Fvector2	min, max;
+		float		z;
+		if (xform_b0(min, max, z, m_xform_01, B.min.x, B.min.y, B.min.z)) return true;
+		if (xform_b1(min, max, z, m_xform_01, B.min.x, B.min.y, B.max.z)) return true;
+		if (xform_b1(min, max, z, m_xform_01, B.max.x, B.min.y, B.max.z)) return true;
+		if (xform_b1(min, max, z, m_xform_01, B.max.x, B.min.y, B.min.z)) return true;
+		if (xform_b1(min, max, z, m_xform_01, B.min.x, B.max.y, B.min.z)) return true;
+		if (xform_b1(min, max, z, m_xform_01, B.min.x, B.max.y, B.max.z)) return true;
+		if (xform_b1(min, max, z, m_xform_01, B.max.x, B.max.y, B.max.z)) return true;
+		if (xform_b1(min, max, z, m_xform_01, B.max.x, B.max.y, B.min.z)) return true;
+		return !!Raster.test(min.x, min.y, max.x, max.y, z);
+	}
+
+	ICF bool visible(Fbox3& B)
+	{
+		if (!bEnabled)
+			return true;
+
+		if (B.contains(Device.vCameraPosition))
+			return true;
+
+		return _visible(B, m_xform_01);
+	}
+
+	ICF bool visible(Fsphere& S)
+	{
+		Fbox B; B.setb(S.P, Fvector().set(S.R, S.R, S.R));
+		return visible(B);
+	}
+
+	ICF bool visible(Fbox2& B, float depth)
+	{
+		if (!bEnabled)
+			return true;
+
+		return Raster.test(B.min.x, B.min.y, B.max.x, B.max.y, depth);
+	}
+
+	ICF bool visible(vis_data& vis)
+	{
+		if (Device.dwFrame < vis.hom_frame)	return true;				// not at this time :)
+		if (!bEnabled)						return true;				// return - everything visible
+
+		// Now, the test time comes
+		// 0. The object was hidden, and we must prove that each frame	- test		| frame-old, tested-new, hom_res = false;
+		// 1. The object was visible, but we must to re-check it		- test		| frame-new, tested-???, hom_res = true;
+		// 2. New object slides into view								- delay test| frame-old, tested-old, hom_res = ???;
+		u32 frame_current = Device.dwFrame;
+		// u32	frame_prev		= frame_current-1;
+
+#ifdef DEBUG
+		Device.Statistic->RenderCALC_HOM.Begin();
+#endif
+		bool result = _visible(vis.box, m_xform_01);
+		u32  delay = 1;
+		if (result)
+		{
+			// visible	- delay next test
+			delay = ::Random.randI(5 * 2, 5 * 5);
+		}
+		else {
+			// hidden	- shedule to next frame
+		}
+		vis.hom_frame = frame_current + delay;
+		vis.hom_tested = frame_current;
+#ifdef DEBUG
+		Device.Statistic->RenderCALC_HOM.End();
+#endif
+
+		return result;
+	}
+
+	ICF bool visible(sPoly& P)
+	{
+		if (!bEnabled)
+		{
+			return true;
+		}
+
+		// Find min/max points of xformed-box
+		Fvector2 min, max;
+		float z;
+
+		if (xform_b0(min, max, z, m_xform_01, P.front().x, P.front().y, P.front().z))
+		{
+			return true;
+		}
+
+		for (u32 it = 1; it < P.size(); it++)
+		{
+			if (xform_b1(min, max, z, m_xform_01, P[it].x, P[it].y, P[it].z))
+			{
+				return true;
+			}
+		}
+
+		return Raster.test(min.x, min.y, max.x, max.y, z);
+	}
 
 	CHOM();
 	~CHOM();
--- src/Layers/xrRender/occRasterizer.cpp
+++ src/Layers/xrRender/occRasterizer.cpp
@@ -188,28 +188,3 @@ void occRasterizer::on_dbg_render()
 	}
 #endif
 }
-
-
-IC	BOOL test_Level(occD* depth, int dim, float _x0, float _y0, float _x1, float _y1, occD z)
-{
-	int x0 = iFloor(_x0 * dim + .5f);	clamp(x0, 0, dim - 1);
-	int x1 = iFloor(_x1 * dim + .5f);	clamp(x1, x0, dim - 1);
-	int y0 = iFloor(_y0 * dim + .5f);	clamp(y0, 0, dim - 1);
-	int y1 = iFloor(_y1 * dim + .5f);	clamp(y1, y0, dim - 1);
-
-	for (int y = y0; y <= y1; y++)
-	{
-		occD* base = depth + y * dim;
-		occD* it = base + x0;
-		occD* end = base + x1;
-		for (; it <= end; it++)
-			if (z < *it)	return TRUE;
-	}
-	return FALSE;
-}
-
-BOOL occRasterizer::test(float _x0, float _y0, float _x1, float _y1, float _z)
-{
-	occD z = df_2_s32up(_z) + 1;
-	return test_Level(get_depth_level(0), occ_dim_0, _x0, _y0, _x1, _y1, z);
-}
