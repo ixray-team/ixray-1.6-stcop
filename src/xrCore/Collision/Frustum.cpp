a7b028754d573ce76bef95a34b39ebd3955647a9
v2v3v4 <terran.erre@mail.ru>
2025-12-28 10:05:29 +0300

    Inline frequently called code

--- src/xrCore/Collision/Frustum.cpp
+++ src/xrCore/Collision/Frustum.cpp
@@ -44,7 +44,7 @@ void			CFrustum::_add			(Fvector& P1, Fvector& P2, Fvector&P3)
 #define			My			4
 #define			Mz			5
 
-u32				frustum_aabb_remap [8][6]	=
+u32 XRCORE_API frustum_aabb_remap[8][6]
 {
 	{ Mx,My,Mz,mx,my,mz}, 
 	{ Mx,My,mz,mx,my,Mz}, 
@@ -55,89 +55,6 @@ u32				frustum_aabb_remap [8][6]	=
 	{ mx,my,Mz,Mx,My,mz}, 
 	{ mx,my,mz,Mx,My,Mz}
 };
-
-//////////////////////////////////////////////////////////////////////
-EFC_Visible	CFrustum::testSphere			(Fvector& c, float r, u32& test_mask) const
-{
-	u32	bit = 1;
-	for (int i=0; i<p_count; i++, bit<<=1)
-	{
-		if (test_mask&bit) {
-			float cls = planes[i].classify	(c);
-			if (cls>r) { test_mask=0; return fcvNone;}	// none  - return
-			if (_abs(cls)>=r) test_mask&=~bit;			// fully - no need to test this plane
-		}
-	}
-	return test_mask ? fcvPartial:fcvFully;
-}
-
-BOOL CFrustum::testSphere_dirty(const Fvector& c, float r) const
-{
-	switch (p_count) {
-		case 12:if (planes[11].classify(c)>r)	return FALSE;
-		case 11:if (planes[10].classify(c)>r)	return FALSE;
-		case 10:if (planes[9].classify(c)>r)	return FALSE;
-		case 9:	if (planes[8].classify(c)>r)	return FALSE;
-		case 8:	if (planes[7].classify(c)>r)	return FALSE;
-		case 7:	if (planes[6].classify(c)>r)	return FALSE;
-		case 6:	if (planes[5].classify(c)>r)	return FALSE;
-		case 5:	if (planes[4].classify(c)>r)	return FALSE;
-		case 4:	if (planes[3].classify(c)>r)	return FALSE;
-		case 3:	if (planes[2].classify(c)>r)	return FALSE;
-		case 2:	if (planes[1].classify(c)>r)	return FALSE;
-		case 1:	if (planes[0].classify(c)>r)	return FALSE;
-		case 0:	break;
-		default:	NODEFAULT;
-	}
-	return TRUE;
-}
-
-EFC_Visible	CFrustum::testAABB			(const float* mM, u32& test_mask) const
-{
-	// go for trivial rejection or acceptance using "faster overlap test"
-	u32		bit = 1;
-
-	for (int i=0; i<p_count; i++, bit<<=1)
-	{
-		if (test_mask&bit) {
-			EFC_Visible	r	= AABB_OverlapPlane(planes[i],mM);
-			if (fcvFully==r)	test_mask&=~bit;					// fully - no need to test this plane
-			else if (fcvNone==r){ test_mask=0; return fcvNone;	}	// none - return
-		}
-	}
-	return test_mask ? fcvPartial:fcvFully;
-}
-
-EFC_Visible	CFrustum::testSAABB			(Fvector& c, float r, const float* mM, u32& test_mask) const
-{
-	u32	bit = 1;
-	for (int i=0; i<p_count; i++, bit<<=1)
-	{
-		if (test_mask&bit) {
-			float cls = planes[i].classify(c);
-			if (cls>r) { test_mask=0; return fcvNone;}	// none  - return
-			if (_abs(cls)>=r) test_mask&=~bit;			// fully - no need to test this plane
-			else {
-				EFC_Visible	r_	= AABB_OverlapPlane(planes[i],mM);
-				if (fcvFully==r_)	test_mask&=~bit;					// fully - no need to test this plane
-				else if (fcvNone==r_){ test_mask=0; return fcvNone;	}	// none - return
-			}
-		}
-	}
-	return test_mask ? fcvPartial:fcvFully;
-}
-
-BOOL		CFrustum::testPolyInside_dirty(Fvector* p, int count) const
-{
-	Fvector* e = p+count;
-	for (int i=0; i<p_count; i++)
-	{
-		const fplane &P = planes[i];
-		for (Fvector* I=p; I!=e; I++)
-			if (P.classify(*I)>0) return false;
-	}
-	return true;
-}
 
 //////////////////////////////////////////////////////////////////////
 void CFrustum::CreateFromPoints(Fvector* p, int count, Fvector& COP)
@@ -278,66 +195,6 @@ void CFrustum::CreateOccluder(Fvector* p, int count, Fvector& vBase, CFrustum& c
 			_add(vBase,p[i],p[next]);
 		}
 	}
-}
-
-sPoly*	CFrustum::ClipPoly(sPoly& S, sPoly& D) const
-{
-	sPoly*	src		= &D;
-	sPoly*	dest	= &S;
-	for (int i=0; i<p_count; i++)
-	{
-		// cache plane and swap lists
-		const fplane &P = planes[i];
-		std::swap		(src,dest);
-		dest->clear		();
-
-		// classify all points relative to plane #i
-		float	cls	[FRUSTUM_SAFE];
-		for (u32 j=0; j<src->size(); j++) cls[j]=P.classify((*src)[j]);
-
-		// clip everything to this plane
-		cls[src->size()] = cls[0];
-		src->push_back((*src)[0]);
-		Fvector D_; float denum,t;
-		for (u32 j=0; j<src->size()-1; j++)
-		{
-			if ((*src)[j].similar((*src)[j+1],EPS_S)) continue;
-
-			if (negative(cls[j]))
-			{
-				dest->push_back((*src)[j]);
-				if (positive(cls[j+1]))
-				{
-					// segment intersects plane
-					D_.sub((*src)[j+1],(*src)[j]);
-					denum = P.n.dotproduct(D_);
-					if (denum!=0) {
-						t = -cls[j]/denum; //VERIFY(t<=1.f && t>=0);
-						dest->last().mad((*src)[j],D_,t);
-						dest->inc();
-					}
-				}
-			} else {
-				// J - outside
-				if (negative(cls[j+1]))
-				{
-					// J+1  - inside
-					// segment intersects plane
-					D_.sub((*src)[j+1],(*src)[j]);
-					denum = P.n.dotproduct(D_);
-					if (denum!=0) {
-						t = -cls[j]/denum; //VERIFY(t<=1.f && t>=0);
-						dest->last().mad((*src)[j],D_,t);
-						dest->inc();
-					}
-				}
-			}
-		}
-
-		// here we end up with complete polygon in 'dest' which is inside plane #i
-		if (dest->size()<3) return 0;
-	}
-	return dest;
 }
 
 BOOL CFrustum::CreateFromClipPoly(Fvector* p, int count, Fvector& vBase, CFrustum& clip)
