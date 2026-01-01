3306847c316fb5bc5e5f05594524ab39a18c3a06
ForserX <sv3nk@yandex.ru>
2026-01-01 22:35:25 +0300

    Система зависимостей для аддонов

--- src/xrCore/xrAddons.h
+++ src/xrCore/xrAddons.h
@@ -1,23 +1,34 @@
 #pragma once
 
+#include "xrCore.h"
+
 class CAddonManager final
 {
 public:
 	struct AddonInfo
 	{
-		shared_str EntryDir;
-		shared_str AddonName;
-		shared_str ScriptInit;
-	};
+		shared_str EntryDir;     // Корень аддона
+		shared_str AddonName;    // Уникальное имя
+		shared_str ScriptInit;   // init-скрипт (без .script)
 
-	xr_vector<AddonInfo> Addons;
+		xr_vector<xr_string> Dependencies;
+	};
 
 public:
 	CAddonManager() = default;
-	void CanApply(xr_string& TempPath, CLocatorAPI::file& Desc);
+	~CAddonManager() = default;
+
+	void Initialize();
+	bool CanApply(xr_string& TempPath, CLocatorAPI::file& Desc);
 
 private:
+	void CollectAddons();
 	void ReadMetaInfo(const xr_string& InitFile);
+	void ResolveDependencies();
+	void MountAddons();
+
+public:
+	xr_vector<AddonInfo> Addons;
 };
 
 extern XRCORE_API CAddonManager* g_pAddonsManager;
