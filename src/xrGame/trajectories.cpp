adea47cd43a64e14c2fd7fe3d7a26021b82a3f62
mnelenpridumivat <ragrind@mail.ru>
2026-01-08 15:33:50 +0300

    Fix box_x_axis calculation in trajectory_check_collision

--- src/xrGame/trajectories.cpp
+++ src/xrGame/trajectories.cpp
@@ -144,9 +144,9 @@ bool trajectory_check_collision (float 							low,
 		if (std::abs(box_z_axis.x) > epsilon || std::abs(box_z_axis.z) > epsilon ) {
 
 			Fvector const down			=	{ 0, -1, 0 };
-			Fvector							box_x_axis_;
-			box_x_axis_.crossproduct			(box_z_axis, down);
-			box_y_axis.crossproduct			(box_z_axis, box_x_axis_);
+
+			box_x_axis.crossproduct			(box_z_axis, down);
+			box_y_axis.crossproduct			(box_z_axis, box_x_axis);
 		}
 		else
 		{
