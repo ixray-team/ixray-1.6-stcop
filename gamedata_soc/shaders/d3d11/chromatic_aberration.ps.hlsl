ea7d3a74f1058ee3fb35d9cfd6b9f58b0780bb6d
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-01-10 18:38:56 +0300

    Sync shaders

--- gamedata_soc/shaders/d3d11/chromatic_aberration.ps.hlsl
+++ gamedata_soc/shaders/d3d11/chromatic_aberration.ps.hlsl
@@ -1,16 +1,15 @@
 #include "common.hlsli"
 
-float4 screen_res;
-
 float4 main(PSInputFullscreen I) : SV_Target
 {
-    float intensity = 1.5;
-    float2 offset = saturate(distance(I.texcoord, float2(0.5, 0.5))) * screen_res.zw * intensity;
+    float2 from_center = I.texcoord - 0.5;
+    float4 rg_shift = from_center.xyxy * float2(0.99065, 0.99373).xxyy + 0.5; //LVutner: Precalculated for 0.55 intensity, see AMD code.
 
     float3 col;
-	col.x = s_image.SampleLevel(smp_rtlinear, I.texcoord + offset, 0.0).x;
-	col.y = s_image.SampleLevel(smp_rtlinear, I.texcoord, 0.0).y;
-	col.z = s_image.SampleLevel(smp_rtlinear, I.texcoord - offset, 0.0).z;
-    return float4(col, 1);
+	col.x = s_image.SampleLevel(smp_rtlinear, rg_shift.xy, 0.0).x;
+	col.y = s_image.SampleLevel(smp_rtlinear, rg_shift.zw, 0.0).y;
+	col.z = s_image.SampleLevel(smp_rtlinear, I.texcoord, 0.0).z;
+    return float4(col, 1.0);
 }
+
 
