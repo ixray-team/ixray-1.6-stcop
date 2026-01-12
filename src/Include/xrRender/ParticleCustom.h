1d3ee9ca9233d6a4f815098d61359d0b0f4f38dc
ForserX <sv3nk@yandex.ru>
2026-01-13 01:22:55 +0300

    Port `xrEngineCore` into Linux

--- src/Include/xrRender/ParticleCustom.h
+++ src/Include/xrRender/ParticleCustom.h
@@ -2,7 +2,54 @@
 
 namespace PAPI
 {
-	enum PActionEnum;
+	enum PActionEnum
+	{
+		PAAvoidID,			// Avoid entering the domain of space.
+		PABounceID,			// Bounce particles off a domain of space.
+		PACallActionListID_obsolette,	// 
+		PACopyVertexBID,	// Set the secondary position from current position.
+		PADampingID,		// Dampen particle velocities.
+		PAExplosionID,		// An Explosion.
+		PAFollowID,			// Accelerate toward the previous particle in the effect.
+		PAGravitateID,		// Accelerate each particle toward each other particle.
+		PAGravityID,		// Acceleration in the given direction.
+		PAJetID,			// 
+		PAKillOldID,		// 
+		PAMatchVelocityID,	// 
+		PAMoveID,			// 
+		PAOrbitLineID,		// 
+		PAOrbitPointID,		// 
+		PARandomAccelID,	// 
+		PARandomDisplaceID,	// 
+		PARandomVelocityID,	// 
+		PARestoreID,		// 
+		PASinkID,			// 
+		PASinkVelocityID,	// 
+		PASourceID,			// 
+		PASpeedLimitID,		// 
+		PATargetColorID,	// 
+		PATargetSizeID,		// 
+		PATargetRotateID,	// 
+		PATargetRotateDID,	// 
+		PATargetVelocityID,	// 
+		PATargetVelocityDID,// 
+		PAVortexID,			// 
+        PATurbulenceID,     //
+        PAScatterID, 	    //
+		// Binders
+		PABindVelocityValueID,
+		PABindRotationValueID,
+		PABindSizeValueID,
+		PABindColorValueID,
+		PABindColorAlphaID,
+		// Animators
+		PAColorAnimatorID,
+		PASizeAnimatorID,
+		PAVelocityAnimatorID,
+		PAVelocityRotationAnimatorID,
+		action_enum_force_dword = u32(-1)
+	};
+
 	struct ParticleAction;
 }
 
