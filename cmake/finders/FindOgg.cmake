365c2cc40f705f6573072277687c78111bf33fb7
ForserX <sv3nk@yandex.ru>
2026-01-13 23:20:56 +0300

    Port `xrEngineCore` into **Linux** (Full)

--- cmake/finders/FindOgg.cmake
+++ cmake/finders/FindOgg.cmake
@@ -1,1 +1,8 @@
 # HACK for Vorbis
+
+if(TARGET Ogg::ogg)
+    set(Ogg_FOUND TRUE CACHE INTERNAL "")
+else()
+    message(FATAL_ERROR "FindOgg.cmake: target Ogg::ogg not found! "
+                       "Make sure Ogg is loaded via modules or FetchContent before calling find_package(Ogg).")
+endif()
