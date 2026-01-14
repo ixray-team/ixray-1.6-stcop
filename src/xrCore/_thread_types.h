78fe0f764259626cbc4d1eac677ebc2a21fb5945
v2v3v4 <terran.erre@mail.ru>
2026-01-14 22:40:51 +0300

    Apply `LineSpacing` for `CGameFont`

--- src/xrCore/_thread_types.h
+++ src/xrCore/_thread_types.h
@@ -25,16 +25,16 @@ using xr_atomic_float = std::atomic<float>;
 #ifdef IXR_WINDOWS
 using xr_task_group = concurrency::task_group;
 
-template <typename T, typename U>
-using xr_concurrent_unordered_map = concurrency::concurrent_unordered_map<T, U>;
+template <typename T, typename U, typename H = ::std::hash<T>>
+using xr_concurrent_unordered_map = concurrency::concurrent_unordered_map<T, U, H>;
 
 template <typename T>
 using xr_concurrent_vector = concurrency::concurrent_vector<T>;
 #else
 using xr_task_group = tbb::task_group;
 
-template <typename T, typename U>
-using xr_concurrent_unordered_map = tbb::concurrent_unordered_map<T, U>;
+template <typename T, typename U, typename H = std::hash<T>>
+using xr_concurrent_unordered_map = tbb::concurrent_unordered_map<T, U, H>;
 template <typename T>
 using xr_concurrent_vector = tbb::concurrent_vector<T>;
 #endif
