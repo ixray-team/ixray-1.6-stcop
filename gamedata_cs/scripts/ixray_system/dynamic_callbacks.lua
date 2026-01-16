421ebc1ce59068cffdb62f2bb67c120f33b784b6
DESKTOP-GH3M5SF\vipff <emelnikov@scid.ru>
2026-01-16 04:52:12 +0300

    CS IXR Framework

--- gamedata_cs/scripts/ixray_system/dynamic_callbacks.lua
+++ gamedata_cs/scripts/ixray_system/dynamic_callbacks.lua
@@ -8,7 +8,41 @@ local intercepts =
 	load_state = {}
 }
 
+
+
+function AddIntercept(name, args_map)
+	-- FFx0001 retranslate call to signals module is installed
+	if ixr_framework and ixr_framework.is_module_loaded("ixr_signals") then
+		ixr_framework.get_module("ixr_signals").add_intercept(name, args_map)
+	else
+		SemiLog("! error ixr_signals not initialized:: [SKIP] AddIntercept: (" .. tostring(name) .. ",...)")
+	end
+end
+
+function RemoveIntercept(name)
+	-- FFx0001 retranslate call to signals module is installed
+	if ixr_framework and ixr_framework.is_module_loaded("ixr_signals") then
+		ixr_framework.get_module("ixr_signals").remove_intercept(name)
+	else
+		SemiLog("! error ixr_signals not initialized:: [SKIP] RemoveIntercept: (" .. tostring(name) .. ",...)")
+	end
+end
+
+
+-- duplicate for dead air and anomaly
+function callback_set(name, func_or_userdata)
+	RegisterScriptCallback(name, func_or_userdata)
+end
+
 function RegisterScriptCallback(name, func_or_userdata)
+	-- FFx0001 retranslate call to signals module is installed
+	if ixr_framework and ixr_framework.is_module_loaded("ixr_signals") then
+		ixr_framework.get_module("ixr_signals").subscribe_to_event(name, func_or_userdata)
+		return
+	else
+		SemiLog("! error ixr_signals not initialized:: [SKIP] RegisterScriptCallback: (" .. tostring(name) .. ",...)")
+	end
+	
 	if (func_or_userdata == nil) then
 		SemiLog("! func_or_userdata == nil")
 		callstack()
@@ -28,13 +62,44 @@ function RegisterScriptCallback(name, func_or_userdata)
 	end
 end
 
+
+
+-- duplicate for dead air and anomaly
+function callback_unset(name, func_or_userdata)
+	UnregisterScriptCallback(name, func_or_userdata)
+end
+
 function UnregisterScriptCallback(name, func_or_userdata)
+	-- FFx0001 retranslate call to signals module is installed
+	if ixr_framework and ixr_framework.is_module_loaded("ixr_signals") then
+		ixr_framework.get_module("ixr_signals").un_subscribe_from_event(name, func_or_userdata)
+		return
+	else
+		SemiLog("! error ixr_signals not initialized:: [SKIP] UnregisterScriptCallback: (" .. tostring(name) .. ",...)")
+	end
+	
 	if (intercepts[name]) then
 		intercepts[name][func_or_userdata] = nil
 	end
 end
 
-function SendScriptCallback(name,...)
+
+
+-- duplicate for dead air and anomaly
+function make_callback(name, ...)
+	SendScriptCallback(name, ...)
+end
+
+function SendScriptCallback(name, ...)
+	-- FFx0001 retranslate call to signals module is installed
+	if ixr_framework and ixr_framework.is_module_loaded("ixr_signals") then
+		ixr_framework.get_module("ixr_signals").send_event(name, ...)
+		return
+	else
+		SemiLog("! error ixr_signals not initialized:: [SKIP] SendScriptCallback: (" .. tostring(name) .. ",...)")
+	end
+
+	
 	if (intercepts[name]) then
 		for func_or_userdata,v in pairs(intercepts[name]) do 
 			if (type(func_or_userdata) == "function") then 
