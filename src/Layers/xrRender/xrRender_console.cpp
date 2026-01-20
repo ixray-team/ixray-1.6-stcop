c9143abd9f28c127f419b0f609aee17377ff52bd
Drombeys <dima.zolotov02@gmail.com>
2026-01-20 11:30:08 +0300

    Fix wallmark display

--- src/Layers/xrRender/xrRender_console.cpp
+++ src/Layers/xrRender/xrRender_console.cpp
@@ -86,8 +86,8 @@ float		ps_r__Detail_rainbow_hemi	= 0.75f	;
 float		ps_r__Tree_SBC				= 1.5f	;	// scale bias correct
 
 float		ps_r__WallmarkTTL			= 50.f	;
-float		ps_r__WallmarkSHIFT			= 0.0001f;
-float		ps_r__WallmarkSHIFT_V		= 0.0001f;
+float		ps_r__WallmarkSHIFT			= 0.001f;
+float		ps_r__WallmarkSHIFT_V		= 0.001f;
 
 float		ps_r__GLOD_ssa_start		= 256.f	;
 float		ps_r__GLOD_ssa_end			=  64.f	;
