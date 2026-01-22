00fff6161758e0b6922c4c85ce010768a77d76ae
ForserX <sv3nk@yandex.ru>
2026-01-23 01:03:33 +0300

    Небольшая сортировка файлов

---
+++ src/xrCore/compression/lzo/lzo_compressor.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "lzo/lzo1.h"
+
+XRCORE_API int lzo_compress_dict(
+	const lzo_bytep in , lzo_uint  in_len,
+	lzo_bytep out, lzo_uintp out_len,
+	lzo_voidp wrkmem,
+	const lzo_bytep dict, lzo_uint dict_len
+);
+
+XRCORE_API int lzo_decompress_dict(
+	const lzo_bytep in,  lzo_uint  in_len,
+	lzo_bytep out, lzo_uintp out_len,
+	lzo_voidp wrkmem /* NOT USED */,
+	const lzo_bytep dict, lzo_uint dict_len
+);
+
+XRCORE_API int lzo_initialize();
---
+++ src/xrCore/compression/ppmd/ppmd_compressor.h
@@ -0,0 +1,22 @@
+#ifndef PPMD_COMPRESSOR_H
+#define PPMD_COMPRESSOR_H
+
+namespace compression
+{
+	namespace ppmd 
+	{
+		class stream;
+	}
+}
+
+XRCORE_API u32 ppmd_compress			(void *dest_buffer, const u32 &dest_buffer_size, const void *source_buffer, const u32 &source_buffer_size);
+XRCORE_API u32 ppmd_trained_compress	(void *dest_buffer, const u32 &dest_buffer_size, const void *source_buffer, const u32 &source_buffer_size, compression::ppmd::stream * tmodel);
+XRCORE_API u32 ppmd_decompress			(void *dest_buffer, const u32 &dest_buffer_size, const void *source_buffer, const u32 &source_buffer_size);
+XRCORE_API u32 ppmd_trained_decompress	(void *dest_buffer, const u32 &dest_buffer_size, const void *source_buffer, const u32 &source_buffer_size, compression::ppmd::stream * tmodel);
+
+using ppmd_yield_callback_t = xr_delegate<void()>;
+XRCORE_API u32 ppmd_compress_mt		(void *dest_buffer, const u32 &dest_buffer_size, const void *source_buffer, const u32 &source_buffer_size, ppmd_yield_callback_t ycb);
+XRCORE_API u32 ppmd_decompress_mt	(void *dest_buffer, const u32 &dest_buffer_size, const void *source_buffer, const u32 &source_buffer_size, ppmd_yield_callback_t ycb);
+
+
+#endif // PPMD_COMPRESSOR_H
