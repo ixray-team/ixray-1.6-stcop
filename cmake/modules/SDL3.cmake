7ca822bce515a8aea980715e1a155108f46f0e26
ForserX <sv3nk@yandex.ru>
2026-01-23 00:43:23 +0300

    CMake: Cleanup

--- cmake/modules/SDL3.cmake
+++ cmake/modules/SDL3.cmake
@@ -8,12 +8,14 @@ FetchContent_Declare(
 
 set(SDL_SHARED ON CACHE BOOL "" FORCE)
 set(SDL_STATIC OFF CACHE BOOL "" FORCE)
+set(SDL_TEST_LIBRARY OFF CACHE BOOL "" FORCE)
 
 FetchContent_MakeAvailable(SDL3)
 
 set_target_properties(SDL_uclibc PROPERTIES FOLDER "3rd Party")
 set_target_properties(SDL3-shared PROPERTIES FOLDER "3rd Party")
 
+# SDL Image
 download_single_header(
     "https://raw.githubusercontent.com/libsdl-org/SDL_image/refs/heads/main/include/SDL3_image/SDL_image.h"
     "SDL_Ext/SDL_image.h"
