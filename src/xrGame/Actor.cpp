8979db7cc83f119beb3a56c4c11ba2bb4f50bdb5
Shtrecker <gogels235@gmail.com>
2026-01-22 15:17:36 +0200

    Полоса заряда и время перезарядки для гаусс пушки

--- src/xrGame/Actor.cpp
+++ src/xrGame/Actor.cpp
@@ -1790,7 +1790,6 @@ void CActor::UpdateCL()
 			Device.hudViewportData.renderZoomRotateFactor = pWeapon->GetAimFactor();
 			Device.hudViewportData.isRenderActive = pWeapon->IsScopeAttached() && (pWeapon->GetAimFactor() > 0.0f) && (pWeapon->GetZoomFactor() > 0.0f);
 			Device.hudViewportData.ActorWeaponCondition = pWeapon->GetCondition();
-			Device.hudViewportData.ActorWeaponLoading = 1.0f;
 		}
 	}
 	else
@@ -1804,7 +1803,6 @@ void CActor::UpdateCL()
 			Device.hudViewportData.renderZoomRotateFactor = 0.0f;
 			Device.hudViewportData.isRenderActive = false;
 			Device.hudViewportData.ActorWeaponCondition = -1.0f;
-			Device.hudViewportData.ActorWeaponLoading = 1.0f;
 
 			// Switch back to third-person if was forced
 			if (bLook_cam_fp_zoom && cam_active == eacFirstEye) {
