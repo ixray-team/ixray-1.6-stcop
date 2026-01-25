8574e68f19e5764475ec5d57abfea0eb0a486f90
Desert Cliff <156225353+mrGlock5@users.noreply.github.com>
2026-01-25 14:29:48 +0300

    add inventory sort filters
    Дока у Дромбейза
    
    p.s.
    Попозже переработаю механизм добавления кастомных фильтров, а пока что так

---
+++ src/xrGame/ui/InventorySorter.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "inventory_space.h"
+
+class CInventoryItem;
+class CInventory;
+
+enum class EInventorySortCategory : u8
+{
+    All = 0,
+    Weapons,
+    Ammo,
+    Armor,
+    Devices,
+    Consumables,
+    Artefacts,
+    Attachments,
+    CustomStart = 8
+};
+
+struct SInventorySortCategoryInfo
+{
+    shared_str _id;
+    shared_str _name;
+    shared_str _hint;
+    shared_str _iconTexture;
+    bool _hasIcon = false;
+    bool _hasText = false;
+    xr_set<shared_str> _itemSections;
+    xr_set<CLASS_ID> _itemClsids;
+    bool _isCustom = false;
+};
+
+class CInventorySorter final
+{
+public:
+    CInventorySorter();
+    ~CInventorySorter() = default;
+
+    void Initialize();
+    void LoadCustomCategories();
+    
+    EInventorySortCategory GetItemCategory(PIItem item) const;
+    bool ItemMatchesCategory(PIItem item, EInventorySortCategory category) const;
+    
+    u32 GetCategoriesCount() const { return _categories.size(); }
+    EInventorySortCategory GetCategoryByIndex(u32 index) const;
+    const SInventorySortCategoryInfo* GetCategoryInfo(EInventorySortCategory category) const;
+    
+    void AddCustomCategory(const shared_str& id, const shared_str& name, const shared_str& hint);
+    void AddItemToCustomCategory(const shared_str& categoryId, const shared_str& itemSection);
+    void AddClsidToCustomCategory(const shared_str& categoryId, CLASS_ID clsid);
+    
+    void SortItems(TIItemContainer& items, EInventorySortCategory category) const;
+    
+private:
+    void InitializeDefaultCategories();
+    void LoadCategoryFromXml(const shared_str& categoryId, EInventorySortCategory category);
+    
+    bool IsWeapon(PIItem item) const;
+    bool IsAmmo(PIItem item) const;
+    bool IsArmor(PIItem item) const;
+    bool IsDevice(PIItem item) const;
+    bool IsConsumable(PIItem item) const;
+    bool IsArtefact(PIItem item) const;
+    bool IsAttachment(PIItem item) const;
+    bool MatchesCustomCategory(PIItem item, const shared_str& categoryId) const;
+    
+    xr_map<EInventorySortCategory, SInventorySortCategoryInfo> _categories;
+    xr_map<shared_str, EInventorySortCategory> _customCategoryMap;
+    u32 _customCategoryCounter = 0;
+};
