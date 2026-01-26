2d7f62ab887bae472a54391b40a428c7f2d0f504
Dimakuzmin <scorpion261096@gmail.com>
2026-01-26 11:13:19 +0200

    Убрал сжатие при 2м проходе (Теперь только расчет)
    Подключил новый uv_grid

--- src/utils/xrLC_Light/xrDeflector.h
+++ src/utils/xrLC_Light/xrDeflector.h
@@ -18,8 +18,7 @@ class XRLC_LIGHT_API CDeflector
 {
 public:
    	bool ApplyLmap			= false;
- 	bool ApplyResolution	= false;
- 	bool bMerged			= false;
+  	bool bMerged			= false;
  
 	Fvector				normal;
  	xr_vector<UVtri>	UVpolys;
@@ -102,9 +101,12 @@ public:
 	bool ApplyColors();
  	void ApplyColor(size_t INDEX, base_color_c& C);
  
-	// Stage 3
-	void LowerResolutionGPU();
-	void ApplyExpandBordersGPU();
+	// Stage 2
+ 	void ApplyExpandBordersGPU();
+
+
+	// Clearing Memory
+	void DealocateMemory() { layer.clear_memory(); };
 };
 
 
@@ -127,6 +129,8 @@ extern XRLC_LIGHT_API void		DumpDeflctor	( u32 id );
 extern XRLC_LIGHT_API void		DumpDeflctor	( const CDeflector &d );
 extern XRLC_LIGHT_API void		DeflectorsStats ();
 extern XRLC_LIGHT_API void		DumpDeflctor	( u32 id );
+
+extern XRLC_LIGHT_API void		GetApplyStats();
 
 #define rms_zero	((4+g_params().m_lm_rms_zero)/2)
 #define rms_shrink	((8+g_params().m_lm_rms)/2)
