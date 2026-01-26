f1dcf2abb45e07bce6a0fc8587035196e0f23a3f
Dimakuzmin <scorpion261096@gmail.com>
2026-01-26 11:09:16 +0200

    uv_grid * алгоритм поиска быстро вместо hash2d

--- src/utils/xrLC_Light/uv_tri.h
+++ src/utils/xrLC_Light/uv_tri.h
@@ -1,11 +1,19 @@
-#pragma once
+﻿#pragma once
 #include "tcf.h"
 #include "xrFace.h"
 
-struct XRLC_LIGHT_API UVtri :
-	public _TCF
+struct XRLC_LIGHT_API UVtri : public _TCF
 {
 	Face* owner;
 
 	bool similar(const UVtri& uv, float eps = EPS) const;
+
+	// нормализованный AABB [0..1]
+	Fvector2 uv_min_n;
+	Fvector2 uv_max_n;
+
+	// 🔥 НОВОЕ
+	void computeAABB(const Fbox2& bounds);
+	bool overlapsCell(u32 cx, u32 cy) const;
 };
+
