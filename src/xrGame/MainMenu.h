3cbedaaf3986a6f310ea89ed2a17d57d78f6cf89
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-01-26 16:41:27 +0300

    Implement stick movement for shniaga

--- src/xrGame/MainMenu.h
+++ src/xrGame/MainMenu.h
@@ -130,7 +130,8 @@ public:
 	virtual void	IR_OnKeyboardRelease			(int dik);
 	virtual void	IR_OnKeyboardHold				(int dik);
 
-	virtual void	IR_GamepadKeyPress		(int id);
+	virtual void	IR_GamepadKeyPress				(int id);
+	virtual void	IR_GamepadUpdateStick			(int id, Fvector2 value);
 	virtual void	IR_OnMouseWheel					(int direction)	;
 
 	bool			OnRenderPPUI_query				();
