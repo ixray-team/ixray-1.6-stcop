95ff62d69f562ac771c78739bc584d77cbe14c09
Hozar_2002 <hoz2002team@gmail.com>
2026-01-26 12:10:02 +0600

    Improve PBR reflections quality

--- gamedata/shaders/d3d11/combine_1.ps.hlsl
+++ gamedata/shaders/d3d11/combine_1.ps.hlsl
@@ -25,7 +25,7 @@ float4 main(PSInputFullscreen I) : SV_Target
 #ifndef USE_LEGACY_LIGHT
 	#ifdef USE_SSLR_REFLECTIONS
 		float3 SpecularIrradance = s_refl.SampleLevel(smp_rtlinear, I.texcoord, 0.0).xyz;
-		SpecularIrradance *= rcp(1.00001f - SpecularIrradance);
+		SpecularIrradance *= SpecularIrradance > 0.0f ? rcp(1.0f - SpecularIrradance) : 0.0f;
 	#else
 		float3 SpecularIrradance = CompureSpecularIrradance(reflect(O.View, O.Normal), O.Hemi, O.Roughness);
 	#endif
@@ -37,7 +37,7 @@ float4 main(PSInputFullscreen I) : SV_Target
 #endif
 
     float3 Color = Occ * Ambient + Light;
-
+	
     float Fog = PushGamma(saturate(O.ViewDist * fog_params.w + fog_params.x));
     Color = lerp(Color, PushGamma(fog_color.xyz), Fog);
 
