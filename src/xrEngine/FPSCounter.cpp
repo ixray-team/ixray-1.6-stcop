67ab0488e33088ee09ef457a2b453f0f692f38e1
Aphile <taiko6661@gmail.com>
2026-01-28 01:03:04 +0500

    Обновилл fps counter (лок фпс для меню и игры + выбор позиции счётчика фпс через rs_fps_pos), починил EMA сглаживание для дельты и в rs_stats для fps. Сделал две реализации fps lock: spinlock & sleep. Вырезал бессмысленный (???) rsConstantFps.

--- src/xrEngine/FPSCounter.cpp
+++ src/xrEngine/FPSCounter.cpp
@@ -1,64 +1,93 @@
 #include "stdafx.h"
-#include "device.h"
-#include "Stats.h"
-#include "GameFont.h"
-#include "XR_IOConsole.h"
 #include "FPSCounter.h"
-#include "../xrCore/appinfo.h"
 
 ENGINE_API XRay::Hardware::FPSCounter* pFPSCounter = nullptr;
 
-using xr_clock = std::chrono::high_resolution_clock;
-
-enum DebugTextColor : u64
-{
-	DTC_FPS_INFO = 0xFFFF8080,
-};
-
 XRay::Hardware::FPSCounter::FPSCounter()
 {
-	pCGameFont = g_FontManager->CloneFont("ui_font_console");
+	font_ = g_FontManager->CloneFont("ui_font_console");
+	VERIFY(font_);
 }
+
+u32 fps_text_current_pos = 1;
+xr_token fps_text_pos_tokens[5] = {
+	{"top-left", 0},
+	{"top-right", 1},
+	{"bottom-left", 2},
+	{"bottom-right", 3},
+	{0, 0}
+};
 
 void XRay::Hardware::FPSCounter::OnRender()
 {
-	auto GetMonitorHZLambda = []() -> u32
+	static float smoothed_fps = 60.0f;
+	static float visible_fps  = 60.0f;
+
+	if (Device.fTimeDelta > EPS_S)
 	{
-		int DisplayID = SDL_GetDisplayForWindow(g_AppInfo.Window);
-		if (DisplayID < 0)
+		if (!is_initialized_)
 		{
-			return 0u;
+			last_time_ = clock::now();
+			is_initialized_ = true;
+		}
+		
+		int raw_fps  = lroundf(1.f / Device.fTimeDelta);
+		smoothed_fps = static_cast<float>(raw_fps) * fps_smoothing_alpha + 
+					   smoothed_fps * (1.f - fps_smoothing_alpha);
+
+		if (time_point now = clock::now(); now - last_time_ >= std::chrono::milliseconds(1000 / 2))
+		{
+			visible_fps = smoothed_fps;
+			last_time_ = now;
 		}
 
-		const SDL_DisplayMode* DisplayModePtr = SDL_GetDesktopDisplayMode(DisplayID);
-		if (DisplayModePtr == nullptr)
+		shared_str text = shared_str().printf("FPS: %d", static_cast<int>(visible_fps));
+		
+		if (strcmp(fps_text_pos_tokens[fps_text_current_pos].name, "top-left") == 0)
 		{
-			return 0u;
+#ifndef MASTER_GOLD
+			font_->OutSet(
+					10,
+					CImGuiManager::Instance().IsGUIRendering() ? text_screen_padding + 20 : text_screen_padding
+				);
+#else
+			font_->OutSet(
+					10,
+					text_screen_padding
+				);
+#endif
 		}
-
-		return static_cast<u32>(DisplayModePtr->refresh_rate);
-	};
-
-	pCGameFont->SetHeight(0.013f);
-
-	static auto lastFrameTime = xr_clock::now();
-	auto currentTime = xr_clock::now();
-	currentTime = xr_clock::now();
-	std::chrono::duration<double> frameDuration = currentTime - lastFrameTime;
-	lastFrameTime = currentTime;
-
-	float fps = 0.f;
-	static float prevFps = 0.f;
-
-	if ((Device.dwFrame % GetMonitorHZLambda()) == 0)
-		fps = 1.f / (float)frameDuration.count();
-
-	fps = (fps == 0.0f) ? prevFps : fps;
-
-	pCGameFont->SetAligment(CGameFont::alLeft);
-	pCGameFont->SetColor(DebugTextColor::DTC_FPS_INFO);
-	pCGameFont->Out(psCurrentVidMode[0] - pCGameFont->GetHeight() * 4.5f, 35, "FPS: %i", static_cast<int>(fps));
-	pCGameFont->OnRender();
-
-	prevFps = fps;
+		else if (strcmp(fps_text_pos_tokens[fps_text_current_pos].name, "top-right") == 0)
+		{
+#ifndef MASTER_GOLD
+			font_->OutSet(
+					static_cast<float>(Device.TargetWidth) - font_->SizeOf_(*text) - 10,
+					CImGuiManager::Instance().IsGUIRendering() ? text_screen_padding + 20 : text_screen_padding
+				);
+#else
+			font_->OutSet(
+					static_cast<float>(Device.TargetWidth) - font_->SizeOf_(*text) - 10,
+					text_screen_padding
+				);
+#endif
+		}
+		else if (strcmp(fps_text_pos_tokens[fps_text_current_pos].name, "bottom-left") == 0)
+		{
+			font_->OutSet(
+					text_screen_padding,
+					static_cast<float>(Device.TargetHeight) - font_->GetHeight() - 10
+				);
+		}
+		else if (strcmp(fps_text_pos_tokens[fps_text_current_pos].name, "bottom-right") == 0)
+		{
+			font_->OutSet(
+					static_cast<float>(Device.TargetWidth) - font_->SizeOf_(*text) - text_screen_padding,
+					static_cast<float>(Device.TargetHeight) - font_->GetHeight() - text_screen_padding
+				);
+		}
+		
+		font_->SetColor(text_color);
+		font_->OutNext(*text);
+		font_->OnRender();
+	}
 }
