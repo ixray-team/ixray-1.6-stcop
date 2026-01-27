d43e2eae7dcd75bdea692d27a058a8c8feada7c4
Dimakuzmin <scorpion261096@gmail.com>
2026-01-27 11:51:14 +0200

    Убрал немного лишних стадий
    Deflectors* code refactory
    Implicit* code refactory

--- src/utils/xrLC_Light/xrLight_ImplicitCalcGlobs.h
+++ src/utils/xrLC_Light/xrLight_ImplicitCalcGlobs.h
@@ -1,26 +1,18 @@
 #pragma once
 #include "xrFace.h"
-#include "hash2D.h"
 
 class ImplicitDeflector;
-typedef hash2D <Face*, 384, 384> IHASH;
 
 class ImplicitCalcGlobs
 {
-	IHASH* ImplicitHash;
-	ImplicitDeflector* defl;
+ 	ImplicitDeflector* defl;
 
 public:
-	ImplicitCalcGlobs() :
-		defl(0), ImplicitHash(0)
+	ImplicitCalcGlobs() : defl(0) 
 	{
 	}
-
-	IC IHASH& Hash()
-	{
-		R_ASSERT(ImplicitHash);
-		return *ImplicitHash;
-	}
+	 
+	vecFace& query(float px, float py);
 
 	IC ImplicitDeflector& DATA()
 	{
@@ -28,7 +20,7 @@ public:
 		return *defl;
 	}
 
-	void Allocate();
-	void Deallocate();
+	// void Allocate();
+	// void Deallocate();
 	void Initialize(ImplicitDeflector& d);
 };
