98f9db7832a13c0b550240f16d0916e85521d57f
GladiatorVS <gladiatorvsoff@gmail.com>
2026-01-28 23:39:03 +0300

    Splash screens have been added for all original add-ons.

--- src/xrPlay/splash_eff_sp.h
+++ src/xrPlay/splash_eff_sp.h
@@ -3,6 +3,15 @@ namespace splash
 {
     namespace spooky
     {
-        //hz chto pridumat
+        int getBackgroundID(EEngineExternalPlatform platform)
+        {
+            if (platform == EEngineExternalPlatform::ShadowOfChernobyl)
+                return IDB_SOC_SPLASH_BG_HW;
+
+            if (platform == EEngineExternalPlatform::ClearSky)
+                return IDB_CS_SPLASH_BG_HW;
+
+            return IDB_COP_SPLASH_BG_HW;
+        }
     }
 }
