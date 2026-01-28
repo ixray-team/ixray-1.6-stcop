33e088556cdb539f7d88fb0bef04dbd5c596a681
GladiatorVS <gladiatorvsoff@gmail.com>
2026-01-29 00:12:27 +0300

    Level Builder icon

--- src/utils/xrForms/resource.h
+++ src/utils/xrForms/resource.h
@@ -1,6 +1,7 @@
-// Microsoft Visual C++ generated include file
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
 // Used by resource.rc
-
+//
 #define ID_VIEW_NDEPTH                  2
 #define IDDEBUG                         3
 #define ID_VIEW_NHEIGHT                 3
@@ -16,6 +17,7 @@
 #define IDD_VERIFY                      104
 #define IDD_STEP                        105
 #define IDD_NVIEW                       107
+#define IDI_ICON1                       108
 #define IDC_LOG                         1000
 #define IDC_FILE                        1001
 #define IDC_LINE                        1002
@@ -30,15 +32,13 @@
 #define IDC_P_TEXT                      1013
 #define IDC_PHASE_TIME                  1014
 #define IDC_PROGRESS1                   1015
+#define IDC_STATIC                      -1
 
-#ifndef IDC_STATIC
-#define IDC_STATIC -1
-#endif
-
-// Next default values for _new objects
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           101
