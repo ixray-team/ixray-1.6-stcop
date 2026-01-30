672e1f8ab8d529d8086791a6a19bca1c68453dd5
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-01-30 19:12:41 +0300

    Add gamepad sensibility

--- src/xrEngine/IInputReceiver.h
+++ src/xrEngine/IInputReceiver.h
@@ -37,4 +37,7 @@ public:
 ENGINE_API extern float			psMouseSens;
 ENGINE_API extern float			psMouseUISens;
 ENGINE_API extern float			psMouseSensScale;
-ENGINE_API extern Flags32		psMouseInvert;
+ENGINE_API extern bool			psMouseInvert;
+
+ENGINE_API extern float			psGamepadSens;
+ENGINE_API extern bool			psGamepadInvert;
