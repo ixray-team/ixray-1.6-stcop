292ed85bd38cb555ebf843756bccd4735991fc2d
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-01-30 08:04:22 +0300

    Fix build

--- src/utils/xrLC/b_globals.h
+++ src/utils/xrLC/b_globals.h
@@ -9,9 +9,6 @@ const	u32								c_PM_FaceLimit			= 128;			// face-limit
 const	float							c_PM_MetricLimit_static	= 0.10f;		// vertex-count-simplification-limit
 const	float							c_PM_MetricLimit_mu		= 0.05f;		// vertex-count-simplification-limit
 
-BOOL	exact_normalize					(Fvector3&	a);
-BOOL	exact_normalize					(float*		a);
-
 
 //#include "../xrLC_Light/xrFace.h"
 //#include "../xrLC_Light/xrDeflector.h"
