9e42729dcd7dfe75820d43d711795796cfbfd666
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-02-01 00:38:08 +0300

    Remove Dear ImGui gamepad initialization

--- src/xrEngine/ImGuiManager.cpp
+++ src/xrEngine/ImGuiManager.cpp
@@ -56,7 +56,6 @@ void CImGuiManager::InitPlatform()
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
 	//io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
 
