f8cfc2b8436e9fbfa07708cdf98f543c1a02e501
ForserX <sv3nk@yandex.ru>
2026-02-01 00:31:08 +0300

    Entity Component System: Initial and add `CDamageManager`

--- src/xrGame/damage_manager.h
+++ src/xrGame/damage_manager.h
@@ -8,24 +8,18 @@
 
 #pragma once
 
-class CDamageManager
+struct TDamageManager
 {
-protected:
-	float				m_default_hit_factor;
-	float				m_default_wound_factor;
-	CObject				*m_object;
+	float m_default_hit_factor;
+	float m_default_wound_factor;
+	CObject* m_object;
 
-public:
-						CDamageManager	();
-	virtual				~CDamageManager	();
-	virtual	DLL_Pure	*_construct		();
-	virtual	void		reload			(LPCSTR section, CInifile const * ini);
-	virtual	void		reload			(LPCSTR section, LPCSTR sub_section, CInifile const * ini);
-	
-	virtual	void		HitScale		(const int bone_num, float& hit_scale, float& wound_scale, bool aim_bullet=false);
+	void reload(LPCSTR section, CInifile const* ini);
+	void reload(LPCSTR section, LPCSTR sub_section, CInifile const* ini);
+
+	void HitScale(const int bone_num, float& hit_scale, float& wound_scale, bool aim_bullet = false);
 
 private:
-			void		load_section	(LPCSTR section, CInifile const * ini);
-			// init default params
-			void		init_bones		(LPCSTR section, CInifile const * ini);
+	void load_section(LPCSTR section, CInifile const* ini);
+	void init_bones(LPCSTR section, CInifile const* ini);
 };
