8166bcb0861ca46bc54b960eeea9db3cde485a1d
Hozar2002 <hoz2002team@gmail.com>
2026-02-01 20:08:07 +0600

    Fix icorrent shadow quality on point lights

--- src/Layers/xrRender/light.cpp
+++ src/Layers/xrRender/light.cpp
@@ -382,42 +382,54 @@ void	light::xform_calc			()
 	}
 }
 
-void	light::optimize_smap_size()
+void light::optimize_smap_size()
 {
 	int _cached_size = X.S.size;
-	X.S.posX	= 0;
-	X.S.posY	= 0;
-	X.S.size	= SMAP_adapt_max;
+
+	X.S.posX = 0;
+	X.S.posY = 0;
+
+	X.S.size = SMAP_adapt_max;
 	X.S.transluent = FALSE;
+
 	// Compute approximate screen area (treating it as an point light) - R*R/dist_sq
 	// Note: we clamp screen space area to ONE, although it is not correct at all
-	float	dist = Device.vCameraPosition.distance_to(SpatialComponent->spatial.sphere.P) - SpatialComponent->spatial.sphere.R;
-			if (dist<0)	dist	= 0;
-	float	ssa					= clampr	(range*range / (1.f+dist*dist),0.f,1.f);
 
-	// compute how much duelling frusta occurs	[-1..1]-> 1 + [-0.5 .. +0.5]
-	float	duel_dot			= 1.f -	0.5f*Device.vCameraDirection.dotproduct(direction);
+	float dist = Device.vCameraPosition.distance_to(SpatialComponent->spatial.sphere.P) - SpatialComponent->spatial.sphere.R;
+
+	if (dist < 0)	
+	{
+		dist = 0;
+	}
+
+	float ssa = clampr(range * range / (1.f + dist * dist), 0.f, 1.f);
 
 	// compute how large the light is - give more texels to larger lights, assume 8m as being optimal radius
-	float	sizefactor			= range/8.f;				// 4m = .5, 8m=1.f, 16m=2.f, 32m=4.f
+	float sizefactor = range / 8.f; // 4m = .5, 8m=1.f, 16m=2.f, 32m=4.f
 
 	// compute how wide the light frustum is - assume 90deg as being optimal
-	float	widefactor			= cone/deg2rad(90.f);	// 
+	float widefactor = cone / PI_DIV_2;
 
 	// factors
-	float	factor0				= powf	(ssa,		1.f/2.f);		// ssa is quadratic
-	float	factor2				= powf	(duel_dot,	1.f/4.f);		// difficult to fast-change this -> visible
-	float	factor3				= powf	(sizefactor,1.f/4.f);		// this shouldn't make much difference
-	float	factor4				= powf	(widefactor,1.f/2.f);		// make it linear ???
-	float	factor				= ps_r2_ls_squality * factor0 /** factor1*/ * factor2 * factor3 * factor4;
-	
+	float factor0 = powf(ssa, 1.f / 2.f); // ssa is quadratic
+	float factor1 = powf(sizefactor, 1.f / 4.f); // this shouldn't make much difference
+	float factor2 = powf(widefactor, 1.f / 2.f); // make it linear ???
+
+	if (flags.type == IRender_Light::SPOT)
+	{
+		float duel_dot = 1.f - 0.5f * Device.vCameraDirection.dotproduct(direction);
+		factor0 *= powf(duel_dot, 1.f / 4.f);
+	}
+
+	float factor = ps_r2_ls_squality * factor0 * factor1 * factor2;
+
 	// final size calc
-	u32 _size					= iFloor( factor * SMAP_adapt_optimal );
-	if (_size<SMAP_adapt_min)	_size	= SMAP_adapt_min;
-	if (_size>SMAP_adapt_max)	_size	= SMAP_adapt_max;
-	int _epsilon				= iCeil	(float(_size)*0.01f);
-	int _diff					= std::abs	(int(_size)-int(_cached_size));
-	X.S.size					= (_diff>=_epsilon)?_size:_cached_size;
+	u32 _size = iFloor(factor * SMAP_adapt_optimal);
+	if (_size < SMAP_adapt_min)	_size = SMAP_adapt_min;
+	if (_size > SMAP_adapt_max)	_size = SMAP_adapt_max;
+	int _epsilon = iCeil(float(_size) * 0.01f);
+	int _diff = std::abs(int(_size) - int(_cached_size));
+	X.S.size = (_diff >= _epsilon) ? _size : _cached_size;
 }
 
 //								+X,				-X,				+Y,				-Y,			+Z,				-Z
