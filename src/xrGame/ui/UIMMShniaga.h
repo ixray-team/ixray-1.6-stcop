05e0b9bad5a858f7fa1545c4f92974b1a465653e
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-02-01 12:27:44 +0300

    Fix input in load dialog

--- src/xrGame/ui/UIMMShniaga.h
+++ src/xrGame/ui/UIMMShniaga.h
@@ -21,6 +21,7 @@ public:
 
 	virtual bool 		OnMouseAction					(float x, float y, EUIMessages mouse_action);
 	virtual bool 		OnKeyboardAction				(int dik, EUIMessages keyboard_action);
+	virtual bool 		OnGamepadKeyAction				(int id, EUIMessages keyboard_action);
 	virtual void 		SendMessage				(CUIWindow* pWnd, s16 msg, void* pData = 0);
 			void 		SetVisibleMagnifier		(bool f);
 	virtual void		OnDeviceReset			();
