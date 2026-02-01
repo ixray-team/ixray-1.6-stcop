c05834f8c6a7ac42c54a3d117851b4377491a93e
Shtrecker <gogels235@gmail.com>
2026-02-01 18:30:42 +0200

    Improve and fix bullet bones scheme

--- src/xrGame/WeaponShotgun.cpp
+++ src/xrGame/WeaponShotgun.cpp
@@ -202,6 +202,7 @@ void CWeaponShotgun::switch2_StartReload()
 {
 	u8 type_to_update = m_bUseLastAmmoType && m_LastShotAmmoType != undefined_ammo_type ? m_LastShotAmmoType : GetTargetAmmoType();
 	UpdateAmmoBones(m_ammo_bones_mag, iAmmoElapsed, type_to_update);
+	UpdateMagAmmoBones(m_mag_bone_type, type_to_update);
 
 	PlayAnimOpenWeapon();
 	SetPending(TRUE);
@@ -218,9 +219,9 @@ void CWeaponShotgun::switch2_StartReload()
 
 void CWeaponShotgun::switch2_AddCartgidge()
 {
+	UpdateAmmoBones(m_ammo_bones_mag, iAmmoElapsed, GetTargetAmmoType());
+	UpdateMagAmmoBones(m_mag_bone_type, GetTargetAmmoType());
 	m_bIsReloaded = false;
-	PlayAnimAddOneCartridgeWeapon();
-	SetPending(TRUE);
 
 	if (ParentIsActor() && m_sounds.FindSoundItem("sndAddCartridgeEmpty", false) && iAmmoElapsed + iAmmoChamberElapsed == 0)
 	{
@@ -234,11 +235,15 @@ void CWeaponShotgun::switch2_AddCartgidge()
 	{
 		PlaySound("sndAddCartridge", get_LastFP());
 	}
+
+	SetPending(TRUE);
+	PlayAnimAddOneCartridgeWeapon();
 }
 
 void CWeaponShotgun::switch2_EndReload()
 {
 	UpdateAmmoBones(m_ammo_bones_mag, iAmmoElapsed, GetTargetAmmoType());
+	UpdateMagAmmoBones(m_mag_bone_type, GetTargetAmmoType());
 	SetPending(TRUE);
 
 	PlayAnimCloseWeapon();
@@ -431,5 +436,14 @@ void CWeaponShotgun::OnMotionMark(u32 state, const motion_marks& mark)
 				AddCartridge(1);
 			}
 		}
+	}
+
+	if (ParentIsActor() && state == eReload && m_bTriStateReload && mark.name == "Left")
+	{
+		u32 current_configuration = iAmmoElapsed + iAmmoChamberElapsed + 1;
+		bool for_grenade = IsGrenadeMode();
+		UpdateAmmoBones(for_grenade ? m_ammo_bones_gl : m_ammo_bones_mag, current_configuration, GetTargetAmmoType(for_grenade));
+		UpdateLiteAmmoBones(current_configuration);
+		UpdateMagAmmoBones(m_mag_bone_type, GetTargetAmmoType(for_grenade));
 	}
 }
