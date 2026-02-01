5fc1ecd464de96f85e7d1e88e02d2712d72458ce
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-02-01 12:27:44 +0300

    Fix input in load dialog

--- src/xrGame/ui/UIMessageBoxEx.h
+++ src/xrGame/ui/UIMessageBoxEx.h
@@ -26,6 +26,7 @@ public:
 	void 	OnNOClicked			(CUIWindow*, void*);
 
 	virtual bool	OnKeyboardAction			(int dik, EUIMessages keyboard_action);
+	virtual bool	OnGamepadKeyAction			(int id, EUIMessages gamepad_action);
 	virtual bool	NeedCenterCursor	()const	 {return false;}
 
 	virtual CUIWindow* ui_cast_window() { return this; }
