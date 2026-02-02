39280545f0ece59f1eeaa1b69ecd1736ea7f75bc
Desert Cliff <156225353+mrGlock5@users.noreply.github.com>
2026-02-03 00:44:41 +0300

    Compass Bar (горизонтальный компас)
    Для полноценных тестов, нужно ставить мой аддон, там нужные текстуры
    https://github.com/ixray-community/ixray-hd-hud/tree/renewed-perception-2-0
    
    Он еще требует инструментов для тонкой работы - то есть, его нужно еще доделывать. Есть откровенные косяки

--- src/xrGame/map_manager.h
+++ src/xrGame/map_manager.h
@@ -26,6 +26,7 @@ public:
 	void					RemoveMapLocationByObjectID (u16 id); //call on destroy object
 	void					RemoveMapLocation			(CMapLocation* ml);
 	CMapLocation*			GetMapLocation				(const shared_str& spot_type, u16 id);
+	CMapLocation*			GetActiveTaskCompassLocation	();
 	void					GetMapLocations				(const shared_str& spot_type, u16 id, xr_vector<CMapLocation*>& res);
 	void					DisableAllPointers			();
 	bool					GetMapLocationsForObject	(u16 id, xr_vector<CMapLocation*>& res);
