75ad7e77f1e747afaa30bbc049ae08809327e586
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-02-04 07:42:36 +0300

    Unify shaders

--- gamedata_cs/shaders/d3d11/deffer_detail.vs.hlsl
+++ gamedata_cs/shaders/d3d11/deffer_detail.vs.hlsl
@@ -42,20 +42,22 @@ void main(in v_detail I, out p_bumped_new O, uint instance_id : SV_InstanceID)
 {
 	//LVutner: Read our structured buffer
 	InstanceData det = detail_buffer[instance_id];
-
-	float3x3 mmhpb = setMatrix(det.hpb);
+	float3x3 m_rotate = setMatrix(det.hpb);
 
 	float hemi = abs(det.hemi);
 	float sun = sign(det.hemi)*0.25f+0.25f;
 
-    float4 m0 = float4(mmhpb[0]*det.scale, det.pos.x);
-    float4 m1 = float4(mmhpb[1]*det.scale, det.pos.y);
-    float4 m2 = float4(mmhpb[2]*det.scale, det.pos.z);
+    float4 m0 = float4(m_rotate[0]*det.scale, det.pos.x);
+    float4 m1 = float4(m_rotate[1]*det.scale, det.pos.y);
+    float4 m2 = float4(m_rotate[2]*det.scale, det.pos.z);
 
     float4 pos, pos_old;
     pos.x = dot(m0, float4(I.pos.xyz, 1.0));
     pos.y = dot(m1, float4(I.pos.xyz, 1.0));
     pos.z = dot(m2, float4(I.pos.xyz, 1.0));
+	
+	float3 N = unpack_normal(I.N.zyx);
+	N = mul(m_rotate, N).xyz * 2.0f;
 
     pos.w = 1.0f;
     pos_old = pos;
@@ -78,10 +80,10 @@ void main(in v_detail I, out p_bumped_new O, uint instance_id : SV_InstanceID)
 
     float3 Pe = mul(m_WV, pos);
 
-    float3 N;
-    N.x = pos.x - m0.w;
-    N.y = pos.y - m1.w + 0.75f;
-    N.z = pos.z - m2.w;
+  //  float3 N;
+    // N.x = pos.x - m0.w;
+    // N.y = pos.y - m1.w + 0.75f;
+    // N.z = pos.z - m2.w;
 
     O.tcdh = float4(I.tc.xy, hemi, sun);
     O.position = float4(Pe, 1.0f);
