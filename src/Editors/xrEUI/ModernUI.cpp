8cb2c52774be5deb77a9ae74e098304c739b8c46
ForserX <sv3nk@yandex.ru>
2026-02-10 14:00:51 +0300

    Переключение тем

--- src/Editors/xrEUI/ModernUI.cpp
+++ src/Editors/xrEUI/ModernUI.cpp
@@ -94,7 +94,9 @@ XREUI_API bool XRay::ImGui::ToggleButton(const char* Label, bool* Flags, const I
 	return Changed;
 }
 
-std::array<ImColor, 11> DefaultTheme =
+std::array<ImColor, 11>* CurrentTheme = nullptr;
+
+std::array<ImColor, 11> PurpleTheme =
 {
 	ImColor(63, 71, 101, 255),
 	ImColor(29, 129, 136, 255),
@@ -106,6 +108,21 @@ std::array<ImColor, 11> DefaultTheme =
 	ImColor(0.f, 0.f, 0.f, 0.3f), // PanelTint
 	ImColor(0.f, 0.f, 0.f, 0.f),  // ButtonTint
 	ImColor(1.f, 1.f, 1.f, 0.6f), // ContentIconTint
+	ImColor(0.f, 0.f, 0.f, 0.8f)  // PanelBackgroundTint
+};
+
+std::array<ImColor, 11> DarkTheme =
+{
+	ImColor(51, 51, 51, 255),
+	ImColor(121, 113, 189, 255),
+	ImColor(0.f, 0.f, 0.f, 0.0f), //  ToolbarButtonTint
+	ImColor(0.f, 0.f, 0.f, 0.3f), //  ToolbarTint
+	ImColor(0.f, 0.f, 0.f, 0.5f), //  BackgroundTint
+	ImColor(0.f, 0.f, 0.f, 0.4f), //  TableTint
+	ImColor(0.f, 0.f, 0.f, 0.4f), //  TabBarTint
+	ImColor(0.f, 0.f, 0.f, 0.3f), //  PanelTint
+	ImColor(0.f, 0.f, 0.f, 0.f),  //  ButtonTint
+	ImColor(1.f, 1.f, 1.f, 0.6f), //  ContentIconTint
 	ImColor(0.f, 0.f, 0.f, 0.8f)  // PanelBackgroundTint
 };
 
@@ -122,12 +139,29 @@ ImColor AlphaBlend(const ImColor& Base, const ImColor& Tint)
 	return ImColor(Out);
 }
 
+XREUI_API void XRay::ImGui::SetupColorsList(int ID)
+{
+	switch (ID)
+	{
+		case 0: CurrentTheme = &PurpleTheme; break;
+		case 1: CurrentTheme = &DarkTheme; break;
+	}
+
+	EditorColors.clear();
+}
+
 XREUI_API ImColor XRay::ImGui::GetEditorColor(EEditorColors Color)
 {
 	if (EditorColors.contains(Color))
 	{
 		return EditorColors[Color];
 	}
+
+	if (CurrentTheme == nullptr)
+	{
+		CurrentTheme = &PurpleTheme;
+	}
+	std::array<ImColor, 11>& DefaultTheme = *CurrentTheme;
 
 	ImColor Clr = DefaultTheme[(size_t)EEditorColors::Main];
 
