38a37fabfeadf2ce8a862fda2739d21ea502daf2
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-02-14 19:04:06 +0300

    Add gamepad support for cars

--- src/xrGame/WeaponStatMgun.h
+++ src/xrGame/WeaponStatMgun.h
@@ -93,7 +93,9 @@ protected:
 //HolderCustom
 public:
 	virtual bool			Use					(const Fvector& pos,const Fvector& dir,const Fvector& foot_pos) {return !Owner();};
+	virtual void			OnGamepadAxisMove	(int id, Fvector2 value) {};
 	virtual void			OnMouseMove			(int x, int y);
+	virtual void			OnGamepadKeyPress	(int id){};
 	virtual void			OnKeyboardPress		(int dik);
 	virtual void			OnKeyboardRelease	(int dik);
 	virtual void			OnKeyboardHold		(int dik);
