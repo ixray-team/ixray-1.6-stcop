55f8e3c9b3c92de27fc79bffaf4be0e68bf5aa25
Hozar2002 <hoz2002team@gmail.com>
2026-02-15 12:51:25 +0600

    Implement per-target `SetRS` functional on `D3D11`
    Раньше `SimulatorStates::set_RS` задавал стейты сразу для всех целей рендера, в связи с чем кодер не мог по разному смешивать таргеты (нативными методами). Теперь допустимо поведение с явным указанием индекса цели рендера.

--- src/Layers/xrRender/tss.h
+++ src/Layers/xrRender/tss.h
@@ -25,6 +25,7 @@ public:
 	{
 		container.set_TSS(S,N,V);
 	}
+#ifndef USE_DX11
 	IC void SetColor	(SimulatorStates& container, u32 S, u32 A1, u32 OP, u32 A2)
 	{
 		Set(container,S,D3DTSS_COLOROP,	OP);
@@ -73,16 +74,17 @@ public:
 		SetAlpha		(container,S,A1,OP,A2);
 		Set				(container,S,D3DTSS_ALPHAARG0,A3);
 	}
+#endif
 };
 
 class  CSimulatorRS
 {
 public:
-	IC void Set			(SimulatorStates& container, u32 N, u32 V)
+	IC void Set			(SimulatorStates& container, u32 N, u32 V, u32 I)
 	{
 		//	Igor: XBox has render states 400 and hire
 		//R_ASSERT(N<256);
-		container.set_RS(N,V);
+		container.set_RS(N, V, I);
 	}
 };
 
@@ -97,12 +99,17 @@ public:
 	IC void				Invalidate	()									{ container.clear(); }
 	IC void				SetTSS		(u32 S, u32 N, u32 V)				{ TSS.Set(container,S,N,V);		}
 	IC void				SetSAMP		(u32 S, u32 N, u32 V)				{ container.set_SAMP(S,N,V);	}
+#ifndef USE_DX11
 	IC void				SetColor	(u32 S, u32 a, u32 b, u32 c)		{ TSS.SetColor(container,S,a,b,c);}
 	IC void				SetColor3	(u32 S, u32 a, u32 b, u32 c, u32 d)	{ TSS.SetColor3(container,S,a,b,c,d);}
 	IC void				SetAlpha	(u32 S, u32 a, u32 b, u32 c)		{ TSS.SetAlpha(container,S,a,b,c);}
 	IC void				SetAlpha3	(u32 S, u32 a, u32 b, u32 c, u32 d)	{ TSS.SetAlpha3(container,S,a,b,c,d);}
-	IC void				SetRS		(u32 N, u32 V)						{ RS.Set(container,N,V);	}
-	IC SimulatorStates&	GetContainer()									{ return container; }
+#endif //USE_DX11
+
+	IC void				SetRS(u32 I, u32 N, u32 V) { RS.Set(container, N, V, I); }
+	IC void				SetRS(u32 N, u32 V) { SetRS(u32(-1), N, V); }
+
+	IC SimulatorStates& GetContainer() { return container; }
 };
 
 #endif
