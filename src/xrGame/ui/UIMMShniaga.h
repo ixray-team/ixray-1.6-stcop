fb276700e3b12cc710015cdb9f0f83feb8cf5bc8
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-02-20 00:26:19 +0300

    Implement action repeaters

--- src/xrGame/ui/UIMMShniaga.h
+++ src/xrGame/ui/UIMMShniaga.h
@@ -19,9 +19,10 @@ public:
 	virtual void 		Update					();
 	virtual void 		Draw					();
 
-	virtual bool 		OnMouseAction					(float x, float y, EUIMessages mouse_action);
-	virtual bool 		OnKeyboardAction				(int dik, EUIMessages keyboard_action);
-	virtual bool 		OnGamepadKeyAction				(int id, EUIMessages keyboard_action);
+	virtual bool 		OnMouseAction			(float x, float y, EUIMessages mouse_action);
+	virtual bool 		OnKeyboardAction		(int dik, EUIMessages keyboard_action);
+	virtual bool 		OnGamepadKeyAction		(int id, EUIMessages keyboard_action);
+	virtual bool		OnGamepadKeyHold		(int key);
 	virtual void 		SendMessage				(CUIWindow* pWnd, s16 msg, void* pData = 0);
 			void 		SetVisibleMagnifier		(bool f);
 	virtual void		OnDeviceReset			();
