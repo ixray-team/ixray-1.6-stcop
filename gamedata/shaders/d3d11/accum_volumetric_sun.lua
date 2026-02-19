aeae53cfb4731a76184acd0bdc13abb74f692afa
papa_doenitz <valuev@physics.msu.ru>
2026-02-20 00:43:30 +0600

    Sun Volumetrics fix and edit.

--- gamedata/shaders/d3d11/accum_volumetric_sun.lua
+++ gamedata/shaders/d3d11/accum_volumetric_sun.lua
@@ -1,3 +1,5 @@
+local tex_noise = "shaders\\blue_noise_3x3"
+
 function normal(shader, t_base, t_second, t_detail)
     shader:begin("stub_fullscreen_triangle", "accum_volumetric_sun")
         :fog(false)
@@ -8,6 +10,7 @@ function normal(shader, t_base, t_second, t_detail)
     shader:dx10texture("s_smap_sun", "$user$smap_depth_sun")
     shader:dx10texture("s_position", "$user$position")
     shader:dx10texture("jitter0", "$user$jitter_0")
+    shader:dx10texture("blue_noise", tex_noise)
 
     shader:dx10sampler("smp_nofilter")
     shader:dx10sampler("smp_jitter")
