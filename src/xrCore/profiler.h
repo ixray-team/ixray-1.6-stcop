e9598083df0dfbcf41a909c41cc20651d617e5ec
ForserX <sv3nk@yandex.ru>
2026-02-22 14:03:47 +0300

    Add `Tracy` support
    Co-Authored-By: Александр <63524972+aphileyes@users.noreply.github.com>

--- src/xrCore/profiler.h
+++ src/xrCore/profiler.h
@@ -10,6 +10,17 @@
 #	define PROF_SAVE_CAPTURE(Name) OPTICK_SAVE_CAPTURE(Name)
 #	define PROF_FRAME(Name) OPTICK_FRAME(Name)
 #	define PROF_EVENT(Name) OPTICK_EVENT(Name)
+#elif defined(IXRAY_PROFILER_TRACY)
+#   define TRACY_ENABLE
+#   include "tracy/Tracy.hpp"
+#   define PROF_THREAD(Name)
+#   define PROF_START_THREAD(Name) // Не нужно в Tracy - потоки определяются автоматически
+#   define PROF_STOP_THREAD() // Не нужно в Tracy
+#   define PROF_START_CAPTURE()
+#   define PROF_STOP_CAPTURE()
+#   define PROF_SAVE_CAPTURE(Name)
+#   define PROF_FRAME(Name) FrameMarkNamed(Name)
+#   define PROF_EVENT(Name) ZoneTransientNC(TracyConcat(__tracy_zone, __LINE__), Name, 0, true);
 #else
 #	define PROF_THREAD(Name)
 #	define PROF_START_THREAD(Name)
