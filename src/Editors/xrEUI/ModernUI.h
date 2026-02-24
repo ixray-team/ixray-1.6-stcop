c43c2ac5ffdeab859bf03f81116d03898f1a8f10
xroo <xrooshka@gmail.com>
2026-02-25 00:30:19 +0100

    + Sizes Array,
    * ToggleFlagButton & table items styling WIP

--- src/Editors/xrEUI/ModernUI.h
+++ src/Editors/xrEUI/ModernUI.h
@@ -14,14 +14,29 @@ namespace XRay::ImGui
 		TabBarTint,
 		PanelTint,
 		ButtonTint,
+		ButtonBorderTint,
 		ContentIconTint,
 		PanelBackgroundTint,
+		HoverTint,
+		ActiveTint,
+	};
+	enum EEditorSizes
+	{
+		ButtonSize,
+		ButtonBorderSize,
+		ButtonRadius,
+		ButtonPaddingW,
+		ButtonPaddingH,
+		TextFieldPadding,
+		IndicatorWidth,
 	};
 
 	XREUI_API void SetupColorsList(int ID);
 	XREUI_API ImColor GetEditorColor(EEditorColors Color);
+	XREUI_API float GetEditorSize(EEditorSizes Size);
 	XREUI_API bool InputVector3(const char* Label, float V[3], float Step);
 	XREUI_API bool TumblerButton(const char* Label, bool& State, ImVec2 Size = {0, 0});
 	XREUI_API bool ToggleFlagButton(const char* Label, uint32_t* Flags, uint32_t Mask, const ImVec2& Size);
 	XREUI_API bool ToggleButton(const char* Label, bool* Flags, const ImVec2& Size);
 }
+ImColor AlphaBlend(const ImColor& Base, const ImColor& Tint);
