c01bc7716ff394e64c8bcc4f1536a18563352df5
Shtrecker <gogels235@gmail.com>
2026-02-24 16:58:36 +0200

    Add torch logic like gunslinger mod

--- src/xrGame/Actor.cpp
+++ src/xrGame/Actor.cpp
@@ -2778,6 +2778,8 @@ void CActor::OnItemDrop(CInventoryItem *inventory_item, bool just_before_destroy
 {
 	CInventoryOwner::OnItemDrop(inventory_item, just_before_destroy);
 
+	static const bool TorchOnlyOutfit = EngineExternal()[EEngineExternalGame::EnableTorchOnlyInOutfit];
+
 	CCustomOutfit* outfit = inventory_item->cast_outfit();
 	if (outfit && inventory_item->m_ItemCurrPlace.type == eItemPlaceSlot)
 	{
@@ -2787,6 +2789,15 @@ void CActor::OnItemDrop(CInventoryItem *inventory_item, bool just_before_destroy
 		{
 			GetNightVisionEffector()->SwitchNightVision(false);
 		}
+
+		if (TorchOnlyOutfit && !outfit->bIsHelmetAvaliable)
+		{
+			CTorch* pTorch = static_cast<CTorch*>(inventory().ItemFromSlot(TORCH_SLOT));
+			if (pTorch != nullptr)
+			{
+				pTorch->Switch(false);
+			}
+		}
 	}
 
 	CHelmet* helmet = inventory_item->cast_helmet();
@@ -2795,6 +2806,15 @@ void CActor::OnItemDrop(CInventoryItem *inventory_item, bool just_before_destroy
 		if (GetNightVisionEffector() && GetNightVisionEffector()->GetStatus())
 		{
 			GetNightVisionEffector()->SwitchNightVision(false);
+		}
+
+		if (TorchOnlyOutfit)
+		{
+			CTorch* pTorch = static_cast<CTorch*>(inventory().ItemFromSlot(TORCH_SLOT));
+			if (pTorch != nullptr)
+			{
+				pTorch->Switch(false);
+			}
 		}
 	}
 
