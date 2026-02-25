6598f889333504b5fa27f56f034ad46c7c9dce8b
Shtrecker <gogels235@gmail.com>
2026-02-25 12:12:58 +0200

    Fix slots restore after outfit change (AGAIN...)

--- src/xrGame/HudPdaAnimator.cpp
+++ src/xrGame/HudPdaAnimator.cpp
@@ -163,11 +163,23 @@ void CHudPdaAnimator::OnAnimationEnd(u32 state)
 			break;
 		}
 
+		if (!g_player_hud->m_need_reload)
+		{
+			break;
+		}
+
 		u8& slot_to_restore = m_manager->SlotToRestore();
 		bool& restore_device = m_manager->RestoreDevice();
 
-		if (slot_to_restore > 0 && m_manager->Parent()->inventory().ItemFromSlot(slot_to_restore))
+		PIItem item_to_restore = m_manager->Parent()->inventory().ItemFromSlot(slot_to_restore);
+		if (slot_to_restore > 0 && item_to_restore != nullptr)
 		{
+			u16 real_slot = item_to_restore->BaseSlot();
+			if (real_slot != INV_SLOT_2 && real_slot != KNIFE_SLOT && real_slot != BOLT_SLOT && real_slot != PISTOL_SLOT_NEW)
+			{
+				restore_device = false;
+			}
+
 			m_manager->Parent()->inventory().Activate(slot_to_restore);
 			slot_to_restore = 0;
 		}
