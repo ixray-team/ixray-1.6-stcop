845ed31bcebb009653c577162853df403c159081
Shtrecker <gogels235@gmail.com>
2026-02-25 10:56:15 +0200

    Add nvg scope jitter

--- src/xrGame/Actor.cpp
+++ src/xrGame/Actor.cpp
@@ -1854,6 +1854,7 @@ void CActor::UpdateCL()
 			Device.hudViewportData.isRenderActive = false;
 			Device.hudViewportData.ActorWeaponCondition = -1.0f;
 			Device.hudViewportData.renderScopeBrightnessValue = 0.0f;
+			Device.hudViewportData.renderScopeBrightnessJitterValue = 0.0f;
 
 			// Switch back to third-person if was forced
 			if (bLook_cam_fp_zoom && cam_active == eacFirstEye) {
