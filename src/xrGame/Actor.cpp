c1627f3950c478d6ba2af8e57aca5dd3227ee7b2
Shtrecker <gogels235@gmail.com>
2026-02-25 15:17:53 +0200

    СХЛОПНУТЬ С `Fix slots restore after outfit change (AGAIN...)`

--- src/xrGame/Actor.cpp
+++ src/xrGame/Actor.cpp
@@ -1644,41 +1644,43 @@ void CActor::UpdateCL()
 			{
 				g_player_hud->load_default();
 			}
-	
-			u16 saved_old_slot = NO_ACTIVE_SLOT;
-	
-			if (HudAnimator()->SlotToRestore() > saved_old_slot)
-			{
-				old_slot = HudAnimator()->SlotToRestore();
-				HudAnimator()->SlotToRestore() = saved_old_slot;
-			}
 
-			if (HudAnimator()->RestoreDevice())
+			if (HudAnimator()->TargetAnimator() == nullptr)
 			{
-				need_restore_detector = true;
-				HudAnimator()->RestoreDevice() = false;
-			}
+				u16 saved_old_slot = NO_ACTIVE_SLOT;
 
-			if (old_slot > 0 && inventory().ItemFromSlot(old_slot) != nullptr)
-			{
-				saved_old_slot = inventory().ItemFromSlot(old_slot)->BaseSlot();
-				inventory().Activate(old_slot);
-				old_slot = 0;
-			}
+				if (HudAnimator()->SlotToRestore() > saved_old_slot)
+				{
+					old_slot = HudAnimator()->SlotToRestore();
+					HudAnimator()->SlotToRestore() = saved_old_slot;
+				}
 
-			bool bres = (saved_old_slot == NO_ACTIVE_SLOT || saved_old_slot == INV_SLOT_2 || saved_old_slot == PISTOL_SLOT_NEW || saved_old_slot == KNIFE_SLOT || saved_old_slot == BOLT_SLOT);
+				if (HudAnimator()->RestoreDevice())
+				{
+					need_restore_detector = true;
+					HudAnimator()->RestoreDevice() = false;
+				}
 
-			if (!bres)
-			{
-				need_restore_detector = false;
-			}
+				if (old_slot > 0 && inventory().ItemFromSlot(old_slot) != nullptr)
+				{
+					saved_old_slot = inventory().ItemFromSlot(old_slot)->BaseSlot();
+					inventory().Activate(old_slot);
+					old_slot = 0;
+				}
 
-			if (need_restore_detector && GetDevice(true) != nullptr)
-			{
-				need_restore_detector = false;
-				GetDevice(true)->switch_device();
+				bool bres = (saved_old_slot == NO_ACTIVE_SLOT || saved_old_slot == INV_SLOT_2 || saved_old_slot == PISTOL_SLOT_NEW || saved_old_slot == KNIFE_SLOT || saved_old_slot == BOLT_SLOT);
+
+				if (!bres)
+				{
+					need_restore_detector = false;
+				}
+
+				if (need_restore_detector && GetDevice(true) != nullptr)
+				{
+					need_restore_detector = false;
+					GetDevice(true)->switch_device();
+				}
 			}
-	
 		}
 	}
 
