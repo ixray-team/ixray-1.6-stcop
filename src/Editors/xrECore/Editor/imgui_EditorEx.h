5da138c9e75331871c58dd7b83004647ae2282df
xroo <xrooshka@gmail.com>
2026-03-01 01:08:29 +0100

    * A bit of polish in menu Bar and Tool Bar

--- src/Editors/xrECore/Editor/imgui_EditorEx.h
+++ src/Editors/xrECore/Editor/imgui_EditorEx.h
@@ -5,7 +5,7 @@ inline ECORE_API bool IconButton(
 	const char* id,
 	ImTextureRef texture,
 	ImDrawFlags rounding_flags = ImDrawFlags_RoundCornersAll,
-	float rounding = 6.0f, ImVec2 button_size = { 26.f,26.f }, ImVec2 image_size = { 18.0f, 18.0f });
+	float rounding = 6.0f, ImVec2 button_size = { 26.f,26.f }, ImVec2 image_size = { 20.0f, 20.0f });
 
 inline ECORE_API bool TextToggleButton(
 	const char* id,
