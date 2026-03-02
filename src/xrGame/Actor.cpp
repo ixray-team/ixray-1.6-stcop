3d4bf9ee0ac9ca2642e9e2b989c16e004b69018b
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-03-02 22:23:50 +0300

    Add ability to drag corpse with gamepad

--- src/xrGame/Actor.cpp
+++ src/xrGame/Actor.cpp
@@ -2428,7 +2428,7 @@ void CActor::shedule_Update	(u32 DT)
 								bool b_allow_drag = !!pSettings->line_exist("ph_capture_visuals", pEntityAlive->cNameVisual());
 								if (b_allow_drag)
 								{
-									m_sDefaultObjAction = m_sDeadCharacterUseOrDragAction;
+									m_sDefaultObjAction = pInput->GetControllerMode() ? m_sDeadCharacterUseOrDragActionGamepad : m_sDeadCharacterUseOrDragAction;
 								}
 								else
 								{
@@ -2445,7 +2445,7 @@ void CActor::shedule_Update	(u32 DT)
 							bool b_allow_drag = !!pSettings->line_exist("ph_capture_visuals", pEntityAlive->cNameVisual());
 							if (b_allow_drag)
 							{
-								m_sDefaultObjAction = m_sDeadCharacterUseOrDragAction;
+								m_sDefaultObjAction = pInput->GetControllerMode() ? m_sDeadCharacterUseOrDragActionGamepad : m_sDeadCharacterUseOrDragAction;
 							}
 							else if (pEntityAlive->cast_inventory_owner())
 							{
