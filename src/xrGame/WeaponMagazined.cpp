640d0a8639d1cbddbe36aa19e5d7ddb0079bad44
Shtrecker <gogels235@gmail.com>
2026-03-06 10:56:31 +0200

    Add firemode motion mark

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -1093,10 +1093,16 @@ void CWeaponMagazined::OnStateSwitch	(u32 S)
 		switch2_Showing	();
 		break;
 	case eHiding:
+	{
+		if (ParentIsActor() && GetCurrentFireMode() != GetQueueSize())
+		{
+			SetQueueSize(GetCurrentFireMode());
+		}
 		if (H_Parent() && H_Parent()->cast_inventory_owner())
 			m_sounds_enabled = H_Parent()->cast_inventory_owner()->CanPlayShHdRldSounds();
-		switch2_Hiding	();
+		switch2_Hiding();
 		break;
+	}
 	case eHidden:
 		switch2_Hidden	();
 		break;
@@ -1738,6 +1744,7 @@ void CWeaponMagazined::OnAnimationEnd(u32 state)
 		{
 			if (state == eSwitchMode)
 			{
+				SetQueueSize(GetCurrentFireMode());
 				UpdateFiremodeAnimations();
 			}
 			else if (state == eFire || state == eReload || state == eMisfire)
@@ -2093,13 +2100,13 @@ void CWeaponMagazined::switch2_FireMode()
 		anim_name.printf("%s%d_to_", *anim_name, m_iPrevFireMode);
 	}
 
-	if (GetQueueSize() == -1)
+	if (GetCurrentFireMode() == -1)
 	{
 		anim_name.printf("%s%s", *anim_name, "a");
 	}
 	else
 	{
-		anim_name.printf("%s%d", *anim_name, GetQueueSize());
+		anim_name.printf("%s%d", *anim_name, GetCurrentFireMode());
 	}
 
 	if (HudAnimationExist(anim_name))
@@ -3259,11 +3266,13 @@ void CWeaponMagazined::ChangeFireMode(u16 cmd)
 		m_iCurFireMode = (m_iCurFireMode - 1 + m_aFireModes.size()) % (s8)m_aFireModes.size();
 	}
 
-	SetQueueSize(GetCurrentFireMode());
-
 	if (m_eAnimationsFlags.test(EAnimationsFlags::af_firemode))
 	{
 		SwitchState(eSwitchMode);
+	}
+	else
+	{
+		SetQueueSize(GetCurrentFireMode());
 	}
 };
 
@@ -3653,6 +3662,11 @@ void CWeaponMagazined::OnMotionMark(u32 state, const motion_marks& mark)
 		}
 
 		m_eDevicesFlags.zero();
+	}
+
+	if (state == eSwitchMode && mark.name == "Right")
+	{
+		SetQueueSize(GetCurrentFireMode());
 	}
 }
 
