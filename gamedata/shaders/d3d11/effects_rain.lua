77538ea2b240df8e4f8b34de0b0653b913a9d434
ForserX <sv3nk@yandex.ru>
2026-03-06 18:38:27 +0300

    Вернул ебучий дождь

--- gamedata/shaders/d3d11/effects_rain.lua
+++ gamedata/shaders/d3d11/effects_rain.lua
@@ -1,4 +1,4 @@
-function normal(shader, t_base, t_second, t_detail)
+function витя_нормал(shader, t_base, t_second, t_detail)
     shader:begin("stub_default", "rain_test")
 
     :zb(true, false)
@@ -10,4 +10,15 @@ function normal(shader, t_base, t_second, t_detail)
 
     shader:dx10texture("s_depth", "$user$depth")
     shader:dx10sampler("smp_depth")
+end
+
+function normal(shader, t_base, t_second, t_detail)
+    shader:begin("stub_default", "stub_srgb")
+
+    :zb(true, false)
+	:blend(true, blend.srcalpha, blend.invsrcalpha)
+	:aref(true, 0)
+
+    shader:dx10texture("s_base", t_base)
+    shader:dx10sampler("smp_base")
 end
