a33b7250af79012ea69562aeeb1106865e837fff
xroo <xrooshka@gmail.com>
2026-03-08 00:58:49 +0100

    * Updated my tool a bit

--- src/Editors/LevelEditor/UI/UIWidgetsTest.h
+++ src/Editors/LevelEditor/UI/UIWidgetsTest.h
@@ -4,9 +4,15 @@ class CUIWidgetsTest : public IEditorWnd
 {
 private:
     CUIWidgetsTest();
+
+    void DemoWindowWidgetsButtons();
     void Draw() override;
 
 public:
+    void InitIcons();
+
+    xr_hash_map<xr_string, ref_texture> Icons;
+
     static CUIWidgetsTest& Instance();
     void Show(bool value);
 };
