43004566a2bfd463e748c31970f7108c681bdb18
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-03-07 21:06:32 +0300

    Sync shaders CS

--- gamedata_cs/shaders/d3d11/effects_rain.lua
+++ gamedata_cs/shaders/d3d11/effects_rain.lua
@@ -1,7 +1,21 @@
+function витя_нормал(shader, t_base, t_second, t_detail)
+    shader:begin("stub_default", "rain_test")
+
+    :zb(true, false)
+    :blend(true, blend.one, blend.invsrcalpha)
+    :aref(false, 0)
+
+    shader:dx10texture("s_base", t_base)
+    shader:dx10sampler("smp_base")
+
+    shader:dx10texture("s_depth", "$user$depth")
+    shader:dx10sampler("smp_depth")
+end
+
 function normal(shader, t_base, t_second, t_detail)
     shader:begin("stub_default", "stub_srgb")
-	
-	:zb(true, false)
+
+    :zb(true, false)
 	:blend(true, blend.srcalpha, blend.invsrcalpha)
 	:aref(true, 0)
 
