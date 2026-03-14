be041e4279e043af681d797321c385f8e3839e2e
LVutner <kkamil724@gmail.com>
2026-03-14 22:23:40 +0100

    * UPD: [Shaders] swizzlin

--- gamedata/shaders/d3d11/sslr_temporal.cs.hlsl
+++ gamedata/shaders/d3d11/sslr_temporal.cs.hlsl
@@ -69,9 +69,12 @@ float4 median9(float4 a1, float4 a2, float4 a3, float4 a4, float4 a5, float4 a6,
 
 RWTexture2D<float4> u_sslr : register(u0);
 
-[numthreads(8, 8, 1)]
-void main(uint3 DTid : SV_DispatchThreadID)
+[numthreads(64, 1, 1)]
+void main(uint2 Gid : SV_GroupID, uint GI : SV_GroupIndex)
 {
+    uint2 GTid = thread_remap_8x8(GI);
+    uint2 DTid = Gid * 8 + GTid;
+
 	//LVutner: Making my life easier.
 	PSInputFullscreen I;
 	I.hpos.xy = float2(DTid.xy) + 0.5; //half-pix
