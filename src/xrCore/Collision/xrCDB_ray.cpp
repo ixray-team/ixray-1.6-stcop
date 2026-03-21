242a5cfe516ff7a1bfff5a9c856386e50d61c78a
v2v3v4 <terran.erre@mail.ru>
2026-03-21 19:10:02 +0300

    расширение функционала трейсинга, новые плюшки для LevelInspector
    Добавлены новые способы захвата пересечений со статикой, один через сферу другой кастомная реализация в которой можно реализовать собственный методпоиска полигонов коллизии, убрана математика из функционала трейсинга в свои классы обёртки, прочие улучшения и фиксы.

--- src/xrCore/Collision/xrCDB_ray.cpp
+++ src/xrCore/Collision/xrCDB_ray.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "xrCDB.h"
 #include "override/Model.h"
-
+#include "cl_intersect.h"
 using namespace CDB;
 using namespace Opcode;
 
@@ -17,60 +17,14 @@ struct cform_ray_collider final
 	bool bFirst = false;
 	bool bNearest = false;
 
-	ICF bool _tri(Fvector* tri, float& u, float& v, float& range)
-	{
-		Fvector edge1, edge2, tvec, pvec, qvec;
-		float det,inv_det;
-		
-		// find vectors for two edges sharing vert0
-		const Fvector& p0 = tri[0];
-		const Fvector& p1 = tri[1];
-		const Fvector& p2 = tri[2];
-		edge1.sub(p1, p0);
-		edge2.sub(p2, p0);
-		// begin calculating determinant - also used to calculate U parameter
-		// if determinant is near zero, ray lies in plane of triangle
-		pvec.crossproduct(fwd_dir, edge2);
-		det = edge1.dotproduct(pvec);
-
-		if (bCull)
-		{						
-			if (det < EPS)  return false;
-			tvec.sub(pos, p0);						// calculate distance from vert0 to ray origin
-			u = tvec.dotproduct(pvec);					// calculate U parameter and test bounds
-			if (u < 0.f || u > det) return false;
-			qvec.crossproduct(tvec, edge1);				// prepare to test V parameter
-			v = fwd_dir.dotproduct(qvec);			// calculate V parameter and test bounds
-			if (v < 0.f || u + v > det) return false;
-			range = edge2.dotproduct(qvec);				// calculate t, scale parameters, ray intersects triangle
-			inv_det = 1.0f / det;
-			range	*= inv_det;
-			u		*= inv_det;
-			v		*= inv_det;
-		}
-		else
-		{			
-			if (det > -EPS && det < EPS) return false;
-			inv_det = 1.0f / det;
-			tvec.sub(pos, p0);						// calculate distance from vert0 to ray origin
-			u = tvec.dotproduct(pvec)*inv_det;			// calculate U parameter and test bounds
-			if (u < 0.0f || u > 1.0f)    return false;
-			qvec.crossproduct(tvec, edge1);				// prepare to test V parameter
-			v = fwd_dir.dotproduct(qvec)*inv_det;	// calculate V parameter and test bounds
-			if (v < 0.0f || u + v > 1.0f) return false;
-			range = edge2.dotproduct(qvec)*inv_det;		// calculate t, ray intersects triangle
-		}
-		return true;
-	}
-	
-	ICF void _prim(DWORD prim)
+	ICF void _prim(size_t prim)
 	{
 		float u,v,r;
 		auto& Tri = tris[prim];
 		auto& TriVerts = Tri.verts;
 		Fvector tri_verts[3] = { verts[TriVerts[0]], verts[TriVerts[1]], verts[TriVerts[2]] };
 
-		if (!_tri(tri_verts, u, v, r))
+		if (!TestRayTri(pos, fwd_dir, tri_verts, u, v, r, bCull))
 			return;
 
 		if (r<=0 || r>rRange)
@@ -129,9 +83,9 @@ struct cform_ray_collider final
 	{
 		Fvector& center = (Fvector&)node->mAABB.mCenter;
 		Fvector& extents = (Fvector&)node->mAABB.mExtents;
-		Fbox BB; BB.set(center-extents, center+extents);
+
 		Fvector P;
-		if (!BB.Pick2(pos, fwd_dir, P))
+		if (!Fbox(center-extents,center+extents).Pick2(pos, fwd_dir, P))
 			return;
 		
 		if (P.distance_to_sqr(pos) > rRange2)
@@ -157,11 +111,10 @@ struct cform_ray_collider final
 void COLLIDER::ray_query(const MODEL* m_def, const Fvector& r_start, const Fvector& r_dir, float r_range)
 {
 	PROF_EVENT("COLLIDER::ray_query");
-	m_def->wait_loading();
-
 	if (!m_def || m_def->tree == nullptr)
 		return;
 
+	m_def->wait_loading();
 	// Get nodes
 	const AABBNoLeafTree* T = (const AABBNoLeafTree*)m_def->tree->GetTree();
 	const AABBNoLeafNode* N = T->GetNodes();
