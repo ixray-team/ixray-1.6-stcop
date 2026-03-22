f297b18025f9d7cb7cf0fe21c26593ae5714c64c
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-03-22 11:32:03 +0300

    Fix HUD issues

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -3472,7 +3472,7 @@ bool CWeaponMagazined::GetBriefInfo( II_BriefInfo& info )
 
 	info.cur_ammo = int_str;
 
-	if (infinite_fire() || unlimited_ammo())
+	if (infinite_fire())
 	{
 		info.cur_ammo = "∞";
 	}
