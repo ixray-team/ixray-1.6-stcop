9019ffb2e63654b0702f8c1f88209ce3c8bdf58a
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-03-29 23:23:44 +0300

    Add SoC hands support

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -3872,7 +3872,7 @@ bool CWeapon::MovingAnimAllowedNow()
 
 bool CWeapon::IsHudModeNow()
 {
-	return !!GetHUDmode();
+	return !!GetHUDmode() && !HudItemData()->m_model_combined;
 }
 
 void CWeapon::ZoomInc()
