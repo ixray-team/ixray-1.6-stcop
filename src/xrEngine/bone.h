be663e8354c18967939189040c7fcdcc5f197e1d
v2v3v4 <terran.erre@mail.ru>
2026-03-30 23:14:47 +0300

    блоодмарки и кровавая резня скиннинга
    Теперь нужно подождать пока @Hozar2002 найдет время чтобы фиксануть  и сами шейдеры

--- src/xrEngine/bone.h
+++ src/xrEngine/bone.h
@@ -69,69 +69,6 @@ public:
 };
 #pragma pack(pop)
 
-#pragma pack( push,2 )
-struct ENGINE_API vertBoned1W			// (3+3+3+3+2+1)*4 = 15*4 = 60 bytes
-{
-	Fvector	P;
-	Fvector	N;
-	Fvector	T;
-	Fvector	B;
-	float	u,v;
-	u32		matrix;
-	void	get_pos( Fvector& p ) const { p.set(P); }
-#ifdef	DEBUG	
-	static const u8 bones_count = 1;
-	u16		get_bone_id(u8 bone)const{ VERIFY(bone<bones_count); return u16(matrix); }
-#endif
-};
-struct ENGINE_API vertBoned2W			// (1+3+3 + 1+3+3 + 2)*4 = 16*4 = 64 bytes
-{
-	u16		matrix0;
-	u16		matrix1;
-	Fvector	P;
-	Fvector	N;
-	Fvector	T;
-	Fvector	B;
-	float	w;
-	float	u,v;
-	void	get_pos( Fvector& p ) { p.set(P); }
-#ifdef	DEBUG
-	static const u8 bones_count = 2;
-	u16		get_bone_id(u8 bone)const{ VERIFY(bone<bones_count); return bone==0 ? matrix0 : matrix1; }
-#endif
-};
-struct ENGINE_API vertBoned3W          // 70 bytes
-{
-	u16		m		[3];
-	Fvector	P;
-	Fvector	N;
-	Fvector	T;
-	Fvector	B;
-	float	w		[2];
-	float	u,v;
-	void	get_pos( Fvector& p ) { p.set(P); }
-#ifdef	DEBUG
-	static const u8 bones_count = 3;
-	u16		get_bone_id(u8 bone)const{ VERIFY(bone<bones_count); return m[bone]; }
-#endif
-};
-struct ENGINE_API vertBoned4W       //76 bytes
-{
-	u16		m		[4];
-	Fvector	P;
-	Fvector	N;
-	Fvector	T;
-	Fvector	B;
-	float	w		[3];
-	float	u,v;
-	void	get_pos( Fvector& p ) { p.set(P); }
-#ifdef	DEBUG
-	static const u8 bones_count = 4;
-	u16		get_bone_id(u8 bone)const{ VERIFY(bone<bones_count); return m[bone]; }
-#endif
-};
-#pragma pack(pop)
-
 #pragma pack( push,1 )
 enum EJointType
 {
--- src/xrGame/Level_bullet_manager_firetrace.cpp
+++ src/xrGame/Level_bullet_manager_firetrace.cpp
@@ -189,7 +189,6 @@ void CBulletManager::FireShotmark (SBullet* bullet, const Fvector& vDir, const F
 
 	if (R.O)
 	{
-/*  add_SkeletonWallmark not implemented now...
 		particle_dir		 = vDir;
 		particle_dir.invert	();
 
@@ -209,7 +208,7 @@ void CBulletManager::FireShotmark (SBullet* bullet, const Fvector& vDir, const F
 													bullet->dir, 
 													bullet->wallmark_size);
 		}
-*/
+
 	} 
 	else 
 	{
