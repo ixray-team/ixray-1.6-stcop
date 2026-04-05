21029ab74f4a2ebe06493b6afde289a34be587a4
GladiatorVS <gladiatorvsoff@gmail.com>
2026-04-05 16:05:42 +0300

    Gamepad buttons defines

---
+++ src/xrUI/UIGamepadButtons.h
@@ -0,0 +1,271 @@
+/*
+
+HOW TO SEARCH IN FONTFORGE:
+View->Goto and U+ICONNAME
+
+EXAMPLE: 
+#define XBOX_Start	ICON_E006 -> GOTO U+E006
+
+*/
+
+
+/*
+SOURCE
+*/
+
+#define ICON_E000 "\xEE\x80\x80"
+#define ICON_E001 "\xEE\x80\x81"
+#define ICON_E002 "\xEE\x80\x82"
+#define ICON_E003 "\xEE\x80\x83"
+#define ICON_E004 "\xEE\x80\x84"
+#define ICON_E005 "\xEE\x80\x85"
+#define ICON_E006 "\xEE\x80\x86"
+#define ICON_E007 "\xEE\x80\x87"
+#define ICON_E008 "\xEE\x80\x88"
+#define ICON_E009 "\xEE\x80\x89"
+#define ICON_E00A "\xEE\x80\x8A"
+#define ICON_E00B "\xEE\x80\x8B"
+#define ICON_E00C "\xEE\x80\x8C"
+#define ICON_E00D "\xEE\x80\x8D"
+#define ICON_E00E "\xEE\x80\x8E"
+#define ICON_E00F "\xEE\x80\x8F"
+
+#define ICON_E010 "\xEE\x80\x90"
+#define ICON_E011 "\xEE\x80\x91"
+#define ICON_E012 "\xEE\x80\x92"
+#define ICON_E013 "\xEE\x80\x93"
+#define ICON_E014 "\xEE\x80\x94"
+#define ICON_E015 "\xEE\x80\x95"
+#define ICON_E016 "\xEE\x80\x96"
+#define ICON_E017 "\xEE\x80\x97"
+#define ICON_E018 "\xEE\x80\x98"
+#define ICON_E019 "\xEE\x80\x99"
+#define ICON_E01A "\xEE\x80\x9A"
+#define ICON_E01B "\xEE\x80\x9B"
+#define ICON_E01C "\xEE\x80\x9C"
+#define ICON_E01D "\xEE\x80\x9D"
+#define ICON_E01E "\xEE\x80\x9E"
+#define ICON_E01F "\xEE\x80\x9F"
+
+#define ICON_E020 "\xEE\x80\xA0"
+#define ICON_E021 "\xEE\x80\xA1"
+#define ICON_E022 "\xEE\x80\xA2"
+#define ICON_E023 "\xEE\x80\xA3"
+#define ICON_E024 "\xEE\x80\xA4"
+#define ICON_E025 "\xEE\x80\xA5"
+#define ICON_E026 "\xEE\x80\xA6"
+#define ICON_E027 "\xEE\x80\xA7"
+#define ICON_E028 "\xEE\x80\xA8"
+#define ICON_E029 "\xEE\x80\xA9"
+#define ICON_E02A "\xEE\x80\xAA"
+#define ICON_E02B "\xEE\x80\xAB"
+#define ICON_E02C "\xEE\x80\xAC"
+#define ICON_E02D "\xEE\x80\xAD"
+#define ICON_E02E "\xEE\x80\xAE"
+#define ICON_E02F "\xEE\x80\xAF"
+
+#define ICON_E030 "\xEE\x80\xB0"
+#define ICON_E031 "\xEE\x80\xB1"
+#define ICON_E032 "\xEE\x80\xB2"
+#define ICON_E033 "\xEE\x80\xB3"
+#define ICON_E034 "\xEE\x80\xB4"
+#define ICON_E035 "\xEE\x80\xB5"
+#define ICON_E036 "\xEE\x80\xB6"
+#define ICON_E037 "\xEE\x80\xB7"
+#define ICON_E038 "\xEE\x80\xB8"
+#define ICON_E039 "\xEE\x80\xB9"
+#define ICON_E03A "\xEE\x80\xBA"
+#define ICON_E03B "\xEE\x80\xBB"
+#define ICON_E03C "\xEE\x80\xBC"
+#define ICON_E03D "\xEE\x80\xBD"
+#define ICON_E03E "\xEE\x80\xBE"
+#define ICON_E03F "\xEE\x80\xBF"
+
+#define ICON_E040 "\xEE\x81\x80"
+#define ICON_E041 "\xEE\x81\x81"
+#define ICON_E042 "\xEE\x81\x82"
+#define ICON_E043 "\xEE\x81\x83"
+#define ICON_E044 "\xEE\x81\x84"
+#define ICON_E045 "\xEE\x81\x85"
+#define ICON_E046 "\xEE\x81\x86"
+#define ICON_E047 "\xEE\x81\x87"
+#define ICON_E048 "\xEE\x81\x88"
+#define ICON_E049 "\xEE\x81\x89"
+#define ICON_E04A "\xEE\x81\x8A"
+#define ICON_E04B "\xEE\x81\x8B"
+#define ICON_E04C "\xEE\x81\x8C"
+#define ICON_E04D "\xEE\x81\x8D"
+#define ICON_E04E "\xEE\x81\x8E"
+#define ICON_E04F "\xEE\x81\x8F"
+
+#define ICON_E050 "\xEE\x81\x90"
+#define ICON_E051 "\xEE\x81\x91"
+#define ICON_E052 "\xEE\x81\x92"
+#define ICON_E053 "\xEE\x81\x93"
+#define ICON_E054 "\xEE\x81\x94"
+#define ICON_E055 "\xEE\x81\x95"
+#define ICON_E056 "\xEE\x81\x96"
+#define ICON_E057 "\xEE\x81\x97"
+#define ICON_E058 "\xEE\x81\x98"
+#define ICON_E059 "\xEE\x81\x99"
+#define ICON_E05A "\xEE\x81\x9A"
+#define ICON_E05B "\xEE\x81\x9B"
+#define ICON_E05C "\xEE\x81\x9C"
+#define ICON_E05D "\xEE\x81\x9D"
+#define ICON_E05E "\xEE\x81\x9E"
+#define ICON_E05F "\xEE\x81\x9F"
+
+#define ICON_E060 "\xEE\x81\xA0"
+#define ICON_E061 "\xEE\x81\xA1"
+#define ICON_E062 "\xEE\x81\xA2"
+#define ICON_E063 "\xEE\x81\xA3"
+#define ICON_E064 "\xEE\x81\xA4"
+#define ICON_E065 "\xEE\x81\xA5"
+#define ICON_E066 "\xEE\x81\xA6"
+#define ICON_E067 "\xEE\x81\xA7"
+#define ICON_E068 "\xEE\x81\xA8"
+#define ICON_E069 "\xEE\x81\xA9"
+#define ICON_E06A "\xEE\x81\xAA"
+#define ICON_E06B "\xEE\x81\xAB"
+#define ICON_E06C "\xEE\x81\xAC"
+#define ICON_E06D "\xEE\x81\xAD"
+#define ICON_E06E "\xEE\x81\xAE"
+#define ICON_E06F "\xEE\x81\xAF"
+
+#define ICON_E070 "\xEE\x81\xB0"
+#define ICON_E071 "\xEE\x81\xB1"
+#define ICON_E072 "\xEE\x81\xB2"
+#define ICON_E073 "\xEE\x81\xB3"
+#define ICON_E074 "\xEE\x81\xB4"
+#define ICON_E075 "\xEE\x81\xB5"
+#define ICON_E076 "\xEE\x81\xB6"
+#define ICON_E077 "\xEE\x81\xB7"
+#define ICON_E078 "\xEE\x81\xB8"
+#define ICON_E079 "\xEE\x81\xB9"
+#define ICON_E07A "\xEE\x81\xBA"
+#define ICON_E07B "\xEE\x81\xBB"
+#define ICON_E07C "\xEE\x81\xBC"
+#define ICON_E07D "\xEE\x81\xBD"
+#define ICON_E07E "\xEE\x81\xBE"
+#define ICON_E07F "\xEE\x81\xBF"
+
+#define ICON_E080 "\xEE\x82\x80"
+#define ICON_E081 "\xEE\x82\x81"
+#define ICON_E082 "\xEE\x82\x82"
+#define ICON_E083 "\xEE\x82\x83"
+#define ICON_E084 "\xEE\x82\x84"
+#define ICON_E085 "\xEE\x82\x85"
+#define ICON_E086 "\xEE\x82\x86"
+#define ICON_E087 "\xEE\x82\x87"
+#define ICON_E088 "\xEE\x82\x88"
+#define ICON_E089 "\xEE\x82\x89"
+#define ICON_E08A "\xEE\x82\x8A"
+#define ICON_E08B "\xEE\x82\x8B"
+#define ICON_E08C "\xEE\x82\x8C"
+#define ICON_E08D "\xEE\x82\x8D"
+#define ICON_E08E "\xEE\x82\x8E"
+#define ICON_E08F "\xEE\x82\x8F"
+
+#define ICON_E090 "\xEE\x82\x90"
+#define ICON_E091 "\xEE\x82\x91"
+#define ICON_E092 "\xEE\x82\x92"
+#define ICON_E093 "\xEE\x82\x93"
+#define ICON_E094 "\xEE\x82\x94"
+#define ICON_E095 "\xEE\x82\x95"
+#define ICON_E096 "\xEE\x82\x96"
+#define ICON_E097 "\xEE\x82\x97"
+#define ICON_E098 "\xEE\x82\x98"
+#define ICON_E099 "\xEE\x82\x99"
+#define ICON_E09A "\xEE\x82\x9A"
+#define ICON_E09B "\xEE\x82\x9B"
+#define ICON_E09C "\xEE\x82\x9C"
+#define ICON_E09D "\xEE\x82\x9D"
+#define ICON_E09E "\xEE\x82\x9E"
+#define ICON_E09F "\xEE\x82\x9F"
+
+#define ICON_E0A0 "\xEE\x82\xA0"
+#define ICON_E0A1 "\xEE\x82\xA1"
+#define ICON_E0A2 "\xEE\x82\xA2"
+#define ICON_E0A3 "\xEE\x82\xA3"
+#define ICON_E0A4 "\xEE\x82\xA4"
+#define ICON_E0A5 "\xEE\x82\xA5"
+#define ICON_E0A6 "\xEE\x82\xA6"
+#define ICON_E0A7 "\xEE\x82\xA7"
+#define ICON_E0A8 "\xEE\x82\xA8"
+#define ICON_E0A9 "\xEE\x82\xA9"
+#define ICON_E0AA "\xEE\x82\xAA"
+#define ICON_E0AB "\xEE\x82\xAB"
+#define ICON_E0AC "\xEE\x82\xAC"
+#define ICON_E0AD "\xEE\x82\xAD"
+#define ICON_E0AE "\xEE\x82\xAE"
+#define ICON_E0AF "\xEE\x82\xAF"
+
+#define ICON_E0B0 "\xEE\x82\xB0"
+#define ICON_E0B1 "\xEE\x82\xB1"
+#define ICON_E0B2 "\xEE\x82\xB2"
+#define ICON_E0B3 "\xEE\x82\xB3"
+#define ICON_E0B4 "\xEE\x82\xB4"
+#define ICON_E0B5 "\xEE\x82\xB5"
+#define ICON_E0B6 "\xEE\x82\xB6"
+#define ICON_E0B7 "\xEE\x82\xB7"
+#define ICON_E0B8 "\xEE\x82\xB8"
+#define ICON_E0B9 "\xEE\x82\xB9"
+#define ICON_E0BA "\xEE\x82\xBA"
+#define ICON_E0BB "\xEE\x82\xBB"
+#define ICON_E0BC "\xEE\x82\xBC"
+#define ICON_E0BD "\xEE\x82\xBD"
+#define ICON_E0BE "\xEE\x82\xBE"
+#define ICON_E0BF "\xEE\x82\xBF"
+
+/* ============== XBOX ============== */
+
+#define XBOX_A				ICON_E000
+#define XBOX_B				ICON_E001
+#define XBOX_X				ICON_E002
+#define XBOX_Y				ICON_E003
+#define XBOX_Back			ICON_E004
+#define XBOX_Guide			ICON_E005 // used for Xbox Game Bar
+#define XBOX_Start			ICON_E006
+#define XBOX_LS 			ICON_E007
+#define XBOX_RS  			ICON_E008
+#define XBOX_LB  			ICON_E009
+#define XBOX_RB  			ICON_E00A
+//KPECTOBuHA
+#define XBOX_DPAD_UP  		ICON_E00B
+#define XBOX_DPAD_DOWN  	ICON_E00C
+#define XBOX_DPAD_LEFT  	ICON_E00D
+#define XBOX_DPAD_RIGHT  	ICON_E00E
+//RESERVED
+#define XBOX_RESERVED_00  	ICON_E00F //15
+#define XBOX_RESERVED_01  	ICON_E010 //16
+#define XBOX_RESERVED_02  	ICON_E011 //17
+#define XBOX_RESERVED_03  	ICON_E012 //18
+#define XBOX_RESERVED_04  	ICON_E013 //19
+#define XBOX_RESERVED_05  	ICON_E014 //20
+#define XBOX_RESERVED_06  	ICON_E015 //21
+#define XBOX_RESERVED_07  	ICON_E016 //22
+#define XBOX_RESERVED_08  	ICON_E017 //23
+#define XBOX_RESERVED_09  	ICON_E018 //24
+#define XBOX_RESERVED_10  	ICON_E019 //25
+//CTuK LEFT
+#define XBOX_LSTICK_UP  	ICON_E01A
+#define XBOX_LSTICK_DOWN  	ICON_E01B
+#define XBOX_LSTICK_LEFT  	ICON_E01C
+#define XBOX_LSTICK_RIGHT  	ICON_E01D
+//CTuK RIGHT
+#define XBOX_RSTICK_UP  	ICON_E01E
+#define XBOX_RSTICK_DOWN  	ICON_E01F
+#define XBOX_RSTICK_LEFT  	ICON_E020
+#define XBOX_RSTICK_RIGHT  	ICON_E021
+
+/* ============== PS4 ============== */
+
+//<...>
+
+/* ============== PS5 ============== */
+
+//<...>
+
+/* ============== NINTENDO ============== */
+
+//<...>
