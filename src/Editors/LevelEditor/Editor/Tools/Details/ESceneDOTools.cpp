54a0f454e8eaefc707f6b0b0522f54840d315a63
Drombeys <dima.zolotov02@gmail.com>
2026-04-05 17:36:45 +0300

    Extend limits for detail texture atlas generation
    Лучше улучшить тут логирование

--- src/Editors/LevelEditor/Editor/Tools/Details/ESceneDOTools.cpp
+++ src/Editors/LevelEditor/Editor/Tools/Details/ESceneDOTools.cpp
@@ -714,7 +714,7 @@ bool EDetailManager::Export(LPCSTR path)
 	    	*remap_object_it	= (u8)new_idx++;
 
     xr_string 			do_tex_name = ChangeFileExt(fn,"_details");
-    int res = ImageLib.CreateMergedTexture(textures, do_tex_name.c_str(), STextureParams::tfDXT5, 256, 2048, 256, 2048, offsets, scales, rotated, remap);
+    int res = ImageLib.CreateMergedTexture(textures, do_tex_name.c_str(), STextureParams::tfDXT5, 256, 8192, 256, 8192, offsets, scales, rotated, remap);
     if (1!=res)			bRes=FALSE;
 
     pb->Inc				("export geometry");
