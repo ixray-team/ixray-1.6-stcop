12e1a19d7c5dc6646329571900f9499a5c843bfb
ForserX <sv3nk@yandex.ru>
2026-04-11 18:52:48 +0300

    Очистка (опять)

--- src/xrGame/artefact_script.cpp
+++ src/xrGame/artefact_script.cpp
@@ -3,16 +3,9 @@
 #include "Artefact.h"
 #include "MercuryBall.h"
 #include "GraviArtifact.h"
-#include "BlackDrops.h"
 #include "BastArtifact.h"
 #include "BlackGraviArtifact.h"
-#include "DummyArtifact.h"
-#include "ZudaArtifact.h"
-#include "ThornArtifact.h"
-#include "FadedBall.h"
 #include "ElectricBall.h"
-#include "RustyHairArtifact.h"
-#include "GalantineArtifact.h"
 #include "cta_game_artefact.h"
 
 using namespace luabind;
@@ -22,23 +15,33 @@ void CArtefact::script_register(lua_State *L)
 {
 	module(L)
 	[
-		class_<CArtefact			,CGameObject>("CArtefact")
-		.def(						constructor<>() )
-		.def("FollowByPath",		&CArtefact::FollowByPath)
-		.def("SwitchVisibility",	&CArtefact::SwitchVisibility)
-		.def("GetAfRank",			&CArtefact::GetAfRank),
+		class_<CArtefact ,CGameObject>("CArtefact")
+			.def(constructor<>())
+			.def("FollowByPath", &CArtefact::FollowByPath)
+			.def("SwitchVisibility", &CArtefact::SwitchVisibility)
+			.def("GetAfRank", &CArtefact::GetAfRank),
 
-		class_<CMercuryBall			,CArtefact>("CMercuryBall").def		(constructor<>()),
-		class_<CBlackDrops			,CArtefact>("CBlackDrops").def		(constructor<>()),
+		class_<CMercuryBall			,CArtefact>("CMercuryBall").def(constructor<>()),
 		class_<CBlackGraviArtefact	,CArtefact>("CBlackGraviArtefact").def(constructor<>()),
-		class_<CBastArtefact		,CArtefact>("CBastArtefact").def		(constructor<>()),
-		class_<CDummyArtefact		,CArtefact>("CDummyArtefact").def		(constructor<>()),
-		class_<CZudaArtefact		,CArtefact>("CZudaArtefact").def		(constructor<>()),
-		class_<CThornArtefact		,CArtefact>("CThornArtefact").def		(constructor<>()),
-		class_<CFadedBall			,CArtefact>("CFadedBall").def			(constructor<>()),
-		class_<CElectricBall		,CArtefact>("CElectricBall").def		(constructor<>()),
-		class_<CRustyHairArtefact	,CArtefact>("CRustyHairArtefact").def	(constructor<>()),
-		class_<CGalantineArtefact	,CArtefact>("CGalantineArtefact").def	(constructor<>()),
-		class_<CGraviArtefact		,CArtefact>("CGraviArtefact").def		(constructor<>())
+		class_<CBastArtefact		,CArtefact>("CBastArtefact").def(constructor<>()),
+		class_<CElectricBall		,CArtefact>("CElectricBall").def(constructor<>()),
+		class_<CGraviArtefact		,CArtefact>("CGraviArtefact").def(constructor<>())
 	];
+
+	luabind::object artefact_class = luabind::get_globals(L)["CArtefact"];
+
+	auto MakeAliasLambda = [&artefact_class, L](const char* TypeID)
+	{
+		lua_pushstring(L, TypeID);
+		artefact_class.pushvalue();
+		lua_settable(L, LUA_GLOBALSINDEX);
+	};
+
+	MakeAliasLambda("CDummyArtefact");
+	MakeAliasLambda("CZudaArtefact");
+	MakeAliasLambda("CThornArtefact");
+	MakeAliasLambda("CRustyHairArtefact");
+	MakeAliasLambda("CFadedBall");
+	MakeAliasLambda("CBlackDrops");
+	MakeAliasLambda("CGalantineArtefact");
 }
