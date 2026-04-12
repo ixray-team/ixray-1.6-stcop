f178357e9ff4096929f9e1081d39e9c1ba52ece0
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-04-12 15:37:01 +0300

    для упрощения отладки шрифтов

--- cmake/modules/FreeType.cmake
+++ cmake/modules/FreeType.cmake
@@ -1,41 +1,33 @@
 include(FetchContent)
 
-if(WIN32)
-    set(ENGINE_FREETYPE ${CMAKE_BINARY_DIR}/packages/ImeSense.Packages.FreeType.2.13.2/)
-    add_imported_lib(
-        FreeType::FreeType
-        "${ENGINE_FREETYPE}native/include/"
-        "${ENGINE_FREETYPE}native/lib/${CMAKE_VS_PLATFORM_NAME}/Release/freetype.lib"
-        "${ENGINE_FREETYPE}native/bin/${CMAKE_VS_PLATFORM_NAME}/Release/freetype.dll"
-    )
+if(PkgConfig_FOUND)
+	pkg_check_modules(FREETYPE QUIET freetype2)
+endif()
+
+if(FREETYPE_FOUND)
+	add_library(FreeType::FreeType INTERFACE IMPORTED)
+	set_target_properties(FreeType::FreeType PROPERTIES
+		INTERFACE_INCLUDE_DIRECTORIES "${FREETYPE_INCLUDE_DIRS}"
+		INTERFACE_LINK_LIBRARIES "${FREETYPE_LIBRARIES}"
+	)
 else()
-    if(PkgConfig_FOUND)
-        pkg_check_modules(FREETYPE QUIET freetype2)
-    endif()
+	set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+	FetchContent_Declare(
+		freetype
+		GIT_REPOSITORY https://gitlab.freedesktop.org/freetype/freetype.git
+		GIT_TAG VER-2-14-3
+	)
+	FetchContent_MakeAvailable(freetype)
 
-    if(FREETYPE_FOUND)
-        add_library(FreeType::FreeType INTERFACE IMPORTED)
-        set_target_properties(FreeType::FreeType PROPERTIES
-            INTERFACE_INCLUDE_DIRECTORIES "${FREETYPE_INCLUDE_DIRS}"
-            INTERFACE_LINK_LIBRARIES "${FREETYPE_LIBRARIES}"
-        )
-    else()
-        set(CMAKE_POSITION_INDEPENDENT_CODE ON)
-        FetchContent_Declare(
-            freetype
-            GIT_REPOSITORY https://gitlab.freedesktop.org/freetype/freetype.git
-            GIT_TAG VER-2-13-2
-        )
-        FetchContent_MakeAvailable(freetype)
+	set_target_properties(freetype PROPERTIES FOLDER "3rd Party")
 
-        if(TARGET freetype)
-            add_library(FreeType::FreeType ALIAS freetype)
-        elseif(NOT TARGET FreeType::FreeType)
-            add_library(FreeType::FreeType INTERFACE IMPORTED)
-            set_target_properties(FreeType::FreeType PROPERTIES
-                INTERFACE_INCLUDE_DIRECTORIES "${freetype_SOURCE_DIR}/include"
-                INTERFACE_LINK_LIBRARIES "freetype"
-            )
-        endif()
-    endif()
+	if(TARGET freetype)
+		add_library(FreeType::FreeType ALIAS freetype)
+	elseif(NOT TARGET FreeType::FreeType)
+		add_library(FreeType::FreeType INTERFACE IMPORTED)
+		set_target_properties(FreeType::FreeType PROPERTIES
+			INTERFACE_INCLUDE_DIRECTORIES "${freetype_SOURCE_DIR}/include"
+			INTERFACE_LINK_LIBRARIES "freetype"
+		)
+	endif()
 endif()
