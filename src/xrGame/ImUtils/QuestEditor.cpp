8ccab4df4572b62d744efdb305f6ab7dcbd8e4e1
wh1t3lord <git.matvey@yandex.ru>
2026-04-18 12:22:04 +0300

    ADD: Quest Editor;

--- src/xrGame/ImUtils/QuestEditor.cpp
+++ src/xrGame/ImUtils/QuestEditor.cpp
@@ -20,7 +20,6 @@
 
 struct CImGuiQuestEditor
 {
-
 };
 
 CImGuiQuestEditor* g_pQuestEditor = nullptr;
@@ -31,7 +30,7 @@ void RequestHandler_QuestEditor(const SRequestData& req)
 		return;
 
 	R_ASSERT2(
-		static_cast<eImGuiEditorType>(req.editor_type) == eImGuiEditorType::kQuestEditor, 
+		static_cast<eImGuiEditorType>(req.editor_type) == eImGuiEditorType::kQuestEditor,
 		"invalid data came you should debug code"
 	);
 
@@ -52,6 +51,15 @@ void RequestHandler_QuestEditor(const SRequestData& req)
 	{
 		break;
 	}
+	case eRequestType_QuestEditor::kDeselectCurrentSelectedOrHideWindow:
+	{
+		if (g_pQuestEditor)
+		{
+			Engine.External.EditorStates[static_cast<u8>(EditorUI::Tools_QuestEditor)] = false;
+		}
+
+		break;
+	}
 	case eRequestType_QuestEditor::kShutdown:
 	{
 		if (g_pQuestEditor)
@@ -67,6 +75,24 @@ void RequestHandler_QuestEditor(const SRequestData& req)
 
 void QuestEditor_OnPressed(int key)
 {
+	switch (key)
+	{
+	case SDL_Scancode::SDL_SCANCODE_ESCAPE:
+	{
+		if (Engine.External.EditorStates[static_cast<u8>(EditorUI::Tools_QuestEditor)])
+		{
+			if (g_pQuestEditor)
+			{
+				SRequestData req;
+				req.editor_type = (u32)eImGuiEditorType::kQuestEditor;
+				req.request_type = (u32)eRequestType_QuestEditor::kDeselectCurrentSelectedOrHideWindow;
+
+				AllEditors_SendRequest(req);
+			}
+		}
+		break;
+	}
+	}
 }
 
 void QuestEditor_OnReleased(int key)
@@ -80,6 +106,46 @@ void InitImGuiQuestEditor()
 void DestroyQuestEditorWindow()
 {
 
+}
+
+void RenderQuestEditor_Draw_QuestList()
+{
+	if (g_pQuestEditor)
+	{
+		ImGui::TableNextColumn();
+
+		ImGui::Text("Quest!");
+
+	}
+}
+
+void RenderQuestEditor_Draw_NodeEd()
+{
+	if (g_pQuestEditor)
+	{
+		ImGui::TableNextColumn();
+
+		ImGui::Text("Node");
+
+	}
+}
+
+void RenderQuestEditor_Draw_Headings(const char* const* pNames, unsigned char total_count)
+{
+	if (pNames == nullptr)
+		return;
+
+	if (total_count == 0)
+		return;
+
+	for (unsigned char i = 0; i < total_count; ++i)
+	{
+		const char* pName = pNames[i];
+
+		ImGui::TableSetupColumn(pName);
+	}
+
+	ImGui::TableHeadersRow();
 }
 
 void RenderQuestEditor()
@@ -101,4 +167,36 @@ void RenderQuestEditor()
 		g_pQuestEditor = new CImGuiQuestEditor();
 	}
 
+
+	if (g_pQuestEditor)
+	{
+		constexpr const char* _kQuestEditor_TableHeadings[] = {
+			"Locations",
+			"Node editor"
+		};
+
+		constexpr unsigned char _kQuestEditor_TableHeadingsCount = static_cast<unsigned char>(sizeof(_kQuestEditor_TableHeadings)) / static_cast<unsigned char>(sizeof(_kQuestEditor_TableHeadings[0]));
+
+
+		if (ImGui::Begin("Quest Editor"))
+		{
+			if (ImGui::BeginTable("##QE_Table", _kQuestEditor_TableHeadingsCount))
+			{
+				RenderQuestEditor_Draw_Headings(
+					_kQuestEditor_TableHeadings,
+					_kQuestEditor_TableHeadingsCount
+				);
+
+				ImGui::TableNextRow();
+
+				RenderQuestEditor_Draw_QuestList();
+
+				RenderQuestEditor_Draw_NodeEd();
+
+				ImGui::EndTable();
+			}
+		}
+
+		ImGui::End();
+	}
 }
