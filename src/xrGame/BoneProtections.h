c58da07345c68f26d3d3c1818f6ae82b5cf6d62b
Shtrecker <gogels235@gmail.com>
2026-04-19 12:27:53 +0300

    Clear & Refactor

--- src/xrGame/BoneProtections.h
+++ src/xrGame/BoneProtections.h
@@ -31,15 +31,15 @@ struct SBoneProtections final
     float m_fHitFrac{ 0.1f };
     HitFractionType m_hitFracType{ HitFractionNPC };
 
-	typedef xr_map<s16,BoneProtection>		storage_type;
-	typedef storage_type::iterator	storage_it;
+	using storage_type = xr_map<u16, BoneProtection>;
+	using storage_it = storage_type::iterator;
 						SBoneProtections	()								{m_default.koeff = 1.0f; m_default.armor = 0; m_fHitFrac = 0.1f; }
 	BoneProtection		m_default;
 	storage_type		m_bones_koeff;
 	void				reload				(const shared_str& outfit_section, IKinematics* kinematics);
 	void				add					(const shared_str& outfit_section, IKinematics* kinematics);
-	float				getBoneProtection	(s16 bone_id);
-	float				getBoneArmor		(s16 bone_id);
-	bool				getBonePassBullet	(s16 bone_id);
+	float				getBoneProtection	(u16 bone_id);
+	float				getBoneArmor		(u16 bone_id);
+	bool				getBonePassBullet	(u16 bone_id);
 };
 
