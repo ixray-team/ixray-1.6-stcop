a71b92216d2c28f9ba73a6f3a05a7db3db6201d2
v2v3v4 <terran.erre@mail.ru>
2026-04-19 19:56:43 +0300

    максимальный жмых для травы на DX11 без полного перепила CDetailManager
    r__fast_details_update теперь не только ускоряет появление травинок а так же включает полную отложенную отправку инстансов травинок на GPU

--- src/Layers/xrRender/DetailModel.h
+++ src/Layers/xrRender/DetailModel.h
@@ -15,17 +15,18 @@ public:
 	};
 	
 #ifdef USE_DX11
-	ref_geom					hw_Geom;
-	IRHIBuffer*			hw_VB;
-	IRHIBuffer*			hw_IB;
+	ref_geom hw_Geom;
+	IRHIBuffer* hw_VB;
+	IRHIBuffer* hw_IB;
+	std::pair<IRHIBuffer*, IRHIShaderResourceView*> DetailGPUBoundBuffers[2][3];
 #endif
 	
-	xr_vector<SlotItem> m_items[3][2];
-	void			Load		(IReader* S);
-	void			Optimize	();
-	virtual void	Unload		();
+	xr_vector<SlotItem> m_items[2][3];
+	void Load(IReader* S);
+	void Optimize();
+	virtual void Unload();
 
-	virtual void	transfer	(Fmatrix& mXform, fvfVertexOut* vDest, u32 C, u16* iDest, u32 iOffset);
-	virtual void	transfer	(Fmatrix& mXform, fvfVertexOut* vDest, u32 C, u16* iDest, u32 iOffset, float du, float dv);
-	virtual			~CDetail	();
+	virtual void transfer(Fmatrix& mXform, fvfVertexOut* vDest, u32 C, u16* iDest, u32 iOffset);
+	virtual void transfer(Fmatrix& mXform, fvfVertexOut* vDest, u32 C, u16* iDest, u32 iOffset, float du, float dv);
+	virtual ~CDetail();
 };
