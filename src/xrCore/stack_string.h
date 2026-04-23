3a526baa1daae7b6eb302c131176f0297847a7c6
wh1t3lord <git.matvey@yandex.ru>
2026-04-23 22:21:16 +0300

    ADD: fixing compilation on linux;

--- src/xrCore/stack_string.h
+++ src/xrCore/stack_string.h
@@ -527,8 +527,9 @@ IC void xr_strlwr(xr_stack_string<StringLength>& src)
 	}
 }
 
+#ifdef IXR_WINDOWS
 template<xr_ssnt_t StringLength>
-IC std::enable_if_t<std::is_same_v<xr_char_t, wchar_t> || std::is_same_v<xr_char_t, char16_t>, void>
+IC std::enable_if_t<std::is_same_v<xr_char_t, wchar_t>, void>
 xr_strlwr(xr_stack_wstring<StringLength>& src)
 {
 	for (auto& Char : src)
@@ -536,3 +537,14 @@ xr_strlwr(xr_stack_wstring<StringLength>& src)
 		Char = std::towlower(Char);
 	}
 }
+#elif defined(IXR_LINUX)
+template<xr_ssnt_t StringLength>
+IC std::enable_if_t<std::is_same_v<xr_char_t, char16_t>, void>
+xr_strlwr(xr_stack_wstring<StringLength>& src)
+{
+	for (auto& Char : src)
+	{
+		Char = std::towlower(Char);
+	}
+}
+#endif
