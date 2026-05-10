a828688d992d03dd45423041565da6974bcb32a4
FFx0001 <vip.ffx0001@mail.ru>
2026-05-10 23:19:16 +0300

    use_electric_curve = true cascade_curves = true cascade_curves_by_anomalies = true max_count_electric_curves = 6 max_trace_curve_distance = 12;
    max_curve_damage = 0.025
    max_curve_impulse = 0.12
    
    electric_curve_particle_path = ffx0001\curve
    
    electric_core_target_damage = FFx0001\electra\electric_core_target_damage
    electric_core_loop = FFx0001\electra\electric_core_loop
    electric_curve_start = FFx0001\electra\electric_curve_start
    electric_curve_loop = FFx0001\electra\electric_curve_loop
    electric_curve_end = FFx0001\electra\electric_curve_end
    
    draw_debug = false
    use_movement = true
    max_processing_distance = 200
    movement_speed = 1.2
    movement_radius = 30
    use_movement_always_mode = true
    use_movement_magnetic_on_inside_alive_mode = false
    movement_magnetic_on_inside_alive_mode_speed = 4
    use_movement_magnetic_on_take_artefacts_mode = false
    milliseconds_time_magnetic_on_take_artefacts = 10000
    movement_magnetic_on_take_artefacts_mode_speed = 3

---
+++ src/xrGame/ElectricCurve.cpp
@@ -0,0 +1,4 @@
+#include "StdAfx.h"
+#include "pch_script.h"
+#include "ElectricCurve.h"
+
---
+++ src/xrGame/RandomSoundEmmiter.cpp
@@ -0,0 +1,78 @@
+#include "StdAfx.h"
+#include "RandomSoundEmmiter.h"
+
+CRandomSoundEmmiter::CRandomSoundEmmiter(const char* section, const char* soundParameter, esound_type _sound_type, int _game_type)
+{
+	sound_type = _sound_type;
+	game_type = _game_type;
+	Load(section, soundParameter);
+}
+
+void CRandomSoundEmmiter::Load(const char* section, const char* soundParameter)
+{
+	soundsArray.clear();
+
+	if (pSettings->line_exist(section, soundParameter))
+	{
+		xr_string unsplittedPaths = pSettings->r_string(section, soundParameter);
+		if (!unsplittedPaths.empty())
+		{
+			xr_vector<xr_string> paths = unsplittedPaths.RemoveWhitespaces().Split();
+			for (xr_string& sound_path : paths)
+			{
+				soundsArray.emplace_back().create(sound_path.c_str(), sound_type, game_type);
+			}
+		}
+	}
+}
+
+void CRandomSoundEmmiter::Stop()
+{
+	for (ref_sound& sound : soundsArray)
+	{
+		if (sound.is_playing()) {
+			sound.stop();
+		}
+	}
+}
+
+
+bool CRandomSoundEmmiter::IsPlaying()
+{
+	for (ref_sound& sound : soundsArray)
+	{
+		if (sound.is_playing()) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void CRandomSoundEmmiter::UpdatePosition(const Fvector& pos)
+{
+	for (ref_sound& sound : soundsArray)
+	{
+		if (sound.is_playing() && sound._feedback()) {
+			sound.set_position(pos);
+		}
+	}
+}
+
+void CRandomSoundEmmiter::UpdateVolume(float volume)
+{
+	for (ref_sound& sound : soundsArray)
+	{
+		sound.set_volume(volume);
+	}
+}
+
+void CRandomSoundEmmiter::PlayRandomSound(CObject* O, const Fvector& pos, u32 flags, float delay, float volume)
+{
+	if (!soundsArray.empty())
+	{
+		ref_sound snd = soundsArray[::Random.randI(soundsArray.size())];
+		snd.play_at_pos(O, pos, flags, delay);
+		snd.set_volume(volume);
+	}
+}
