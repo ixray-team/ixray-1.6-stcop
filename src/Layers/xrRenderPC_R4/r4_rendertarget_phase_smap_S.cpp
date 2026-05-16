e6abfb793205c5e643cd2d0222887668a6374c78
Hozar2002 <hoz2002team@gmail.com>
2026-05-16 14:24:14 +0600

    частичный фикс прозрачных теней

--- src/Layers/xrRenderPC_R4/r4_rendertarget_phase_smap_S.cpp
+++ src/Layers/xrRenderPC_R4/r4_rendertarget_phase_smap_S.cpp
@@ -2,13 +2,14 @@
 
 void CRenderTarget::phase_smap_spot_clear()
 {
+	GRHI->ClearTarget(rt_smap_surf->pRT, ERTColor::Black);
 	GRHI->ClearDepthStencil(rt_smap_depth->pZRT, ERHI_CLEAR_TARGET::DEPTH, 1.0f, 0L);
 }
 
-void CRenderTarget::phase_smap_spot		(light* L)
+void CRenderTarget::phase_smap_spot(light* L)
 {
 	// Targets + viewport
-	u_setrt(nullptr, nullptr, nullptr, rt_smap_depth->pZRT);
+	u_setrt(rt_smap_surf, nullptr, nullptr, rt_smap_depth->pZRT);
 
 	RHIViewport VP = {(float)L->X.S.posX, (float)L->X.S.posY, (float)L->X.S.size, (float)L->X.S.size, 0, 1};
 	GRHI->SetViewport(VP);
@@ -23,45 +24,7 @@ void CRenderTarget::phase_smap_spot		(light* L)
 	RCache.set_ColorWriteEnable(false);
 }
 
-void	CRenderTarget::phase_smap_spot_tsh	(light* L)
+void CRenderTarget::phase_smap_spot_tsh(light* L)
 {
-	VERIFY(!"Implement clear of the buffer for tsh!");
-	VERIFY(RImplementation.o.Tshadows);
-
-	RCache.set_ColorWriteEnable		();
-	if (IRender_Light::OMNIPART == L->flags.type)
-	{
-		// omni-part
-		GRHI->ClearTarget(RCache.get_RT(), ERTColor::Black);
-	}
-	else
-	{
-		// real-spot
-		// Select color-mask
-		ref_shader		shader			= L->s_spot;
-		if (!shader)	shader			= s_accum_spot;
-		RCache.set_Element				(shader->E[ SE_L_FILL ]	);
-
-		// Fill vertex buffer
-		Fvector2						p0,p1;
-		u32		Offset;
-		u32		C						= color_rgba	(255,255,255,255);
-		float	_w						= float(L->X.S.size);
-		float	_h						= float(L->X.S.size);
-		float	d_Z						= EPS_S;
-		float	d_W						= 1.f;
-		p0.set							(.5f/_w, .5f/_h);
-		p1.set							((_w+.5f)/_w, (_h+.5f)/_h );
-
-		FVF::TL* pv						= (FVF::TL*) RCache.Vertex.Lock	(4,g_combine->vb_stride,Offset);
-		pv->set							(EPS,			float(_h+EPS),	d_Z,	d_W, C, p0.x, p1.y);	pv++;
-		pv->set							(EPS,			EPS,			d_Z,	d_W, C, p0.x, p0.y);	pv++;
-		pv->set							(float(_w+EPS),	float(_h+EPS),	d_Z,	d_W, C, p1.x, p1.y);	pv++;
-		pv->set							(float(_w+EPS),	EPS,			d_Z,	d_W, C, p1.x, p0.y);	pv++;
-		RCache.Vertex.Unlock			(4,g_combine->vb_stride);
-		RCache.set_Geometry				(g_combine);
-
-		// draw
-		RCache.Render					(ERHI_PRIMITIVE_TOPOLOGY::TRIANGLE_LIST,Offset,0,4,0,2);
-	}
+	RCache.set_ColorWriteEnable();
 }
