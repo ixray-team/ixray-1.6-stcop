5aa145bf7507607adf663b819441e63ea66fd832
ForserX <sv3nk@yandex.ru>
2026-05-17 00:11:53 +0300

    Fix all-time rebuilding `theora`

--- cmake/modules/Theora.cmake
+++ cmake/modules/Theora.cmake
@@ -13,28 +13,36 @@ else()
     set(THEORA_BUILD_DIR ${CMAKE_BINARY_DIR}/theora_build)
     set(THEORA_INSTALL_DIR ${THEORA_BUILD_DIR}/install)
     
-    file(MAKE_DIRECTORY ${THEORA_INSTALL_DIR}/include)
-    file(MAKE_DIRECTORY ${THEORA_INSTALL_DIR}/lib)
+    if(NOT EXISTS ${THEORA_INSTALL_DIR}/lib/libtheora.a OR NOT EXISTS ${THEORA_INSTALL_DIR}/include/theora/theora.h)
+        message(STATUS "Theora not found, will build from source...")
+        
+        file(MAKE_DIRECTORY ${THEORA_INSTALL_DIR}/include)
+        file(MAKE_DIRECTORY ${THEORA_INSTALL_DIR}/lib)
 
-    add_custom_target(theora_build_target
-        COMMAND ${CMAKE_COMMAND} -E remove_directory ${THEORA_BUILD_DIR}
-        COMMAND ${CMAKE_COMMAND} -E make_directory ${THEORA_BUILD_DIR}
-        COMMAND cd ${THEORA_BUILD_DIR} &&
-            wget -q https://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.gz &&
-            tar -xzf libtheora-1.1.1.tar.gz &&
-            cd libtheora-1.1.1 &&
-            CC=${CMAKE_C_COMPILER}
-            CXX=${CMAKE_CXX_COMPILER}
-            CFLAGS=${CMAKE_C_FLAGS}
-            ./configure --prefix=${THEORA_INSTALL_DIR} --disable-shared --enable-static --with-pic --disable-examples &&
-            make -j${CMAKE_BUILD_PARALLEL_LEVEL} &&
-            make install
-        BYPRODUCTS 
-            ${THEORA_INSTALL_DIR}/lib/libtheora.a
-            ${THEORA_INSTALL_DIR}/include/theora/theora.h
-        COMMENT "Building Theora from source"
-        VERBATIM
-    )
+        add_custom_command(
+            OUTPUT 
+                ${THEORA_INSTALL_DIR}/lib/libtheora.a
+                ${THEORA_INSTALL_DIR}/include/theora/theora.h
+            COMMAND ${CMAKE_COMMAND} -E make_directory ${THEORA_BUILD_DIR}
+            COMMAND cd ${THEORA_BUILD_DIR} &&
+                wget -q -nc https://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.gz &&
+                tar -xzf libtheora-1.1.1.tar.gz &&
+                cd libtheora-1.1.1 &&
+                CC=${CMAKE_C_COMPILER}
+                CXX=${CMAKE_CXX_COMPILER}
+                CFLAGS=${CMAKE_C_FLAGS}
+                ./configure --prefix=${THEORA_INSTALL_DIR} --disable-shared --enable-static --with-pic --disable-examples &&
+                make -j${CMAKE_BUILD_PARALLEL_LEVEL} &&
+                make install
+            COMMENT "Building Theora from source"
+            VERBATIM
+        )
+        
+        add_custom_target(theora_build_target DEPENDS ${THEORA_INSTALL_DIR}/lib/libtheora.a)
+    else()
+        message(STATUS "Theora already built, skipping...")
+        add_custom_target(theora_build_target ALL)
+    endif()
     
     add_library(Theora::theora INTERFACE IMPORTED)
     add_dependencies(Theora::theora theora_build_target)
