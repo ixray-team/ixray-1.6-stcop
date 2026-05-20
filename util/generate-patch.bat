b0abe6031cc5bc88671d7ae0f68abaaddc4e67df
Drombeys <dima.zolotov02@gmail.com>
2026-05-20 15:49:07 +0300

    Add publish build for Clear Sky and Shadow of Chernobyl to GitHub Actions

--- util/generate-patch.bat
+++ util/generate-patch.bat
@@ -7,14 +7,24 @@ if "%~1"=="" (
 )
 set compressor=%~1
 
-:: Get assets
-if not exist gamedata_origin (
-    git clone ./.git ./temp
-    cd temp
-    git checkout 2045fd565ee903b1ea1a386afd8da73466e65d1d
-    cd ..
-    move temp/gamedata gamedata_origin
-)
+git clone https://github.com/ixray-team/ixray-multiplatform-assets ./temp
+move temp/gamedata_cop gamedata_origin
+move temp/gamedata_cs gamedata_cs_origin
+move temp/gamedata_soc gamedata_soc_origin
+::move temp/gamedata_coc gamedata_coc_origin
 
 :: Generate patch
 %compressor% -diff gamedata gamedata_origin -out patch
+%compressor% -diff gamedata_cs gamedata_cs_origin -out patch_cs
+%compressor% -diff gamedata_soc gamedata_soc_origin -out patch_soc
+::%compressor% -diff gamedata_coc gamedata_coc_origin -out patch_coc
+
+:: Cleanup
+echo Cleaning up...
+rd /s /q temp 2>nul
+rd /s /q gamedata_origin 2>nul
+rd /s /q gamedata_cs_origin 2>nul
+rd /s /q gamedata_soc_origin 2>nul
+::rd /s /q gamedata_coc_origin 2>nul
+
+echo Cache cleared successfully!
