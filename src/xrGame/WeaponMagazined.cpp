3e03c46762c10a79838af17be96ae448c466d426
Shtrecker <gogels235@gmail.com>
2026-05-29 14:54:59 +0300

    Fix firemode action

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -2718,7 +2718,7 @@ bool CWeaponMagazined::SwitchMode()
  
 void CWeaponMagazined::ChangeFireMode(u16 cmd)
 {
-	if (!HasFireModes() || GetState() != eIdle)
+	if (!HasFireModes() || GetNextState() != eIdle)
 	{
 		return;
 	}
