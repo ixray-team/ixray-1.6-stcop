b17c09995227168572b4c8648cd0b232d2b498a9
Shtrecker <gogels235@gmail.com>
2026-05-30 17:08:15 +0300

    Add jump in zoom

--- src/xrGame/player_hud.h
+++ src/xrGame/player_hud.h
@@ -114,6 +114,11 @@ struct weapon_inertion final
 	base_params aim_move_to_llookout_offset;
 	base_params aim_move_from_llookout_offset;
 
+	base_params aim_move_jump_offset;
+	base_params aim_move_fall_offset;
+	base_params aim_move_landing_offset;
+	base_params aim_move_landing2_offset;
+
 	base_params move_rlookout_offset;
 	base_params move_llookout_offset;
 
