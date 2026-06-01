cd0a5e44e6ba197012aaa2286ce4dc78a8ec96e7
ForserX <sv3nk@yandex.ru>
2026-06-01 14:25:45 +0300

    DX костыль

--- src/Layers/xrRenderTiramisu/Core/Passes/UI/XRayRenderUIPass.cpp
+++ src/Layers/xrRenderTiramisu/Core/Passes/UI/XRayRenderUIPass.cpp
@@ -4,176 +4,183 @@
 
 XRayRenderUIPass::XRayRenderUIPass()
 {
-    
-    { 
-        nri::BufferDesc BufferDescription = {};
-        BufferDescription.size = 16 * 1024* 6* sizeof(FXRayUIVertex);
-        BufferDescription.usage = nri::BufferUsageBits::VERTEX_BUFFER;
-        NRI_CHECK(GRenderDevice.CoreInterface.CreateBuffer(*GRenderDevice.Device, BufferDescription, GeometryBuffer));
-        
-        nri::ResourceGroupDesc ResourceGroupDescription = {};
-        ResourceGroupDescription.memoryLocation = nri::MemoryLocation::DEVICE;
-        ResourceGroupDescription.bufferNum = 1;
-        ResourceGroupDescription.buffers = &GeometryBuffer;
-        ResourceGroupDescription.textureNum = 0;
+	
+	{ 
+		nri::BufferDesc BufferDescription = {};
+		BufferDescription.size = 16 * 1024* 6* sizeof(FXRayUIVertex);
+		BufferDescription.usage = nri::BufferUsageBits::VERTEX_BUFFER;
+		NRI_CHECK(GRenderDevice.CoreInterface.CreateBuffer(*GRenderDevice.Device, BufferDescription, GeometryBuffer));
+		
+		nri::ResourceGroupDesc ResourceGroupDescription = {};
+		ResourceGroupDescription.memoryLocation = nri::MemoryLocation::DEVICE;
+		ResourceGroupDescription.bufferNum = 1;
+		ResourceGroupDescription.buffers = &GeometryBuffer;
+		ResourceGroupDescription.textureNum = 0;
 
-        VERIFY( GRenderDevice.HelperInterface.CalculateAllocationNumber(*GRenderDevice.Device, ResourceGroupDescription) == 1);
-        NRI_CHECK(GRenderDevice.HelperInterface.AllocateAndBindMemory(*GRenderDevice.Device, ResourceGroupDescription, &GeometryMemory));
-    }
-     
-    { 
-        nri::BufferDesc BufferDescription = {};
-        BufferDescription.size = 16 * 1024* 6* sizeof(FXRayUIVertex);
-        NRI_CHECK(GRenderDevice.CoreInterface.CreateBuffer(*GRenderDevice.Device, BufferDescription, UploadBuffer));
-        
-        nri::ResourceGroupDesc ResourceGroupDescription = {};
-        ResourceGroupDescription.memoryLocation = nri::MemoryLocation::DEVICE_UPLOAD;
-        ResourceGroupDescription.bufferNum = 1;
-        ResourceGroupDescription.buffers = &UploadBuffer;
-        ResourceGroupDescription.textureNum = 0;
+		VERIFY( GRenderDevice.HelperInterface.CalculateAllocationNumber(*GRenderDevice.Device, ResourceGroupDescription) == 1);
+		NRI_CHECK(GRenderDevice.HelperInterface.AllocateAndBindMemory(*GRenderDevice.Device, ResourceGroupDescription, &GeometryMemory));
+	}
+	 
+	{ 
+		nri::BufferDesc BufferDescription = {};
+		BufferDescription.size = 16 * 1024* 6* sizeof(FXRayUIVertex);
+		NRI_CHECK(GRenderDevice.CoreInterface.CreateBuffer(*GRenderDevice.Device, BufferDescription, UploadBuffer));
+		
+		nri::ResourceGroupDesc ResourceGroupDescription = {};
+		ResourceGroupDescription.memoryLocation = nri::MemoryLocation::DEVICE_UPLOAD;
+		ResourceGroupDescription.bufferNum = 1;
+		ResourceGroupDescription.buffers = &UploadBuffer;
+		ResourceGroupDescription.textureNum = 0;
 
-        VERIFY( GRenderDevice.HelperInterface.CalculateAllocationNumber(*GRenderDevice.Device, ResourceGroupDescription) == 1);
-        NRI_CHECK(GRenderDevice.HelperInterface.AllocateAndBindMemory(*GRenderDevice.Device, ResourceGroupDescription, &UploadBufferMemory));
-    }
-    
-    {
-        nri::VertexStreamDesc vertexStreamDesc = {};
-        vertexStreamDesc.bindingSlot = 0;
+		VERIFY( GRenderDevice.HelperInterface.CalculateAllocationNumber(*GRenderDevice.Device, ResourceGroupDescription) == 1);
+		NRI_CHECK(GRenderDevice.HelperInterface.AllocateAndBindMemory(*GRenderDevice.Device, ResourceGroupDescription, &UploadBufferMemory));
+	}
+	
+	{
+		nri::VertexStreamDesc vertexStreamDesc = {};
+		vertexStreamDesc.bindingSlot = 0;
 
-        nri::VertexInputDesc vertexInputDesc = {};
-        vertexInputDesc.attributes = FXRayUIVertex::VertexAttributeDescription;
-        vertexInputDesc.attributeNum = 3;
-        vertexInputDesc.streams = &vertexStreamDesc;
-        vertexInputDesc.streamNum = 1;
+		nri::VertexInputDesc vertexInputDesc = {};
+		vertexInputDesc.attributes = FXRayUIVertex::VertexAttributeDescription;
+		vertexInputDesc.attributeNum = 3;
+		vertexInputDesc.streams = &vertexStreamDesc;
+		vertexInputDesc.streamNum = 1;
 
-        nri::InputAssemblyDesc inputAssemblyDesc = {};
-        inputAssemblyDesc.topology = nri::Topology::TRIANGLE_LIST;
+		nri::InputAssemblyDesc inputAssemblyDesc = {};
+		inputAssemblyDesc.topology = nri::Topology::TRIANGLE_LIST;
 
-        nri::RasterizationDesc rasterizationDesc = {};
-        rasterizationDesc.fillMode = nri::FillMode::SOLID;
-        rasterizationDesc.cullMode = nri::CullMode::NONE;
+		nri::RasterizationDesc rasterizationDesc = {};
+		rasterizationDesc.fillMode = nri::FillMode::SOLID;
+		rasterizationDesc.cullMode = nri::CullMode::NONE;
 
-        nri::ColorAttachmentDesc colorAttachmentDesc = {};
-        colorAttachmentDesc.format = nri::Format::RGBA8_UNORM;
-        colorAttachmentDesc.colorWriteMask = nri::ColorWriteBits::RGBA;
-        colorAttachmentDesc.blendEnabled = true;
-        colorAttachmentDesc.colorBlend = { nri::BlendFactor::SRC_ALPHA, nri::BlendFactor::ONE_MINUS_SRC_ALPHA, nri::BlendOp::ADD};
+		nri::ColorAttachmentDesc colorAttachmentDesc = {};
+		colorAttachmentDesc.format = nri::Format::RGBA8_UNORM;
+		colorAttachmentDesc.colorWriteMask = nri::ColorWriteBits::RGBA;
+		colorAttachmentDesc.blendEnabled = true;
+		colorAttachmentDesc.colorBlend = { nri::BlendFactor::SRC_ALPHA, nri::BlendFactor::ONE_MINUS_SRC_ALPHA, nri::BlendOp::ADD};
 
-        nri::OutputMergerDesc outputMergerDesc = {};
-        outputMergerDesc.colors = &colorAttachmentDesc;
-        outputMergerDesc.colorNum = 1;
+		nri::OutputMergerDesc outputMergerDesc = {};
+		outputMergerDesc.colors = &colorAttachmentDesc;
+		outputMergerDesc.colorNum = 1;
 
-        XRayShaderDefinesContainer ShaderDefinesContainer;
+		XRayShaderDefinesContainer ShaderDefinesContainer;
 
-        nri::ShaderDesc shaderStages[2] = {};
-        {
-            const xr_vector<char>& ShaderCode = GRenderResourcesManager->GlobalShadersManager->GetShader("ui_screen_transform", EXRayShaderType::Vertex, ShaderDefinesContainer);
-            shaderStages[0].stage = nri::StageBits::VERTEX_SHADER;
-            shaderStages[0].bytecode = ShaderCode.data();
-            shaderStages[0].size = ShaderCode.size();
-            shaderStages[0].entryPointName = "Main";
-        }
-        {
-            const xr_vector<char>& ShaderCode = GRenderResourcesManager->GlobalShadersManager->GetShader("ui",EXRayShaderType::Pixel,ShaderDefinesContainer);
-            shaderStages[1].stage = nri::StageBits::FRAGMENT_SHADER;
-            shaderStages[1].bytecode = ShaderCode.data();
-            shaderStages[1].size = ShaderCode.size();
-            shaderStages[1].entryPointName = "Main";
-        }
-        
-        nri::GraphicsPipelineDesc graphicsPipelineDesc = {};
-        graphicsPipelineDesc.pipelineLayout = GRenderResourcesManager->GlobalPipelineLayout;
-        graphicsPipelineDesc.vertexInput = &vertexInputDesc;
-        graphicsPipelineDesc.inputAssembly = inputAssemblyDesc;
-        graphicsPipelineDesc.rasterization = rasterizationDesc;
-        graphicsPipelineDesc.outputMerger = outputMergerDesc;
-        graphicsPipelineDesc.shaders = shaderStages;
-        graphicsPipelineDesc.shaderNum = 2;
-        graphicsPipelineDesc.cache = nullptr;
-        NRI_CHECK(GRenderDevice.CoreInterface.CreateGraphicsPipeline(*GRenderDevice.Device, graphicsPipelineDesc, Pipeline));
+		nri::ShaderDesc shaderStages[2] = {};
+		{
+			const xr_vector<char>& ShaderCode = GRenderResourcesManager->GlobalShadersManager->GetShader("ui_screen_transform", EXRayShaderType::Vertex, ShaderDefinesContainer);
+			shaderStages[0].stage = nri::StageBits::VERTEX_SHADER;
+			shaderStages[0].bytecode = ShaderCode.data();
+			shaderStages[0].size = ShaderCode.size();
+			shaderStages[0].entryPointName = "Main";
+		}
+		{
+			const xr_vector<char>& ShaderCode = GRenderResourcesManager->GlobalShadersManager->GetShader("ui",EXRayShaderType::Pixel,ShaderDefinesContainer);
+			shaderStages[1].stage = nri::StageBits::FRAGMENT_SHADER;
+			shaderStages[1].bytecode = ShaderCode.data();
+			shaderStages[1].size = ShaderCode.size();
+			shaderStages[1].entryPointName = "Main";
+		}
+		
+		nri::GraphicsPipelineDesc graphicsPipelineDesc = {};
+		graphicsPipelineDesc.pipelineLayout = GRenderResourcesManager->GlobalPipelineLayout;
+		graphicsPipelineDesc.vertexInput = &vertexInputDesc;
+		graphicsPipelineDesc.inputAssembly = inputAssemblyDesc;
+		graphicsPipelineDesc.rasterization = rasterizationDesc;
+		graphicsPipelineDesc.outputMerger = outputMergerDesc;
+		graphicsPipelineDesc.shaders = shaderStages;
+		graphicsPipelineDesc.shaderNum = 2;
+		graphicsPipelineDesc.cache = nullptr;
+		NRI_CHECK(GRenderDevice.CoreInterface.CreateGraphicsPipeline(*GRenderDevice.Device, graphicsPipelineDesc, Pipeline));
 
-    }
-    
-    BufferBarrierDescription.buffer = GeometryBuffer;
-    BufferBarrierDescription.before = {nri::AccessBits::NONE, nri::StageBits::NONE};
-    BufferBarrierDescription.after = {nri::AccessBits::NONE, nri::StageBits::NONE};
+	}
+	
+	BufferBarrierDescription.buffer = GeometryBuffer;
+	BufferBarrierDescription.before = {nri::AccessBits::NONE, nri::StageBits::NONE};
+	BufferBarrierDescription.after = {nri::AccessBits::NONE, nri::StageBits::NONE};
 }
 
 XRayRenderUIPass::~XRayRenderUIPass()
 {
-    GRenderDevice.CoreInterface.DestroyBuffer(GeometryBuffer);
-    GRenderDevice.CoreInterface.FreeMemory(GeometryMemory);
-    
-    GRenderDevice.CoreInterface.DestroyBuffer(UploadBuffer);
-    GRenderDevice.CoreInterface.FreeMemory(UploadBufferMemory);
-    
-    GRenderDevice.CoreInterface.DestroyPipeline(Pipeline);
+	GRenderDevice.CoreInterface.DestroyBuffer(GeometryBuffer);
+	GRenderDevice.CoreInterface.FreeMemory(GeometryMemory);
+	
+	GRenderDevice.CoreInterface.DestroyBuffer(UploadBuffer);
+	GRenderDevice.CoreInterface.FreeMemory(UploadBufferMemory);
+	
+	GRenderDevice.CoreInterface.DestroyPipeline(Pipeline);
 }
 
 void XRayRenderUIPass::Upload(nri::CommandBuffer& CurrentCommandBuffer)
 {
-    g_pGamePersistent->OnRenderPPUI_main();	// PP-UI
-    {
-        if (FXRayUIVertex* UploadVertexes = static_cast<FXRayUIVertex*>(GRenderDevice.CoreInterface.MapBuffer(*UploadBuffer, 0, nri::WHOLE_SIZE)))
-        {
-            memcpy(UploadVertexes,GUIRender.Vertexes.data(),GUIRender.Vertexes.size()*sizeof(FXRayUIVertex));
-            GRenderDevice.CoreInterface.UnmapBuffer(*UploadBuffer);
-        }
-    }
-    
-    {
-        nri::BarrierDesc BarrierDescription = {};
-        BarrierDescription.bufferNum = 1;
-        BarrierDescription.buffers  = &BufferBarrierDescription;
-        
-        
-        BufferBarrierDescription.before = BufferBarrierDescription.after;
-        BufferBarrierDescription.after = {nri::AccessBits::COPY_DESTINATION, nri::StageBits::COPY};
-        GRenderDevice.CoreInterface.CmdBarrier(CurrentCommandBuffer,BarrierDescription);
-        
-        GRenderDevice.CoreInterface.CmdCopyBuffer(CurrentCommandBuffer,*GeometryBuffer,0,*UploadBuffer,0,nri::WHOLE_SIZE);
-        
-        BufferBarrierDescription.before = BufferBarrierDescription.after;
-        BufferBarrierDescription.after = {nri::AccessBits::VERTEX_BUFFER, nri::StageBits::VERTEX_SHADER};
-        GRenderDevice.CoreInterface.CmdBarrier(CurrentCommandBuffer,BarrierDescription);
-    }
+	g_pGamePersistent->OnRenderPPUI_main();	// PP-UI
+	{
+		if (GUIRender.Vertexes.size() >= 64000)
+		{
+			return;
+		}
+
+		if (FXRayUIVertex* UploadVertexes = static_cast<FXRayUIVertex*>(GRenderDevice.CoreInterface.MapBuffer(*UploadBuffer, 0, nri::WHOLE_SIZE)))
+		{
+			memcpy(UploadVertexes, GUIRender.Vertexes.data(), GUIRender.Vertexes.size() * sizeof(FXRayUIVertex));
+			GRenderDevice.CoreInterface.UnmapBuffer(*UploadBuffer);
+		}
+	}
+	
+	{
+		nri::BarrierDesc BarrierDescription = {};
+		BarrierDescription.bufferNum = 1;
+		BarrierDescription.buffers  = &BufferBarrierDescription;
+		
+		
+		BufferBarrierDescription.before = BufferBarrierDescription.after;
+		BufferBarrierDescription.after = {nri::AccessBits::COPY_DESTINATION, nri::StageBits::COPY};
+		GRenderDevice.CoreInterface.CmdBarrier(CurrentCommandBuffer,BarrierDescription);
+		
+		GRenderDevice.CoreInterface.CmdCopyBuffer(CurrentCommandBuffer,*GeometryBuffer,0,*UploadBuffer,0,nri::WHOLE_SIZE);
+		
+		BufferBarrierDescription.before = BufferBarrierDescription.after;
+		BufferBarrierDescription.after = {nri::AccessBits::VERTEX_BUFFER, nri::StageBits::VERTEX_SHADER};
+		GRenderDevice.CoreInterface.CmdBarrier(CurrentCommandBuffer,BarrierDescription);
+	}
 }
 
 void XRayRenderUIPass::Render(nri::CommandBuffer& CurrentCommandBuffer)
 {
-    {
-        GRenderDevice.CoreInterface.CmdSetPipeline(CurrentCommandBuffer, *Pipeline);
-        GRenderDevice.CoreInterface.CmdSetIndexBuffer(CurrentCommandBuffer, *GRenderResourcesManager->QuadGeometryBuffer, 0, nri::IndexType::UINT16);
-    
-        nri::VertexBufferDesc vertexBufferDesc = {};
-        vertexBufferDesc.buffer = GRenderResourcesManager->QuadGeometryBuffer;
-        vertexBufferDesc.offset = GRenderResourcesManager->QuadGeometryOffset;
-        vertexBufferDesc.stride = sizeof(FXRayUIVertex);
-        GRenderDevice.CoreInterface.CmdSetVertexBuffers(CurrentCommandBuffer, 0, &vertexBufferDesc, 1);
-    
-        
-        GRenderDevice.CoreInterface.CmdDrawIndexed(CurrentCommandBuffer, {6, 1, 0, 0, GRenderResourcesManager->WhiteTexture->GetOrCreateHeapIndex()});
-    }
-    {
-        GRenderDevice.CoreInterface.CmdSetPipeline(CurrentCommandBuffer, *Pipeline);
-    
-        nri::VertexBufferDesc VertexBufferDescription = {};
-        VertexBufferDescription.buffer = GeometryBuffer;
-        VertexBufferDescription.offset = 0;
-        VertexBufferDescription.stride = sizeof(FXRayUIVertex);
-        GRenderDevice.CoreInterface.CmdSetVertexBuffers(CurrentCommandBuffer, 0, &VertexBufferDescription, 1);
-        
-        for (const FXRayUIPrimitive& Primitve :GUIRender.Primitivs)
-        {
-            if (Primitve.VertexCount == 0)
-            {
-                continue;
-            }
-            GRenderDevice.CoreInterface.CmdDraw(CurrentCommandBuffer, {Primitve.VertexCount , 1, Primitve.VertexOffset,  Primitve.Texture->GetOrCreateHeapIndex()});
-        }
-    }
-    
-    GUIRender.Flush();
+	if (GUIRender.Vertexes.size() < 64000)
+	{
+		GRenderDevice.CoreInterface.CmdSetPipeline(CurrentCommandBuffer, *Pipeline);
+		GRenderDevice.CoreInterface.CmdSetIndexBuffer(CurrentCommandBuffer, *GRenderResourcesManager->QuadGeometryBuffer, 0, nri::IndexType::UINT16);
+	
+		nri::VertexBufferDesc vertexBufferDesc = {};
+		vertexBufferDesc.buffer = GRenderResourcesManager->QuadGeometryBuffer;
+		vertexBufferDesc.offset = GRenderResourcesManager->QuadGeometryOffset;
+		vertexBufferDesc.stride = sizeof(FXRayUIVertex);
+		GRenderDevice.CoreInterface.CmdSetVertexBuffers(CurrentCommandBuffer, 0, &vertexBufferDesc, 1);
+	
+		
+		GRenderDevice.CoreInterface.CmdDrawIndexed(CurrentCommandBuffer, {6, 1, 0, 0, GRenderResourcesManager->WhiteTexture->GetOrCreateHeapIndex()});
+	}
+	if (GUIRender.Vertexes.size() < 64000)
+	{
+		GRenderDevice.CoreInterface.CmdSetPipeline(CurrentCommandBuffer, *Pipeline);
+	
+		nri::VertexBufferDesc VertexBufferDescription = {};
+		VertexBufferDescription.buffer = GeometryBuffer;
+		VertexBufferDescription.offset = 0;
+		VertexBufferDescription.stride = sizeof(FXRayUIVertex);
+		GRenderDevice.CoreInterface.CmdSetVertexBuffers(CurrentCommandBuffer, 0, &VertexBufferDescription, 1);
+		
+		for (const FXRayUIPrimitive& Primitve :GUIRender.Primitivs)
+		{
+			if (Primitve.VertexCount == 0)
+			{
+				continue;
+			}
+			GRenderDevice.CoreInterface.CmdDraw(CurrentCommandBuffer, {Primitve.VertexCount , 1, Primitve.VertexOffset,  Primitve.Texture->GetOrCreateHeapIndex()});
+		}
+	}
+	
+	GUIRender.Flush();
 }
 
 
--- src/xrPlay/xrPlay.cpp
+++ src/xrPlay/xrPlay.cpp
@@ -1,5 +1,19 @@
 #include "Application.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	__declspec(dllexport) extern const uint32_t D3D12SDKVersion;
+	const uint32_t D3D12SDKVersion = 619;
+
+	__declspec(dllexport) extern const char* D3D12SDKPath;
+	const char* D3D12SDKPath = "AgilitySDK/";
+
+#ifdef __cplusplus
+}
+#endif
+
 int ENTRY_FUNCTION(ENTRY_ARGS)
 {
 	CApplication App(ENTRY_ARGS_PUSH);
