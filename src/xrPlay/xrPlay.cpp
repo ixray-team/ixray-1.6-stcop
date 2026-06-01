2e8e79458e2b7b3a09bb2f858167e3a92800a26a
ForserX <sv3nk@yandex.ru>
2026-06-01 14:25:45 +0300

    DX костыль

--- src/xrPlay/xrPlay.cpp
+++ src/xrPlay/xrPlay.cpp
@@ -12,6 +12,21 @@
 
 #include "../xrCore/git_version.h"
 #include "UIEditorMain.h"
+// This file is auto-generated during project deployment. Do not modify!
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	__declspec(dllexport) extern const uint32_t D3D12SDKVersion;
+	const uint32_t D3D12SDKVersion = 619;
+
+	__declspec(dllexport) extern const char* D3D12SDKPath;
+	const char* D3D12SDKPath = "AgilitySDK/";
+
+#ifdef __cplusplus
+}
+#endif
+
 
 void EnumerateDisplayModes()
 {
