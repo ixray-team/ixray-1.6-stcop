b238316beb4e1916c0b7630901ae3a807e52791f
Hozar2002 <hoz2002team@gmail.com>
2026-06-01 17:43:06 +0600

    Допил заснеживания

--- src/Layers/xrRenderPC_R4/blender_deffer_model.cpp
+++ src/Layers/xrRenderPC_R4/blender_deffer_model.cpp
@@ -86,7 +86,6 @@ void CBlender_deffer_model::Compile(CBlender_Compile& C)
 			case SE_R2_NORMAL_HQ:
 			case SE_R2_NORMAL_LQ:
 			{
-				RImplementation.addShaderOption("FORWARD_LIGHT", "1");
 				uber_forward(C, SE_R2_NORMAL_LQ != C.iElement, "deffer_model", "forward_base", true, true);
 				break;
 			}
