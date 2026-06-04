c3ade79fb6ba4f2b07591d7ee8704af14063801f
ForserX <sv3nk@yandex.ru>
2026-06-04 21:58:39 +0300

    Вроде так

--- src/xrEngine/x_ray.cpp
+++ src/xrEngine/x_ray.cpp
@@ -260,6 +260,7 @@ ENGINE_API void EngineLoopAndDestroy()
 	xr_delete(g_SpatialSpace);
 	DEL_INSTANCE(g_pGamePersistent);
 
+	xr_delete(g_FontManager);
 	xr_delete(pApp);
 	g_pEventManager->Event.Dump();
 
