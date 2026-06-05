77c7d3badb3a44c1cb77b0033441f494ba13dac7
Aphile <taiko6661@gmail.com>
2026-06-05 14:30:18 +0500

    ADD: implement downcast to all CAnomalyZone derives.

--- src/xrGame/NoGravityZone.h
+++ src/xrGame/NoGravityZone.h
@@ -1,15 +1,18 @@
 #pragma once
 #include "AnomalyZone.h"
 
-class CNoGravityZone final :
-	public CAnomalyZone
+class CNoGravityZone final : public CAnomalyZone
 {
-typedef CAnomalyZone inherited;
+	using inherited = CAnomalyZone;
+
 public:
+	CNoGravityZone* cast_no_gravity_zone() override { return this; }
+
 protected:
-	virtual		void	enter_Zone						(SZoneObjectInfo& io)				;
-	virtual		void	exit_Zone						(SZoneObjectInfo& io)				;
+	virtual void enter_Zone(SZoneObjectInfo& io);
+	virtual void exit_Zone(SZoneObjectInfo& io);
+
 private:
-				void	switchGravity					(SZoneObjectInfo& io,bool val)		;
-	virtual		void	UpdateWorkload					(u32	dt	)						;
+	void switchGravity(SZoneObjectInfo& io, bool val);
+	virtual void UpdateWorkload(u32 dt);
 };
