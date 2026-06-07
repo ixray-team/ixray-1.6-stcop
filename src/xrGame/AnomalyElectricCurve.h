8bd2dc4ec96e34c707d567223d2f505832eaad31
FFx0001 <vip.ffx0001@mail.ru>
2026-06-07 03:58:11 +0300

    curves

---
+++ src/xrGame/AnomalyElectricCurve.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "AnomalyZone.h"
+#include "ElectricCurve.h"
+#include "RandomSoundEmmiter.h"
+
+class CAnomalyZone;
+class CArtefact;
+class CGameObject;
+
+struct TAnomalyElectricCurve final
+{
+private:
+	xr_vector<CRandomSoundEmmiter*> m_snd_emmiter_electric_core_target_damage;
+	xr_vector<CRandomSoundEmmiter*> m_snd_emmiter_electric_core_loop;
+	xr_vector<CRandomSoundEmmiter*> m_snd_emmiter_electric_curve_start;
+	xr_vector<CRandomSoundEmmiter*> m_snd_emmiter_electric_curve_loop;
+	xr_vector<CRandomSoundEmmiter*> m_snd_emmiter_electric_curve_end;
+
+	CAnomalyZone* m_currentAnomalyObject = nullptr;
+	shared_str m_electric_curve_particle_path;
+	Fvector m_initial_spawn_position;
+	xr_vector<SElectricCurve> m_electric_curves;
+	CGameObject* lastDamagedObject = nullptr;
+
+	bool draw_dbg = false;
+
+	bool m_use_electric_curve = false;
+	bool m_cascade_curves = false;
+	bool m_cascade_curves_by_anomalies = false;
+
+	u8 m_max_count_electric_curves = 1;
+	float max_trace_curve_distance = 15.f;
+	float m_max_curve_damage = 0.005f;
+	float m_max_curve_impulse = 0.012f;
+	float max_processing_distance = 200.0f;
+	float blastTimeProcessing = 0.0f;
+	float max_blastTimeProcessing = 5000.0f;
+	float m_max_curve_radius = 5.0f;
+
+	Fmatrix& XFORM() { return m_currentAnomalyObject->XFORM(); }
+	void AffectCurveDamade(CGameObject* obj);
+	void OnBlastElectricCurvesUpdate(CGameObject* obj);
+
+public:
+	void BeginComponent(CAnomalyZone* O);
+	void EndComponent();
+
+	void Load(LPCSTR section);
+	void SetInitialSpawnPosition(Fvector vector) { m_initial_spawn_position = Fvector(vector.x, vector.y, vector.z);} // clone
+	bool AlwaysTheCrow();
+	bool IsEnabled();
+	void InitElectricCurves();
+	void OnActorTakeArtefact(float scan_radius, CArtefact* artefact, Fvector actorPos);
+	void AffectBlast(CGameObject* blastedObject);
+	bool IsNeedScanObjects();
+	float GetScanRadius() { return m_max_curve_radius / 2; }
+	float GetBarierRadius() { return m_max_curve_radius; }
+	void Update(bool isUpdateCL);
+
+private:
+	ECS_COMPONENT(TAnomalyElectricCurve)
+		ECS_END
+};
---
+++ src/xrGame/AnomalyMovement.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "AnomalyZone.h"
+
+class CAnomalyZone;
+class CGameObject;
+class CArtefact;
+
+struct TAnomalyMovement final
+{
+private:
+	CAnomalyZone* m_currentAnomalyObject = nullptr;
+	CGameObject* lastDamagedObject = nullptr;
+
+	Fvector m_initial_spawn_position;
+	bool m_use_movement = false;
+	float max_processing_distance = 200.0f;
+	bool m_use_movement_magnetic_on_take_artefacts_mode = false;
+	bool draw_dbg = false;
+	bool m_use_movement_always_mode = false;
+	bool m_use_movement_magnetic_on_inside_alive_mode = false;
+	float movement_magnetic_on_inside_alive_mode_speed = 0.f;
+	float m_timer_magnetic_on_take_artefacts = 0.f;
+	float m_max_timer_magnetic_on_take_artefacts = 0.f;
+	float movement_magnetic_on_take_artefacts_mode_speed = 0.f;
+	float m_movement_speed = 0.f;
+	float m_movement_radius = 0.f;
+	Fvector m_target_position;
+
+	Fmatrix& XFORM() { return m_currentAnomalyObject->XFORM(); }
+	void MoveToFromDelta(Fvector newPos, float speed);
+	Fvector GetLVPos(Fvector newPos);
+
+
+public:
+	void BeginComponent(CAnomalyZone* O);
+	void EndComponent();
+
+	void Load(LPCSTR section);
+	void SetInitialSpawnPosition(Fvector vector) { m_initial_spawn_position = Fvector(vector.x, vector.y, vector.z); } // clone
+	bool AlwaysTheCrow();
+	bool IsEnabled();
+	void OnActorTakeArtefact(float scan_radius, CArtefact* artefact, Fvector actorPos);
+	void AffectBlast(CGameObject* blastedObject);
+	bool IsNeedScanObjects();
+	float GetScanRadius() { return m_movement_radius / 2; }
+	float GetBarierRadius() { return m_movement_radius; }
+	void Update(CGameObject* m_best_magnetic_target, bool isUpdateCL);
+
+private:
+	ECS_COMPONENT(TAnomalyMovement)
+		ECS_END
+};
