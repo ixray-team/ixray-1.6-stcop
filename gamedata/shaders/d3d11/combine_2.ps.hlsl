b333ebb138f5f07eeabada9b91db8393dde08f41
Hozar2002 <hoz2002team@gmail.com>
2026-06-10 18:26:16 +0600

    Фикс пересветов с блумом

--- gamedata/shaders/d3d11/combine_2.ps.hlsl
+++ gamedata/shaders/d3d11/combine_2.ps.hlsl
@@ -43,6 +43,7 @@ float3 main(PSInputFullscreen I) : SV_Target
     #endif
 	
     Color.xyz = Color.xyz + Bloom.xyz * 0.1666f * bloom_params.x;
+	Color.xyz *= rcp(bloom_params.x + 1.0f);
 #else
     Bloom = s_bloom.Sample(smp_rtlinear, I.texcoord);
 
