da7588e664d240de0cb4395887d2d4794a070f04
FFx0001 <vip.ffx0001@mail.ru>
2026-06-12 04:05:01 +0300

    rain collide pg anoms

---
+++ src/xrGame/AnomalyRainCollide.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "AnomalyZone.h"
+
+class CGameObject;
+class CArtefact;
+
+struct TAnomalyRainCollide final
+{
+private:
+	CAnomalyZone* m_currentAnomalyObject = nullptr;
+	bool m_use_ground_rain_collide_particles = false;
+	bool m_use_air_rain_collide_particles = false;
+
+	shared_str m_ground_particle_path;
+	shared_str m_air_particle_path;
+
+	Fmatrix& XFORM() { return m_currentAnomalyObject->XFORM(); }
+
+	bool IsEnabled() { return m_currentAnomalyObject && m_currentAnomalyObject->IsEnabled(); }
+	void SpawnGroundParticle(Fvector position);
+	void SpawnAirParticle(Fvector position);
+	CParticlesObject* PlayNewPG(shared_str path, Fvector position);
+
+public:
+	void BeginComponent(IECSOwner* O) { m_currentAnomalyObject = smart_cast<CAnomalyZone*>(O); }
+	void EndComponent() {}
+	void Load(LPCSTR section);
+	void OnRainCollide(Fvector rainCollisionPosition);
+	void Update();
+
+private:
+	ECS_COMPONENT(TAnomalyRainCollide)
+		ECS_END
+};
