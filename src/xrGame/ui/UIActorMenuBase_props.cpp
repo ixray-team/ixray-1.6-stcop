16985989dc19e5052007f6b32ecc1f78cad183a9
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-06-12 11:32:35 +0300

    Enable attaching addons for upgrade & trade modes

--- src/xrGame/ui/UIActorMenuBase_props.cpp
+++ src/xrGame/ui/UIActorMenuBase_props.cpp
@@ -49,9 +49,9 @@ void CUIActorMenuBase::ActivatePropertiesBox()
 	{
 		PropertiesBoxForWeapon(cell_item, item, b_show);
 	}
+	PropertiesBoxForAddon(item, b_show);
 	if (m_currMenuMode == mmInventory || m_currMenuMode == mmDeadBodySearch)
 	{
-		PropertiesBoxForAddon(item, b_show);
 		PropertiesBoxForUsing(item, b_show);
 		PropertiesBoxForQuickSlots(cell_item, item, b_show);
 		PropertiesBoxForPlaying(item, b_show);
