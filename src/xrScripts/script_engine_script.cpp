43598c4215d89e9f88e0f61c61020d044d5eab2f
Aphile <taiko6661@gmail.com>
2026-07-04 19:11:18 +0500

    baza cheeeck
    baza fix
    
    baza fix 2
    
    baza fix 3
    
    BAZA? BAZA!

--- src/xrScripts/script_engine_script.cpp
+++ src/xrScripts/script_engine_script.cpp
@@ -315,6 +315,14 @@ void DumpLuaExports()
 	});
 }
 
+bool IsTracyConnected()
+{
+#ifdef IXRAY_PROFILER_TRACY
+	return tracy::GetProfiler().IsConnected();
+#endif
+	return false;
+}
+
 #pragma optimize("s",on)
 void CScriptEngine::script_register(lua_State *L)
 {
@@ -344,14 +352,15 @@ void CScriptEngine::script_register(lua_State *L)
 		def("IsSupportMP",						&CheckMP),
 		def("IsEditor",							&IsEditorMode),
 		def("help",								&DumpLuaExports),
-		def("try_load_file",					&TryLoadFile)
+		def("try_load_file",					&TryLoadFile),
 #ifdef XRGAME_EXPORTS
-		,def("device",							&get_device),
+		def("device",							&get_device),
 		def("load_script_to_global",			&LoadScriptToGlobal_script),
 		def("TinyLog",							&MyLog),
 		def("trigger_assert",					&trigger_assert),
-		def("trigger_vs_log",					&trigger_vs_log)
+		def("trigger_vs_log",					&trigger_vs_log),
 #endif // #ifdef XRGAME_EXPORTS
+		def("IsTracyConnected", &IsTracyConnected)
 	];
 
 	if (DevicePtr != nullptr && Device.IsEditorMode())
