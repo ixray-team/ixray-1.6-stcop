7e41b6254364f4512a06141e97e11e2aa4223923
Aphile <taiko6661@gmail.com>
2026-07-04 19:11:18 +0500

    baza cheeeck
    baza fix
    
    baza fix 2
    
    baza fix 3
    
    BAZA? BAZA!

--- gamedata/scripts/ixray_system/global.lua
+++ gamedata/scripts/ixray_system/global.lua
@@ -36,28 +36,55 @@ end
 --  ####################################################################################################################
 --                                         TRACY LOGIC
 --  ####################################################################################################################
-IS_TRACY_PROFILER_CONNECTED = false
-TRACY_PREFIX = "[LUA] "
-TRACY_DELIMETER = "::"
 
 function UpdateTracyState()
     if (not IsTracyConnected) then
         return
     end
     
-    IS_TRACY_PROFILER_CONNECTED = IsTracyConnected()
+    _G.IS_TRACY_PROFILER_CONNECTED = IsTracyConnected()
 end
 
-function PROF_EVENT(name, callable)
-    if (not tracy) or (not IS_TRACY_PROFILER_CONNECTED) then
-        return
-    end
-    
-    tracy.ZoneBeginN(TRACY_PREFIX .. ffx_callable_utils.find_caller_source(3) .. TRACY_DELIMETER .. tostring(name))
+function PROF_EVENT_BEGIN(name)
+	if tracy and _G.IS_TRACY_PROFILER_CONNECTED then
+		local data = ffx_callable_utils.find_caller_source_tracy(3, true)
+        tracy.ZoneBeginN(string.format("[%s (%d,%d)] %s", tostring(data.file_name), tostring(data.line_begin), tostring(data.line_end), tostring(name)))
+	end
+end
+
+function PROF_EVENT_END()
+	if tracy and _G.IS_TRACY_PROFILER_CONNECTED then
+        tracy.ZoneEnd()
+	end
+end
+
+function PROF_EVENT_CLOSURE(name, callable)
+	if tracy and _G.IS_TRACY_PROFILER_CONNECTED then
+		local podrobnaya_infa = true
+	
+		if podrobnaya_infa then
+			local data = ffx_callable_utils.find_caller_source_tracy(3, true)
+			tracy.ZoneBeginN(string.format("[%s (%d,%d)] %s", tostring(data.file_name), tostring(data.line_begin), tostring(data.line_end), tostring(name)))
+		else
+			tracy.ZoneBeginN(string.format("[LUA] %s", tostring(name)))
+		end
+		
+		local result
+		
+		if callable then
+			result = callable()
+		end
+		
+		tracy.ZoneEnd()
+		
+		return result
+    else
         if callable then
-            callable()
-        end
-    tracy.ZoneEnd()
+			return callable()
+		end
+	end
+	
+	return nil
 end
 
 --  ####################################################################################################################
