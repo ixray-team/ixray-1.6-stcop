94f267f7043be98bd57ee25539334c9fc60103ce
ForserX <sv3nk@yandex.ru>
2026-07-11 14:09:49 +0300

    CMake: Cleanup

--- cmake/modules/amd_ags.cmake
+++ cmake/modules/amd_ags.cmake
@@ -1,4 +1,10 @@
 # AMD AGS SDK
+set(AMD_AGS_SDK_ZIP ${DEP_DIR}/Amd.Ags.Sdk.5.4.2.zip)
+set(AMD_AGS_SDK_URL https://github.com/ixray-team/ixray-packages/releases/download/d2023.12.8/Amd.Ags.Sdk.5.4.2.zip)
+set(AMD_AGS_SDK_DIR ${DEP_DIR}/amd_ags_sdk)
+download_and_extract_sdk(${AMD_AGS_SDK_URL} ${AMD_AGS_SDK_ZIP} ${AMD_AGS_SDK_DIR})
+set(AMD_AGS_SDK ${AMD_AGS_SDK_DIR}/)
+
 set(AMD_AGS_INCLUDE "${CMAKE_BINARY_DIR}/dep/amd_ags_sdk/inc/")
 set(AMD_AGS_LIB     "")  # SDK линковка через DLL
 set(AMD_AGS_DLL     "${CMAKE_BINARY_DIR}/dep/amd_ags_sdk/lib/amd_ags_${NUGET_PACKAGE_PLATFORM}.dll")
