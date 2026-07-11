15a4adf8d49dc2b5c985e7757f70772ed9658866
ForserX <sv3nk@yandex.ru>
2026-07-11 14:09:49 +0300

    CMake: Cleanup

--- cmake/modules/discord_game_sdk.cmake
+++ cmake/modules/discord_game_sdk.cmake
@@ -5,6 +5,12 @@ else()
     set(DISCORD_PLATFORM_EX x86_64)
 endif()
 
+set(DISCORD_GAME_SDK_ZIP ${DEP_DIR}/discord_gamesdk_3.2.1.zip)
+set(DISCORD_GAME_SDK_URL https://github.com/ixray-team/ixray-packages/releases/download/d2023.12.8/Discord.GameSdk.3.2.1.zip)
+set(DISCORD_GAME_SDK_DIR ${DEP_DIR}/discord_gamesdk)
+download_and_extract_sdk(${DISCORD_GAME_SDK_URL} ${DISCORD_GAME_SDK_ZIP} ${DISCORD_GAME_SDK_DIR})
+set(DISCORD_GAME_SDK ${DISCORD_GAME_SDK_DIR}/)
+
 set(DISCORD_SDK_DIR ${CMAKE_BINARY_DIR}/dep/discord_gamesdk)
 set(DISCORD_LIB   ${DISCORD_SDK_DIR}/lib/${DISCORD_PLATFORM_EX}/discord_game_sdk.dll.lib)
 set(DISCORD_DLL   ${DISCORD_SDK_DIR}/lib/${DISCORD_PLATFORM_EX}/discord_game_sdk.dll)
