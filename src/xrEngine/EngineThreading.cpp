a5678f4e22087de34c30557da957ffbde282beb7
Hozar2002 <hoz2002team@gmail.com>
2026-07-13 00:56:54 +0600

    Фикс звука с 3д статиками

--- src/xrEngine/EngineThreading.cpp
+++ src/xrEngine/EngineThreading.cpp
@@ -82,7 +82,7 @@ void XRay::Engine::GameThread()
 	if (::Sound != nullptr)
 	{
 		Device.Statistic->Sound.Begin();
-		::Sound->update(Device.mView, Device.vCameraPosition, Device.vCameraDirection, Device.vCameraTop);
+		::Sound->update(Device.mView_saved, Device.vCameraPosition_saved, Device.vCameraDirection_saved, Device.vCameraTop_saved);
 		Device.Statistic->Sound.End();
 	}
 }
