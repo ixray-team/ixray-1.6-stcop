fdcdba911ea2ce06e23d977867068e0a89dbce35
Aphile <taiko6661@gmail.com>
2026-07-14 14:48:27 +0500

    Refactor: Убрать профайлер из релиза. Теперь релизная сборка нормально собирается, даже если с смаке не проставлены или проставлен хотя бы один профайлер.

--- src/xrCore/profiler.h
+++ src/xrCore/profiler.h
@@ -1,43 +1,71 @@
 #pragma once
 
-#ifdef IXRAY_PROFILER
-#	include <optick.h>
-#	define PROF_THREAD(Name) OPTICK_THREAD(Name)
-#	define PROF_START_THREAD(Name) OPTICK_START_THREAD(Name) OPTICK_EVENT(Name)
-#	define PROF_STOP_THREAD() OPTICK_STOP_THREAD()
-#	define PROF_START_CAPTURE() OPTICK_START_CAPTURE()
-#	define PROF_STOP_CAPTURE() OPTICK_STOP_CAPTURE()
-#	define PROF_SAVE_CAPTURE(Name) OPTICK_SAVE_CAPTURE(Name)
-#	define PROF_FRAME(Name) OPTICK_FRAME(Name)
-#	define PROF_EVENT(Name) OPTICK_EVENT(Name)
-#	define PROF_MESSAGE(Name)
-#	define PROF_MEM_ALLOC_CAPTURE(Ptr, Size)
-#	define PROF_MEM_FREE_CAPTURE(Ptr)
-#elifdef IXRAY_PROFILER_TRACY
-#	include <tracy/Tracy.hpp>
-#	include <tracy/TracyC.h>
-#	define TRACY_CALLSTACK 8
-#   define PROF_THREAD(Name)
-#   define PROF_START_THREAD(Name) TracyCZoneCtx zone; TracyFiberEnter(Name); TracyCZone(ctx, 1); zone = ctx;
-#   define PROF_STOP_THREAD() TracyCZoneEnd(zone); TracyFiberLeave;
-#   define PROF_START_CAPTURE()
-#   define PROF_STOP_CAPTURE()
-#   define PROF_SAVE_CAPTURE(Name)
-#   define PROF_FRAME(Name) FrameMark;
-#	define PROF_EVENT(Name) ZoneScopedN(Name);
-#	define PROF_MESSAGE(Name) TracyMessageL(Name)
-#	define PROF_MEM_ALLOC_CAPTURE(Ptr, Size) TracyAlloc(Ptr, Size);
-#	define PROF_MEM_FREE_CAPTURE(Ptr) TracyFree(Ptr);
-#else // no profiler
-#	define PROF_THREAD(Name)
-#	define PROF_START_THREAD(Name)
-#	define PROF_STOP_THREAD()
-#	define PROF_START_CAPTURE()
-#	define PROF_STOP_CAPTURE()
-#	define PROF_SAVE_CAPTURE(Name)
-#	define PROF_FRAME(Name)
-#	define PROF_EVENT(Name)
-#	define PROF_MESSAGE(Name)
+#ifdef DEBUG_DRAW
+#	ifdef IXRAY_PROFILER
+#		include <optick.h>
+#		define PROF_THREAD(Name) OPTICK_THREAD(Name);
+#		define PROF_START_THREAD(Name) OPTICK_START_THREAD(Name) OPTICK_EVENT(Name);
+#		define PROF_STOP_THREAD() OPTICK_STOP_THREAD();
+#		define PROF_START_CAPTURE() OPTICK_START_CAPTURE();
+#		define PROF_STOP_CAPTURE() OPTICK_STOP_CAPTURE();
+#		define PROF_SAVE_CAPTURE(Name) OPTICK_SAVE_CAPTURE(Name);
+#		define PROF_FRAME(Name) OPTICK_FRAME(Name);
+#		define PROF_EVENT(Name) OPTICK_EVENT(Name);
+#		define PROF_MESSAGE(Name);
+#		define PROF_MEM_ALLOC_CAPTURE(Ptr, Size);
+#		define PROF_MEM_FREE_CAPTURE(Ptr);
+#	elifdef IXRAY_PROFILER_TRACY
+#		include <tracy/Tracy.hpp>
+#		include <tracy/TracyC.h>
+#		define TRACY_CALLSTACK 8
+#   	define PROF_THREAD(Name);
+#   	define PROF_START_THREAD(Name) TracyCZoneCtx zone; TracyFiberEnter(Name); TracyCZone(ctx, 1); zone = ctx;
+#   	define PROF_STOP_THREAD() TracyCZoneEnd(zone); TracyFiberLeave;
+#   	define PROF_START_CAPTURE();
+#   	define PROF_STOP_CAPTURE();
+#   	define PROF_SAVE_CAPTURE(Name)
+#   	define PROF_FRAME(Name) FrameMark;
+#		define PROF_EVENT(Name) ZoneScopedN(Name);
+#		define PROF_MESSAGE(Name) TracyMessageL(Name)
+#		define PROF_MEM_ALLOC_CAPTURE(Ptr, Size) TracyAlloc(Ptr, Size);
+#		define PROF_MEM_FREE_CAPTURE(Ptr) TracyFree(Ptr);
+#	else // no profiler
+#		define PROF_THREAD(Name);
+#		define PROF_START_THREAD(Name);
+#		define PROF_STOP_THREAD();
+#		define PROF_START_CAPTURE();
+#		define PROF_STOP_CAPTURE();
+#		define PROF_SAVE_CAPTURE(Name);
+#		define PROF_FRAME(Name);
+#		define PROF_EVENT(Name);
+#		define PROF_MESSAGE(Name);
+#		define PROF_EVENT_DYNAMIC(...) {};
+#		define PROF_MEM_ALLOC_CAPTURE(Ptr, Size);
+#		define PROF_MEM_FREE_CAPTURE(Ptr);
+#	endif
+#else
+/*
+	 --> Define #undef for new profilers <-- in order to avoid using constructions like:
+	 #ifdef defined(IXRAY_PROFILER_%NEW_PROF_NAME%) && defined(DEBUG_DRAW)
+		profiler specific logic
+	 #endif
+	 
+	 To keep lightweight checks like:
+	 #ifdef IXRAY_PROFILER_%PROF_NAME%
+		profiler specific logic
+	 #endif
+*/
+#	undef IXRAY_PROFILER
+#	undef IXRAY_PROFILER_TRACY
+#	define PROF_THREAD(Name);
+#	define PROF_START_THREAD(Name);
+#	define PROF_STOP_THREAD();
+#	define PROF_START_CAPTURE();
+#	define PROF_STOP_CAPTURE();
+#	define PROF_SAVE_CAPTURE(Name);
+#	define PROF_FRAME(Name);
+#	define PROF_EVENT(Name);
+#	define PROF_MESSAGE(Name);
 #	define PROF_EVENT_DYNAMIC(...) {};
 #	define PROF_MEM_ALLOC_CAPTURE(Ptr, Size)
 #	define PROF_MEM_FREE_CAPTURE(Ptr)
