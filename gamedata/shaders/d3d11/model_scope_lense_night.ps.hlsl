5ccf4e3f3f949beda1a5c6d47f68d177bd49bcd6
Hozar2002 <hoz2002team@gmail.com>
2026-07-21 02:29:23 +0600

    Исправлены шейдеры ПНВ и Партиклов

--- gamedata/shaders/d3d11/model_scope_lense_night.ps.hlsl
+++ gamedata/shaders/d3d11/model_scope_lense_night.ps.hlsl
@@ -12,7 +12,8 @@ struct v2p
 
 float4 main(v2p I, float4 pos2d : SV_POSITION) : SV_Target
 {
-    if (m_hud_params.y * m_hud_params.a < 0.0001f) {
+    if (m_hud_params.y * m_hud_params.a < 0.0001f)
+	{
 		return 0.0f;
 	}
 	
@@ -20,7 +21,10 @@ float4 main(v2p I, float4 pos2d : SV_POSITION) : SV_Target
 	t_base.xyz = detonemap(t_base.xyz);
 	
     float4 t_vp2 = s_image[pos2d.xy];
+	
+	t_vp2.xyz = LinearToGamma(t_vp2.xyz) * 3.5f; t_vp2.xyz /= t_vp2.xyz + 1.0f;
 	t_vp2.xyz = calc_night_vision_effect(I.tc0, t_vp2, float3(1.0, 2.0, 1.0));
+	t_vp2.xyz = GammaToLinear(t_vp2.xyz);
 	
 	float alpha = m_hud_params.y * m_hud_params.a;
 	t_vp2 *= saturate(alpha * 2.0f - 1.0f);
