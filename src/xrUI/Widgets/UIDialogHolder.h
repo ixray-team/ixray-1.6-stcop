d3188732a586a52e1a813b403fd0919b9c610b5e
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-07-26 12:34:38 +0300

    Add UI function for gyroscope movement

--- src/xrUI/Widgets/UIDialogHolder.h
+++ src/xrUI/Widgets/UIDialogHolder.h
@@ -64,4 +64,5 @@ public:
 	virtual bool 			IR_UIOnGamepadUpdateStick		(int id, Fvector2 value);
 	virtual bool 			IR_UIOnGamepadKeyHold			(int id);
 	virtual bool 			IR_UIOnGyroscopeMove			(Fvector3 value);
+	virtual bool 			IR_UIOnTouchpadMove				(Fvector2 value);
 };
