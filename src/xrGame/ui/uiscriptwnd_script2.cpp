d4e660ac13f352798b72527c05f4ee0f5abd3f5f
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-07-26 17:11:38 +0300

    Add touchpad support for radial menu

--- src/xrGame/ui/uiscriptwnd_script2.cpp
+++ src/xrGame/ui/uiscriptwnd_script2.cpp
@@ -30,6 +30,7 @@ export_class script_register_ui_window2(export_class &&instance)
 		.def("OnGamepadKeyHold",	&BaseType::OnGamepadKeyHold, &WrapType::OnGamepadKeyHold_static)
 		.def("OnGamepadStick",	&BaseType::OnGamepadStickAction, &WrapType::OnGamepadStick_static)
 		.def("OnGyroscope",		&BaseType::OnGyroscopeAction, &WrapType::OnGyroscope_static)
+		.def("OnTouchpad",		&BaseType::OnTouchpadAction, &WrapType::OnTouchpad_static)
 		.def("Update",			&BaseType::Update, &WrapType::Update_static)
 		.def("Dispatch",		&BaseType::Dispatch, &WrapType::Dispatch_static)
 
