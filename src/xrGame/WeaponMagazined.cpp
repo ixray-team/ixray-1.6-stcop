085411a2c0f97cfcc85c0967c86a026c9208ee8d
Shtrecker <gogels235@gmail.com>
2026-07-28 09:05:19 +0300

    Delete trash

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -567,7 +567,7 @@ void CWeaponMagazined::FireStart()
 				SwitchState(eFire);
 			}
 		}
-		else if (!IsPending() && GetState() != eFire || CurrentState == eEmptyClick && !m_bBlockEmptyClick)
+		else if (!IsPending() && GetState() != eFire)
 		{
 			if (IsActor && m_eAnimationsFlags.test(EAnimationsFlags::af_empty_click))
 			{
@@ -579,7 +579,7 @@ void CWeaponMagazined::FireStart()
 			}
 		}
 	}
-	else if (!IsPending() && GetState() != eFire || CurrentState == eEmptyClick && !m_bBlockEmptyClick)
+	else if (!IsPending() && GetState() != eFire)
 	{
 		if (parent != nullptr)
 		{
@@ -1738,12 +1738,6 @@ void CWeaponMagazined::OnAnimationEnd(u8 state)
 		case eIdle:
 			switch2_Idle();
 		break;
-		case eEmptyClick:
-		{
-			m_bBlockEmptyClick = false;
-			SwitchState(eIdle);
-			break;
-		}
 		case eFire:
 		{
 			if (ParentIsActor())
@@ -1793,6 +1787,7 @@ void CWeaponMagazined::OnAnimationEnd(u8 state)
 		case eMagCheck:
 		case eFiremodeCheck:
 		case eSafemodeSwitch:
+		case eEmptyClick:
 		{
 			if (state == eSwitchMode)
 			{
@@ -1854,7 +1849,6 @@ void CWeaponMagazined::switch2_Empty()
 	auto play_motion_if_exists = [&](const shared_str& motion_name)
 	{
 		SetPending(true);
-		m_bBlockEmptyClick = true;
 		PlayHUDMotion(SetCurrentStateAnimation(motion_name), EHudMixType::eMixAll, eEmptyClick);
 		if (CActor* pActor = H_Parent() != nullptr ? H_Parent()->cast_actor() : nullptr)
 		{
