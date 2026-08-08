e3ab18211414b68298a19081e94c5df64f2c2f10
ForserX <sv3nk@yandex.ru>
2026-08-09 02:08:29 +0300

    Исправил партки в SDK на DX11 (@Hozar2002, чекни)

--- gamedata/shaders/editor/particle_distort.ps.hlsl
+++ gamedata/shaders/editor/particle_distort.ps.hlsl
@@ -3,25 +3,17 @@
 struct v2p
 {
     float4 hpos : SV_POSITION;
-    float2 tc0 : TEXCOORD0; // base
-    float4 proj : TEXCOORD1; // base
-    float4 c : COLOR0; // diffuse
+    float2 tc0 : TEXCOORD0;
+    float4 proj : TEXCOORD1;
+    float4 c : COLOR0;
     float fog : FOG;
 };
 
-// Pixel
-uniform Texture2D s_distort;
-uniform Texture2D s_image;
+Texture2D s_distort;
 
 float4 main(v2p I) : SV_Target
 {
-	float2 uv = I.proj.xy / I.proj.w * float2(0.5f, -0.5f) + 0.5f;
-    float4 distort = s_distort.Sample(smp_nofilter, I.tc0);
-	
-    float factor = distort.a * dot(I.c.xyz, 0.33f);
-    float2 offset = (distort.xy - (127.0f / 255.0f)) * factor;
-    float3 image = s_image.Sample(smp_nofilter, uv + offset * 0.05f).xyz;
-	
-    return float4(image.xyz, 1.0f);
+    float4 distort = s_distort.Sample(smp_linear, I.tc0);
+    float factor = distort.w * dot(I.c.xyz, 0.3333f);
+    return float4(distort.xyz, factor);
 }
-
