51ba2051aefc2aba0af622e0cae5d2c7188a7249
Aphile <taiko6661@gmail.com>
2026-08-10 01:20:00 +0500

    Add: Управление в деморекорде как было, специально для @Hozar2002

--- src/xrEngine/FDemoRecord.h
+++ src/xrEngine/FDemoRecord.h
@@ -1,6 +1,3 @@
-#if !defined(AFX_FDEMORECORD_H__D7638760_FB61_11D3_B4E3_4854E82A090D__INCLUDED_)
-#define AFX_FDEMORECORD_H__D7638760_FB61_11D3_B4E3_4854E82A090D__INCLUDED_
-
 #pragma once
 
 #include "IInputReceiver.h"
@@ -11,59 +8,62 @@ class ENGINE_API CDemoRecord :
 	public IInputReceiver,
 	public pureRender
 {
-private:
-	static struct force_position 
+	static struct force_position
 	{
-			bool	set_position;
-			Fvector p;
+		bool set_position;
+		Fvector p;
 	} g_position;
 
-	IWriter*	file;
-	Fvector		HPB;
-	Fvector		Position;
-	Fmatrix		Camera;
-	u32			Stage;
+	IWriter* file;
+	Fvector HPB;
+	Fvector Position;
+	Fmatrix Camera;
+	u32 Stage;
 
-	Fvector		FrameTopDelta;
-    Fvector		FrameRightDelta;
-	Fvector		Velocity;
-	Fvector		AngularVelocity;
-	
-	xr_vector<Fvector> KeyframesPositions; 
+	Fvector FrameTopDelta;
+	Fvector FrameRightDelta;
+	Fvector Velocity;
+	Fvector AngularVelocity;
 
-	bool		m_bMakeCubeMap;
-	bool		m_bMakeScreenshot;
-	int			m_iLMScreenshotFragment;
-	bool		m_bMakeLevelMap;
-	bool		m_bEnableAcceleration = false;
+	xr_vector<Fvector> KeyframesPositions;
 
-	float		CameraTransformFactor = 1.f;
+	bool m_bMakeCubeMap;
+	bool m_bMakeScreenshot;
+	int m_iLMScreenshotFragment;
+	bool m_bMakeLevelMap;
+	bool m_bEnableAcceleration = false;
+	bool NewInputSchema;
 
-	void		MakeCubeMapFace			(Fvector &D, Fvector &N);
-	void		MakeLevelMapProcess		();
-	void		MakeScreenshotFace		();
-	void		RecordKey				();
-	void		MakeCubemap				();
-	void		MakeScreenshot			();
-	void		MakeLevelMapScreenshot	(bool bHQ);
+	float CameraTransformFactor;
+	float m_fSpeed0;
+	float m_fSpeed1;
+	float m_fSpeed2;
+	float m_fSpeed3;
+
+	void MakeCubeMapFace(Fvector& D, Fvector& N);
+	void MakeLevelMapProcess();
+	void MakeScreenshotFace();
+	void RecordKey();
+	void MakeCubemap();
+	void MakeScreenshot();
+	void MakeLevelMapScreenshot(bool bHQ);
+
 public:
-				CDemoRecord				(const char *name, float life_time=60*60*1000);
-	virtual		~CDemoRecord();
+	CDemoRecord(const char* name, float life_time = 60 * 60 * 1000);
+	virtual ~CDemoRecord();
 
-	virtual void IR_OnKeyboardPress		(int dik);
-	virtual void IR_OnKeyboardRelease	(int dik);
-	virtual void IR_OnKeyboardHold		(int dik);
-	virtual void IR_OnMouseMove			(int dx, int dy);
-	virtual void IR_OnMouseWheel		(int direction);
-	virtual void IR_OnMouseHold			(int btn);
-	virtual void IR_GamepadUpdateStick	(int id, Fvector2 value);
-	virtual void IR_GamepadKeyPress		(int id);
+	virtual void IR_OnKeyboardPress(int dik);
+	virtual void IR_OnKeyboardRelease(int dik);
+	virtual void IR_OnKeyboardHold(int dik);
+	virtual void IR_OnMouseMove(int dx, int dy);
+	virtual void IR_OnMouseWheel(int direction);
+	virtual void IR_OnMouseHold(int btn);
+	virtual void IR_GamepadUpdateStick(int id, Fvector2 value);
+	virtual void IR_GamepadKeyPress(int id);
 
-	virtual bool ProcessCam				(SCamEffectorInfo& info);
-	static	void SetGlobalPosition		( const Fvector &p ) { g_position.p.set(p), g_position.set_position= true; }
-	static	void GetGlobalPosition		( Fvector &p ) { p.set( g_position.p ); }
-	bool		 m_b_redirect_input_to_level;
-	virtual void OnRender				();
+	virtual bool ProcessCam(SCamEffectorInfo& info);
+	static void SetGlobalPosition(const Fvector& p) { g_position.p.set(p), g_position.set_position = true; }
+	static void GetGlobalPosition(Fvector& p) { p.set(g_position.p); }
+	bool m_b_redirect_input_to_level;
+	virtual void OnRender();
 };
-
-#endif // !defined(AFX_FDEMORECORD_H__D7638760_FB61_11D3_B4E3_4854E82A090D__INCLUDED_)
