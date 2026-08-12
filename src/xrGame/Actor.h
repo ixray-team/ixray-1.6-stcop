b1876a5a4865fa068b846f0daf8764ecb444963f
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-08-12 20:52:59 +0300

    Add smooth inertion for gamepad movement

--- src/xrGame/Actor.h
+++ src/xrGame/Actor.h
@@ -977,6 +977,7 @@ public:
 	float fSprintFactor = 0;
 	float m_SprintFovFactor = 7.0f;
 	bool IsActorBurning();
+	IC Fvector2 MovementFactor() const { return leftStickThreshold; };
 };
 
 extern bool		isActorAccelerated			(u32 mstate, bool ZoomMode);
