c2884dc051be5790c04937a2c709055cce2b80bb
Shtrecker <gogels235@gmail.com>
2026-08-19 08:38:33 +0300

    For input

--- src/xrGame/WeaponMagazined.cpp
+++ src/xrGame/WeaponMagazined.cpp
@@ -3381,7 +3381,12 @@ bool CWeaponMagazined::SwitchMode()
  
 void CWeaponMagazined::ChangeFireMode(u16 cmd)
 {
-	if (!HasFireModes() || IsPending() && GetNextState() != eIdle)
+	if (!HasFireModes())
+	{
+		return;
+	}
+
+	if (IsPending())
 	{
 		return;
 	}
