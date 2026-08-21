a66ed12781883390259c5d8f716ee6bf8f032f85
Shtrecker <gogels235@gmail.com>
2026-08-21 08:07:57 +0300

    Fix scopes hands positions

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -2567,6 +2567,11 @@ bool CWeapon::SilencerAttachable()
 
 void CWeapon::UpdateScopePosition()
 {
+	if (bUseAltScope)
+	{
+		return;
+	}
+
 	auto HID = HudItemData();
 
 	if (HID != nullptr && ScopeAttachable())
