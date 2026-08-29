36abe782a075e1e2f95cfc5473fda2d7ed0a910a
Drombeys <dima.zolotov02@gmail.com>
2026-08-29 17:42:27 +0300

    Fix RPG rocket iterator

--- src/xrGame/RocketLauncher.cpp
+++ src/xrGame/RocketLauncher.cpp
@@ -90,7 +90,7 @@ void CRocketLauncher::DetachRocket(u16 rocket_id, bool bLaunch)
 
 	if (It_l != m_launched_rockets.end())
 	{
-		(*It)->m_bLaunched = bLaunch;
+		(*It_l)->m_bLaunched = bLaunch;
 		(*It_l)->H_SetParent(nullptr);
 		m_launched_rockets.erase(It_l);
 	}
