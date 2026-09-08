8c5bdb8b4ae92e4f8bbbd77004c5d27511cf0913
Aphile <taiko6661@gmail.com>
2026-09-08 22:47:55 +0500

    Add: Убрал slowmode для CAnomalyZone, а также, все аномалии после >50m обновляются раз в секунду.

--- src/xrGame/AnomalyZone.cpp
+++ src/xrGame/AnomalyZone.cpp
@@ -623,6 +623,11 @@ void CAnomalyZone::UpdateWorkload	(u32 dt)
 	
 }
 
+float CAnomalyZone::shedule_Scale()
+{
+	return f_optimize_dist / FASTMODE_DISTANCE;
+}
+
 // ГГ берет артефакт с земли где то в пределах 160 метров от артефакта до аномалии
 void CAnomalyZone::OnActorTakeArtefact(float scan_radius, CArtefact* artefact, Fvector actorPos)
 {
@@ -872,11 +877,7 @@ void CAnomalyZone::UpdateCL()
 {
 	UpdateComponents(true);
 	inherited::UpdateCL();
-
-	if (m_zone_flags.test(eFastMode))
-	{
-		UpdateWorkload(Device.dwTimeDelta);
-	}
+	UpdateWorkload(Device.dwTimeDelta);
 }
 
 // called as usual
@@ -940,18 +941,6 @@ void CAnomalyZone::shedule_Update(u32 dt)
 		}
 
 		inherited::shedule_Update(dt);
-
-		// check "fast-mode" border
-		float	cam_distance	= Device.vCameraPosition.distance_to(P)-s.R;
-		
-		if (cam_distance>FASTMODE_DISTANCE && !m_zone_flags.test(eAlwaysFastmode) )	
-		{
-			o_switch_2_slow();
-		}
-		else									
-		{
-			o_switch_2_fast();
-		}
 
 		if (!m_zone_flags.test(eFastMode))
 		{
