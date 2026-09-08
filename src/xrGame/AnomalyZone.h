825a9bd6c15779be97287480fc1433e735df0482
Aphile <taiko6661@gmail.com>
2026-09-08 22:47:55 +0500

    Add: Убрал slowmode для CAnomalyZone, а также, все аномалии после >50m обновляются раз в секунду.

--- src/xrGame/AnomalyZone.h
+++ src/xrGame/AnomalyZone.h
@@ -49,6 +49,7 @@ public:
 	
 	virtual		void	UpdateCL						();
 	virtual		void	UpdateWorkload					(u32 dt);
+	virtual		float	shedule_Scale					() override;
 	virtual		void	shedule_Update					(u32 dt);
 	virtual		void	enter_Zone						(SZoneObjectInfo& io);
 	virtual		void	exit_Zone						(SZoneObjectInfo& io);
