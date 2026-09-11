a5e7ee74f70a466f30ca6f8162b7abe815ba2fb9
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-09-11 23:59:12 +0300

    Add dynamic wallmarks toggle into options

--- src/Layers/xrRender/xrRender_console.cpp
+++ src/Layers/xrRender/xrRender_console.cpp
@@ -104,7 +104,7 @@ float		ps_r__Tree_SBC				= 1.5f	;	// scale bias correct
 float		ps_r__WallmarkTTL			= 50.f	;
 float		ps_r__WallmarkSHIFT			= 0.001f;
 float		ps_r__WallmarkSHIFT_V		= 0.001f;
-bool		ps_r__WallmarkDyn			= false;
+bool		ps_r__WallmarkDyn			= true;
 
 float		ps_r__GLOD_ssa_start		= 256.f	;
 float		ps_r__GLOD_ssa_end			=  64.f	;
