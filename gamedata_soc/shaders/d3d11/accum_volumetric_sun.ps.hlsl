3f665c59dd930afe54a8bd85520c655719922684
Hozar2002 <hoz2002team@gmail.com>
2026-09-13 23:16:59 +0600

    Откатил волуметрики солнца
    Если не понравятся - откатить обратно

--- gamedata_soc/shaders/d3d11/accum_volumetric_sun.ps.hlsl
+++ gamedata_soc/shaders/d3d11/accum_volumetric_sun.ps.hlsl
@@ -1,133 +1,223 @@
-/*
-Made for I-Xray engine by Papa Doenitz, based on original GSC code and Inigo Quilez's article (https://iquilezles.org/articles/fog/).
-This shader calculates volumetric sun shafts (god rays) using ray marching in screen space.
-The shader samples the shadow map to determine how much light is reaching each pixel, and applies a fog effect based on distance and view direction.
-This software is protected by copyright Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License (CC BY-NC-SA 4.0). For more information, please visit https://creativecommons.org/licenses/by-nc-sa/4.0/.
-If you want to use or modify this sofware, please credit the original author (Papa Doenitz) and provide a link to the original source and Inigo Quilez article. You may not use this shader for commercial purposes without permission from the author. If you create a derivative work, you must share it under the same license.
-*/
-
 #include "common.hlsli"
+
+struct PSInput
+{
+	float4 hpos : SV_POSITION;
+	float2 texcoord : TEXCOORD0;
+};
+
+#undef USE_ULTRA_SHADOWS
+
+#define RAY_PATH 2.0h
+#define JITTER_TEXTURE_SIZE 64.0f
+
+#ifdef SUN_SHAFTS_QUALITY
+    #if SUN_SHAFTS_QUALITY == 1
+        // #define FILTER_LOW
+        #define RAY_SAMPLES 20
+    #elif SUN_SHAFTS_QUALITY == 2
+        // #define FILTER_LOW
+        #define RAY_SAMPLES 20
+    #elif SUN_SHAFTS_QUALITY == 3
+        // #define FILTER_LOW
+        #define RAY_SAMPLES 40
+    #endif
+#endif
+
 #include "shadow.hlsli"
 
-#if SUN_SHAFTS_QUALITY == 1
-#define RAY_SAMPLES 6
-#elif SUN_SHAFTS_QUALITY == 2
-#define RAY_SAMPLES 12
-#elif SUN_SHAFTS_QUALITY == 3
-#define RAY_SAMPLES 18
-#endif
-
-// --- Medium/scattering tuning knobs ---
-static const float PHASE_G = 0.5f; // Schlick g (0 isotropic, higher = forward)
-static const float SCATTER_RATIO = 0.5f; // fraction of extinction that goes to scattering
-static const float ABSORB_TINT_STRENGTH = 0.5f; // 0 = gray absorption, 1 = tinted by fog_color
-
-// Sun shafts intensity param (x used as density scale)
 float4 sun_shafts_intensity;
 
-// Schlick phase function (normalized form)
-float PhaseFunction_Schlick(float g, float cos_theta)
-{
-    float gg = g * g;
-    float nom = 1.0f - gg;
-    float denom = 4.0f * PI * (1.0f + g * cos_theta) * (1.0f + g * cos_theta);
-    return nom / denom;
-}
-
-float4 main(PSInputFullscreen I) : SV_Target
+float4 main(PSInput I) : SV_Target
 {
 #ifndef SUN_SHAFTS_QUALITY
-    return float4(0,0,0,0);
-#else //SUN_SHAFTS_QUALITY
-
-    IXRayGbuffer O = (IXRayGbuffer)NULL;
-    GbufferUnpack((uint2)I.hpos.xy, O);
-    
-    float3 Pview = O.Point * 0.996f;
+    return float4(0, 0, 0, 0);
+#else //	SUN_SHAFTS_QUALITY
+	IXRayGbuffer O = (IXRayGbuffer)NULL;
+	GbufferUnpack((uint2)I.hpos.xy, O);
 	
-	float PviewSqr = dot(Pview, Pview);
-	float PviewSqrDiv = PviewSqr > 0.0f ? rsqrt(PviewSqr) : 0.0f;
-	
-	float zFar = min(PviewSqr * PviewSqrDiv, fog_params.z); 
-	float3 viewDir = Pview * PviewSqrDiv;
-	
-	Pview = viewDir * zFar;
+    float3 P = O.Point;
 
     float4 J0 = s_blue_noise[uint3(uint2(I.hpos.xy) % 128, uint(m_taa_jitter.w) % 32)];
+	float coeff = (RAY_SAMPLES - J0.x) / (RAY_SAMPLES * RAY_SAMPLES);
+	float3 direction = P * coeff;
 
-    float3 deltaView = Pview / float(RAY_SAMPLES);
-    float3 startView = deltaView * J0.x;
-    float3 P0view = Pview - startView;
+    float depth = P.z;
+    float deltaDepth = direction.z;
 
-    float depth = P0view.z; 
-    float deltaDepth = deltaView.z; 
+    P = mul(m_invV, float4(P, 1.0f));
+    direction = mul(m_invV, direction);
 
-    float3 PW = mul(m_invV, float4(P0view, 1.f)).xyz;
-    float3 deltaW = mul(m_invV, float4(deltaView, 0.f)).xyz;
+    float4 current = mul(m_shadow_sun[2], float4(P, 1.0f));
+    float4 delta = mul(m_shadow_sun[2], float4(direction, 0.0f));
 
-    float4 current = mul(m_shadow_sun[2], float4(PW, 1.f));
-    float4 deltaS = mul(m_shadow_sun[2], float4(deltaW, 0.f));
+    float res = 0.0f;
+    float max_density = sun_shafts_intensity.x;
+    float density = max_density / RAY_SAMPLES;
 
-#ifndef USE_LEGACY_LIGHT
-    float3 fogTint = GammaToLinear(fog_color.rgb);
-#else
-    float3 fogTint = fog_color.xyz * fog_color.xyz;
-#endif
+    if(O.Depth > 0.9999f) 
+	{
+		depth = 0.0f;
+        res = max_density;
+    }
 
-    fogTint /= max(max(fogTint.r, fogTint.g), fogTint.b + EPS_S);
-
-    float density = 0.1f * max(sun_shafts_intensity.x, 0.f);
-    float sigma_t = density;
-    float3 sigma_s = sigma_t * SCATTER_RATIO * fogTint;
-    float3 absorbTint = lerp(1.f.xxx, saturate(1.f.xxx - fogTint), ABSORB_TINT_STRENGTH);
-    float3 sigma_a = sigma_t * (1.f - SCATTER_RATIO) * absorbTint;
-    float3 sigma_t_rgb = sigma_s + sigma_a;
-    
-    float3 sunCol = (Ldynamic_color.rgb); // i wont use gamma here
-
-    float3 Ldir = normalize(Ldynamic_dir.xyz);
-
-    float3 radiance = 0.f;
-    float3 T = 1.f.xxx;
-
-    float stepLen = max(length(deltaW), 1e-5f);
-    
-    float distToCam = length(PW - eye_position);
-    float FarFadeStart = 0.8f * fog_params.z;
-    float tFar = saturate((distToCam - FarFadeStart) / max(fog_params.z - FarFadeStart, 1e-4));
-    tFar = tFar * tFar * (3.f - 2.f * tFar);
-    float cosTheta = dot(Ldir, O.View);
-    // since density is isomorphic and light dir constant - we can take phase out of loop
-    float phase = PhaseFunction_Schlick(PHASE_G, cosTheta);
-	
     [loop]
-    for (int i = 0; i < RAY_SAMPLES; ++i)
+	for(int i = 0; i < RAY_SAMPLES; ++i)
     {
-        if (depth > 0.3f)
-        {   
-            float shadowValid = (current.x >= 0.f && current.x <= 1.f && current.y >= 0.f && current.y <= 1.f) ? 1.f : 0.f;
-            float vis = s_smap_sun.SampleCmpLevelZero(smp_smap, float3(current.xy, 2), current.z).x;
-            float visSafe = lerp(1.f, vis, shadowValid);
-            vis = lerp(visSafe, 1.f, tFar);
-
-            float3 Lin = sunCol * vis;
-            radiance += T * (Lin * sigma_s * phase) * stepLen;
-            T *= exp(-sigma_t_rgb * stepLen);
+        if (depth > 0.3)
+        {
+			res += density * s_smap_sun.SampleCmpLevelZero(smp_smap, float3(current.xy, 2), current.z).x;
         }
 
         depth -= deltaDepth;
-        current -= deltaS;
+        current -= delta;
     }
-    
-    float3 fogNeutral = lerp(fogTint, Luminance(fogTint), 0.6f);
-    radiance = lerp(radiance, fogNeutral * Luminance(radiance), tFar);
-    
-#ifdef USE_LEGACY_LIGHT
-	radiance = sqrt(radiance);
-#endif
 
-    return float4(radiance, 1.0f);
+    float fSturation = dot(O.View, -Ldynamic_dir.xyz);
 
-#endif //SUN_SHAFTS_QUALITY
+    //	Normalize dot product to
+    fSturation = 0.4f * fSturation + 0.6f;
+
+    float fog = saturate(O.ViewDist * fog_params.w + fog_params.x);
+    res = lerp(res, max_density, fog);
+    res *= fSturation;
+
+    return GammaToLinear(res * Ldynamic_color);
+#endif // SUN_SHAFTS_QUALITY
 }
+
+
+
+// /*
+// Made for I-Xray engine by Papa Doenitz, based on original GSC code and Inigo Quilez's article (https://iquilezles.org/articles/fog/).
+// This shader calculates volumetric sun shafts (god rays) using ray marching in screen space.
+// The shader samples the shadow map to determine how much light is reaching each pixel, and applies a fog effect based on distance and view direction.
+// This software is protected by copyright Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License (CC BY-NC-SA 4.0). For more information, please visit https://creativecommons.org/licenses/by-nc-sa/4.0/.
+// If you want to use or modify this sofware, please credit the original author (Papa Doenitz) and provide a link to the original source and Inigo Quilez article. You may not use this shader for commercial purposes without permission from the author. If you create a derivative work, you must share it under the same license.
+// */
+
+// #include "common.hlsli"
+// #include "shadow.hlsli"
+
+// #if SUN_SHAFTS_QUALITY == 1
+// #define RAY_SAMPLES 6
+// #elif SUN_SHAFTS_QUALITY == 2
+// #define RAY_SAMPLES 12
+// #elif SUN_SHAFTS_QUALITY == 3
+// #define RAY_SAMPLES 18
+// #endif
+
+// // --- Medium/scattering tuning knobs ---
+// static const float PHASE_G = 0.5f; // Schlick g (0 isotropic, higher = forward)
+// static const float SCATTER_RATIO = 0.5f; // fraction of extinction that goes to scattering
+// static const float ABSORB_TINT_STRENGTH = 0.5f; // 0 = gray absorption, 1 = tinted by fog_color
+
+// // Sun shafts intensity param (x used as density scale)
+// float4 sun_shafts_intensity;
+
+// // Schlick phase function (normalized form)
+// float PhaseFunction_Schlick(float g, float cos_theta)
+// {
+    // float gg = g * g;
+    // float nom = 1.0f - gg;
+    // float denom = 4.0f * PI * (1.0f + g * cos_theta) * (1.0f + g * cos_theta);
+    // return nom / denom;
+// }
+
+// float4 main(PSInputFullscreen I) : SV_Target
+// {
+// #ifndef SUN_SHAFTS_QUALITY
+    // return float4(0,0,0,0);
+// #else //SUN_SHAFTS_QUALITY
+
+    // IXRayGbuffer O = (IXRayGbuffer)NULL;
+    // GbufferUnpack((uint2)I.hpos.xy, O);
+    
+    // float3 Pview = O.Point * 0.996f;
+	
+	// float PviewSqr = dot(Pview, Pview);
+	// float PviewSqrDiv = PviewSqr > 0.0f ? rsqrt(PviewSqr) : 0.0f;
+	
+	// float zFar = min(PviewSqr * PviewSqrDiv, fog_params.z); 
+	// float3 viewDir = Pview * PviewSqrDiv;
+	
+	// Pview = viewDir * zFar;
+
+    // float4 J0 = s_blue_noise[uint3(uint2(I.hpos.xy) % 128, uint(m_taa_jitter.w) % 32)];
+
+    // float3 deltaView = Pview / float(RAY_SAMPLES);
+    // float3 startView = deltaView * J0.x;
+    // float3 P0view = Pview - startView;
+
+    // float depth = P0view.z; 
+    // float deltaDepth = deltaView.z; 
+
+    // float3 PW = mul(m_invV, float4(P0view, 1.f)).xyz;
+    // float3 deltaW = mul(m_invV, float4(deltaView, 0.f)).xyz;
+
+    // float4 current = mul(m_shadow_sun[2], float4(PW, 1.f));
+    // float4 deltaS = mul(m_shadow_sun[2], float4(deltaW, 0.f));
+
+// #ifndef USE_LEGACY_LIGHT
+    // float3 fogTint = GammaToLinear(fog_color.rgb);
+// #else
+    // float3 fogTint = fog_color.xyz * fog_color.xyz;
+// #endif
+
+    // fogTint /= max(max(fogTint.r, fogTint.g), fogTint.b + EPS_S);
+
+    // float density = 0.1f * max(sun_shafts_intensity.x, 0.f);
+    // float sigma_t = density;
+    // float3 sigma_s = sigma_t * SCATTER_RATIO * fogTint;
+    // float3 absorbTint = lerp(1.f.xxx, saturate(1.f.xxx - fogTint), ABSORB_TINT_STRENGTH);
+    // float3 sigma_a = sigma_t * (1.f - SCATTER_RATIO) * absorbTint;
+    // float3 sigma_t_rgb = sigma_s + sigma_a;
+    
+    // float3 sunCol = (Ldynamic_color.rgb); // i wont use gamma here
+
+    // float3 Ldir = normalize(Ldynamic_dir.xyz);
+
+    // float3 radiance = 0.f;
+    // float3 T = 1.f.xxx;
+
+    // float stepLen = max(length(deltaW), 1e-5f);
+    
+    // float distToCam = length(PW - eye_position);
+    // float FarFadeStart = 0.8f * fog_params.z;
+    // float tFar = saturate((distToCam - FarFadeStart) / max(fog_params.z - FarFadeStart, 1e-4));
+    // tFar = tFar * tFar * (3.f - 2.f * tFar);
+    // float cosTheta = dot(Ldir, O.View);
+    // // since density is isomorphic and light dir constant - we can take phase out of loop
+    // float phase = PhaseFunction_Schlick(PHASE_G, cosTheta);
+	
+    // [loop]
+    // for (int i = 0; i < RAY_SAMPLES; ++i)
+    // {
+        // if (depth > 0.3f)
+        // {   
+            // float shadowValid = (current.x >= 0.f && current.x <= 1.f && current.y >= 0.f && current.y <= 1.f) ? 1.f : 0.f;
+            // float vis = s_smap_sun.SampleCmpLevelZero(smp_smap, float3(current.xy, 2), current.z).x;
+            // float visSafe = lerp(1.f, vis, shadowValid);
+            // vis = lerp(visSafe, 1.f, tFar);
+
+            // float3 Lin = sunCol * vis;
+            // radiance += T * (Lin * sigma_s * phase) * stepLen;
+            // T *= exp(-sigma_t_rgb * stepLen);
+        // }
+
+        // depth -= deltaDepth;
+        // current -= deltaS;
+    // }
+    
+    // float3 fogNeutral = lerp(fogTint, Luminance(fogTint), 0.6f);
+    // radiance = lerp(radiance, fogNeutral * Luminance(radiance), tFar);
+    
+// #ifdef USE_LEGACY_LIGHT
+	// radiance = sqrt(radiance);
+// #endif
+
+    // return float4(radiance, 1.0f);
+
+// #endif //SUN_SHAFTS_QUALITY
+// }
 
