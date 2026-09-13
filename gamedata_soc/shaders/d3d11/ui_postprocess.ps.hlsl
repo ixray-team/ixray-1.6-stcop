114b19f1f096f3c755f2649ada63d6ddecfc1977
St4lker0k765 <126080360+St4lker0k765@users.noreply.github.com>
2026-09-13 21:51:22 +0300

    Схлопнуть с Покраска иконок WIP

--- gamedata_soc/shaders/d3d11/ui_postprocess.ps.hlsl
+++ gamedata_soc/shaders/d3d11/ui_postprocess.ps.hlsl
@@ -1,10 +1,13 @@
 #include "common.hlsli"
 
+uniform float4 static_color;
+
 float4 main(PSInputFullscreen I) : SV_Target
 {
 	float4 Color = s_image.SampleLevel(smp_rtlinear, I.texcoord, 0);
 	Color.xyz = tonemap(Color.xyz, 1.5f);
+	Color.w = Color.w > 0.0f; // temp
 
-	return Color;
+	return Color * static_color;
 }
 
