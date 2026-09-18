9be656dae75f8fc989b4b89796b1cbf325147c0e
mnelenpridumivat <ragrind@mail.ru>
2026-09-18 13:31:06 +0300

    Fix -> "Replace all u16, 0xffff and u16(-1) with ALife namespace type and constant"

--- src/xrServerEntities/xrServer_Objects_ALife.cpp
+++ src/xrServerEntities/xrServer_Objects_ALife.cpp
@@ -2569,7 +2569,7 @@ bool CSE_Shell::Spawn_Read(NET_Packet& tNetPacket)
 		return false;
 	}
 
-	tNetPacket.r_u16(weapon_id);
+	tNetPacket >> weapon_id;
 	tNetPacket.r_vec3(eject_dir);
 	tNetPacket.r_vec3(parent_vel);
 	tNetPacket.r_float(eject_speed);
@@ -2582,7 +2582,7 @@ void CSE_Shell::Spawn_Write(NET_Packet& tNetPacket, bool bLocal)
 {
 	inherited::Spawn_Write(tNetPacket, true);
 	
-	tNetPacket.w_u16(weapon_id);
+	tNetPacket << weapon_id;
 	tNetPacket.w_vec3(eject_dir);
 	tNetPacket.w_vec3(parent_vel);
 	tNetPacket.w_float(eject_speed);
